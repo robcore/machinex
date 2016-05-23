2013894cb1e33158236a6e37ccc3c42b826aedda
robcore <robpatershuk@gmail.com>
2016-05-23 09:28:55 -0600

    tkkg topology and gadget patches 202-211-thx man

--- drivers/usb/gadget/f_mass_storage.c
+++ drivers/usb/gadget/f_mass_storage.c
@@ -963,6 +963,9 @@ static int do_read_cd(struct fsg_common *common)
 		/* read all data  - 2352 byte */
 		amount_left = 2352;
 	} else {
+	if (curlun->cdrom)
+		file_offset = ((loff_t) lba) << 11;
+	else
 		file_offset = ((loff_t) lba) << 9;
 		/* Carry out the file reads */
 		amount_left = common->data_size_from_cmnd;
@@ -970,6 +973,8 @@ static int do_read_cd(struct fsg_common *common)
 
 	if (unlikely(amount_left == 0))
 		return -EIO;		/* No default reply */
+	if (curlun->cdrom)
+		amount_left <<= 2;
 
 	for (;;) {
 
@@ -1697,7 +1702,7 @@ static int do_read_capacity(struct fsg_common *common, struct fsg_buffhd *bh)
 
 	put_unaligned_be32(curlun->num_sectors - 1, &buf[0]);
 						/* Max logical block */
-	put_unaligned_be32(curlun->blksize, &buf[4]);/* Block length */
+	put_unaligned_be32(curlun->cdrom ? 2048 : curlun->blksize, &buf[4]);/* Block length */
 	return 8;
 }
 
@@ -1968,7 +1973,7 @@ static int do_read_format_capacities(struct fsg_common *common,
 
 	put_unaligned_be32(curlun->num_sectors, &buf[0]);
 						/* Number of blocks */
-	put_unaligned_be32(curlun->blksize, &buf[4]);/* Block length */
+	put_unaligned_be32(curlun->cdrom ? 2048 : curlun->blksize, &buf[4]);/* Block length */
 	buf[4] = 0x02;				/* Current capacity */
 	return 12;
 }
@@ -2370,7 +2375,9 @@ static int check_command(struct fsg_common *common, int cmnd_size,
 		return -EINVAL;
 	}
 
-	/* Check that only command bytes listed in the mask are non-zero */
+	/* Check that only command bytes listed in the mask are non-zero
+	 * Some BIOSes put some non-zero values in READ_TOC requests in
+	 * the last two bytes */
 	common->cmnd[1] &= 0x1f;			/* Mask away the LUN */
 	for (i = 1; i < cmnd_size; ++i) {
 		if (common->cmnd[i] && !(mask & (1 << i))) {
@@ -2544,7 +2551,7 @@ static int do_scsi_command(struct fsg_common *common)
 #ifdef _SUPPORT_MAC_
 				      (0xf<<6) | (1<<1), 1,
 #else
-				      (7<<6) | (1<<1), 1,
+				      (0xf<<6) | (1<<1), 1,
 #endif
 				      "READ TOC");
 		if (reply == 0)
@@ -3211,6 +3218,7 @@ static int fsg_main_thread(void *common_)
 static DEVICE_ATTR(ro, 0644, fsg_show_ro, fsg_store_ro);
 static DEVICE_ATTR(nofua, 0644, fsg_show_nofua, fsg_store_nofua);
 static DEVICE_ATTR(file, 0644, fsg_show_file, fsg_store_file);
+static DEVICE_ATTR(cdrom, 0644, fsg_show_cdrom, fsg_store_cdrom);
 #ifdef CONFIG_USB_MSC_PROFILING
 static DEVICE_ATTR(perf, 0644, fsg_show_perf, fsg_store_perf);
 #endif
@@ -3334,6 +3342,9 @@ static struct fsg_common *fsg_common_init(struct fsg_common *common,
 		if (rc)
 			goto error_luns;
 		rc = device_create_file(&curlun->dev, &dev_attr_nofua);
+		if (rc)
+			goto error_luns;
+		rc = device_create_file(&curlun->dev, &dev_attr_cdrom);
 		if (rc)
 			goto error_luns;
 #ifdef CONFIG_USB_MSC_PROFILING
@@ -3482,6 +3493,7 @@ static void fsg_common_release(struct kref *ref)
 #ifdef CONFIG_USB_MSC_PROFILING
 			device_remove_file(&lun->dev, &dev_attr_perf);
 #endif
+			device_remove_file(&lun->dev, &dev_attr_cdrom);
 			device_remove_file(&lun->dev, &dev_attr_nofua);
 			device_remove_file(&lun->dev, &dev_attr_ro);
 			device_remove_file(&lun->dev, &dev_attr_file);
