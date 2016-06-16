98ca034fe32a42883ace8b2bf496db5354183ca2
robcore <robpatershuk@gmail.com>
2016-06-16 15:10:08 -0600

    imported wakeup reason from tkkg/dorimanx

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -33,6 +33,10 @@
 #include "../base.h"
 #include "power.h"
 
+#if defined (CONFIG_MACH_JF)
+#include <linux/module.h>
+#endif
+
 typedef int (*pm_callback_t)(struct device *);
 
 /*
@@ -684,6 +688,11 @@ static void dpm_drv_timeout(unsigned long data)
 	BUG();
 }
 
+#if defined (CONFIG_MACH_JF)
+static int nsec64_measure_resume_spend = 10000;// over 10ms
+module_param_named(resume_spend, nsec64_measure_resume_spend, int, S_IRUGO | S_IWUSR | S_IWGRP);
+#endif
+
 /**
  * dpm_resume - Execute "resume" callbacks for non-sysdev devices.
  * @state: PM transition of the system being carried out.
@@ -716,7 +725,16 @@ void dpm_resume(pm_message_t state)
 		if (!is_async(dev)) {
 			int error;
 
+#if defined (CONFIG_MACH_JF)
+			ktime_t stime,etime;
+#endif
+
 			mutex_unlock(&dpm_list_mtx);
+
+#if defined (CONFIG_MACH_JF)
+			if (nsec64_measure_resume_spend)
+				stime=ktime_get();
+#endif
 
 			error = device_resume(dev, state, false);
 			if (error) {
@@ -725,6 +743,21 @@ void dpm_resume(pm_message_t state)
 				dpm_save_failed_dev(dev_name(dev));
 				pm_dev_err(dev, state, "", error);
 			}
+
+#if defined (CONFIG_MACH_JF)
+			if (nsec64_measure_resume_spend) {
+				int usecs;
+				u64 usecs64;
+				etime = ktime_get();
+				usecs64 = ktime_to_ns(ktime_sub(etime, stime));
+				do_div(usecs64, NSEC_PER_USEC);
+				usecs=usecs64;
+				if (usecs64>(u64)nsec64_measure_resume_spend-1) {
+					printk(KERN_EMERG "* DPM device: %s (%s) %d\n", dev_name(dev),
+						(dev->driver ? dev->driver->name : "no driver"),usecs);
+				}
+			}
+#endif
 
 			mutex_lock(&dpm_list_mtx);
 		}
@@ -1019,7 +1052,7 @@ int dpm_suspend_end(pm_message_t state)
 	int error = dpm_suspend_late(state);
 	if (error)
 		return error;
-	
+
 	error = dpm_suspend_noirq(state);
 	if (error) {
 		dpm_resume_early(resume_event(state));
