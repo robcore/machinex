7311a0007a52eddd5c34dc2c1c78f29bfe2c3b1f
robcore <robpatershuk@gmail.com>
2016-06-16 16:16:08 -0600

    pm sleep - cleanup remnants of wq based sync

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -886,38 +886,6 @@ power_attr(cpufreq_min_limit);
 power_attr(cpufreq_table);
 #endif
 
-static ssize_t hard_reset_ctl_show(struct kobject *kobj,
-		struct kobj_attribute *attr, char *buf)
-{
-	return sprintf(buf, "%d\n", pm8xxx_hard_reset_enabled());
-}
-
-static ssize_t hard_reset_ctl_store(struct kobject *kobj,
-					struct kobj_attribute *attr,
-					const char *buf, size_t n)
-{
-	int ret = 0;
-	int enable;
-
-	ret = sscanf(buf, "%d", &enable);
-
-	if (ret != 1 || enable > 1)
-		return -EINVAL;
-
-	ret = pm8xxx_hard_reset_control(enable);
-	if (ret)
-		return -EPERM;
-
-	ret = resout_irq_control(enable);
-	if (ret)
-		return -EPERM;
-
-	pr_info("hard_reset_controlled = %d\n", enable);
-
-	return n;
-}
-
-power_attr(hard_reset_ctl);
 
 static struct attribute *g[] = {
 	&state_attr.attr,
@@ -951,7 +919,6 @@ static struct attribute *g[] = {
 	&cpufreq_max_limit_attr.attr,
 	&cpufreq_table_attr.attr,
 #endif
-	&hard_reset_ctl_attr.attr,
 	NULL,
 };
 
