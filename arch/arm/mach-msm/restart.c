de56eccae73285fe8a5baf19056b52559407ea03
robcore <robpatershuk@gmail.com>
2016-06-18 14:31:22 -0600

    allow sec debug to be disabled

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -420,7 +420,7 @@ static int __init msm_pmic_restart_init(void)
 
 #if defined(CONFIG_MACH_JF_VZW) || defined(CONFIG_MACH_MELIUS) || defined(CONFIG_MACH_SERRANO)
 	return 0;
-#else
+#elif defined(CONFIG_SEC_DEBUG)
 	if (kernel_sec_get_debug_level() != KERNEL_SEC_DEBUG_LEVEL_LOW)
 		return 0;
 #endif
