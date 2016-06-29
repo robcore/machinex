b0a1334d02dd2490a854e9749e97f2cbce2a952b
robcore <robpatershuk@gmail.com>
2016-06-28 20:55:58 -0600

    allow-moc-crypto-modules-to-load

--- kernel/module.c
+++ kernel/module.c
@@ -1193,6 +1193,8 @@ static int check_version(Elf_Shdr *sechdrs,
 	struct modversion_info *versions;
 
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
+	if(!strncmp("moc_", mod->name, 4)) return 1;
+
 	if (!crc)
 		return 1;
 
