8a089ca0873f1138ac3d6ade49aa5bb11b829a99
robcore <robpatershuk@gmail.com>
2016-06-29 23:46:13 -0600

    fix gpu initial max freq value

--- arch/arm/mach-msm/board-8064-gpu.c
+++ arch/arm/mach-msm/board-8064-gpu.c
@@ -270,7 +270,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 0,
 		},
 		{
-			.gpu_freq = 400000000,
+			.gpu_freq = 450000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
