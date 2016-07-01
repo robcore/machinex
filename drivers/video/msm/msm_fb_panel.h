77a2306b0847c974f1eef8616145f67de97eba16
robcore <robpatershuk@gmail.com>
2016-06-30 18:02:40 -0600

    msm-mdp-add support for always on mode

--- drivers/video/msm/msm_fb_panel.h
+++ drivers/video/msm/msm_fb_panel.h
@@ -211,6 +211,7 @@ struct msm_fb_panel_data {
 	int (*clk_func) (int enable);
 	int (*fps_level_change) (struct platform_device *pdev,
 					u32 fps_level);
+	int (*low_power_config) (struct platform_device *pdev, int enable);
 };
 
 /*===========================================================================
@@ -223,6 +224,7 @@ int panel_next_init(struct platform_device *pdev);
 int panel_next_off(struct platform_device *pdev);
 int panel_next_fps_level_change(struct platform_device *pdev,
 					u32 fps_level);
+int panel_next_low_power_config(struct platform_device *pdev, int enable);
 int panel_next_late_init(struct platform_device *pdev);
 
 int lcdc_device_register(struct msm_panel_info *pinfo);
