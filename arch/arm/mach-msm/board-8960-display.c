584adbce65b5c974e4c43a459689b01c12b49dbc
robcore <robpatershuk@gmail.com>
2016-06-30 18:21:21 -0600

    various cm tweaks, see patch for details

--- arch/arm/mach-msm/board-8960-display.c
+++ arch/arm/mach-msm/board-8960-display.c
@@ -1706,8 +1706,8 @@ static struct msm_bus_vectors mdp_ui_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 248832000 * 2,
-		.ib = 311040000 * 2,
+		.ab = 216000000 * 2,
+		.ib = 270000000 * 2,
 	},
 };
 
@@ -1716,8 +1716,8 @@ static struct msm_bus_vectors mdp_vga_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 248832000 * 2,
-		.ib = 311040000 * 2,
+		.ab = 216000000 * 2,
+		.ib = 270000000 * 2,
 	},
 };
 
@@ -1726,8 +1726,8 @@ static struct msm_bus_vectors mdp_720p_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 304128000 * 2,
-		.ib = 380160000 * 2,
+		.ab = 230400000 * 2,
+		.ib = 288000000 * 2,
 	},
 };
 
@@ -1736,8 +1736,8 @@ static struct msm_bus_vectors mdp_1080p_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 407808000 * 4,
-		.ib = 509760000 * 4,
+		.ab = 334080000 * 2,
+		.ib = 417600000 * 2,
 	},
 };
 
@@ -1791,7 +1791,7 @@ static struct msm_panel_common_pdata mdp_pdata = {
 #else
 	.mem_hid = MEMTYPE_EBI1,
 #endif
-	.cont_splash_enabled = 0x00,
+	.cont_splash_enabled = 0x01,
 	.splash_screen_addr = 0x00,
 	.splash_screen_size = 0x00,
 	.mdp_iommu_split_domain = 0,
--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -66,8 +66,8 @@ static int ssr_magic_number = 0;
 static int restart_mode;
 #ifndef CONFIG_SEC_DEBUG
 void *restart_reason;
-int kernel_sec_get_debug_level(void);
 #endif
+int kernel_sec_get_debug_level(void);
 #define KERNEL_SEC_DEBUG_LEVEL_LOW      (0x574F4C44)
 int pmic_reset_irq;
 static void __iomem *msm_tmr0_base;
@@ -205,30 +205,6 @@ static void cpu_power_off(void *data)
 		;
 }
 
-static int irq_enabled;
-static int status;
-
-int resout_irq_control(int enable)
-{
-	if (!irq_enabled)
-		return -1;
-
-	if (enable ^ status) {
-		if (enable) {
-			enable_irq(pmic_reset_irq);
-			status = 1;
-			pr_info("%s : %d\n", __func__, status);
-		} else {
-			disable_irq_nosync(pmic_reset_irq);
-			status = 0;
-			pr_info("%s : %d\n", __func__, status);
-		}
-	} else
-		return -1;
-
-	return 0;
-}
-
 static irqreturn_t resout_irq_handler(int irq, void *dev_id)
 {
 	pr_warn("%s PMIC Initiated shutdown\n", __func__);
@@ -361,19 +337,6 @@ void msm_restart(char mode, const char *cmd)
 		} else if (strlen(cmd) == 0) {
 			printk(KERN_NOTICE "%s : value of cmd is NULL.\n", __func__);
 			__raw_writel(0x12345678, restart_reason);
-#ifndef CONFIG_MACH_JF
-		} else if (!strncmp(cmd, "nvbackup", 8)) {
-			__raw_writel(0x77665511, restart_reason);
-		} else if (!strncmp(cmd, "nvrestore", 9)) {
-			__raw_writel(0x77665512, restart_reason);
-		} else if (!strncmp(cmd, "nverase", 7)) {
-			__raw_writel(0x77665514, restart_reason);
-		} else if (!strncmp(cmd, "nvrecovery", 10)) {
-			__raw_writel(0x77665515, restart_reason);
-#endif
-        } else if (!strncmp(cmd, "diag", 4)
-                  && !kstrtoul(cmd + 4, 0, &value)) {
-                __raw_writel(0xabcc0000 | value, restart_reason);
 		} else {
 			__raw_writel(0x77665501, restart_reason);
 		}
@@ -383,12 +346,10 @@ void msm_restart(char mode, const char *cmd)
 		set_kernel_crash_magic_number();
 reset:
 #endif /* CONFIG_LGE_CRASH_HANDLER */
-#if 1
 	else {
 		printk(KERN_NOTICE "%s : clear reset flag\r\n", __func__);
 		__raw_writel(0x12345678, restart_reason);
 	}
-#endif
 	__raw_writel(0, msm_tmr0_base + WDT0_EN);
 	if (!(machine_is_msm8x60_fusion() || machine_is_msm8x60_fusn_ffa())) {
 		mb();
@@ -439,7 +400,7 @@ static int __init msm_pmic_restart_init(void)
 {
 	int rc;
 
-#if defined(CONFIG_MACH_JF_VZW) || defined(CONFIG_MACH_MELIUS) || defined(CONFIG_MACH_SERRANO)
+#if defined(CONFIG_MACH_JF_VZW) || defined(CONFIG_MACH_MELIUS)
 	return 0;
 #elif defined(CONFIG_SEC_DEBUG)
 	if (kernel_sec_get_debug_level() != KERNEL_SEC_DEBUG_LEVEL_LOW)
@@ -452,8 +413,6 @@ static int __init msm_pmic_restart_init(void)
 					"restart_from_pmic", NULL);
 		if (rc < 0)
 			pr_err("pmic restart irq fail rc = %d\n", rc);
-		irq_enabled = 1;
-		status = 1;
 	} else {
 		pr_warn("no pmic restart interrupt specified\n");
 	}
