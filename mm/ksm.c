7dc573965355a491eb2849e7e717584245a5ae9a
robcore <robpatershuk@gmail.com>
2016-07-05 22:33:02 -0600

    mm-ksm-do not set a default for use_deferred_timer, leave that to the user/init

--- mm/ksm.c
+++ mm/ksm.c
@@ -190,7 +190,7 @@ static unsigned int ksm_thread_pages_to_scan = 256;
 static unsigned int ksm_thread_sleep_millisecs = 1500;
 
 /* Boolean to indicate whether to use deferred timer or not */
-static bool use_deferred_timer = true;
+static bool use_deferred_timer;
 
 #define KSM_RUN_STOP	0
 #define KSM_RUN_MERGE	1
