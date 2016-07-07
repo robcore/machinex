14213d95ed8996f964f26c455fd8277d6f3585b9
robcore <robpatershuk@gmail.com>
2016-07-07 00:48:20 -0600

    EXPERIMENTAL - implemented some base support for new frequency relation C, this is tentative

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -212,8 +212,9 @@ void unlock_policy_rwsem_write(int cpu);
  *                      CPUFREQ DRIVER INTERFACE                     *
  *********************************************************************/
 
-#define CPUFREQ_RELATION_L 0  /* lowest frequency at or above target */
-#define CPUFREQ_RELATION_H 1  /* highest frequency below or at target */
+#define CPUFREQ_RELATION_L 0	/* lowest frequency at or above target */
+#define CPUFREQ_RELATION_H 1	/* highest frequency below or at target */
+#define CPUFREQ_RELATION_C 2	/* closest frequency to target */
 
 struct freq_attr;
 
