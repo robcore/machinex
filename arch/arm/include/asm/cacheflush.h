52a85dd824083751de69571e7f8b86b0e4c3297b
robcore <robpatershuk@gmail.com>
2016-07-08 21:31:13 -0600

    ARM-cache-v7-use-movw-movt-instructions

--- arch/arm/include/asm/cacheflush.h
+++ arch/arm/include/asm/cacheflush.h
@@ -233,7 +233,7 @@ extern void copy_to_user_page(struct vm_area_struct *, struct page *,
 static inline void __flush_icache_all(void)
 {
 	__flush_icache_preferred();
-	dsb();
+	dsb(ishst);
 }
 /*
  * Flush caches up to Level of Unification Inner Shareable
