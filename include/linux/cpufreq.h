d7019e073ee6b42da71dbdb77b1ce54dd770a597
robcore <robpatershuk@gmail.com>
2016-07-10 21:28:26 -0600

    BACKPORT - cpufreq: Fix governor start/stop race condition

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -104,7 +104,9 @@ struct cpufreq_policy {
 					 * governors are used */
 	unsigned int            util;  /* CPU utilization at max frequency */
 	unsigned int		policy; /* see above */
-	struct cpufreq_governor	*governor; /* see below */
+        struct cpufreq_governor *governor; /* see below */
+        void                    *governor_data;
+        bool                    governor_enabled; /* governor start/stop flag */
 
 	struct work_struct	update; /* if update_policy() needs to be
 					 * called, but you're in IRQ context */
@@ -176,20 +178,23 @@ static inline unsigned long cpufreq_scale(unsigned long old, u_int div, u_int mu
 #define CPUFREQ_GOV_START  1
 #define CPUFREQ_GOV_STOP   2
 #define CPUFREQ_GOV_LIMITS 3
+#define CPUFREQ_GOV_POLICY_INIT 4
+#define CPUFREQ_GOV_POLICY_EXIT 5
 
 struct cpufreq_governor {
-	char	name[CPUFREQ_NAME_LEN];
-	int	(*governor)	(struct cpufreq_policy *policy,
-				 unsigned int event);
-	ssize_t	(*show_setspeed)	(struct cpufreq_policy *policy,
-					 char *buf);
-	int	(*store_setspeed)	(struct cpufreq_policy *policy,
-					 unsigned int freq);
-	unsigned int max_transition_latency; /* HW must be able to switch to
-			next freq faster than this value in nano secs or we
-			will fallback to performance governor */
-	struct list_head	governor_list;
-	struct module		*owner;
+        char    name[CPUFREQ_NAME_LEN];
+        int     initialized;
+        int     (*governor)     (struct cpufreq_policy *policy,
+                                 unsigned int event);
+        ssize_t (*show_setspeed)        (struct cpufreq_policy *policy,
+                                         char *buf);
+        int     (*store_setspeed)       (struct cpufreq_policy *policy,
+                                         unsigned int freq);
+        unsigned int max_transition_latency; /* HW must be able to switch to
+                        next freq faster than this value in nano secs or we
+                        will fallback to performance governor */
+        struct list_head        governor_list;
+        struct module           *owner;
 };
 
 /*
