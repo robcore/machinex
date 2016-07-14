a0091fead3bdb09f79ea471ac300751dc3b615f7
robcore <robpatershuk@gmail.com>
2016-07-14 01:12:57 -0600

    Revert-power-main-Add-conditional-compilation-for-touch-nodes

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -750,6 +750,8 @@ static ssize_t hard_reset_ctl_store(struct kobject *kobj,
 power_attr(hard_reset_ctl);
 
 static struct attribute *g[] = {
+	&touch_event_attr.attr,
+	&touch_event_timer_attr.attr,
 	&state_attr.attr,
 #ifdef CONFIG_PM_TRACE
 	&pm_trace_attr.attr,
@@ -758,8 +760,6 @@ static struct attribute *g[] = {
 #ifdef CONFIG_PM_SLEEP
 	&pm_async_attr.attr,
 	&wakeup_count_attr.attr,
-	&touch_event_attr.attr,
-	&touch_event_timer_attr.attr,
 #ifdef CONFIG_PM_DEBUG
 	&pm_test_attr.attr,
 #endif
