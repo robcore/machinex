d10c03b8131aea59bbfdc0d4bacc862d1aa9683d
robcore <robpatershuk@gmail.com>
2016-07-14 01:11:58 -0600

    PM-Sleep-Add-wakeup_source_activate-and-wakeup_source

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -14,6 +14,7 @@
 #include <linux/suspend.h>
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
+#include <trace/events/power.h>
 
 #include "power.h"
 
@@ -378,6 +379,7 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	 * active wakeup source should bring the system
 	 * out of PM_SUSPEND_FREEZE state
 	 */
+	unsigned int cec;
 	freeze_wake();
 
 	ws->active = true;
@@ -385,7 +387,9 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	ws->last_time = ktime_get();
 
 	/* Increment the counter of events in progress. */
-	atomic_inc(&combined_event_count);
++	cec = atomic_inc_return(&combined_event_count);
++
++	trace_wakeup_source_activate(ws->name, cec);
 }
 
 /**
@@ -460,7 +464,7 @@ EXPORT_SYMBOL_GPL(pm_stay_awake);
  */
 static void wakeup_source_deactivate(struct wakeup_source *ws)
 {
-	unsigned int cnt, inpr;
+	unsigned int cnt, inpr, cec;
 	ktime_t duration;
 	ktime_t now;
 
@@ -495,7 +499,8 @@ static void wakeup_source_deactivate(struct wakeup_source *ws)
 	 * Increment the counter of registered wakeup events and decrement the
 	 * couter of wakeup events in progress simultaneously.
 	 */
-	atomic_add(MAX_IN_PROGRESS, &combined_event_count);
+	cec = atomic_add_return(MAX_IN_PROGRESS, &combined_event_count);
+	trace_wakeup_source_deactivate(ws->name, cec);
 
 	split_counters(&cnt, &inpr);
 	if (!inpr && waitqueue_active(&wakeup_count_wait_queue))
