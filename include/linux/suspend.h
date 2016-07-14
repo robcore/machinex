d99c6c25ee67ad954ffc491ab718f59ecb7be8bf
robcore <robpatershuk@gmail.com>
2016-07-14 17:22:06 -0600

    PM / sleep: Mechanism for aborting system suspends unconditionally

--- include/linux/suspend.h
+++ include/linux/suspend.h
@@ -367,6 +367,8 @@ extern int unregister_pm_notifier(struct notifier_block *nb);
 extern bool events_check_enabled;
 
 extern bool pm_wakeup_pending(void);
+static inline void pm_system_wakeup(void) {}
+static inline void pm_wakeup_clear(void) {}
 extern bool pm_get_wakeup_count(unsigned int *count);
 extern bool pm_save_wakeup_count(unsigned int count);
 
@@ -412,6 +414,8 @@ static inline int unregister_pm_notifier(struct notifier_block *nb)
 #define pm_notifier(fn, pri)	do { (void)(fn); } while (0)
 
 static inline bool pm_wakeup_pending(void) { return false; }
+static inline void pm_system_wakeup(void) {}
+static inline void pm_wakeup_clear(void) {}
 
 static inline void lock_system_sleep(void) {}
 static inline void unlock_system_sleep(void) {}
