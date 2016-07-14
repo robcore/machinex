1b864175af20cddad6bbedab0d3aab1b99ad21f4
robcore <robpatershuk@gmail.com>
2016-07-14 14:49:29 -0600

    revert to right before wakeup source statistics following android logic

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -72,6 +72,10 @@ static int try_to_freeze_tasks(bool user_only)
 			todo += wq_busy;
 		}
 
+		if (todo && has_wake_lock(WAKE_LOCK_SUSPEND)) {
+			wakeup = 1;
+			break;
+		}
 		if (!todo || time_after(jiffies, end_time))
 			break;
 
@@ -214,7 +218,7 @@ int freeze_kernel_threads(void)
 {
 	int error;
 
-	error = sys_sync();
+	error = suspend_sys_sync_wait();
 	if (error)
 		return error;
 
