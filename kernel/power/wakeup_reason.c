4b6ebfc0a347ff1c5d73b9399f9c0641334c27ff
robcore <robpatershuk@gmail.com>
2016-07-15 00:20:23 -0600

    add back guard condition for wakeup reasons

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -80,6 +80,13 @@ void log_wakeup_reason(int irq)
 
 	spin_lock(&resume_reason_lock);
 
+	if (irq_count == MAX_WAKEUP_REASON_IRQS) {
+		spin_unlock(&resume_reason_lock);
+		printk(KERN_WARNING "Resume caused by more than %d IRQs\n",
+				MAX_WAKEUP_REASON_IRQS);
+		return;
+	}
+
 	irq_list[irqcount++] = irq;
 	spin_unlock(&resume_reason_lock);
 }
