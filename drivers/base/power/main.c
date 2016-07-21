e399888c5ba9006b1cca5178b8962faeb3d50def
robcore <robpatershuk@gmail.com>
2016-07-21 17:54:02 -0600

    PM-Move-disabling-enabling-runtime-PM-to-late-suspend

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -995,6 +995,8 @@ static int device_suspend_late(struct device *dev, pm_message_t state)
 	pm_callback_t callback = NULL;
 	char *info = NULL;
 
+	__pm_runtime_disable(dev, false);
+
 	if (dev->power.syscore)
 		return 0;
 
