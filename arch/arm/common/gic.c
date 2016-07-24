f2d0c5d234a195e44fe361d16cca2d4d2070e6b2
robcore <robpatershuk@gmail.com>
2016-07-23 19:37:54 -0600

    dont need this logging

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -50,7 +50,6 @@
 #include <asm/system.h>
 
 #include <mach/socinfo.h>
-#include <mach/msm_rtb.h>
 
 union gic_base {
 	void __iomem *common_base;
@@ -451,14 +450,14 @@ asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs)
 		if (likely(irqnr > 15 && irqnr < 1021)) {
 			irqnr = irq_find_mapping(gic->domain, irqnr);
 			handle_IRQ(irqnr, regs);
-			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
+//			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 			continue;
 		}
 		if (irqnr < 16) {
 			writel_relaxed_no_log(irqstat, cpu_base + GIC_CPU_EOI);
 #ifdef CONFIG_SMP
 			handle_IPI(irqnr, regs);
-			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
+//			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 #endif
 			continue;
 		}
