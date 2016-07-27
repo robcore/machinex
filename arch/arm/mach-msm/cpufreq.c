4e772a814d537bf914e2dcfcb22118f95d5f5eb9
robcore <robpatershuk@gmail.com>
2016-07-26 23:20:49 -0600

    msm-cpufreq-Add-pm-notifiers

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -17,9 +17,9 @@
  *
  */
 
-#ifdef CONFIG_HAS_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
+//#ifdef CONFIG_HAS_POWERSUSPEND
+//#include <linux/powersuspend.h>
+//#endif
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/cpufreq.h>
@@ -465,7 +465,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 
 	return 0;
 }
-#ifdef CONFIG_HAS_POWERSUSPEND
+/*#ifdef CONFIG_HAS_POWERSUSPEND
 static void msm_cpu_power_suspend(struct power_suspend *h)
 {
 #ifdef CONFIG_CPUFREQ_LIMIT_MAX_FREQ
@@ -494,12 +494,12 @@ static void msm_cpu_power_resume(struct power_suspend *h)
 }
 
 static struct power_suspend msm_cpu_power_suspend_handler = {
-/*	.level = POWER_SUSPEND_LEVEL_BLANK_SCREEN,
-*/
+	.level = POWER_SUSPEND_LEVEL_BLANK_SCREEN,
+
 	.suspend = msm_cpu_power_suspend,
 	.resume = msm_cpu_power_resume,
 };
-#endif
+#endif */
 
 static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 		unsigned long action, void *hcpu)
@@ -557,24 +557,20 @@ static struct notifier_block __refdata msm_cpufreq_cpu_notifier = {
 	.notifier_call = msm_cpufreq_cpu_callback,
 };
 
-/*
- * Define suspend/resume for cpufreq_driver. Kernel will call
- * these during suspend/resume with interrupts disabled. This
- * helps the suspend/resume variable get's updated before cpufreq
- * governor tries to change the frequency after coming out of suspend.
- */
-static int msm_cpufreq_suspend(struct cpufreq_policy *policy)
+static int msm_cpufreq_suspend(void)
 {
 	int cpu;
 
 	for_each_possible_cpu(cpu) {
+		mutex_lock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
 		per_cpu(cpufreq_suspend, cpu).device_suspended = 1;
+		mutex_unlock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
 	}
 
-	return 0;
+	return NOTIFY_DONE;
 }
 
-static int msm_cpufreq_resume(struct cpufreq_policy *policy)
+static int msm_cpufreq_resume(void)
 {
 	int cpu;
 
@@ -582,8 +578,27 @@ static int msm_cpufreq_resume(struct cpufreq_policy *policy)
 		per_cpu(cpufreq_suspend, cpu).device_suspended = 0;
 	}
 
-	return 0;
+	return NOTIFY_DONE;
 }
+
+static int msm_cpufreq_pm_event(struct notifier_block *this,
+				unsigned long event, void *ptr)
+{
+	switch (event) {
+	case PM_POST_HIBERNATION:
+	case PM_POST_SUSPEND:
+		return msm_cpufreq_resume();
+	case PM_HIBERNATION_PREPARE:
+	case PM_SUSPEND_PREPARE:
+		return msm_cpufreq_suspend();
+	default:
+		return NOTIFY_DONE;
+	}
+}
+
+static struct notifier_block msm_cpufreq_pm_notifier = {
+	.notifier_call = msm_cpufreq_pm_event,
+};
 
 static struct freq_attr *msm_freq_attr[] = {
 	&cpufreq_freq_attr_scaling_available_freqs,
@@ -597,8 +612,6 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 	.verify		= msm_cpufreq_verify,
 	.target		= msm_cpufreq_target,
 	.get		= msm_cpufreq_get_freq,
-	.suspend	= msm_cpufreq_suspend,
-	.resume		= msm_cpufreq_resume,
 	.name		= "msm",
 	.attr		= msm_freq_attr,
 };
@@ -801,10 +814,10 @@ static int __init msm_cpufreq_register(void)
 	platform_driver_probe(&msm_cpufreq_plat_driver, msm_cpufreq_probe);
 	msm_cpufreq_wq = alloc_workqueue("msm-cpufreq", WQ_HIGHPRI, 0);
 	register_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
-
-#ifdef CONFIG_HAS_POWERSUSPEND
-	register_power_suspend(&msm_cpu_power_suspend_handler);
-#endif
+	register_pm_notifier(&msm_cpufreq_pm_notifier)
+//#ifdef CONFIG_HAS_POWERSUSPEND
+//	register_power_suspend(&msm_cpu_power_suspend_handler);
+//#endif
 	return cpufreq_register_driver(&msm_cpufreq_driver);
 }
 
