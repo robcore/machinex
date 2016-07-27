2778241ecef055b8ed6582d049884c0b7cb1c4eb
robcore <robpatershuk@gmail.com>
2016-07-27 11:27:05 -0600

    fixup suspend-state-PM_SUSPEND_FREEZE

--- include/linux/suspend.h
+++ include/linux/suspend.h
@@ -187,11 +187,6 @@ struct platform_suspend_ops {
 	void (*recover)(void);
 };
 
-struct platform_freeze_ops {
-	int (*begin)(void);
-	void (*end)(void);
-};
-
 #ifdef CONFIG_SUSPEND
 /**
  * suspend_set_ops - set platform dependent suspend operations
@@ -199,7 +194,6 @@ struct platform_freeze_ops {
  */
 extern void suspend_set_ops(const struct platform_suspend_ops *ops);
 extern int suspend_valid_only_mem(suspend_state_t state);
-extern void freeze_set_ops(const struct platform_freeze_ops *ops);
 extern void freeze_wake(void);
 
 /**
@@ -226,7 +220,6 @@ extern int pm_suspend(suspend_state_t state);
 
 static inline void suspend_set_ops(const struct platform_suspend_ops *ops) {}
 static inline int pm_suspend(suspend_state_t state) { return -ENOSYS; }
-static inline void freeze_set_ops(const struct platform_freeze_ops *ops) {}
 static inline void freeze_wake(void) {}
 #endif /* !CONFIG_SUSPEND */
 
