0abce88c98b59c51883ef7f3f1a5b732b7796ac1
robcore <robpatershuk@gmail.com>
2016-07-26 20:31:34 -0600

    HID: fix a couple more off-by-ones

--- drivers/video/msm/mipi_samsung_octa.c
+++ drivers/video/msm/mipi_samsung_octa.c
@@ -568,6 +568,20 @@ static int mipi_samsung_disp_on_in_video_engine(struct platform_device *pdev)
 	mfd->resume_state = MIPI_RESUME_STATE;
 	touch_display_status = MIPI_RESUME_STATE;
 
+#ifdef CONFIG_STATE_NOTIFIER
+		if (!use_fb_notifier)
+			state_resume();
+#endif
+
+#ifdef CONFIG_POWERSUSPEND
+		/* Yank555.lu : hook to handle powersuspend tasks (wakeup) */
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
+#ifdef CONFIG_LCD_NOTIFY
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
+#endif
+
 	if ((msd.mpd->manufacture_id & 0xFF) == 0)
 		mipi_samsung_disp_send_cmd(mfd, PANEL_NEED_FLIP, false);
 
@@ -587,19 +601,6 @@ static int mipi_samsung_disp_on_in_video_engine(struct platform_device *pdev)
 		pm8xxx_gpio_config(pm_gpio5, &gpio_get_param);
 	}
 
-#ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_resume();
-#endif
-
-#ifdef CONFIG_POWERSUSPEND
-	/* Yank555.lu : hook to handle powersuspend tasks (wakeup) */
-	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
-#endif
-
-#ifdef CONFIG_LCD_NOTIFY
-	lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
-#endif
 
 	pr_info("[%s] ID : 0x%x LDI : %s", __func__, msd.mpd->manufacture_id,
 		ldi_manupacture == LDI_MAGNA ? "LDI_MAGNA" : "LDI_LSI");
