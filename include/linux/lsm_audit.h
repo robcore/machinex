0a60326251f2e9863904a449d3c820aab84f9208
robcore <robpatershuk@gmail.com>
2016-07-28 00:49:10 -0600

    LSM-remove-the-task-field-from-common_audit_data

--- include/linux/lsm_audit.h
+++ include/linux/lsm_audit.h
@@ -53,7 +53,7 @@ struct common_audit_data {
 #define LSM_AUDIT_DATA_KMOD	8
 #define LSM_AUDIT_DATA_INODE	9
 #define LSM_AUDIT_DATA_DENTRY	10
-	struct task_struct *tsk;
+#define LSM_AUDIT_DATA_IOCTL_OP	11
 	union 	{
 		struct path path;
 		struct dentry *dentry;
