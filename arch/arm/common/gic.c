0a56d0abfbb33b119ec6ef70b22ee88665e03004
robcore <robpatershuk@gmail.com>
2016-07-28 17:07:45 -0600

    PM-wakeup_reason-correctly-deduce-wakeup-interrupts

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -275,7 +275,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
 
-		log_wakeup_reason(i + gic->irq_offset);
+		log_base_wakeup_reason(i + gic->irq_offset);
 	}
 }
 
--- kernel/irq/irqdesc.c
+++ kernel/irq/irqdesc.c
@@ -14,6 +14,7 @@
 #include <linux/kernel_stat.h>
 #include <linux/radix-tree.h>
 #include <linux/bitmap.h>
+#include <linux/wakeup_reason.h>
 
 #include "internals.h"
 #ifdef CONFIG_SEC_DEBUG
@@ -339,8 +340,12 @@ int generic_handle_irq(unsigned int irq)
 		sec_debug_irq_sched_log(irq, (void *)desc->handle_irq,
 			irqs_disabled());
 #endif
-	generic_handle_irq_desc(irq, desc);
-	return 0;
+	if (unlikely(logging_wakeup_reasons()))
+		return log_possible_wakeup_reason(irq,
+				desc,
+				generic_handle_irq_desc);
+
+	return generic_handle_irq_desc(irq, desc);
 }
 EXPORT_SYMBOL_GPL(generic_handle_irq);
 
