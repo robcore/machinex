fc7e14ff356888c3bcb9003b667b0ca4fd49e712
robcore <robpatershuk@gmail.com>
2016-07-28 04:11:09 -0600

    10.4

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -429,9 +429,9 @@ static void an30259a_start_led_pattern(int mode)
 		break;
 	case BOOTING:
 		pr_info("LED Booting Pattern on\n");
-		//leds_on(LED_G, true, true, LED_G_CURRENT);
+		leds_on(LED_G, true, true, LED_G_CURRENT);
 		leds_on(LED_B, true, true, LED_B_CURRENT);
-		//leds_set_slope_mode(client, LED_G, 0, 15, 0, 0, 1, 1, 0, 0, 0, 0);
+		leds_set_slope_mode(client, LED_G, 0, 0, 5, 1, 1, 1, 0, 0, 0, 0);
 		leds_set_slope_mode(client, LED_B, 0, 15, 0, 0, 1, 1, 0, 0, 0, 0);
 		break;
 	default:
