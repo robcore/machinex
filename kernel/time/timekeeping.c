2d20befe3adf636eafde478e99ccb2c08423ec85
robcore <robpatershuk@gmail.com>
2016-07-28 07:56:53 -0600

    small config/compile changes

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -35,7 +35,7 @@ __cacheline_aligned_in_smp DEFINE_SEQLOCK(xtime_lock);
 int __read_mostly timekeeping_suspended;
 
 /* Flag for if there is a persistent clock on this platform */
-bool __read_mostly persistent_clock_exist = false;
+bool __read_mostly persistent_clock_exist = true;
 
 /**
  * timekeeper_setup_internals - Set up internals to use clocksource clock.
