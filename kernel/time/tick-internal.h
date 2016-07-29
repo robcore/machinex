6546c827222be15e6be2cb98c05265f161f3c89f
robcore <robpatershuk@gmail.com>
2016-07-29 02:23:11 -0600

    time-Kill-xtime_lock-replacing-it-with-jiffies_lock

--- kernel/time/tick-internal.h
+++ kernel/time/tick-internal.h
@@ -152,4 +152,3 @@ static inline int tick_device_is_functional(struct clock_event_device *dev)
 
 int __clockevents_update_freq(struct clock_event_device *dev, u32 freq);
 extern void do_timer(unsigned long ticks);
-extern seqlock_t xtime_lock;
