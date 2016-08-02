de74ecc216a3d8daaa552ccdf9a3050e19583edb
robcore <robpatershuk@gmail.com>
2016-08-02 17:02:10 -0600

    refactor cpu set-gov to introduce cpu get-gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -351,7 +351,7 @@ int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu);
 int cpufreq_update_policy(unsigned int cpu);
 bool have_governor_per_policy(void);
 int cpufreq_set_gov(char *target_gov, unsigned int cpu);
-
+char *cpufreq_get_gov(unsigned int cpu);
 /*
  * Governor specific info that can be passed to modules that subscribe
  * to CPUFREQ_GOVINFO_NOTIFIER
