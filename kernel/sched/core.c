0fae4620ce7eda472b0455cef7c148906d744488
robcore <robpatershuk@gmail.com>
2016-08-02 16:31:26 -0600

    remove previous implementation of get_online_cpus to prepare for next patch

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7618,6 +7618,12 @@ void __init sched_init_smp(void)
 	alloc_cpumask_var(&non_isolated_cpus, GFP_KERNEL);
 	alloc_cpumask_var(&fallback_doms, GFP_KERNEL);
 
+
+	/*
+	 * There's no userspace yet to cause hotplug operations; hence all the
+	 * cpu masks are stable and all blatant races in the below code cannot
+	 * happen.
+	 */
 	mutex_lock(&sched_domains_mutex);
 	init_sched_domains(cpu_active_mask);
 	cpumask_andnot(non_isolated_cpus, cpu_possible_mask, cpu_isolated_map);
