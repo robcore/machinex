2787f7b27593ba04513fb167de88c813bfa91492
robcore <robpatershuk@gmail.com>
2016-08-02 18:12:27 -0600

    msm_limiter: final update, i will see if it works without thermal mitigation implementation - long shot

--- arch/arm/mach-msm/msm_limiter.c
+++ arch/arm/mach-msm/msm_limiter.c
@@ -23,7 +23,7 @@
 #define MSM_LIMITER_MINOR		3
 
 /* Recommended to set below values from userspace. */
-#define FREQ_CONTROL			0
+#define FREQ_CONTROL			1
 #define DEBUG_MODE			0
 #define MPD_ENABLED			0
 
@@ -42,7 +42,7 @@
 #if defined(CONFIG_ARCH_MSM8916)
 #define DEFAULT_SUSP_MAX_FREQUENCY	998400
 #elif defined(CONFIG_ARCH_APQ8084)
-#define DEFAULT_SUSP_MAX_FREQUENCY	1728000
+#define DEFAULT_SUSP_MAX_FREQUENCY	300000
 #endif
 #if defined(CONFIG_ARCH_MSM8916)
 #define DEFAULT_RESUME_MAX_FREQUENCY	1209600
