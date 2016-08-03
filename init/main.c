a613c9a5d12e80063ab8908cac5cf2ea6854dd68
robcore <robpatershuk@gmail.com>
2016-08-03 17:02:14 -0600

    main-fix-the-fault-from-my-previous-updates

--- init/main.c
+++ init/main.c
@@ -250,7 +250,7 @@ early_param("loglevel", loglevel);
 early_param("batt_id_value", battStatus);
 
 /* Change NUL term back to "=", to make "param" the whole string. */
-static int __init repair_env_string(char *param, char *val)
+static int __init repair_env_string(char *param, char *val, const char *unused)
 {
 	if (val) {
 		/* param=val or param="val"? */
@@ -273,7 +273,7 @@ static int __init repair_env_string(char *param, char *val)
 static int __init unknown_bootoption(char *param, char *val,
 				const char *unused)
 {
-	repair_env_string(param, val);
+	repair_env_string(param, val, unused);
 
 	/* Handle obsolete-style parameters */
 	if (obsolete_checksetup(param))
@@ -825,7 +825,7 @@ static void __init do_initcall_level(int level)
 		   static_command_line, __start___param,
 		   __stop___param - __start___param,
 		   level, level,
-		   repair_env_string);
+		   &repair_env_string);
 
 	for (fn = initcall_levels[level]; fn < initcall_levels[level+1]; fn++)
 		do_one_initcall(*fn);
