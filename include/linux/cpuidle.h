d0c6937cea663567f5bf36eb9a4af199b4897378
robcore <robpatershuk@gmail.com>
2016-08-04 00:57:03 -0600

    cpuidle-coupled-add-parallel-barrier-function

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -191,6 +191,10 @@ static inline int cpuidle_play_dead(void) {return -ENODEV; }
 void cpuidle_coupled_parallel_barrier(struct cpuidle_device *dev, atomic_t *a);
 #endif
 
+#ifdef CONFIG_ARCH_NEEDS_CPU_IDLE_COUPLED
+void cpuidle_coupled_parallel_barrier(struct cpuidle_device *dev, atomic_t *a);
+#endif
+
 /******************************
  * CPUIDLE GOVERNOR INTERFACE *
  ******************************/
