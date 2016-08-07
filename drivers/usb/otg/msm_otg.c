a6f7b191504bb26d16470a553383991c268bf0d2
robcore <robpatershuk@gmail.com>
2016-08-06 18:17:01 -0600

    fixup-fastcharge again, way more stable

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -57,6 +57,8 @@
 
 #include <linux/fastchg.h>
 
+#include <linux/fastchg.h>
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1219,14 +1221,8 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 	if (motg->cur_power == mA)
 		return;
 
-	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
-
-	/*
-	 *  Use Power Supply API if supported, otherwise fallback
-	 *  to legacy pm8921 API.
-	 */
-	if (msm_otg_notify_power_supply(motg, mA))
-		pm8921_charger_vbus_draw(mA);
+	pm8921_charger_vbus_draw(mA);
+	msm_otg_notify_power_supply(motg, mA);
 
 	motg->cur_power = mA;
 #endif
@@ -1620,6 +1616,9 @@ static void msm_otg_start_peripheral(struct usb_otg *otg, int on)
 		on = 0;
 #endif
 
+	if (!use_mtp_during_fast_charge && on == 1)
+		on = 0;
+	
 	if (!otg->gadget)
 		return;
 
