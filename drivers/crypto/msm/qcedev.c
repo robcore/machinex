9d11582dfde51aa3c2d092fd6a6aadf4a21e6058
robcore <robpatershuk@gmail.com>
2016-08-10 10:37:18 -0600

    fix up some potential null pointer dereferences

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -848,9 +848,9 @@ static int qcedev_sha_update(struct qcedev_async_req *qcedev_areq,
 				struct qcedev_handle *handle)
 {
 	int err = 0;
-	int i = 0;
-	int j = 0;
-	int k = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
 	int num_entries = 0;
 	uint32_t total = 0;
 
@@ -1597,9 +1597,9 @@ static int qcedev_vbuf_ablk_cipher(struct qcedev_async_req *areq,
 {
 	int err = 0;
 	int di = 0;
-	int i = 0;
-	int j = 0;
-	int k = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
 	uint32_t byteoffset = 0;
 	int num_entries = 0;
 	uint32_t total = 0;
