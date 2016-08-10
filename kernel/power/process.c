acd288df59322486244360274c3bf63372747f87
robcore <robpatershuk@gmail.com>
2016-08-10 09:11:27 -0600

    fixed stupid mistake

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -99,8 +99,8 @@ static int try_to_freeze_tasks(bool user_only)
 		       elapsed_msecs / 1000, elapsed_msecs % 1000);
 	} else if (todo) {
 		printk("\n");
-		printk(KERN_ERR "Freezing of tasks %s after %d.%03d seconds "
-		       " (%d tasks refusing to freeze, wq_busy=%d):\n",
+		printk(KERN_ERR "Freezing of tasks failed after %d.%03d seconds"
+		       "(%d tasks refusing to freeze, wq_busy=%d):\n",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000,
 		       todo - wq_busy, wq_busy);
 
