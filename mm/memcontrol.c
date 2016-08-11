9827626cca6a874befa90e451d363f21bcd90a70
robcore <robpatershuk@gmail.com>
2016-08-10 21:38:44 -0600

    mm-memcontrol-micro-optimize-mem_cgroup_split_huge_fn

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2592,12 +2592,15 @@ static void __mem_cgroup_commit_charge(struct mem_cgroup *memcg,
  */
 void mem_cgroup_split_huge_fixup(struct page *head)
 {
-	struct page_cgroup *head_pc = lookup_page_cgroup(head);
+	struct page_cgroup *head_pc;
 	struct page_cgroup *pc;
 	int i;
 
 	if (mem_cgroup_disabled())
 		return;
+
+	head_pc = lookup_page_cgroup(head);
+
 	for (i = 1; i < HPAGE_PMD_NR; i++) {
 		pc = head_pc + i;
 		pc->mem_cgroup = head_pc->mem_cgroup;
