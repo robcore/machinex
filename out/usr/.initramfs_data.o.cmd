ea15a8d69875b3d408bc36ea3ed3cc7fd44cd69c
robcore <robpatershuk@gmail.com>
2016-08-21 18:26:55 -0600

    disabled profiling

---
+++ out/usr/.initramfs_data.o.cmd
@@ -0,0 +1,29 @@
+cmd_usr/initramfs_data.o := /media/root/robcore/android/machinex/scripts/gcc-wrapper.py /opt/toolchains/arm-cortex_a15-linux-gnueabihf_5.3/bin/arm-cortex_a15-linux-gnueabihf-gcc -Wp,-MD,usr/.initramfs_data.o.d  -nostdinc -isystem /opt/toolchains/arm-cortex_a15-linux-gnueabihf_5.3/bin/../lib/gcc/arm-cortex_a15-linux-gnueabihf/5.3.0/include -I/media/root/robcore/android/machinex/arch/arm/include -Iarch/arm/include/generated -Iinclude  -I/media/root/robcore/android/machinex/include -include /media/root/robcore/android/machinex/include/linux/kconfig.h -D__KERNEL__ -mlittle-endian   -I/media/root/robcore/android/machinex/arch/arm/mach-msm/include -D__ASSEMBLY__ -mabi=aapcs-linux -mno-thumb-interwork -funwind-tables -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a15 -mtune=cortex-a15 -include asm/unified.h -msoft-float -DINITRAMFS_IMAGE="usr/initramfs_data.cpio"   -c -o usr/initramfs_data.o /media/root/robcore/android/machinex/usr/initramfs_data.S
+
+source_usr/initramfs_data.o := /media/root/robcore/android/machinex/usr/initramfs_data.S
+
+deps_usr/initramfs_data.o := \
+    $(wildcard include/config/64bit.h) \
+  /media/root/robcore/android/machinex/arch/arm/include/asm/unified.h \
+    $(wildcard include/config/arm/asm/unified.h) \
+    $(wildcard include/config/thumb2/kernel.h) \
+  /media/root/robcore/android/machinex/include/linux/stringify.h \
+  /media/root/robcore/android/machinex/include/asm-generic/vmlinux.lds.h \
+    $(wildcard include/config/hotplug.h) \
+    $(wildcard include/config/hotplug/cpu.h) \
+    $(wildcard include/config/memory/hotplug.h) \
+    $(wildcard include/config/ftrace/mcount/record.h) \
+    $(wildcard include/config/trace/branch/profiling.h) \
+    $(wildcard include/config/profile/all/branches.h) \
+    $(wildcard include/config/event/tracing.h) \
+    $(wildcard include/config/tracing.h) \
+    $(wildcard include/config/ftrace/syscalls.h) \
+    $(wildcard include/config/function/graph/tracer.h) \
+    $(wildcard include/config/constructors.h) \
+    $(wildcard include/config/generic/bug.h) \
+    $(wildcard include/config/pm/trace.h) \
+    $(wildcard include/config/blk/dev/initrd.h) \
+
+usr/initramfs_data.o: $(deps_usr/initramfs_data.o)
+
+$(deps_usr/initramfs_data.o):
