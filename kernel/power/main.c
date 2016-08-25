9391d4f941097c5a80191ea9a10b8b0266e1155f
robcore <robpatershuk@gmail.com>
2016-08-24 22:42:07 -0600

    oops fix doubled code

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -670,30 +670,6 @@ power_attr(pm_freeze_timeout);
 
 #endif	/* CONFIG_FREEZER*/
 
-#ifdef CONFIG_FREEZER
-static ssize_t pm_freeze_timeout_show(struct kobject *kobj,
-				      struct kobj_attribute *attr, char *buf)
-{
-	return sprintf(buf, "%u\n", freeze_timeout_msecs);
-}
-
-static ssize_t pm_freeze_timeout_store(struct kobject *kobj,
-				       struct kobj_attribute *attr,
-				       const char *buf, size_t n)
-{
-	unsigned long val;
-
-	if (kstrtoul(buf, 10, &val))
-		return -EINVAL;
-
-	freeze_timeout_msecs = val;
-	return n;
-}
-
-power_attr(pm_freeze_timeout);
-
-#endif	/* CONFIG_FREEZER*/
-
 #ifdef CONFIG_SEC_DVFS
 DEFINE_MUTEX(dvfs_mutex);
 static unsigned long dvfs_id;
@@ -958,9 +934,6 @@ static struct attribute * g[] = {
 #ifdef CONFIG_PM_SLEEP_DEBUG
 	&pm_print_times_attr.attr,
 #endif
-#endif
-#ifdef CONFIG_FREEZER
-	&pm_freeze_timeout_attr.attr,
 #endif
 #ifdef CONFIG_FREEZER
 	&pm_freeze_timeout_attr.attr,
