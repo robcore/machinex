2b2fa97a8d59fc77433163a58b920d8aa5891af8
robcore <robpatershuk@gmail.com>
2016-08-26 17:54:30 -0600

    gpu-tiny OC - just changed top freq to 489 from 450 from what i can tell by the specs, also other video driver spec adjustments directly from qualcomm dsi programming guide

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3566,7 +3566,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d[] = {
 	F_GFX3D(266667000, pll2,  1,  3),
 	F_GFX3D(320000000, pll2,  2,  5),
 	F_GFX3D(400000000, pll2,  1,  2),
-	F_GFX3D(450000000, pll15, 1,  2),
+	F_GFX3D(489000000, pll15, 1,  2),
 	F_END
 };
 
@@ -3616,7 +3616,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8930ab[] = {
 static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
-	[VDD_DIG_HIGH]    = 450000000
+	[VDD_DIG_HIGH]    = 489000000
 };
 
 static unsigned long fmax_gfx3d_8064[VDD_DIG_NUM] = {
