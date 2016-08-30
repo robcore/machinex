b97a076fa2854f0d89ef04c4eec8e220269c2c05
robcore <robpatershuk@gmail.com>
2016-08-30 03:06:51 -0600

    my bad i forgot to rename the declaration for my experimental pa_gain_control function, was originally a more confusing pa_gain_unlock

--- sound/soc/codecs/sound_control.c
+++ sound/soc/codecs/sound_control.c
@@ -28,7 +28,7 @@ extern struct snd_soc_codec *snd_engine_codec_ptr;
 int snd_ctrl_enabled = 1;
 static int snd_ctrl_locked = 0;
 static int snd_rec_ctrl_locked = 0;
-static int pa_gain_unlock = 1;
+static int pa_gain_control = 1;
 static int actual_pa_gain = 22;
 
 unsigned int tabla_read(struct snd_soc_codec *codec, unsigned int reg);
