99841b0564f85b7621be9f1358b083af92f18c99
robcore <robpatershuk@gmail.com>
2016-09-12 15:03:01 -0600

    event timer - unlock should not be sitting in the exit case

--- arch/arm/mach-msm/event_timer.c
+++ arch/arm/mach-msm/event_timer.c
@@ -183,8 +183,8 @@ static enum hrtimer_restart event_hrtimer_cb(struct hrtimer *hrtimer)
 	if (next)
 		create_hrtimer(next->expires);
 
-hrtimer_cb_exit:
 	spin_unlock_irqrestore(&event_timer_lock, flags);
+hrtimer_cb_exit:
 	return HRTIMER_NORESTART;
 }
 
