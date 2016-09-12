7d495f235e06aa77ef43b1052e2eb0e5557155cc
robcore <robpatershuk@gmail.com>
2016-09-12 17:59:20 -0600

    oops nope thats what its supposed to be after the android workaround patch

--- drivers/rtc/alarm-dev.c
+++ drivers/rtc/alarm-dev.c
@@ -108,7 +108,7 @@ static long alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 				wake_unlock(&alarm_wake_lock);
 		}
 		alarm_enabled &= ~alarm_type_mask;
-		if (alarm_type == ANDROID_ALARM_RTC_POWEROFF_WAKEUP)
+		if (alarm_type == ANDROID_ALARM_RTC_WAKEUP)
 			set_power_on_alarm(0);
 		spin_unlock_irqrestore(&alarm_slock, flags);
 		if (alarm_type == ANDROID_ALARM_RTC_POWEROFF_WAKEUP)
@@ -143,7 +143,7 @@ from_old_alarm_set:
 		alarm_start_range(&alarms[alarm_type],
 			timespec_to_ktime(new_alarm_time),
 			timespec_to_ktime(new_alarm_time));
-		if ((alarm_type == ANDROID_ALARM_RTC_POWEROFF_WAKEUP) &&
+		if ((alarm_type == ANDROID_ALARM_RTC_WAKEUP) &&
 				(ANDROID_ALARM_BASE_CMD(cmd) ==
 				 ANDROID_ALARM_SET(0)))
 			set_power_on_alarm(new_alarm_time.tv_sec);
