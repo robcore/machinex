dba12f878adcff6790aa1f352aac06de7d0c45de
robcore <robpatershuk@gmail.com>
2016-09-13 23:55:57 -0600

    i will never be finished playing with leds

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -434,18 +434,18 @@ static void an30259a_start_led_pattern(int mode)
  */
 	case POWERING:
 		pr_info("LED Powering Pattern on\n");
-		leds_on(LED_G, true, true, LED_DYNAMIC_CURRENT);
+		leds_on(LED_R, true, true, LED_DYNAMIC_CURRENT);
 		leds_on(LED_B, true, true, LED_DYNAMIC_CURRENT);
-		leds_set_slope_mode(client, LED_G,
-				0, 15, 0, 0, 2, 2, 0, 0, 1, 1);
+		leds_set_slope_mode(client, LED_R,
+				0, 5, 0, 4, 0, 2, 0, 0, 0, 2);
 		leds_set_slope_mode(client, LED_B,
-				0, 15, 0, 0, 2, 2, 1, 1, 0, 0);
+				0, 15, 0, 0, 2, 1, 1, 2, 0, 0);
 
 		break;
 	case BOOTING:
 		pr_info("LED Booting Pattern on\n");
 		leds_on(LED_B, true, true, LED_B_CURRENT);
-		leds_set_slope_mode(client, LED_B, 0, 15, 0, 0, 2, 2, 1, 1, 0, 0);
+		leds_set_slope_mode(client, LED_B, 0, 15, 0, 0, 0, 0, 0, 1, 0, 0);
 
 		break;
 	default:
