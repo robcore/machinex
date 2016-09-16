6c4daeb4f91cbbc0dc7e7819c129fb7fb4c1ff31
robcore <robpatershuk@gmail.com>
2016-09-15 18:27:22 -0600

    revertme

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -247,6 +247,18 @@ static void alarmtimer_dequeue(struct alarm_base *base, struct alarm *alarm)
 }
 
 
+void alarm_restart(struct alarm *alarm)
+{
+	struct alarm_base *base = &alarm_bases[alarm->type];
+	unsigned long flags;
+
+	spin_lock_irqsave(&base->lock, flags);
+	hrtimer_set_expires(&alarm->timer, alarm->node.expires);
+	hrtimer_restart(&alarm->timer);
+	alarmtimer_enqueue(base, alarm);
+	spin_unlock_irqrestore(&base->lock, flags);
+}
+
 /**
  * alarmtimer_fired - Handles alarm hrtimer being fired.
  * @timer: pointer to hrtimer being run
