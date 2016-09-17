f647accf508425892f7696027ac59178704ee58d
robcore <robpatershuk@gmail.com>
2016-09-16 21:33:25 -0600

    cpufreq: Add interface to set/get min/max freq

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -359,6 +359,10 @@ int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu);
 int cpufreq_update_policy(unsigned int cpu);
 bool have_governor_per_policy(void);
 int cpufreq_set_gov(char *target_gov, unsigned int cpu);
+int cpufreq_set_freq(unsigned int max_freq, unsigned int min_freq,
+			unsigned int cpu);
+int cpufreq_get_max(unsigned int cpu);
+int cpufreq_get_min(unsigned int cpu);
 
 /*
  * Governor specific info that can be passed to modules that subscribe
