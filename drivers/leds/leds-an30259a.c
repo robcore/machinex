8ba6aa7e986996b56970f1aa3f9479d1c4addb05
robcore <robpatershuk@gmail.com>
2016-09-16 23:30:03 -0600

    led fun because mark eight is ready baby

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -437,9 +437,9 @@ static void an30259a_start_led_pattern(int mode)
 		leds_on(LED_R, true, true, LED_DYNAMIC_CURRENT);
 		leds_on(LED_G, true, true, LED_DYNAMIC_CURRENT);
 		leds_set_slope_mode(client, LED_R,
-				0, 15, 5, 10, 4, 2, 8, 4, 2, 1);
+				0, 15, 7, 12, 6, 2, 8, 4, 2, 4);
 		leds_set_slope_mode(client, LED_G,
-				0, 15, 10, 5, 2, 4, 1, 2, 4, 8);
+				2, 15, 10, 5, 0, 4, 1, 2, 4, 2);
 
 		break;
 	case BOOTING:
