2d4cd59d760d598e39f0ab71d0f825d077425cce
robcore <robpatershuk@gmail.com>
2016-09-18 16:50:42 -0600

    REVERT - msm-kgsl-Lower-bounds-check-the-number-of-power-levels - we are using a hard limit solution that makes this broken solution (min power level is not 1 for this driver) unneccesary

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1151,8 +1151,7 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 	if (pdata->set_grp_async != NULL)
 		pdata->set_grp_async();
 
-	if (pdata->num_levels > KGSL_MAX_PWRLEVELS ||
-	    pdata->num_levels < 1) {
+	if (pdata->num_levels > KGSL_MAX_PWRLEVELS) {
 		KGSL_PWR_ERR(device, "invalid power level count: %d\n",
 					 pdata->num_levels);
 		result = -EINVAL;
