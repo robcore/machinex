041ddfdef3bb8dd437fb61b7f7d9606c57d03b45
robcore <robpatershuk@gmail.com>
2016-09-19 01:25:19 -0600

    REVERT msm-kgsl-Use-a-constant-mask-for-the-memops-vmflags

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -118,7 +118,7 @@ struct kgsl_pagetable;
 struct kgsl_memdesc;
 
 struct kgsl_memdesc_ops {
-	unsigned int vmflags;
+	int (*vmflags)(struct kgsl_memdesc *);
 	int (*vmfault)(struct kgsl_memdesc *, struct vm_area_struct *,
 		       struct vm_fault *);
 	void (*free)(struct kgsl_memdesc *memdesc);
