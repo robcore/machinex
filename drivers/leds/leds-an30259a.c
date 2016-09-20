ac36e2f2cb9325b339bb7d70edee53d5e01a7429
robcore <robpatershuk@gmail.com>
2016-09-20 11:43:19 -0600

    just in case that needs to be populated, i know the single slope timer doesnt have to be, but perhaps the slope up param needs to be populated...so maybe it wasnt cpu limit, either way ill fix this and then test cpu freq limit again later

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -437,9 +437,9 @@ static void an30259a_start_led_pattern(int mode)
 		leds_on(LED_R, true, true, LED_DYNAMIC_CURRENT);
 		leds_on(LED_G, true, true, LED_DYNAMIC_CURRENT);
 		leds_set_slope_mode(client, LED_R,
-				0, 15, 8, 12, 6, 2, 8, 4, 2, 4);
+				0, 15, 8, 12, 6, 1, 8, 4, 2, 4);
 		leds_set_slope_mode(client, LED_G,
-				2, 2, 10, 5, 0, 4, 1, 2, 4, 2);
+				2, 2, 10, 5, 1, 6, 0, 2, 4, 2);
 
 		break;
 	case BOOTING:
