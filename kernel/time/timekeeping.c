4cf8c8b73c1bc920a90658a9e59214d9f2718a23
robcore <robpatershuk@gmail.com>
2016-09-23 20:02:16 -0600

    timekeeping-Fix-a-few-minor-newline-issues.

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -210,7 +210,6 @@ void getnstimeofday(struct timespec *ts)
 {
 	WARN_ON(__getnstimeofday(ts));
 }
-
 EXPORT_SYMBOL(getnstimeofday);
 
 /**
@@ -301,8 +300,8 @@ void do_gettimeofday(struct timeval *tv)
 	tv->tv_sec = now.tv_sec;
 	tv->tv_usec = now.tv_nsec/1000;
 }
-
 EXPORT_SYMBOL(do_gettimeofday);
+
 /**
  * do_settimeofday - Sets the time of day
  * @tv:		pointer to the timespec variable containing the new time
@@ -336,7 +335,6 @@ int do_settimeofday(const struct timespec *tv)
 
 	return 0;
 }
-
 EXPORT_SYMBOL(do_settimeofday);
 
 
