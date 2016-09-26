9be4d440f7a3cef236cceacdb8cc2784e6a56f0f
robcore <robpatershuk@gmail.com>
2016-09-25 19:56:23 -0600

    some battery fixing

--- arch/arm/mach-msm/board-fusion3-battery.c
+++ arch/arm/mach-msm/board-fusion3-battery.c
@@ -1026,8 +1026,8 @@ sec_battery_platform_data_t sec_battery_pdata = {
 // see which is better
 //	.full_condition_soc = 97,
 // .full_condition_vcell = 4300,
-	.full_condition_soc = 93,
-	.full_condition_vcell = 4250,
+	.full_condition_soc = 97,
+	.full_condition_vcell = 4300,
 
 	.recharge_check_count = 2,
 	.recharge_condition_type =
@@ -1042,7 +1042,9 @@ sec_battery_platform_data_t sec_battery_pdata = {
 	/* Fuel Gauge */
 	.fg_irq = MSM_GPIO_TO_INT(GPIO_FUEL_INT),
 	.fg_irq_attr =
-		IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING,
+		IRQF_TRIGGER_FALLING |
+		IRQF_TRIGGER_RISING |
+		IRQF_ONESHOT,
 	.fuel_alert_soc = 2,
 	.repeated_fuelalert = false,
 	.capacity_calculation_type =
