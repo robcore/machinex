b18b981b76c4630655e2fd0fb9f1562438f747b0
robcore <robpatershuk@gmail.com>
2016-09-26 02:31:07 -0600

    im not even mad, this gave me an excuse to try a higher clk gear in slimbus

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -445,7 +445,7 @@ static void an30259a_start_led_pattern(int mode)
 	case BOOTING:
 		pr_info("LED Booting Pattern on\n");
 		leds_on(LED_G, true, true, LED_R_CURRENT);
-		leds_set_slope_mode(client, LED_R, 1, 15, 0, 0, 1, 1, 0, 0, 0, 0;
+		leds_set_slope_mode(client, LED_R, 1, 15, 0, 0, 1, 1, 0, 0, 0, 0);
 
 		break;
 	default:
