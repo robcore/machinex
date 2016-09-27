ea3de73d91dbb13fd77ff86c2b2c71542d3b551f
robcore <robpatershuk@gmail.com>
2016-09-26 23:38:33 -0600

    one last led tweak lets build

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -437,15 +437,15 @@ static void an30259a_start_led_pattern(int mode)
 		leds_on(LED_B, true, true, LED_DYNAMIC_CURRENT);
 		leds_on(LED_G, true, true, LED_DYNAMIC_CURRENT);
 		leds_set_slope_mode(client, LED_B,
-				0, 15, 10, 1, 4, 1, 1, 0, 2, 4);
+				0, 15, 5, 0, 4, 1, 2, 0, 0, 0);
 		leds_set_slope_mode(client, LED_G,
-				1, 0, 0, 1, 1, 4, 0, 0, 2, 4);
+				0, 0, 0, 1, 1, 2, 0, 0, 2, 1);
 
 		break;
 	case BOOTING:
 		pr_info("LED Booting Pattern on\n");
 		leds_on(LED_R, true, true, LED_R_CURRENT);
-		leds_set_slope_mode(client, LED_R, 1, 15, 0, 0, 1, 1, 0, 0, 0, 0);
+		leds_set_slope_mode(client, LED_R, 0, 15, 0, 0, 1, 1, 0, 0, 0, 0);
 
 		break;
 	default:
