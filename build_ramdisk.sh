301b0a899211f5c9e3426ebe7971b448059c80f1
robcore <robpatershuk@gmail.com>
2016-09-28 21:56:18 -0600

    please work

--- build_ramdisk.sh
+++ build_ramdisk.sh
@@ -2,10 +2,10 @@
 cd /media/root/robcore/AIK;
 rm -rf machinex-new;
 cp -R -p machina-new machinex-new;
-cp -p /media/root/robcore/android/machinex/arch/arm/boot/dhd.ko $(pwd)/machinex-new/system/lib/modules/dhd.ko;
-cp -p /media/root/robcore/android/machinex/arch/arm/boot/scsi_wait_scan.ko $(pwd)/machinex-new/system/lib/modules/scsi_wait_scan.ko;
+cp -p ~/machinex/arch/arm/boot/dhd.ko $(pwd)/machinex-new/system/lib/modules/dhd.ko;
+cp -p ~/machinex/arch/arm/boot/scsi_wait_scan.ko $(pwd)/machinex-new/system/lib/modules/scsi_wait_scan.ko;
 rm $(pwd)/split_img/boot.img-zImage;
-cp -p /media/root/robcore/android/machinex/arch/arm/boot/boot.img-zImage $(pwd)/split_img/boot.img-zImage;
+cp -p ~/machinex/arch/arm/boot/boot.img-zImage $(pwd)/split_img/boot.img-zImage;
 rm image-new.img;
 sh repackimg.sh --sudo;
 cp -p image-new.img $(pwd)/machinex-new/boot.img
