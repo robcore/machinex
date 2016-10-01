51cd751cb82f4a8c3dc480c9fcfecc753ed2dd53
robcore <robpatershuk@gmail.com>
2016-10-01 16:05:19 -0600

    is that better?

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -946,7 +946,7 @@ int msm_pm_wait_cpu_shutdown(unsigned int cpu)
 
 		if (acc_sts & msm_pm_slp_sts[cpu].mask)
 			return 0;
-		udelay(10000);
+		udelay(1000);
 		WARN(++timeout == 10, "CPU%u didn't collape within 10ms\n",
 					cpu);
 	}
