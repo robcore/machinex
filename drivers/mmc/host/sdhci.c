f42b82fd82ef3e835fdc7c79b4a1036b3da145c7
robcore <robpatershuk@gmail.com>
2016-10-02 10:41:11 -0600

    mmc: sdhci: remove too large timeout warning-fix for mmc cards that specifiy a time out longer than what we specify here

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -693,8 +693,6 @@ static u8 sdhci_calc_timeout(struct sdhci_host *host, struct mmc_command *cmd)
 	}
 
 	if (count >= 0xF) {
-		printk(KERN_WARNING "%s: Too large timeout requested for CMD%d!\n",
-		       mmc_hostname(host->mmc), cmd->opcode);
 		count = 0xE;
 
 	return count;
