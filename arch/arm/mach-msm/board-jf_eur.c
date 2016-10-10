ff899fd2e08d97db15ba7bdc28ad63853e895280
robcore <robpatershuk@gmail.com>
2016-10-10 01:30:45 -0600

    quick modem fix for the users test build

--- arch/arm/mach-msm/board-jf_eur.c
+++ arch/arm/mach-msm/board-jf_eur.c
@@ -3163,6 +3163,7 @@ static struct mdm_platform_data mdm_platform_data = {
 	.mdm2ap_status_gpio_run_cfg = &mdm2ap_status_gpio_run_cfg,
 	.sysmon_subsys_id_valid = 1,
 	.sysmon_subsys_id = SYSMON_SS_EXT_MODEM,
+	.no_a2m_errfatal_on_ssr = 1,
 };
 
 static struct mdm_platform_data amdm_platform_data = {
