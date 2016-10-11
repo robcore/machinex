9074201776d6700062b515352dc88a3d7de05eec
robcore <robpatershuk@gmail.com>
2016-10-10 22:32:02 -0600

    remove undefined reference to wakeup reason now that we are not building it

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -40,7 +40,7 @@
 #include <linux/percpu.h>
 #include <linux/slab.h>
 #include <linux/syscore_ops.h>
-#include <linux/wakeup_reason.h>
+//#include <linux/wakeup_reason.h>
 
 #include <asm/irq.h>
 #include <asm/exception.h>
@@ -275,7 +275,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
 
-		log_wakeup_reason(i + gic->irq_offset);
+		//log_wakeup_reason(i + gic->irq_offset);
 	}
 }
 
