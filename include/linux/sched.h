e4eaa6471f5eb525145c5707637a376d27340e31
robcore <robpatershuk@gmail.com>
2016-10-11 13:33:07 -0600

    genirq-reimplement-exit_irq_thread-hook-via-task_work

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1259,10 +1259,6 @@ struct task_struct {
 	unsigned sched_reset_on_fork:1;
 	unsigned sched_contributes_to_load:1;
 
-#ifdef CONFIG_GENERIC_HARDIRQS
-	/* IRQ handler threads */
-	unsigned irq_thread:1;
-#endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
 	pid_t pid;
