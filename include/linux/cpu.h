77553f80b2916d5e25bcb5c86e05551c1d6fa276
robcore <robpatershuk@gmail.com>
2016-10-12 08:47:44 -0600

    hrm

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -278,9 +278,8 @@ void idle_notifier_register(struct notifier_block *n);
 void idle_notifier_unregister(struct notifier_block *n);
 void idle_notifier_call_chain(unsigned long val);
 
-
-extern bool check_cpuboost(int cpu);
 #if 0
+extern bool check_cpuboost(int cpu);
 extern bool wakeup_boost;
 #endif
 
