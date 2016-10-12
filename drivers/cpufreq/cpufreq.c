0116c7a46e54f3a2933cf39b741b79e9eb9a576e
robcore <robpatershuk@gmail.com>
2016-10-12 12:56:02 -0600

    JESUS IM SO DUMB SOMETIMES

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -36,10 +36,10 @@
 #include <linux/syscore_ops.h>
 
 #include <trace/events/power.h>
-
+#ifdef ARM_AUTO_HOTPLUG
 static unsigned int Lenable_auto_hotplug = 1;
 extern void apenable_auto_hotplug(bool state);
-
+#endif
 extern ssize_t get_gpu_vdd_levels_str(char *buf);
 extern void set_gpu_vdd_levels(int uv_tbl[]);
 
@@ -839,6 +839,7 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
+#ifdef ARM_AUTO_HOTPLUG
 static ssize_t show_enable_auto_hotplug(struct cpufreq_policy *policy, char *buf)
 {
        return sprintf(buf, "%u\n", Lenable_auto_hotplug);
@@ -853,6 +854,7 @@ static ssize_t store_enable_auto_hotplug(struct cpufreq_policy *policy,
        apenable_auto_hotplug((bool) Lenable_auto_hotplug);
        return count;
 }
+#endif
 
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 extern ssize_t acpuclk_get_vdd_levels_str(char *buf);
@@ -930,7 +932,9 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
+#ifdef ARM_AUTO_HOTPLUG
 cpufreq_freq_attr_rw(enable_auto_hotplug);
+#endif
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 define_one_global_rw(vdd_levels);
 #endif
@@ -951,7 +955,9 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+#ifdef ARM_AUTO_HOTPLUG
 	&enable_auto_hotplug.attr,
+#endif
 	&GPU_mV_table.attr,
 	&policy_min_freq.attr,
 	&policy_max_freq.attr,
