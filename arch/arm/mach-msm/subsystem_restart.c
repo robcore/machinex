c9198d6a4a37407a45351b206b33c2a183707e7c
robcore <robpatershuk@gmail.com>
2016-10-14 22:34:42 -0600

    fuck that

--- arch/arm/mach-msm/subsystem_restart.c
+++ arch/arm/mach-msm/subsystem_restart.c
@@ -215,23 +215,6 @@ static void subsys_set_state(struct subsys_device *subsys,
 	spin_unlock_irqrestore(&subsys->restart_lock, flags);
 }
 
-static struct subsys_data *_find_subsystem(const char *subsys_name)
-{
-	struct subsys_data *subsys;
-	unsigned long flags;
-
-	mutex_lock(&subsystem_list_lock);
-	list_for_each_entry(subsys, &subsystem_list, list)
-		if (!strncmp(subsys->name, subsys_name,
-				SUBSYS_NAME_MAX_LENGTH)) {
-			mutex_unlock(&subsystem_list_lock);
-			return subsys;
-		}
-	mutex_unlock(&subsystem_list_lock);
-
-	return NULL;
-}
-
 static struct subsys_soc_restart_order *
 update_restart_order(struct subsys_device *dev)
 {
