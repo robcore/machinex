6250609d7f9cd4c8d8e4b1a3eb7e1db4fa50e2f3
robcore <robpatershuk@gmail.com>
2016-10-14 23:21:46 -0600

    ..

--- arch/arm/mach-msm/modem-8960.c
+++ arch/arm/mach-msm/modem-8960.c
@@ -30,7 +30,7 @@
 #include <mach/socinfo.h>
 #include <mach/restart.h>
 #include <mach/msm_smsm.h>
-#include <mach/board_machinex>
+#include <mach/board_machinex.h>
 #include "sysmon.h"
 
 #include "smd_private.h"
