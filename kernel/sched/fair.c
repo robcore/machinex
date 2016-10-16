2ed54dedf29d669d58bc8ae20963730a5ac775c5
robcore <robpatershuk@gmail.com>
2016-10-16 02:02:59 -0600

    sched-fair-Stop-searching-for-tasks-in-newidle-balance-6% performance boost apparently

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5141,7 +5141,12 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 		interval = msecs_to_jiffies(sd->balance_interval);
 		if (time_after(next_balance, sd->last_balance + interval))
 			next_balance = sd->last_balance + interval;
-		if (pulled_task) {
+
+		/*
+		 * Stop searching for tasks to pull if there are
+		 * now runnable tasks on this rq.
+		 */
+		if (pulled_task || this_rq->nr_running > 0) {
 			this_rq->idle_stamp = 0;
 			break;
 		}
