2752d4bfde93c2173d810c0485fc9877f059cc0b
robcore <robpatershuk@gmail.com>
2016-10-16 01:31:06 -0600

    fuck me, my state notifier driver idea failed miserably, neobuddys makes sense now im just an idiot

--- drivers/video/msm/state_notifier.c
+++ drivers/video/msm/state_notifier.c
@@ -9,42 +9,27 @@
  *
  */
 
-#include <linux/state_notifier.h>
-#include <linux/kernel.h>
-#include <linux/notifier.h>
-#include <linux/module.h>
-#include <linux/init.h>
 #include <linux/export.h>
-#include <linux/workqueue.h>
-#include <linux/sched.h>
-#include <linux/timer.h>
-#include <linux/delay.h>
+#include <linux/module.h>
+#include <linux/state_notifier.h>
 
-/*necessary measure given existing conflictions */
-#ifdef pr_fmt
-#undef pr_fmt
-#endif
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #define DEFAULT_SUSPEND_DEFER_TIME 	10
 #define STATE_NOTIFIER			"state_notifier"
-#define ENABLED_ON_BOOT 0 /* Toggle this to enable or disable at startup */
+
 /*
  * debug = 1 will print all
  */
 static unsigned int debug;
 module_param_named(debug_mask, debug, uint, 0644);
 
-/* safety measure despite very unlikely scenario */
-#ifdef dprintk
-#undef drpintk
-#endif
 #define dprintk(msg...)		\
 do {				\
 	if (debug)		\
 		pr_info(msg);	\
 } while (0)
 
-static unsigned int snotifier_enabled = ENABLED_ON_BOOT; /* now a pointer */
+static bool enabled;
+module_param_named(enabled, enabled, bool, 0664);
 static unsigned int suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME;
 module_param_named(suspend_defer_time, suspend_defer_time, uint, 0664);
 static struct delayed_work suspend_work;
@@ -106,92 +91,40 @@ static void _resume_work(struct work_struct *work)
 void state_suspend(void)
 {
 	dprintk("%s: suspend called.\n", STATE_NOTIFIER);
-	if (suspend_in_progress || state_suspended) /* No longer need the !enabled condition due to refactored init function below */
+	if (state_suspended || suspend_in_progress || !enabled)
 		return;
 
-	cancel_work_sync(&resume_work); /* for rapid on/off events */
 	suspend_in_progress = true;
 
-	queue_delayed_work(susp_wq, &suspend_work,
+	queue_delayed_work_on(0, susp_wq, &suspend_work, 
 		msecs_to_jiffies(suspend_defer_time * 1000));
 }
 
 void state_resume(void)
 {
-	if (!state_suspended) /* This is to address your concern of the driver being disabled during suspend */
-		return;
-
 	dprintk("%s: resume called.\n", STATE_NOTIFIER);
 	cancel_delayed_work_sync(&suspend_work);
 	suspend_in_progress = false;
 
 	if (state_suspended)
-		queue_work(susp_wq, &resume_work);
+		queue_work_on(0, susp_wq, &resume_work);
 }
 
-static int state_notifier_init(void)
+static int __init state_notifier_init(void)
 {
-	if (!snotifier_enabled) /* Now the workqueue, and thus, the driver, isn't even allocated unless explicitly enabled above or by userspace */
-		return 0;
-
 	susp_wq = create_singlethread_workqueue("state_susp_wq");
 	if (!susp_wq)
 		pr_err("State Notifier failed to allocate suspend workqueue\n");
-	return -ENOMEM; /* Without the workqueue, the driver won't work, so halt its creation here if that happens */
 
 	INIT_DELAYED_WORK(&suspend_work, _suspend_work);
 	INIT_WORK(&resume_work, _resume_work);
-	pr_info("%s: State Notifier Init\n", __func__);
 
 	return 0;
 }
 
-static void __ref state_notifier_exit(void)
-{
-	cancel_work_sync(&resume_work);
-	cancel_delayed_work_sync(&suspend_work);
-	flush_workqueue(susp_wq);
-	destroy_workqueue(susp_wq);
-
-	pr_info("%s: State Notifier Exit\n", __func__);
-}
-
-/* Master Switch */
-static int set_enabled(const char *val, const struct kernel_param *kp)
-{
-	int ret = 0;
-	unsigned int i;
-
-	ret = kstrtouint(val, 10, &i);
-	if (ret)
-		return -EINVAL;
-
-	if (i < 0 || i > 1)
-		return -ERANGE;
-
-	if (i == snotifier_enabled)
-		return ret;
-
-	snotifier_enabled = i;
-
-	if (snotifier_enabled)
-		ret = state_notifier_init();
-	else
-		state_notifier_exit();
-
-	return ret;
-}
-
-static struct kernel_param_ops enabled_ops = {
-	.set = set_enabled,
-	.get = param_get_uint,
-};
-
-module_param_cb(enabled, &enabled_ops, &snotifier_enabled, 0644);
-
 subsys_initcall(state_notifier_init);
-module_exit(state_notifier_exit);
 
 MODULE_AUTHOR("Pranav Vashi <neobuddy89@gmail.com>");
 MODULE_DESCRIPTION("State Notifier Driver");
 MODULE_LICENSE("GPLv2");
+
