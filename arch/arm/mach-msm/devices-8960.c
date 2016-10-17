8276ad15f310c12d164d8c49713cb828027cee8a
robcore <robpatershuk@gmail.com>
2016-10-17 06:00:08 -0600

    jeeze louise

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -3741,6 +3741,7 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
+		{
 			.gpu_freq = 600000000,
 			.bus_freq = 6,
 			.io_fraction = 0,
