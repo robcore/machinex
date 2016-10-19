9ee2bcf4847542eae52c921d81ee9c7791b95aaa
robcore <robpatershuk@gmail.com>
2016-10-19 15:00:38 -0600

    come to think of it, those should probably be static

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -89,8 +89,8 @@ static int get_input_boost_freq(char *buf, const struct kernel_param *kp)
 {
 	struct cpu_sync *i_sync_info;
 	int i;
-	unsigned int val;
-	int ret;
+	static unsigned int val;
+	static int ret;
 
 		for_each_possible_cpu(i) {
 			i_sync_info = &per_cpu(sync_info, i);
