a2baf1de167a6a2cb97e26dce19371f47b608358
robcore <robpatershuk@gmail.com>
2016-10-21 17:30:32 -0600

    got rid of some of my previous temp scripts

--- reverso.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-patch -p1 -R < "0092-revert-those-im-pretty-sure-we-did-this-a-long-time-.patch"
-patch -p1 -R < "0091-lockdep-check-that-no-locks-held-at-freeze-time.patch"
-patch -p1 -R < "0090-freezer-add-unsafe-versions-of-freezable-helpers-for.patch"
-patch -p1 -R < "0089-freezer-added-unsafe-versions-of-freezable-helpers-f.patch"
-patch -p1 -R < "0088-freezer-define-try_to_freeze_nowarn.patch"
