d72348be087a558a13b500861939c4a535aebad4
robcore <robpatershuk@gmail.com>
2016-10-21 23:10:56 -0600

    sched-fair-Set-se-vruntime-directly-in-place_entity

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1579,9 +1579,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 	}
 
 	/* ensure we never gain time by being placed backwards. */
-	vruntime = max_vruntime(se->vruntime, vruntime);
-
-	se->vruntime = vruntime;
+	se->vruntime = max_vruntime(se->vruntime, vruntime);
 }
 
 static void check_enqueue_throttle(struct cfs_rq *cfs_rq);
