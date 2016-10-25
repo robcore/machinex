fbd328854f8d09c54592c0d68ef7688c967df7d8
robcore <robpatershuk@gmail.com>
2016-10-24 22:31:14 -0600

    Revert that revert

--- drivers/video/msm/external_common.h
+++ drivers/video/msm/external_common.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -60,6 +60,7 @@ struct hdmi_disp_mode_list_type {
 
 struct external_common_state_type {
 	boolean hpd_state;
+	boolean pre_suspend_hpd_state;
 	struct kobject *uevent_kobj;
 	struct msm_fb_data_type *mfd;
 	uint32 video_resolution;
