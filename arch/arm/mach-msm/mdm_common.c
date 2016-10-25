6103869ea2282edba0bc2b271672014955ce660f
robcore <robpatershuk@gmail.com>
2016-10-25 11:07:08 -0600

    lets see if i broke you

--- arch/arm/mach-msm/mdm_common.c
+++ arch/arm/mach-msm/mdm_common.c
@@ -615,18 +615,14 @@ static long mdm_modem_ioctl(struct file *filp, unsigned int cmd,
 			pr_debug("%s Image upgrade not supported\n", __func__);
 		break;
 	case SHUTDOWN_CHARM:
-		if (!mdm_drv->pdata->send_shdn ||
-				!mdm_drv->pdata->sysmon_subsys_id_valid) {
-			pr_debug("%s shutdown not supported for this mdm\n",
-					__func__);
+		if (!mdm_drv->pdata->send_shdn)
 			break;
-		}
 		atomic_set(&mdm_drv->mdm_ready, 0);
 		if (mdm_debug_mask & MDM_DEBUG_MASK_SHDN_LOG)
 			pr_info("Sending shutdown request to mdm\n");
-		ret = sysmon_send_shutdown(mdm_drv->pdata->sysmon_subsys_id);
+		ret = sysmon_send_shutdown(SYSMON_SS_EXT_MODEM);
 		if (ret)
-			pr_err("%s:Graceful shutdown of mdm failed, ret = %d\n",
+			pr_err("%s: Graceful shutdown of the external modem failed, ret = %d\n",
 			   __func__, ret);
 		put_user(ret, (unsigned long __user *) arg);
 		break;
