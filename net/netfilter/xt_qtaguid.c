393975795d3cd0d4ca4f108b7f5d6f394ae8e848
robcore <robpatershuk@gmail.com>
2016-10-25 23:58:54 -0600

    reverted ugh

--- net/netfilter/xt_qtaguid.c
+++ net/netfilter/xt_qtaguid.c
@@ -1810,8 +1810,6 @@ static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
 	MT_DEBUG("qtaguid[%d]: sk=%p got_sock=%d fam=%d proto=%d\n",
 		 par->hooknum, sk, got_sock, par->family, ipx_proto(skb, par));
 	if (sk != NULL) {
-		set_sk_callback_lock = true;
-		read_lock_bh(&sk->sk_callback_lock);
 		MT_DEBUG("qtaguid[%d]: sk=%p->sk_socket=%p->file=%p\n",
 			par->hooknum, sk, sk->sk_socket,
 			sk->sk_socket ? sk->sk_socket->file : (void *)-1LL);
@@ -1891,8 +1889,6 @@ static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
 put_sock_ret_res:
 	if (got_sock)
 		xt_socket_put_sk(sk);
-	if (set_sk_callback_lock)
-		read_unlock_bh(&sk->sk_callback_lock);
 ret_res:
 	MT_DEBUG("qtaguid[%d]: left %d\n", par->hooknum, res);
 	return res;
@@ -2350,21 +2346,6 @@ static int ctrl_cmd_tag(const char *input)
 			 "st@%p ...->f_count=%ld\n",
 			 input, el_socket->sk, sock_tag_entry,
 			 atomic_long_read(&el_socket->file->f_count));
-
-		/*
-		 * The tagged socket should be the same as the one from this
-		 * sockfd_lookup(). Otherwise, some unexpected error happens,
-		 * we just put back this ref and return.
-		 */
-		if (sock_tag_entry->socket != el_socket) {
-			pr_err("qtaguid: ctrl_tag(%s): "
-			       "socket mismatch\n",
-			       input);
-			spin_unlock_bh(&sock_tag_list_lock);
-			res = -EINVAL;
-			goto err_tag_unref_put;
-		}
-
 		/*
 		 * This is a re-tagging, so release the sock_fd that was
 		 * locked at the time of the 1st tagging.
