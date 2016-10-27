70cd7df5f0df87cd07a1b8091be7d11b89e0ae3c
robcore <robpatershuk@gmail.com>
2016-10-27 14:30:36 -0600

    REVERT - forced 48000-44100 freqs for slimbus audio

--- include/linux/slimbus/slimbus.h
+++ include/linux/slimbus/slimbus.h
@@ -308,8 +308,6 @@ enum slim_ch_rate {
 	SLIM_RATE_1HZ,
 	SLIM_RATE_4000HZ,
 	SLIM_RATE_11025HZ,
-	SLIM_RATE_44100HZ,
-	SLIM_RATE_48000HZ,
 };
 
 /*
@@ -1055,7 +1053,7 @@ extern void slim_ctrl_add_boarddevs(struct slim_controller *ctrl);
 extern int slim_register_board_info(struct slim_boardinfo const *info,
 					unsigned n);
 #else
-static inline int slim_register_board_info(struct slim_boardinfo const *info,
+int slim_register_board_info(struct slim_boardinfo const *info,
 					unsigned n)
 {
 	return 0;
