91b45870e16f420d64f28579697072de42c148c1
robcore <robpatershuk@gmail.com>
2016-11-03 04:07:59 -0600

    dont have that yet either fuck FUCK

--- drivers/mmc/card/queue.h
+++ drivers/mmc/card/queue.h
@@ -80,6 +80,4 @@ static inline void mmc_set_nopacked_period(struct mmc_queue *mq, unsigned long n
 	mq->nopacked_period = jiffies + nopacked_jiffies;
 }
 
-extern int mmc_access_rpmb(struct mmc_queue *);
-
 #endif
