ee8084663f760c2aa81dbf675afe306fb5dd8fe5
robcore <robpatershuk@gmail.com>
2016-11-02 21:44:03 -0600

    leds and build time

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -477,9 +477,9 @@ static void an30259a_start_led_pattern(int mode)
 	case BOOTING:
 		pr_info("LED Booting Pattern on\n");
 		leds_on(LED_G, true, true, LED_G_CURRENT);
-		leds_on(LED_R, true, true, LED_R_CURRENT);
-		leds_set_slope_mode(client, LED_G, 0, 0, 0, 15, 1, 1, 0, 0, 0, 0);
-		leds_set_slope_mode(client, LED_R, 0, 15, 7, 0, 1, 1, 0, 0, 0, 0);
+		leds_on(LED_B, true, true, LED_B_CURRENT);
+		leds_set_slope_mode(client, LED_G, 0, 15, 7, 0, 1, 1, 0, 0, 0, 0);
+		leds_set_slope_mode(client, LED_B, 0, 0, 0, 15, 1, 1, 0, 0, 0, 0);
 		break;
 
 	default:
