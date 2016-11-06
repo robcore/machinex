d8abf5fedbed81e66fc9b77c9e05ad3f6eda0843
robcore <robpatershuk@gmail.com>
2016-11-05 21:04:59 -0600

    right i was considering adding another flag but decided against it, build god damnit!

--- arch/arm/mach-msm/subsystem_restart.c
+++ arch/arm/mach-msm/subsystem_restart.c
@@ -669,7 +669,7 @@ static int __init subsys_restart_init(void)
 {
 	restart_level = RESET_SUBSYS_INDEPENDENT;
 
-	ssr_wq = alloc_workqueue("ssr_wq", WQ_HIGHPRI | , 0);
+	ssr_wq = alloc_workqueue("ssr_wq", WQ_HIGHPRI, 0);
 	if (!ssr_wq)
 		panic("%s: out of memory\n", __func__);
 
