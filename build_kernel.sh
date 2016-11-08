414eddff9d8ab7da7a2937ef72100973e4d44408
robcore <robpatershuk@gmail.com>
2016-11-08 13:49:18 -0700

    back to my previous toolchain...something tells me it is far more optimized for this tree

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=/opt/toolchains/arm-cortex_a15-linux-gnueabihf_5.3alt/bin:$PATH
+export PATH=/opt/toolchains/arm-cortex_a15-linux-gnueabihf_5.3/bin:$PATH
 
 if [ -d $(pwd)/out ]; then
 	rm -rf $(pwd)/out;
@@ -23,7 +23,7 @@ find . -type f \( -iname \*.rej \
 				-o -iname \*.ko \) \
 					| parallel rm -fv {};
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-cortex_a15-linux-gnueabihf_5.3alt/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=/opt/toolchains/arm-cortex_a15-linux-gnueabihf_5.3/bin/arm-cortex_a15-linux-gnueabihf-
 env KCONFIG_NOTIMESTAMP=true
 make clean;
 make distclean;
