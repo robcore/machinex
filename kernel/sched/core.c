446b19363eabe71d6cd33c60088d687483af235f
robcore <robpatershuk@gmail.com>
2016-11-08 11:46:27 -0700

    sched-make-sched_set_window-return-failure-when-PELT-used

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1348,7 +1348,7 @@ unsigned long sched_get_busy(int cpu)
 			  NSEC_PER_USEC);
 }
 
-void sched_set_window(u64 window_start, unsigned int window_size)
+int sched_set_window(u64 window_start, unsigned int window_size)
 {
 	int cpu;
 	u64 ws;
@@ -1357,6 +1357,9 @@ void sched_set_window(u64 window_start, unsigned int window_size)
 	unsigned long flags;
 	u64 wallclock;
 	struct task_struct *g, *p;
+
+	if (sched_use_pelt)
+		return -EINVAL;
 
 	delta = window_start - now; /* how many jiffies ahead */
 
@@ -1408,6 +1411,8 @@ void sched_set_window(u64 window_start, unsigned int window_size)
 	}
 
 	local_irq_restore(flags);
+
+	return 0;
 }
 
 /* Keep track of max/min capacity possible across CPUs "currently" */
