73720d69097b8adeb763427713b5618fa848b78e
robcore <robpatershuk@gmail.com>
2016-11-08 21:45:34 -0700

    MODEM HAX - Upon studying the soft reset function, I see that the ordering should be similiar to that of first power on...so now it is

--- arch/arm/mach-msm/mdm2.c
+++ arch/arm/mach-msm/mdm2.c
@@ -258,27 +258,21 @@ static void debug_state_changed(int value)
 
 static void mdm_status_changed(struct mdm_modem_drv *mdm_drv, int value)
 {
-	//pr_debug("%s: id %d: value:%d\n", __func__,
-			 //value, mdm_drv->device_id);
-
 	if (value) {
 		mdm_peripheral_disconnect(mdm_drv);
 		mdm_peripheral_connect(mdm_drv);
 		msleep(100);
+
 		if (GPIO_IS_VALID(mdm_drv->ap2mdm_wakeup_gpio)) {
 			gpio_direction_output(mdm_drv->ap2mdm_wakeup_gpio, 1);
 		} else {
-			mdm_toggle_soft_reset(mdm_drv);
-			mdelay(10);
 			mdm_peripheral_disconnect(mdm_drv);
+			mdm_toggle_soft_reset(mdm_drv);
 			mdm_peripheral_connect(mdm_drv);
-			mdelay(100);
-		if (GPIO_IS_VALID(mdm_drv->ap2mdm_wakeup_gpio)) {
-			gpio_direction_output(mdm_drv->ap2mdm_wakeup_gpio, 1);
+			mdelay(200);
 			}
 		}
 	}
-}
 
 static void mdm_image_upgrade(struct mdm_modem_drv *mdm_drv, int type)
 {
