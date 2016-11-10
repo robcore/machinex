1ccfff6c384584578b166987feeb263d406ef0b3
robcore <robpatershuk@gmail.com>
2016-11-10 02:00:20 -0700

    sched-Handle-cpu-bound-tasks-stuck-on-the-wrong-cpu

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3006,6 +3006,7 @@ void scheduler_tick(void)
 	rq->idle_balance = idle_cpu(cpu);
 	trigger_load_balance(rq, cpu);
 #endif
+	check_for_migration(rq, curr);
 }
 
 notrace unsigned long get_parent_ip(unsigned long addr)
