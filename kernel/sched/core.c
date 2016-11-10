c3a88d5a1d1fe23dbbb861e39b4c9be1d5c1fd8b
robcore <robpatershuk@gmail.com>
2016-11-10 00:16:43 -0700

    some more sched-reworking, as well as reverted the camera stuff again, and got rid of kgsl shared alloc flags again

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3059,10 +3059,10 @@ static inline void schedule_debug(struct task_struct *prev)
 {
 	/*
 	 * Test if we are atomic. Since do_exit() needs to call into
-	 * schedule() atomically, we ignore that path. Otherwise whine
-	 * if we are scheduling when we should not.
+	 * schedule() atomically, we ignore that path for now.
+	 * Otherwise, whine if we are scheduling when we should not be.
 	 */
-	if (unlikely(in_atomic_preempt_off() && prev->state != TASK_DEAD))
+	if (unlikely(in_atomic_preempt_off() && !prev->exit_state))
 		__schedule_bug(prev);
 	rcu_sleep_check();
 
