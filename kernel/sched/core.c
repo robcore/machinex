7cd1f18fda289323f2f90025793fecfa9199210e
robcore <robpatershuk@gmail.com>
2016-11-10 08:11:57 -0700

    kernel folder is building, here goes...

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2056,7 +2056,6 @@ static void finish_task_switch(struct rq *rq, struct task_struct *prev)
 	 * transition, resulting in a double drop.
 	 */
 	prev_state = prev->state;
-	vtime_task_switch(prev);
 	finish_arch_switch(prev);
 	perf_event_task_sched_in(prev, current);
 	finish_lock_switch(rq, prev);
