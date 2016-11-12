984992edfe75c67003bc95f4c8fec332799c6580
robcore <robpatershuk@gmail.com>
2016-11-12 09:52:12 -0700

    ?

--- include/linux/seccomp.h
+++ include/linux/seccomp.h
@@ -17,6 +17,10 @@
 /* Valid flags for SECCOMP_SET_MODE_FILTER */
 #define SECCOMP_FILTER_FLAG_TSYNC	1
 
+/* Valid operations for seccomp syscall. */
+#define SECCOMP_SET_MODE_STRICT	0
+#define SECCOMP_SET_MODE_FILTER	1
+
 /*
  * All BPF programs must return a 32-bit value.
  * The bottom 16-bits are for optional return data.
