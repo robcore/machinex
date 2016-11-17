fea7d4412b3cf1c04d8145eb9edf382f816e711c
robcore <robpatershuk@gmail.com>
2016-11-17 16:57:30 -0700

    gpio-msm-common-Add-wakeup-reason-logging

--- drivers/gpio/gpio-msm-common.c
+++ drivers/gpio/gpio-msm-common.c
@@ -24,6 +24,7 @@
 #include <linux/of.h>
 #include <linux/err.h>
 #include <linux/platform_device.h>
+#include <linux/wakeup_reason.h>
 
 #include <asm/mach/irq.h>
 
@@ -437,8 +438,7 @@ void msm_gpio_show_resume_irq(void)
 		intstat = __msm_gpio_get_intr_status(i);
 		if (intstat) {
 			irq = msm_gpio_to_irq(&msm_gpio.gpio_chip, i);
-			pr_warning("%s: %d triggered\n",
-				__func__, irq-288);
+			log_wakeup_reason(irq);
 		}
 	}
 	spin_unlock_irqrestore(&tlmm_lock, irq_flags);
