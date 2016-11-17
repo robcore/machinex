a7493f84ddb92afbd5190bb19dcbd05287ec9cd1
robcore <robpatershuk@gmail.com>
2016-11-17 14:53:16 -0700

    msm-kgsl-drm-Increased-max-sub-buffers-allowed-to-3-for-triple-buffering

--- include/drm/kgsl_drm.h
+++ include/drm/kgsl_drm.h
@@ -76,7 +76,7 @@ DRM_IOWR(DRM_COMMAND_BASE + DRM_KGSL_GEM_CREATE_FD, \
 struct drm_kgsl_gem_create_fd)
 
 /* Maximum number of sub buffers per GEM object */
-#define DRM_KGSL_GEM_MAX_BUFFERS 2
+#define DRM_KGSL_GEM_MAX_BUFFERS 3
 
 /* Memory types - these define the source and caching policies
    of the GEM memory chunk */
