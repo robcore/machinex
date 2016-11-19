d47b286bb4c626b1fdcf8c1cce7c0d75fc3170cb
robcore <robpatershuk@gmail.com>
2016-11-19 12:22:51 -0700

    cpuidle - add checks to avoid NULL pointer dereference

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -318,6 +318,9 @@ int cpuidle_enable_device(struct cpuidle_device *dev)
 	int ret, i;
 	struct cpuidle_driver *drv = cpuidle_get_driver();
 
+	if (!dev)
+		return -EINVAL;
+
 	if (dev->enabled)
 		return 0;
 	if (!drv || !cpuidle_curr_governor)
@@ -402,8 +405,6 @@ static int __cpuidle_register_device(struct cpuidle_device *dev)
 	struct device *cpu_dev;
 	struct cpuidle_driver *cpuidle_driver;
 
-	if (!dev)
-		return -EINVAL;
 	cpu_dev = get_cpu_device((unsigned long)dev->cpu);
 	cpuidle_driver = cpuidle_get_driver();
 	if (!try_module_get(cpuidle_driver->owner))
@@ -441,6 +442,9 @@ err_sysfs:
 int cpuidle_register_device(struct cpuidle_device *dev)
 {
 	int ret;
+
+	if (!dev)
+		return -EINVAL;
 
 	mutex_lock(&cpuidle_lock);
 
