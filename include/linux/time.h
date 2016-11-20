09bcd13952b393905de920de29cb82af9c4f1cb9
robcore <robpatershuk@gmail.com>
2016-11-19 17:04:50 -0700

    time-remove-obsolete-declaration

--- include/linux/time.h
+++ include/linux/time.h
@@ -213,7 +213,6 @@ static inline bool timeval_valid(const struct timeval *tv)
 extern struct timespec timespec_trunc(struct timespec t, unsigned gran);
 extern int timekeeping_valid_for_hres(void);
 extern u64 timekeeping_max_deferment(void);
-extern void timekeeping_leap_insert(int leapsecond);
 extern int timekeeping_inject_offset(struct timespec *ts);
 
 struct tms;
