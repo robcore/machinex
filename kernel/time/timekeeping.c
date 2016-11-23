9dec55cdc1174411d02d7937965e4ebb427e9a53
robcore <robpatershuk@gmail.com>
2016-11-22 19:47:51 -0700

    newline cleanup

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -146,7 +146,6 @@ static void timekeeping_update(bool clearntp)
 			 timekeeper.clock, timekeeper.mult);
 }
 
-
 /**
  * timekeeping_forward_now - update clock to the current time
  *
@@ -349,7 +348,6 @@ int do_settimeofday(const struct timespec *tv)
 }
 EXPORT_SYMBOL(do_settimeofday);
 
-
 /**
  * timekeeping_inject_offset - Adds or subtracts from the current time.
  * @tv:		pointer to the timespec variable containing the offset
@@ -470,7 +468,6 @@ void getrawmonotonic(struct timespec *ts)
 	timespec_add_ns(ts, nsecs);
 }
 EXPORT_SYMBOL(getrawmonotonic);
-
 
 /**
  * timekeeping_valid_for_hres - Check if timekeeping is suitable for hres
@@ -620,7 +617,6 @@ static void __timekeeping_inject_sleeptime(struct timespec *delta)
 	update_sleep_time(timespec_add(timekeeper.total_sleep_time, *delta));
 }
 
-
 /**
  * timekeeping_inject_sleeptime - Adds suspend interval to timeekeeping values
  * @delta: pointer to a timespec delta value
@@ -655,7 +651,6 @@ void timekeeping_inject_sleeptime(struct timespec *delta)
 	/* signal hrtimers about time change */
 	clock_was_set();
 }
-
 
 /**
  * timekeeping_resume - Resumes the generic timekeeping subsystem.
@@ -967,7 +962,6 @@ static void timekeeping_adjust(s64 offset)
 				timekeeper.ntp_error_shift;
 }
 
-
 /**
  * logarithmic_accumulation - shifted accumulation of cycles
  *
@@ -1021,7 +1015,6 @@ static cycle_t logarithmic_accumulation(cycle_t offset, int shift,
 
 	return offset;
 }
-
 
 /**
  * update_wall_time - Uses the current clocksource to increment the wall time
@@ -1159,7 +1152,6 @@ void getboottime(struct timespec *ts)
 	set_normalized_timespec(ts, -tv_sec, -tv_nsec);
 }
 EXPORT_SYMBOL_GPL(getboottime);
-
 
 /**
  * get_monotonic_boottime - Returns monotonic time since boot
