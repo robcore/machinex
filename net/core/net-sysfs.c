060de1e4abaf371a298126b445a192cd11bab9a3
robcore <robpatershuk@gmail.com>
2016-11-23 18:45:34 -0700

    net-core-apply-pm_runtime_set_memalloc_noio

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -22,6 +22,7 @@
 #include <linux/vmalloc.h>
 #include <linux/export.h>
 #include <linux/jiffies.h>
+#include <linux/pm_runtime.h>
 #include <net/wext.h>
 
 #include "net-sysfs.h"
@@ -1441,6 +1442,8 @@ void netdev_unregister_kobject(struct net_device * net)
 
 	remove_queue_kobjects(net);
 
+	pm_runtime_set_memalloc_noio(dev, false);
+
 	device_del(dev);
 }
 
@@ -1483,6 +1486,8 @@ int netdev_register_kobject(struct net_device *net)
 		device_del(dev);
 		return error;
 	}
+
+	pm_runtime_set_memalloc_noio(dev, true);
 
 	return error;
 }
