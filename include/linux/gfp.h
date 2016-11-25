8e420db5052e72f522d95df2e04bb602041c7364
robcore <robpatershuk@gmail.com>
2016-11-24 17:22:27 -0700

    naw...

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -418,4 +418,12 @@ extern void init_cma_reserved_pageblock(struct page *page);
 
 #endif
 
+#ifdef CONFIG_CMA
+
+/* The below functions must be run on a range from a single zone. */
+extern int alloc_contig_range(unsigned long start, unsigned long end);
+extern void free_contig_range(unsigned long pfn, unsigned nr_pages);
+
+#endif
+
 #endif /* __LINUX_GFP_H */
