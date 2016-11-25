21f87fd991f9236fa3fdf2f7312815d4fbb33d06
robcore <robpatershuk@gmail.com>
2016-11-25 15:14:52 -0700

    ext4: return ENOMEM when mounts fail due to lack of memory

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -3654,6 +3654,7 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 					  GFP_KERNEL);
 	if (sbi->s_group_desc == NULL) {
 		ext4_msg(sb, KERN_ERR, "not enough memory");
+		ret = -ENOMEM;
 		goto failed_mount;
 	}
 
@@ -3711,6 +3712,7 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 	}
 	if (err) {
 		ext4_msg(sb, KERN_ERR, "insufficient memory");
+		ret = err;
 		goto failed_mount3;
 	}
 
