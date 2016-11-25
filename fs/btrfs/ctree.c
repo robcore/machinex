235a9aec83679e094735273b36aa0acd5649c43b
robcore <robpatershuk@gmail.com>
2016-11-24 22:21:38 -0700

    -Btrfs-remove-call-to-btrfs_header_nritems

--- fs/btrfs/ctree.c
+++ fs/btrfs/ctree.c
@@ -1652,8 +1652,6 @@ static noinline int balance_level(struct btrfs_trans_handle *trans,
 	    BTRFS_NODEPTRS_PER_BLOCK(root) / 4)
 		return 0;
 
-	btrfs_header_nritems(mid);
-
 	left = read_node_slot(root, parent, pslot - 1);
 	if (left) {
 		btrfs_tree_lock(left);
@@ -1683,7 +1681,6 @@ static noinline int balance_level(struct btrfs_trans_handle *trans,
 		wret = push_node_left(trans, root, left, mid, 1);
 		if (wret < 0)
 			ret = wret;
-		btrfs_header_nritems(mid);
 	}
 
 	/*
