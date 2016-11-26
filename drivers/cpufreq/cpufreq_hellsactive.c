cb3db8d468f1d6944b70f2fa8fe2b214a4055910
robcore <robpatershuk@gmail.com>
2016-11-25 17:10:14 -0700

    just a tad more hellsactive fine tuning

--- drivers/cpufreq/cpufreq_hellsactive.c
+++ drivers/cpufreq/cpufreq_hellsactive.c
@@ -93,7 +93,7 @@ static unsigned long freq_calc_thresh;
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  */
-#define DEFAULT_MIN_SAMPLE_TIME (60 * USEC_PER_MSEC)
+#define DEFAULT_MIN_SAMPLE_TIME (50 * USEC_PER_MSEC)
 static unsigned long min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
 
 /*
