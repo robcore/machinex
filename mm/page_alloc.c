3aac70db6bd848bec3a263aaa4bb0c1269fced28
robcore <robpatershuk@gmail.com>
2016-11-25 20:49:34 -0700

    mm/buddy: dump PG_compound_lock page flag

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6527,6 +6527,9 @@ static struct trace_print_flags pageflag_names[] = {
 #ifdef CONFIG_MEMORY_FAILURE
 	{1UL << PG_hwpoison,		"hwpoison"	},
 #endif
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+	{1UL << PG_compound_lock,	"compound_lock"	},
+#endif
 #ifdef CONFIG_SCFS_LOWER_PAGECACHE_INVALIDATION
 	{1UL << PG_scfslower,		"scfslower"	},
 	{1UL << PG_nocache,		"nocache"	},
