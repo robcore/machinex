7d80d5e9f1ac6fa0701c767ee0752a80db11c73b
robcore <robpatershuk@gmail.com>
2016-11-27 02:22:45 -0700

    made powersuspend read only, now panel is the only available option and writing to sysfs is disabled.  bumped to version 1.8.0 -- also reverted that regmap devm commit for soc

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -18,6 +18,16 @@
  *
  *  v1.5 - fix hybrid-kernel mode cannot be set through sysfs
  *
+ *  v1.6 - remove autosleep and hybrid modes (autosleep not working on shamu)
+ *
+ *  v1.7 - do only run state change if change actually requests a new state
+ *
+ * v1.7.1 - Add autosleep and hybrid modes back
+ *
+ * v1.8.0 - Make Panel the ONLY available option, and completely remove
+ *			the ability to write to sysfs.
+ *
+ *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -37,10 +47,7 @@
 #define POWER_SUSPEND_INACTIVE	0
 #define POWER_SUSPEND_ACTIVE	1
 
-#define POWER_SUSPEND_AUTOSLEEP	0	// Use kernel autosleep as hook
-#define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
-#define POWER_SUSPEND_PANEL	2	// Use display panel state as hook
-#define POWER_SUSPEND_HYBRID	3	// Use display panel state and autosleep as hook
+#define POWER_SUSPEND_PANEL	1	// Use display panel state as hook
 
 struct power_suspend {
 	struct list_head link;
@@ -51,6 +58,5 @@ struct power_suspend {
 void register_power_suspend(struct power_suspend *handler);
 void unregister_power_suspend(struct power_suspend *handler);
 
-void set_power_suspend_state_autosleep_hook(int new_state);
 void set_power_suspend_state_panel_hook(int new_state);
 #endif
