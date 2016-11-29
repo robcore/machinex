7196f6d7a53d69969fb57c5bf220bcaf13653f23
robcore <robpatershuk@gmail.com>
2016-11-29 00:51:15 -0700

    more

--- arch/arm/include/asm/barrier.h
+++ arch/arm/include/asm/barrier.h
@@ -44,10 +44,9 @@
 #define rmb()		dsb()
 #define wmb()		do { dsb(st); outer_sync(); } while (0)
 #else
-#include <asm/memory.h>
-#define mb()	do { if (arch_is_coherent()) dmb(); else barrier(); } while (0)
-#define rmb()	do { if (arch_is_coherent()) dmb(); else barrier(); } while (0)
-#define wmb()	do { if (arch_is_coherent()) dmb(); else barrier(); } while (0)
+#define mb()		barrier()
+#define rmb()		barrier()
+#define wmb()		barrier()
 #endif
 
 #ifndef CONFIG_SMP
