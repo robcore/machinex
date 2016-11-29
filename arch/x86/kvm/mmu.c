1309598b7774fb1a1637fc6b183d81b730aa2988
robcore <robpatershuk@gmail.com>
2016-11-28 17:07:51 -0700

    bs

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -3556,7 +3556,7 @@ static bool detect_write_flooding(struct kvm_mmu_page *sp)
 	 * Skip write-flooding detected for the sp whose level is 1, because
 	 * it can become unsync, then the guest page is not write-protected.
 	 */
-	if (sp->role.level == 1)
+	if (sp->role.level == PT_PAGE_TABLE_LEVEL)
 		return false;
 
 	return ++sp->write_flooding_count >= 3;
