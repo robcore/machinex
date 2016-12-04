c22e63e23d907a9420ba914e5eb4c47f6ed064ba
robcore <robpatershuk@gmail.com>
2016-12-04 07:43:58 -0700

    media-videobuf2-Fix-a-bug-in-fileio-emulation-error

--- drivers/media/video/videobuf2-core.c
+++ drivers/media/video/videobuf2-core.c
@@ -1851,7 +1851,6 @@ static int __vb2_init_fileio(struct vb2_queue *q, int read)
 	 * (multiplane buffers are not supported).
 	 */
 	if (q->bufs[0]->num_planes != 1) {
-		fileio->req.count = 0;
 		ret = -EBUSY;
 		goto err_reqbufs;
 	}
@@ -1898,6 +1897,7 @@ static int __vb2_init_fileio(struct vb2_queue *q, int read)
 	return ret;
 
 err_reqbufs:
+	fileio->req.count = 0;
 	vb2_reqbufs(q, &fileio->req);
 
 err_kfree:
