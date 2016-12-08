55794ac051783e38578470cc5e741106dc205714
robcore <robpatershuk@gmail.com>
2016-12-08 12:33:46 -0700

    REVERT - GODDAMNED NVIDIA so-called power efficiency bullshit code

--- arch/arm/include/asm/processor.h
+++ arch/arm/include/asm/processor.h
@@ -3,8 +3,6 @@
  *
  *  Copyright (C) 1995-1999 Russell King
  *
- *  Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
@@ -120,8 +118,6 @@ static inline void prefetch(const void *ptr)
 
 #endif
 #endif
-
-#include <asm-generic/processor.h>
 
 #include <asm-generic/processor.h>
 
