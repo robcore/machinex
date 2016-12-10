850763bd1f5ce4ea215de257b131bc4c8bec47fb
robcore <robpatershuk@gmail.com>
2016-12-09 20:23:59 -0700

    REVERT - msm-kgsl-adreno_a3xx-Restore-default-clock-control

--- drivers/gpu/msm/adreno_a3xx.c
+++ drivers/gpu/msm/adreno_a3xx.c
@@ -471,9 +471,6 @@ static unsigned int *build_gmem2sys_cmds(struct adreno_device *adreno_dev,
 	*cmds++ = cp_type0_packet(A3XX_RBBM_CLOCK_CTL, 1);
 	*cmds++ = adreno_a3xx_rbbm_clock_ctl_default(adreno_dev);
 
-	*cmds++ = cp_type0_packet(A3XX_RBBM_CLOCK_CTL, 1);
-	*cmds++ = A3XX_RBBM_CLOCK_CTL_DEFAULT;
-
 	*cmds++ = cp_type3_packet(CP_SET_CONSTANT, 3);
 	*cmds++ = CP_REG(A3XX_RB_MODE_CONTROL);
 
@@ -1269,9 +1266,6 @@ static unsigned int *build_sys2gmem_cmds(struct adreno_device *adreno_dev,
 
 	*cmds++ = cp_type0_packet(A3XX_RBBM_CLOCK_CTL, 1);
 	*cmds++ = adreno_a3xx_rbbm_clock_ctl_default(adreno_dev);
-
-	*cmds++ = cp_type0_packet(A3XX_RBBM_CLOCK_CTL, 1);
-	*cmds++ = A3XX_RBBM_CLOCK_CTL_DEFAULT;
 
 	*cmds++ = cp_type3_packet(CP_SET_CONSTANT, 5);
 	*cmds++ = CP_REG(A3XX_HLSQ_CONTROL_0_REG);
