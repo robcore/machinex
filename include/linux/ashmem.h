28fdd10801bc75b2e71ce5bd7853d31c196c38e8
robcore <robpatershuk@gmail.com>
2016-12-10 22:45:05 -0700

    doubled

--- include/linux/ashmem.h
+++ include/linux/ashmem.h
@@ -60,8 +60,4 @@ int get_ashmem_file(int fd, struct file **filp, struct file **vm_file,
 			unsigned long *len);
 void put_ashmem_file(struct file *file);
 
-int get_ashmem_file(int fd, struct file **filp, struct file **vm_file,
-			unsigned long *len);
-void put_ashmem_file(struct file *file);
-
 #endif	/* _LINUX_ASHMEM_H */
