4e1202b1b15ac91352f9d92efbe027dd583108de
robcore <robpatershuk@gmail.com>
2016-12-11 10:21:25 -0700

    msm-audio-qdsp6v2-Don-t-reset-mmap-ref_cnt-in-RESET_EVENT

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -817,7 +817,6 @@ static int32_t q6asm_mmapcallback(struct apr_client_data *data, void *priv)
 				data->reset_proc,
 				this_mmap.apr);
 		apr_reset(this_mmap.apr);
-		atomic_set(&this_mmap.ref_cnt, 0);
 		this_mmap.apr = NULL;
 		return 0;
 	}
