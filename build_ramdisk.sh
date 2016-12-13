d9099822041e2ba4244bf831988d0f478a2cf89c
robcore <robpatershuk@gmail.com>
2016-12-13 05:09:45 -0700

    adjusted rebuild ramdisk script to new build script

--- build_ramdisk.sh
+++ build_ramdisk.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+PREV=`cat /media/root/robcore/AIK/previous.txt`
+
 cd /media/root/robcore/AIK;
-rm -rf machinex-new;
-cp -R -p machina-new machinex-new;
-cp -p ~/machinex/arch/arm/boot/dhd.ko $(pwd)/machinex-new/system/lib/modules/dhd.ko;
-cp -p ~/machinex/arch/arm/boot/scsi_wait_scan.ko $(pwd)/machinex-new/system/lib/modules/scsi_wait_scan.ko;
-rm $(pwd)/split_img/boot.img-zImage;
-cp -p ~/machinex/arch/arm/boot/boot.img-zImage $(pwd)/split_img/boot.img-zImage;
+rm $PREV/boot.img
 rm image-new.img;
 sh repackimg.sh --sudo;
-cp -p image-new.img $(pwd)/machinex-new/boot.img
+cp -p image-new.img $(pwd)/$PREV/boot.img
+cd $PREV
+zip -r -9 - * > $PREV.zip
