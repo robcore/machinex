24f9f34d375fafd2bdc90fc0b49c39c4011231ce
robcore <robpatershuk@gmail.com>
2016-12-12 21:52:59 -0700

    REVERT - af_netlink-force-credentials-passing-CVE-2012-3520

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -1345,7 +1345,7 @@ static int netlink_sendmsg(struct kiocb *kiocb, struct socket *sock,
 	if (NULL == siocb->scm)
 		siocb->scm = &scm;
 
-	err = scm_send(sock, msg, siocb->scm, true);
+	err = scm_send(sock, msg, siocb->scm);
 	if (err < 0)
 		return err;
 
