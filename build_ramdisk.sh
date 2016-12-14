4f7dcfb90d448dddbdf3ab6de72528dc7d94a21b
robcore <robpatershuk@gmail.com>
2016-12-13 23:25:00 -0700

    crypto-lzo-try-kmalloc-before-vmalloc

--- build_ramdisk.sh
+++ build_ramdisk.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 PREV=`cat /media/root/robcore/AIK/previous.txt`
 
 cd /media/root/robcore/AIK;
-rm $PREV/boot.img
+echo -n "New Name? [ENTER]: "
+read NEW
+cp -pR $PREV $NEW
+rm $NEW/boot.img
 rm image-new.img;
 sh repackimg.sh --sudo;
-cp -p image-new.img $(pwd)/$PREV/boot.img
+cp -p image-new.img $NEW/boot.img
+cd $NEW
+rm $PREV.zip
+zip -r -9 - * > $NEW.zip
