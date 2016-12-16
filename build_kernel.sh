6f5f6630856feddc7307fc7e12dc2256220fe6e8
robcore <robpatershuk@gmail.com>
2016-12-15 22:36:01 -0700

    updated build script to ensure ccache is used and pointed to, as well as updated cleanup script to call new toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -79,6 +79,8 @@ export PATH=/opt/toolchains/arm-cortex_a15-linux-gnueabihf/bin:$PATH
 export ARCH=arm
 #export CROSS_COMPILE=/opt/toolchains/arm-cortex_a15-linux-gnueabihf_5.3/bin/arm-cortex_a15-linux-gnueabihf-
 export CROSS_COMPILE=/opt/toolchains/arm-cortex_a15-linux-gnueabihf/bin/arm-cortex_a15-linux-gnueabihf-
+export USE_CCACHE=1
+export CCACHE_DIR=~/.ccache
 env KCONFIG_NOTIMESTAMP=true
 make clean;
 make distclean;
