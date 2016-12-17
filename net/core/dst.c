10118ec0f61c87de20af16774344dbf869a9b51d
robcore <robpatershuk@gmail.com>
2016-12-16 17:15:28 -0700

    REVERT - reverting to (improperly coded) queue delayed works, which will be caught and repaired by workqueue core and is technically misusing it, however it seems to be more effective at keeping our data connection running...so...yeah. also reverted the quote unquote optimized cfbfillrect because it sucks for our graphics driver

--- net/core/dst.c
+++ net/core/dst.c
@@ -135,7 +135,7 @@ loop:
 		 */
 		if (expires > 4*HZ)
 			expires = round_jiffies_relative(expires);
-		schedule_delayed_work(&dst_gc_work, expires);
+		queue_delayed_work(system_wq, &dst_gc_work, expires);
 	}
 
 	spin_unlock_bh(&dst_garbage.lock);
@@ -214,8 +214,8 @@ void __dst_free(struct dst_entry *dst)
 	if (dst_garbage.timer_inc > DST_GC_INC) {
 		dst_garbage.timer_inc = DST_GC_INC;
 		dst_garbage.timer_expires = DST_GC_MIN;
-		mod_delayed_work(system_wq, &dst_gc_work,
-				 dst_garbage.timer_expires);
+		queue_delayed_work(system_wq, &dst_gc_work,
+					dst_garbage.timer_expires);
 	}
 	spin_unlock_bh(&dst_garbage.lock);
 }
