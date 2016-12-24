9e51074af68d2ce6b964bb6388c06fdeb9ba02e9
robcore <robpatershuk@gmail.com>
2016-12-23 23:58:58 -0700

    conversion part 2

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -1192,8 +1192,8 @@ struct ext4_sb_info {
 	unsigned int s_def_mount_opt;
 	ext4_fsblk_t s_sb_block;
 	atomic64_t s_r_blocks_count;
-	uid_t s_resuid;
-	gid_t s_resgid;
+	kuid_t s_resuid;
+	kgid_t s_resgid;
 	unsigned short s_mount_state;
 	unsigned short s_pad;
 	int s_addr_per_block_bits;
