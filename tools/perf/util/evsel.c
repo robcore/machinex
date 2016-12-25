d847813b308d6cc09f0dbd45df2532f65df80494
robcore <robpatershuk@gmail.com>
2016-12-25 03:57:41 -0700

    perf-evsel-Create-events-initially-disabled-again

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -159,6 +159,7 @@ void perf_evsel__config(struct perf_evsel *evsel, struct perf_record_opts *opts,
 	struct perf_event_attr *attr = &evsel->attr;
 	int track = !evsel->idx; /* only the first counter needs these */
 
+	attr->disabled = 1;
 	attr->sample_id_all = opts->sample_id_all_missing ? 0 : 1;
 	attr->inherit	    = !opts->no_inherit;
 	attr->read_format   = PERF_FORMAT_TOTAL_TIME_ENABLED |
@@ -227,7 +228,6 @@ void perf_evsel__config(struct perf_evsel *evsel, struct perf_record_opts *opts,
 
 	if (!opts->target_pid && !opts->target_tid && !opts->system_wide &&
 	    (!opts->group || evsel == first)) {
-		attr->disabled = 1;
 		attr->enable_on_exec = 1;
 	}
 }
