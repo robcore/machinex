cb4afd31b1b71684ec9d21c0582bc952c384e7c3
robcore <robpatershuk@gmail.com>
2016-12-26 14:00:20 -0700

    DANGER - arm: mm: Define set_memory_* functions for ARM

--- arch/arm/include/asm/cacheflush.h
+++ arch/arm/include/asm/cacheflush.h
@@ -462,4 +462,9 @@ int set_memory_rw(unsigned long addr, int numpages);
 int set_memory_x(unsigned long addr, int numpages);
 int set_memory_nx(unsigned long addr, int numpages);
 
+int set_memory_ro(unsigned long addr, int numpages);
+int set_memory_rw(unsigned long addr, int numpages);
+int set_memory_x(unsigned long addr, int numpages);
+int set_memory_nx(unsigned long addr, int numpages);
+
 #endif
