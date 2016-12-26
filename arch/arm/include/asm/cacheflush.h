e900202a7fc527a302abc9ea68d51f31951b1491
robcore <robpatershuk@gmail.com>
2016-12-26 15:11:41 -0700

    REVERT - arm-mm-Define-set_memory_-functions-for-ARM- we already have them!

--- arch/arm/include/asm/cacheflush.h
+++ arch/arm/include/asm/cacheflush.h
@@ -462,9 +462,4 @@ int set_memory_rw(unsigned long addr, int numpages);
 int set_memory_x(unsigned long addr, int numpages);
 int set_memory_nx(unsigned long addr, int numpages);
 
-int set_memory_ro(unsigned long addr, int numpages);
-int set_memory_rw(unsigned long addr, int numpages);
-int set_memory_x(unsigned long addr, int numpages);
-int set_memory_nx(unsigned long addr, int numpages);
-
 #endif
