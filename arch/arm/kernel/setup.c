d2773d9451b7b3e944ee6175bb81b2411270f99f
robcore <robpatershuk@gmail.com>
2016-12-26 15:03:18 -0700

    ARM-7694-1-ARM-TCM-initialize-TCM-in-paging_init

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -61,7 +61,6 @@
 #include "compat.h"
 #endif
 #include "atags.h"
-#include "tcm.h"
 
 #ifndef MEM_SIZE
 #define MEM_SIZE	(16*1024*1024)
@@ -1104,8 +1103,6 @@ void __init setup_arch(char **cmdline_p)
 		hyp_mode_check();
 
 	reserve_crashkernel();
-
-	tcm_init();
 
 #ifdef CONFIG_MULTI_IRQ_HANDLER
 	handle_arch_irq = mdesc->handle_irq;
