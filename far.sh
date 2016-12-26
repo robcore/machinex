d883a153f29e6794a58257c1bc03565205b5faad
robcore <robpatershuk@gmail.com>
2016-12-25 19:43:41 -0700

    who else wants a piece of me?

--- far.sh
+++ far.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd drivers
-echo 'beginning arch conversions'
+echo 'beginning conversions'
 find . -type f \( -iname \*.h \
 				-o -iname \*.c \) \
-					| parallel sed -i 's/VM_RESERVED/VM_DONTDUMP/' {} \;
+					| parallel sed -i 's/VM_NODUMP/VM_DONTDUMP/' {} \;
 echo 'finito'
