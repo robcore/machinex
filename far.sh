08cb2703b9d2aa9806e5e3b4012721f480cc1c5b
robcore <robpatershuk@gmail.com>
2016-12-25 19:36:34 -0700

    time for some scripted mass conversion

--- far.sh
+++ far.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
-#!/bin/bash
+cd drivers
 echo 'beginning arch conversions'
-cd arch
 find . -type f \( -iname \*.h \
 				-o -iname \*.c \) \
-					| parallel sed -i 's/early_suspend/power_suspend/' {} \;
-echo 'one finished'
-sleep 1
-find . -type f \( -iname \*.h \
-				-o -iname \*.c \) \
-					| parallel sed -i 's/earlysuspend/powersuspend/' {} \;
-echo 'two finished'
-sleep 1
-find . -type f \( -iname \*.h \
-				-o -iname \*.c \) \
-					| parallel sed -i 's/late_resume/power_resume/' {} \;
-echo 'three finished'
-sleep 1
-find . -type f \( -iname \*.h \
-				-o -iname \*.c \) \
-					| parallel sed -i 's/CONFIG_HAS_EARLYSUSPEND/CONFIG_POWERSUSPEND/' {} \;
-sleep 1
-find . -type f \( -iname \*.h \
-				-o -iname \*.c \) \
-					| parallel sed -i 's/CONFIG_EARLYSUSPEND/CONFIG_POWERSUSPEND/' {} \;
-sleep 1
-find . -type f \( -iname \*.h \
-				-o -iname \*.c \) \
-					| parallel sed -i 's/CONFIG_HAS_POWERSUSPEND/CONFIG_POWERSUSPEND/' {} \;
-echo 'finished arch'
+					| parallel sed -i 's/VM_RESERVED/VM_DONTDUMP/' {} \;
+echo 'finito'
