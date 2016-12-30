0337474b4c9f85821eec05f697d070c5157ea447
robcore <robpatershuk@gmail.com>
2016-12-30 12:46:17 -0700

    kill open data mnt dentry and struct open data

--- fs/internal.h
+++ fs/internal.h
@@ -79,11 +79,6 @@ extern struct super_block *user_get_super(dev_t);
 /*
  * open.c
  */
-struct opendata {
-	struct dentry *dentry;
-	struct vfsmount *mnt;
-	struct file *filp;
-};
 struct open_flags {
 	int open_flag;
 	umode_t mode;
