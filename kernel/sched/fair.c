6857077ee3c9b2e8d56a8f20c45bb7467bc4c496
robcore <robpatershuk@gmail.com>
2016-12-30 09:33:49 -0700

    (apparently)sched-Fix-init-NOHZ_IDLE-flag.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5324,13 +5324,16 @@ void set_cpu_sd_state_idle(void)
 	struct sched_domain *sd;
 	int cpu = smp_processor_id();
 
-	if (test_bit(NOHZ_IDLE, nohz_flags(cpu)))
-		return;
-	set_bit(NOHZ_IDLE, nohz_flags(cpu));
-
 	rcu_read_lock();
-	for_each_domain(cpu, sd)
+	sd = rcu_dereference_check_sched_domain(cpu_rq(cpu)->sd);
+
+	if (!sd || sd->nohz_idle)
+		goto unlock;
+	sd->nohz_idle = 1;
+
+	for (; sd; sd = sd->parent)
 	atomic_dec(&sd->groups->sgp->nr_busy_cpus);
+unlock:
 	rcu_read_unlock();
 }
 
