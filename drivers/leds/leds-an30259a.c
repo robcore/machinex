329bc51e83cccb55202c2d60911688cf34ca03b4
robcore <robpatershuk@gmail.com>
2016-12-30 14:10:51 -0700

    FUCK - leds i always forget to change all the values, but screw it green and red is fine it makes like orangy colors or whatever

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -476,10 +476,10 @@ static void an30259a_start_led_pattern(int mode)
 
 	case BOOTING:
 		pr_info("LED Booting Pattern on\n");
-		leds_on(LED_G, true, true, LED_B_CURRENT);
+		leds_on(LED_G, true, true, LED_G_CURRENT);
 		leds_on(LED_R, true, true, LED_R_CURRENT);
-		leds_set_slope_mode(client, LED_B, 0, 15, 0, 0, 1, 1, 2, 0, 0, 2);
-		leds_set_slope_mode(client, LED_R, 0, 0, 0, 5, 1, 1, 2, 0, 0, 2);
+		leds_set_slope_mode(client, LED_G, 0, 15, 0, 0, 1, 1, 2, 3, 3, 2);
+		leds_set_slope_mode(client, LED_R, 0, 0, 0, 5, 1, 1, 2, 3, 3, 2);
 		break;
 
 	default:
