67c2eea9bcef2e4397c7667e0150bfffc0f78345
robcore <robpatershuk@gmail.com>
2016-12-31 08:43:34 -0700

    cpuidle: dont disable cpuidle when entering suspend - this was a fix for an acpi issue on intel boards, and disabling could actually improve power consumption on newer hardware (like ours, comparatively).

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -28,10 +28,13 @@
 #include <linux/sched.h>
 #include <linux/async.h>
 #include <linux/suspend.h>
+#include <trace/events/power.h>
+#include <linux/cpufreq.h>
 #include <linux/timer.h>
 #include <linux/slab.h>
+#include <linux/wakeup_reason.h>
 
-#include <linux/cpuidle.h>
+//#include <linux/cpuidle.h>
 #include "../base.h"
 #include "power.h"
 
@@ -566,7 +569,7 @@ static void dpm_resume_noirq(pm_message_t state)
 	async_synchronize_full();
 	dpm_show_time(starttime, state, "noirq");
 	resume_device_irqs();
-	cpuidle_resume();
+	//cpuidle_resume();
 }
 
 /**
@@ -987,7 +990,7 @@ static int dpm_suspend_noirq(pm_message_t state)
 	ktime_t starttime = ktime_get();
 	int error = 0;
 
-	cpuidle_pause();
+	//cpuidle_pause();
 	suspend_device_irqs();
 	mutex_lock(&dpm_list_mtx);
 	while (!list_empty(&dpm_late_early_list)) {
