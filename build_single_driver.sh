f2a49def195b662b58fa795bff28755a20a843ca
robcore <robpatershuk@gmail.com>
2016-12-31 15:58:48 -0700

    make it prettier

--- build_single_driver.sh
+++ build_single_driver.sh
@@ -44,7 +44,7 @@ make mrproper;
 mkdir $(pwd)/out;
 cp $(pwd)/arch/arm/configs/canadefconfig $(pwd)/out/.config;
 make ARCH=arm -j6 O=$(pwd)/out oldconfig;
-echo -n "What am I building? [ENTER]"
+echo -n "What am I building? [ENTER] "
 read DRV
 make ARCH=arm -S -s -j6 O=$(pwd)/out $(pwd)/$DRV/;
 washme
