239c326e36721bcb02625873e26ab342e2df443a
robcore <robpatershuk@gmail.com>
2017-01-07 23:54:53 -0700

    perhaps we need this

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -261,6 +261,9 @@ static int mipi_dsi_on(struct platform_device *pdev)
 		mdelay(500);
 #endif
 
+	if (mipi_dsi_pdata && mipi_dsi_pdata->dsi_power_save)
+		mipi_dsi_pdata->dsi_power_save(1);
+
 	cont_splash_clk_ctrl(0);
 	mipi_dsi_prepare_ahb_clocks();
 
