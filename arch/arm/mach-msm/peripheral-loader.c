61c9e66e37c992bd307ac5d0a59582b08a613389
robcore <robpatershuk@gmail.com>
2017-01-11 23:16:56 -0700

    msm - peripheral loader - fw - Do not allow pil_put to unload FW if uncounted users are blockedby mutex in pil_get.

--- arch/arm/mach-msm/peripheral-loader.c
+++ arch/arm/mach-msm/peripheral-loader.c
@@ -60,6 +60,7 @@ static const char *pil_states[] = {
 struct pil_device {
 	struct pil_desc *desc;
 	int count;
+	int loaded;
 	enum pil_state state;
 	struct mutex lock;
 	struct device dev;
@@ -70,6 +71,9 @@ struct pil_device {
 	struct delayed_work proxy;
 	struct wake_lock wlock;
 	char wake_name[32];
+
+	/* not locked by mutex lock*/
+	atomic_t blocked_count;
 };
 
 #define to_pil_device(d) container_of(d, struct pil_device, dev)
@@ -413,20 +417,26 @@ void *pil_get(const char *name)
 		goto err_depends;
 	}
 
+	atomic_inc(&pil->blocked_count);
+
 	mutex_lock(&pil->lock);
-	if (!pil->count) {
+	if (!pil->count && !pil->loaded) {
 		ret = load_image(pil);
 		if (ret) {
 			retval = ERR_PTR(ret);
 			goto err_load;
 		}
+		pil->loaded = 1;
 	}
 	pil->count++;
+
 	pil_set_state(pil, PIL_ONLINE);
+	atomic_dec(&pil->blocked_count);
 	mutex_unlock(&pil->lock);
 out:
 	return retval;
 err_load:
+	atomic_dec(&pil->blocked_count);
 	mutex_unlock(&pil->lock);
 	pil_put(pil_d);
 err_depends:
@@ -471,19 +481,26 @@ void pil_put(void *peripheral_handle)
 	if (pil->count == 1)
 		goto unlock;
 	}
-	if (!--pil->count)
+
+	if (!--pil->count && !atomic_read(&pil->blocked_count)) {
 		pil_shutdown(pil);
+		pil->loaded = 0;
+	}
+
 unlock:
 	mutex_unlock(&pil->lock);
 
 	pil_d = find_peripheral(pil->desc->depends_on);
 	module_put(pil->owner);
+
 	if (pil_d) {
 		pil_put(pil_d);
 		put_device(&pil_d->dev);
 	}
+
 	put_device(&pil->dev);
 	return;
+
 err_out:
 	mutex_unlock(&pil->lock);
 	return;
@@ -627,6 +644,7 @@ static void pil_device_release(struct device *dev)
 	struct pil_device *pil = to_pil_device(dev);
 	wake_lock_destroy(&pil->wlock);
 	mutex_destroy(&pil->lock);
+	atomic_set(&pil->blocked_count, 0);
 	kfree(pil);
 }
 
@@ -650,6 +668,7 @@ struct pil_device *msm_pil_register(struct pil_desc *desc)
 		return ERR_PTR(-ENOMEM);
 
 	mutex_init(&pil->lock);
+	atomic_set(&pil->blocked_count, 0);
 	pil->desc = desc;
 	pil->owner = desc->owner;
 	pil->dev.parent = desc->dev;
