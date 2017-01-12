11d4f503efee29b2fea3fdb696b60a12ae9b965f
robcore <robpatershuk@gmail.com>
2017-01-11 17:15:02 -0700

    video-msm-initialize-unset_bl_level.

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -949,7 +949,8 @@ static void msmfb_power_resume(struct power_suspend *h)
 }
 #endif */
 
-static int unset_bl_level, bl_updated;
+static int unset_bl_level = -1;
+static int bl_updated;
 static int bl_level_old;
 
 static int mdp_bl_scale_config(struct msm_fb_data_type *mfd,
