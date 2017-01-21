83963fe017da245be78213e9200afbde0eed5f92
robcore <robpatershuk@gmail.com>
2017-01-20 19:35:49 -0700

    REVERT - devm funcs

--- drivers/rtc/rtc-pm8xxx.c
+++ drivers/rtc/rtc-pm8xxx.c
@@ -760,7 +760,7 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 	if (pdata != NULL)
 		rtc_write_enable = pdata->rtc_write_enable;
 
-	rtc_dd = devm_kzalloc(&pdev->dev, sizeof(*rtc_dd), GFP_KERNEL);
+	rtc_dd = kzalloc(sizeof(*rtc_dd), GFP_KERNEL);
 	if (rtc_dd == NULL) {
 		dev_dbg(&pdev->dev, "Unable to allocate memory!\n");
 		return -ENOMEM;
@@ -772,14 +772,16 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 	rtc_dd->rtc_alarm_irq = platform_get_irq(pdev, 0);
 	if (rtc_dd->rtc_alarm_irq < 0) {
 		dev_dbg(&pdev->dev, "Alarm IRQ resource absent!\n");
-		return -ENXIO;
+		rc = -ENXIO;
+		goto fail_rtc_enable;
 	}
 
 	rtc_resource = platform_get_resource_byname(pdev, IORESOURCE_IO,
 							"pmic_rtc_base");
 	if (!(rtc_resource && rtc_resource->start)) {
 		dev_dbg(&pdev->dev, "RTC IO resource absent!\n");
-		return -ENXIO;
+		rc = -ENXIO;
+		goto fail_rtc_enable;
 	}
 
 	rtc_dd->rtc_base = rtc_resource->start;
@@ -795,7 +797,7 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 	rc = pm8xxx_read_wrapper(rtc_dd, &ctrl_reg, rtc_dd->rtc_base, 1);
 	if (rc < 0) {
 		dev_dbg(&pdev->dev, "RTC control register read failed!\n");
-		return rc;
+		goto fail_rtc_enable;
 	}
 
 	if (!(ctrl_reg & PM8xxx_RTC_ENABLE)) {
@@ -805,7 +807,7 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 		if (rc < 0) {
 			dev_dbg(&pdev->dev, "Write to RTC control register "
 								"failed\n");
-			return rc;
+			goto fail_rtc_enable;
 		}
 	}
 
@@ -814,7 +816,7 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 	rc = pm8xxx_write_wrapper(rtc_dd, &ctrl_reg, rtc_dd->rtc_base, 1);
 	if (rc < 0) {
 		dev_dbg(&pdev->dev, "PM8xxx write failed!\n");
-		return rc;
+		goto fail_rtc_enable;
 	}
 
 	rtc_dd->ctrl_reg = ctrl_reg;
@@ -826,12 +828,13 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 	device_init_wakeup(&pdev->dev, 1);
 
 	/* Register the RTC device */
-	rtc_dd->rtc = devm_rtc_device_register(&pdev->dev, "pm8xxx_rtc",
+	rtc_dd->rtc = rtc_device_register("pm8xxx_rtc", &pdev->dev,
 				&pm8xxx_rtc_ops, THIS_MODULE);
 	if (IS_ERR(rtc_dd->rtc)) {
 		dev_dbg(&pdev->dev, "%s: RTC registration failed (%ld)\n",
 					__func__, PTR_ERR(rtc_dd->rtc));
-		return PTR_ERR(rtc_dd->rtc);
+		rc = PTR_ERR(rtc_dd->rtc);
+		goto fail_rtc_enable;
 	}
 
 	/* Request the alarm IRQ */
@@ -840,7 +843,7 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 				 "pm8xxx_rtc_alarm", rtc_dd);
 	if (rc < 0) {
 		dev_dbg(&pdev->dev, "Request IRQ failed (%d)\n", rc);
-		return rc;
+		goto fail_req_irq;
 	}
 
 #ifdef CONFIG_RTC_AUTO_PWRON_PARAM
@@ -866,6 +869,12 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 
 	return 0;
 
+fail_req_irq:
+	rtc_device_unregister(rtc_dd->rtc);
+fail_rtc_enable:
+	platform_set_drvdata(pdev, NULL);
+	kfree(rtc_dd);
+	return rc;
 }
 
 static int pm8xxx_rtc_remove(struct platform_device *pdev)
@@ -877,7 +886,9 @@ static int pm8xxx_rtc_remove(struct platform_device *pdev)
 #endif
 	device_init_wakeup(&pdev->dev, 0);
 	free_irq(rtc_dd->rtc_alarm_irq, rtc_dd);
+	rtc_device_unregister(rtc_dd->rtc);
 	platform_set_drvdata(pdev, NULL);
+	kfree(rtc_dd);
 
 	return 0;
 }
