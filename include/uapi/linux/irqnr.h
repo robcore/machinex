e99a5772d2e6fdd296c5cada1647c97ecae7bf85
robcore <robpatershuk@gmail.com>
2017-01-22 21:35:14 -0700

    reverted back to this afternoon fuck

--- include/uapi/linux/irqnr.h
+++ include/uapi/linux/irqnr.h
@@ -2,7 +2,3 @@
  * There isn't anything here anymore, but the file must not be empty or patch
  * will delete it.
  */
-/*
- * There isn't anything here anymore, but the file must not be empty or patch
- * will delete it.
- */
