0bee1db1fffd34117a4a4217be5329e3cfcd9bc8
robcore <robpatershuk@gmail.com>
2017-01-23 10:04:18 -0700

    ledsers

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -461,6 +461,7 @@ static void an30259a_start_led_pattern(int mode)
 		leds_on(LED_G, true, false, g_brightness);
 		break;
 
+/* For later
 	case POWERING:
 		pr_info("LED Powering Pattern on\n");
 		leds_on(LED_R, true, true, LED_DYNAMIC_CURRENT);
@@ -472,16 +473,38 @@ static void an30259a_start_led_pattern(int mode)
 				0, 15, 10, 15, 6, 5, 4, 10, 10, 4);
 		leds_set_slope_mode(client, LED_B,
 				0, 15, 10, 15, 6, 5, 4, 10, 10, 4);
+*/
+	case POWERING:
+		pr_info("LED Powering Pattern on\n");
+		leds_on(LED_R, true, true, LED_DYNAMIC_CURRENT);
+		leds_on(LED_G, true, true, LED_DYNAMIC_CURRENT);
+		leds_on(LED_B, true, true, LED_DYNAMIC_CURRENT);
+		leds_set_slope_mode(client, LED_R,
+				0, 15, 11, 0, 6, 5, 4, 10, 10, 4);
+		leds_set_slope_mode(client, LED_G,
+				0, 15, 11, 0, 6, 5, 4, 10, 10, 4);
+		leds_set_slope_mode(client, LED_B,
+				0, 0, 0, 15, 6, 5, 4, 10, 10, 4);
 		break;
 
+/* For later
 	case BOOTING:
 		pr_info("LED Booting Pattern on\n");
 		leds_on(LED_R, true, true, LED_R_CURRENT);
 		leds_on(LED_G, true, true, LED_G_CURRENT);
 		leds_on(LED_B, true, true, LED_B_CURRENT);
-		leds_set_slope_mode(client, LED_R, 0, 15, 8, 0, 1, 0, 2, 1, 1, 0);
+		leds_set_slope_mode(client, LED_R, 0, 5, 5, 0, 1, 0, 2, 1, 1, 0);
 		leds_set_slope_mode(client, LED_G, 0, 15, 8, 15, 1, 0, 2, 1, 1, 0);
-		leds_set_slope_mode(client, LED_B, 0, 0, 8, 15, 0, 1, 2, 1, 1, 0);
+		leds_set_slope_mode(client, LED_B, 0, 15, 10, 15, 0, 1, 2, 1, 1, 0);
+*/
+	case BOOTING:
+		pr_info("LED Booting Pattern on\n");
+		leds_on(LED_R, true, true, LED_R_CURRENT);
+		leds_on(LED_G, true, true, LED_G_CURRENT);
+		leds_on(LED_B, true, true, LED_B_CURRENT);
+		leds_set_slope_mode(client, LED_R, 0, 15, 5, 0, 1, 0, 2, 1, 1, 0);
+		leds_set_slope_mode(client, LED_G, 0, 15, 5, 0, 1, 0, 2, 1, 1, 0);
+		leds_set_slope_mode(client, LED_B, 0, 0, 0, 15, 0, 1, 2, 1, 1, 0);
 
 		break;
 
