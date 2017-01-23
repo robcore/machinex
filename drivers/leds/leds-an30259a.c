9a5ae1570d705ab188972c0fb89aab5f62ba2686
robcore <robpatershuk@gmail.com>
2017-01-23 10:01:27 -0700

    revert reverting that

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -479,9 +479,9 @@ static void an30259a_start_led_pattern(int mode)
 		leds_on(LED_R, true, true, LED_R_CURRENT);
 		leds_on(LED_G, true, true, LED_G_CURRENT);
 		leds_on(LED_B, true, true, LED_B_CURRENT);
-		leds_set_slope_mode(client, LED_R, 0, 5, 5, 0, 1, 0, 2, 1, 1, 0);
+		leds_set_slope_mode(client, LED_R, 0, 15, 8, 0, 1, 0, 2, 1, 1, 0);
 		leds_set_slope_mode(client, LED_G, 0, 15, 8, 15, 1, 0, 2, 1, 1, 0);
-		leds_set_slope_mode(client, LED_B, 0, 15, 10, 15, 0, 1, 2, 1, 1, 0);
+		leds_set_slope_mode(client, LED_B, 0, 0, 8, 15, 0, 1, 2, 1, 1, 0);
 
 		break;
 
