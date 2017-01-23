f342125ccd254f3989a6766df2c64a3d45a23081
robcore <robpatershuk@gmail.com>
2017-01-23 01:19:53 -0700

    pm8x60 - turn off debug mask

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -85,7 +85,7 @@ enum {
 	MSM_PM_DEBUG_HOTPLUG = BIT(8),
 };
 
-static int msm_pm_debug_mask = 1;
+static int msm_pm_debug_mask = 0;
 module_param_named(
 	debug_mask, msm_pm_debug_mask, int, S_IRUGO | S_IWUSR | S_IWGRP
 );
