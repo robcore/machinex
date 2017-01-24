a42c544c0375d15a40007091bfbca71479946cf1
robcore <robpatershuk@gmail.com>
2017-01-24 16:27:30 -0700

    leds

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -480,11 +480,11 @@ static void an30259a_start_led_pattern(int mode)
 		leds_on(LED_G, true, true, LED_DYNAMIC_CURRENT);
 		leds_on(LED_B, true, true, LED_DYNAMIC_CURRENT);
 		leds_set_slope_mode(client, LED_R,
-				0, 15, 0, 0, 5, 5, 4, 10, 10, 4);
+				0, 15, 0, 0, 5, 0, 5, 4, 4, 4, 4);
 		leds_set_slope_mode(client, LED_G,
-				0, 15, 15, 15, 5, 5, 4, 10, 10, 4);
+				0, 10, 15, 10, 5, 5, 4, 4, 4, 4);
 		leds_set_slope_mode(client, LED_B,
-				0, 0, 0, 15, 5, 5, 4, 10, 10, 4);
+				0, 0, 0, 15, 0, 5, 4, 4, 4, 4);
 		break;
 
 /* For later
@@ -503,8 +503,8 @@ static void an30259a_start_led_pattern(int mode)
 		leds_on(LED_G, true, true, LED_G_CURRENT);
 		leds_on(LED_B, true, true, LED_B_CURRENT);
 		leds_set_slope_mode(client, LED_R, 0, 15, 0, 0, 1, 0, 2, 1, 1, 2);
-		leds_set_slope_mode(client, LED_G, 0, 15, 15, 15, 1, 0, 2, 1, 1, 2);
-		leds_set_slope_mode(client, LED_B, 0, 0, 0, 15, 0, 1, 2, 1, 1, 2);
+		leds_set_slope_mode(client, LED_G, 0, 10, 15, 10, 1, 1, 2, 1, 1, 2);
+		leds_set_slope_mode(client, LED_B, 0, 0, 0, 15, 0, 0, 1, 1, 1, 2);
 
 		break;
 
