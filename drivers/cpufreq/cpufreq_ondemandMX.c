95a6f5f02110b9f1293a37f3969bf1a6a4701e87
robcore <robpatershuk@gmail.com>
2017-01-26 01:33:05 -0700

    fix a thing

--- drivers/cpufreq/cpufreq_ondemandMX.c
+++ drivers/cpufreq/cpufreq_ondemandMX.c
@@ -481,8 +481,8 @@ static int od_cpufreq_governor_dbs(struct cpufreq_policy *policy,
 #ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND
 static
 #endif
-struct cpufreq_governor cpufreq_gov_ondemand = {
-	.name			= "ondemand",
+struct cpufreq_governor cpufreq_gov_ondemandMX = {
+	.name			= "ondemandMX",
 	.governor		= od_cpufreq_governor_dbs,
 	.max_transition_latency	= TRANSITION_LATENCY_LIMIT,
 	.owner			= THIS_MODULE,
