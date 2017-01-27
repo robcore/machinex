237677ae2ed182ef0fd4237ef32dcfac5b81ccdd
robcore <robpatershuk@gmail.com>
2017-01-26 23:52:13 -0700

    memory-hotplug-allocate-zone-s-pcp-before-onlining

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6511,7 +6511,6 @@ void __meminit zone_pcp_update(struct zone *zone)
 }
 #endif
 
-#ifdef CONFIG_MEMORY_HOTREMOVE
 void zone_pcp_reset(struct zone *zone)
 {
 	unsigned long flags;
@@ -6525,6 +6524,7 @@ void zone_pcp_reset(struct zone *zone)
 	local_irq_restore(flags);
 }
 
+#ifdef CONFIG_MEMORY_HOTREMOVE
 /*
  * All pages in the range must be isolated before calling this.
  */
