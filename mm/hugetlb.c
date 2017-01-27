6fef02ea03601e7342136f48716584f82f4f047f
robcore <robpatershuk@gmail.com>
2017-01-26 22:18:57 -0700

    mm-Count-the-number-of-pages-affected-in-change_prot

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -3104,7 +3104,7 @@ same_page:
 	return i ? i : -EFAULT;
 }
 
-void hugetlb_change_protection(struct vm_area_struct *vma,
+unsigned long hugetlb_change_protection(struct vm_area_struct *vma,
 		unsigned long address, unsigned long end, pgprot_t newprot)
 {
 	struct mm_struct *mm = vma->vm_mm;
@@ -3112,6 +3112,7 @@ void hugetlb_change_protection(struct vm_area_struct *vma,
 	pte_t *ptep;
 	pte_t pte;
 	struct hstate *h = hstate_vma(vma);
+	unsigned long pages = 0;
 
 	BUG_ON(address >= end);
 	flush_cache_range(vma, address, end);
@@ -3122,8 +3123,10 @@ void hugetlb_change_protection(struct vm_area_struct *vma,
 		ptep = huge_pte_offset(mm, address);
 		if (!ptep)
 			continue;
-		if (huge_pmd_unshare(mm, &address, ptep))
+		if (huge_pmd_unshare(mm, &address, ptep)) {
+			pages++;
 			continue;
+		}
 		pte = huge_ptep_get(ptep);
 		if (unlikely(is_hugetlb_entry_hwpoisoned(pte)))
 			continue;
@@ -3136,6 +3139,7 @@ void hugetlb_change_protection(struct vm_area_struct *vma,
 				make_migration_entry_read(&entry);
 				newpte = swp_entry_to_pte(entry);
 				set_huge_pte_at(mm, address, ptep, newpte);
+				pages++;
 			}
 			continue;
 		}
@@ -3154,6 +3158,8 @@ void hugetlb_change_protection(struct vm_area_struct *vma,
 	 */
 	flush_tlb_range(vma, start, end);
 	mutex_unlock(&vma->vm_file->f_mapping->i_mmap_mutex);
+
+	return pages << h->order;
 }
 
 int hugetlb_reserve_pages(struct inode *inode,
