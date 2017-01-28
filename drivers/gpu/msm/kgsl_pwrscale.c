4f53924d303e491fe8021ab45e402a0565fb1ce4
robcore <robpatershuk@gmail.com>
2017-01-27 21:09:05 -0700

    remove msm dcvs from policy group

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -48,7 +48,7 @@ static struct kgsl_pwrscale_policy *kgsl_pwrscale_policies[] = {
 #ifdef CONFIG_MSM_SLEEP_STATS_DEVICE
 	&kgsl_pwrscale_policy_idlestats,
 #endif
-#ifdef CONFIG_MSM_DCVS
+#if 0
 	&kgsl_pwrscale_policy_msm,
 #endif
 	&kgsl_pwrscale_policy_conservative,
