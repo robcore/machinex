1c0481f2fb50697db0e9538300d8571fb9ae3627
robcore <robpatershuk@gmail.com>
2017-01-28 15:48:10 -0700

    ???

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -96,6 +96,7 @@ struct vm_area_struct;
 
 #define __GFP_NO_KSWAPD	((__force gfp_t)___GFP_NO_KSWAPD)
 #define __GFP_OTHER_NODE ((__force gfp_t)___GFP_OTHER_NODE) /* On behalf of other node */
+#define __GFP_KMEMCG	((__force gfp_t)___GFP_KMEMCG) /* Allocation comes from a memcg-accounted resource */
 #define __GFP_WRITE	((__force gfp_t)___GFP_WRITE)	/* Allocator intends to dirty page */
 
 /*
