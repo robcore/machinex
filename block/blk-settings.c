899416aa518cc95b59a044aca85362999ff425e8
robcore <robpatershuk@gmail.com>
2017-01-28 13:51:12 -0700

    block: discard granularity might not be power of 2

--- block/blk-settings.c
+++ block/blk-settings.c
@@ -609,7 +609,7 @@ int blk_stack_limits(struct queue_limits *t, struct queue_limits *b,
 			bottom = b->discard_granularity + alignment;
 
 			/* Verify that top and bottom intervals line up */
-			if (max(top, bottom) & (min(top, bottom) - 1))
+			if ((max(top, bottom) % min(top, bottom)) != 0)
 				t->discard_misaligned = 1;
 		}
 
@@ -618,7 +618,7 @@ int blk_stack_limits(struct queue_limits *t, struct queue_limits *b,
 		t->discard_granularity = max(t->discard_granularity,
 					     b->discard_granularity);
 		t->discard_alignment = lcm_not_zero(t->discard_alignment, alignment) &
-			(t->discard_granularity - 1);
+			(t->discard_granularity);
 	}
 
 	return ret;
