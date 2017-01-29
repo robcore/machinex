ca9f33484ae2a5fa1429f1580b8d16901631170f
robcore <robpatershuk@gmail.com>
2017-01-28 17:20:02 -0700

    new-helpers-__save_altstack-__compat_save_altstack

--- arch/x86/kernel/signal.c
+++ arch/x86/kernel/signal.c
@@ -409,7 +409,6 @@ static int __setup_rt_frame(int sig, struct k_sigaction *ka, siginfo_t *info,
 	struct rt_sigframe __user *frame;
 	void __user *fp = NULL;
 	int err = 0;
-	struct task_struct *me = current;
 
 	frame = get_sigframe(ka, regs, sizeof(struct rt_sigframe), &fp);
 
@@ -843,10 +842,7 @@ static int x32_setup_rt_frame(int sig, struct k_sigaction *ka,
 		else
 			put_user_ex(0, &frame->uc.uc_flags);
 		put_user_ex(0, &frame->uc.uc_link);
-		put_user_ex(current->sas_ss_sp, &frame->uc.uc_stack.ss_sp);
-		put_user_ex(sas_ss_flags(regs->sp),
-			    &frame->uc.uc_stack.ss_flags);
-		put_user_ex(current->sas_ss_size, &frame->uc.uc_stack.ss_size);
+		err |= __compat_save_altstack(&frame->uc.uc_stack, regs->sp);
 		put_user_ex(0, &frame->uc.uc__pad0);
 		err |= setup_sigcontext(&frame->uc.uc_mcontext, fpstate,
 					regs, set->sig[0]);
