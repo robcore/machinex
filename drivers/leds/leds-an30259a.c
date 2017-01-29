a9e68c1546081afdeaef4a52394abda251c59427
robcore <robpatershuk@gmail.com>
2017-01-29 13:48:06 -0700

    leds

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -480,11 +480,11 @@ static void an30259a_start_led_pattern(int mode)
 		leds_on(LED_G, true, true, LED_DYNAMIC_CURRENT);
 		leds_on(LED_B, true, true, LED_DYNAMIC_CURRENT);
 		leds_set_slope_mode(client, LED_R,
-				0, 0, 0, 5, 6, 4, 5, 5, 5, 5);
+				0, 0, 0, 5, 2, 2, 0, 0, 0, 0);
 		leds_set_slope_mode(client, LED_G,
-				0, 15, 15, 10, 6, 4, 5, 5, 5, 5);
+				0, 15, 15, 10, 2, 2, 0, 0, 0, 0);
 		leds_set_slope_mode(client, LED_B,
-				0, 15, 0, 0, 6, 4, 5, 5, 5, 5);
+				0, 15, 0, 0, 2, 2, 0, 0, 0, 0);
 		break;
 
 /* For later
