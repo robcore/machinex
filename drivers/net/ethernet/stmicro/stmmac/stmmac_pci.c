92c0e8744a735b4f0ed4006b65131fdff0bc0ccf
robcore <robpatershuk@gmail.com>
2017-01-31 22:18:34 -0700

    stmmac-don-t-return-zero-on-failure-path-in-stmmac_p

--- drivers/net/ethernet/stmicro/stmmac/stmmac_pci.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_pci.c
@@ -99,6 +99,7 @@ static int __devinit stmmac_pci_probe(struct pci_dev *pdev,
 	priv = stmmac_dvr_probe(&(pdev->dev), &plat_dat, addr);
 	if (!priv) {
 		pr_err("%s: main driver probe failed", __func__);
+		ret = -ENODEV;
 		goto err_out;
 	}
 	priv->dev->irq = pdev->irq;
