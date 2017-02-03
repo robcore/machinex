b1a6447f13f6f5a57eca441055a0a0385e731505
robcore <robpatershuk@gmail.com>
2017-02-02 23:12:59 -0700

    vmscan-change-type-of-vm_total_pages-to-unsigned-long

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -133,7 +133,7 @@ struct scan_control {
  * From 0 .. whatever.  Higher means more swappy.
  */
 int vm_swappiness = 130;
-long vm_total_pages;	/* The total number of pages which the VM controls */
+unsigned long vm_total_pages;	/* The total number of pages which the VM controls */
 
 static LIST_HEAD(shrinker_list);
 static DECLARE_RWSEM(shrinker_rwsem);
