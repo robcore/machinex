a496d698f937dd4b0dbb32b31dc4c90782a716ce
robcore <robpatershuk@gmail.com>
2017-02-04 01:10:33 -0700

    wtf is happening

--- build_single_driver.sh
+++ build_single_driver.sh
@@ -88,7 +88,7 @@ make distclean;
 make mrproper;
 mkdir $(pwd)/out;
 cp $(pwd)/arch/arm/configs/canadefconfig $(pwd)/out/.config;
-make ARCH=arm -j6 O=$(pwd)/out oldconfig;
+make C=1 ARCH=arm -j6 O=$(pwd)/out oldconfig;
 #echo -n "What am I building? [ENTER] "
 #read DRV
 #make ARCH=arm -S -s -j6 O=$(pwd)/out $(pwd)/$DRV;
