dc96b4ed45149305e2cc192c38ccb27534a92a5b
robcore <robpatershuk@gmail.com>
2017-02-05 00:00:59 -0700

    jesus christ

--- arch/arm/mach-msm/board-8064-wifi.c
+++ arch/arm/mach-msm/board-8064-wifi.c
@@ -295,7 +295,7 @@ static void *wifi_status_cb_devid;
 
 int brcm_wifi_status_register(
 	void (*callback)(int card_present, void *dev_id),
-	void *dev_id, void *mmc_host)
+	void *dev_id, void *mmc_host);
 {
 	if (wifi_status_cb)
 		return -EAGAIN;
