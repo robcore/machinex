3d27cbe06245408211e669fd7be868cac6d0771f
robcore <robpatershuk@gmail.com>
2017-02-07 06:18:17 -0700

    just nail the cpus at the bloody source

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -19,6 +19,7 @@
 #include <linux/completion.h>
 #include <linux/workqueue.h>
 #include <linux/cpumask.h>
+#include <linux/sched/rt.h>
 #include <asm/div64.h>
 #ifdef CONFIG_CPUFREQ_HARDLIMIT
 #include <linux/cpufreq_hardlimit.h>
