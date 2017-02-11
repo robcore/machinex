084cfedfd2f7784efa050cd7d241bc2350a3340d
robcore <robpatershuk@gmail.com>
2017-02-10 23:46:33 -0700

    mmc-core-use-a-more-generic-name-for-slot-function

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -344,6 +344,8 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 
 	mmc_host_clk_init(host);
 
+	host->slot.cd_irq = -EINVAL;
+
 	spin_lock_init(&host->lock);
 	init_waitqueue_head(&host->wq);
 	host->wlock_name = kasprintf(GFP_KERNEL,
