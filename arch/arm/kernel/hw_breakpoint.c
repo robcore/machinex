77fd8c68328579625d31e3c3b5e8cc3514360ccb
robcore <robpatershuk@gmail.com>
2017-02-12 16:12:58 -0700

    ARM-7662-1-hw_breakpoint-reset-debug-logic-on-second.

--- arch/arm/kernel/hw_breakpoint.c
+++ arch/arm/kernel/hw_breakpoint.c
@@ -1007,7 +1007,7 @@ reset_regs:
 static int dbg_reset_notify(struct notifier_block *self,
 				      unsigned long action, void *cpu)
 {
-	if (action == CPU_ONLINE)
+	if ((action & ~CPU_TASKS_FROZEN) == CPU_ONLINE)
 		smp_call_function_single((int)cpu, reset_ctrl_regs, NULL, 1);
 
 	return NOTIFY_OK;
