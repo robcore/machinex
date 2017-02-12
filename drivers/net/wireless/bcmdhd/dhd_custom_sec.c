b32c77905728b596c09f76d160fd7acc2ccb9880
robcore <robpatershuk@gmail.com>
2017-02-11 19:26:45 -0700

    shouldnt filepath_cid be the CID path?

--- drivers/net/wireless/bcmdhd/dhd_custom_sec.c
+++ drivers/net/wireless/bcmdhd/dhd_custom_sec.c
@@ -842,6 +842,7 @@ static int dhd_write_cid_file(const char *filepath_cid, const char *buf, int buf
 	struct file *fp = NULL;
 	mm_segment_t oldfs = {0};
 	int ret = 0;
+	char *filepath_cid = CIDINFO;
 
 	/* File is always created. */
 	fp = filp_open(filepath_cid, O_RDWR | O_CREAT, 0660);
