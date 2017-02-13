3603f7f6184681f043e2e05f8abd6887d3fc9006
robcore <robpatershuk@gmail.com>
2017-02-12 20:06:54 -0700

    fs-Readd-the-fs-module-aliases

--- fs/nfs/super.c
+++ fs/nfs/super.c
@@ -356,6 +356,7 @@ static struct file_system_type nfs4_remote_fs_type = {
 	.fs_flags	= FS_RENAME_DOES_D_MOVE|FS_BINARY_MOUNTDATA,
 };
 MODULE_ALIAS_FS("nfs4");
+MODULE_ALIAS("nfs4");
 EXPORT_SYMBOL_GPL(nfs4_fs_type);
 
 struct file_system_type nfs4_xdev_fs_type = {
