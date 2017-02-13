e4b3d4a6bb8640e0c209311db5df7f10ddf4c26a
robcore <robpatershuk@gmail.com>
2017-02-13 09:42:05 -0700

    bcache-A-block-layer-cache

--- include/linux/cgroup_subsys.h
+++ include/linux/cgroup_subsys.h
@@ -84,3 +84,9 @@ SUBSYS(hugetlb)
 #endif
 
 /* */
+
+#ifdef CONFIG_CGROUP_BCACHE
+SUBSYS(bcache)
+#endif
+
+/* */
--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1518,6 +1518,10 @@ struct task_struct {
 #ifdef CONFIG_HAVE_HW_BREAKPOINT
 	atomic_t ptrace_bp_refcnt;
 #endif
+#if defined(CONFIG_BCACHE) || defined(CONFIG_BCACHE_MODULE)
+	unsigned int	sequential_io;
+	unsigned int	sequential_io_avg;
+#endif
 };
 
 /* Future-safe accessor for struct task_struct's cpus_allowed. */
