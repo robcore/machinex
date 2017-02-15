66d60e673b0fd4dc3230f6dcfff86f23ae72de83
robcore <robpatershuk@gmail.com>
2017-02-14 20:37:42 -0700

    pos.

--- pos.sh
+++ pos.sh
@@ -1,32 +1,53 @@
-#!/bin/bash
-patch -p1 -R < "/root/machinex/patches/0250-REVERT-USB-EHCI-commits.patch"
-patch -p1 -R < "/root/machinex/patches/0249-my-own-little-slimbus-hack-to-see-if-we-can-work-aro.patch"
-patch -p1 -R < "/root/machinex/patches/0248-idr-fix-top-layer-handling.patch"
-patch -p1 -R < "/root/machinex/patches/0247-mac80211.patch"
-patch -p1 -R < "/root/machinex/patches/0246-net-9p-convert-to-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0245-posix-timers.patch"
-patch -p1 -R < "/root/machinex/patches/0244-events.patch"
-patch -p1 -R < "/root/machinex/patches/0243-cgroup.patch"
-patch -p1 -R < "/root/machinex/patches/0242-uio.patch"
-patch -p1 -R < "/root/machinex/patches/0241-thermal.patch"
-patch -p1 -R < "/root/machinex/patches/0240-scsi-conversion.patch"
-patch -p1 -R < "/root/machinex/patches/0239-rpmsg-convert-to-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0238-remoteproc-convert-to-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0237-mmc-convert-to-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0236-mtd-convert-to-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0235-misc-c2port-convert-to-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0234-i2c-convert-to-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0233-gpio-convert-to-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0232-N8.patch"
-patch -p1 -R < "/root/machinex/patches/0231-idr-implement-idr_preload-_end-and-idr_alloc.patch"
-patch -p1 -R < "/root/machinex/patches/0230-idr-refactor-idr_get_new_above.patch"
-patch -p1 -R < "/root/machinex/patches/0229-idr-remove-_idr_rc_to_errno-hack.patch"
-patch -p1 -R < "/root/machinex/patches/0228-idr-relocate-idr_for_each_entry-and-reorganize-id.patch"
-patch -p1 -R < "/root/machinex/patches/0227-idr-deprecate-idr_remove_all.patch"
-patch -p1 -R < "/root/machinex/patches/0226-idr-cosmetic-updates-to-struct-initializer-def.patch"
-patch -p1 -R < "/root/machinex/patches/0224-another-idr-remove-all-removal.patch"
-patch -p1 -R < "/root/machinex/patches/0223-mass-removal-of-idr-remove_all-build-time.patch"
-patch -p1 -R < "/root/machinex/patches/0222-idr-make-idr_destroy-imply-idr_remove_all.patch"
-patch -p1 -R < "/root/machinex/patches/0221-ATTEMPT-idr-fix-a-subtle-bug-in-idr_get_next.patch"
-patch -p1 -R < "/root/machinex/patches/0195-DANGER-USB-EHCI-don-t-turn-on-PORT_SUSPEND-during-po.patch"
-patch -p1 -R < "/root/machinex/patches/0194-DANGER-USB-EHCI-improve-use-of-per-port-status-chang.patch"
+/root/machinex/patches/0150-nope-this-is-garbage.patch
+/root/machinex/patches/0149-SUCK-IT-3.15.patch
+/root/machinex/patches/0148-workqueues-3.14.patch
+/root/machinex/patches/0147-workqueues-are-now-at-3.10.patch
+/root/machinex/patches/0146-and-that.patch
+/root/machinex/patches/0145-k-brought-those-back.patch
+/root/machinex/patches/0144-workqueue-workqueue_congested-shouldnt-translate-WOR.patch
+/root/machinex/patches/0143-workqueue-avoid-false-negative-WARN_ON.patch
+/root/machinex/patches/0142-revert-those-for-a-sec-my-computer-is-grepping-VERY-.patch
+/root/machinex/patches/0140-and-more-doing-this-till-FUCKING-3.15.patch
+/root/machinex/patches/0139-there-now-REALLY-brought-up-workqueues.patch
+/root/machinex/patches/0138-brought-workqueues-back-up-going-all-the-way-to-3.15.patch
+/root/machinex/patches/0137-workqueue-move-struct-worker-definition-to-workqueue.patch
+/root/machinex/patches/0136-REVERTED-back-to-idr-conversion-because-i-am-going-t.patch
+/root/machinex/patches/0134-Reverted-some-rcu-shit-i-will-do-it-when-im-not-exha.patch
+/root/machinex/patches/0133-k-i-cheated-a-bit.patch
+/root/machinex/patches/0130-synced-my-rcu-headers.patch
+
+/root/machinex/patches/0081-rcu-Make-rcu_accelerate_cbs-note-need-for-future.patch
+/root/machinex/patches/0080-rcu-Abstract-rcu_start_future_gp-from-rcu_nocb_wait.patch
+/root/machinex/patches/0079-rcu-Rename-n_nocb_gp_requests-to-need_future_gp.patch
+/root/machinex/patches/0078-rcu-Push-lock-release-to-rcu_start_gp-s-callers.patch
+/root/machinex/patches/0077-rcu-Repurpose-no-CBs-event-tracing-to-future-GP-even.patch
+/root/machinex/patches/0076-trying.also-rcu-Rearrange-locking-in-rcu_start_gp.patch
+/root/machinex/patches/0075-less-fuck-but-still-fuck.patch
+/root/machinex/patches/0074-fuck.patch
+/root/machinex/patches/0073-rcu-Accelerate-RCU-callbacks-at-grace-period-end.patch
+/root/machinex/patches/0072-rcu-Distinguish-rcuo-kthreads-by-RCU-flavor.patch
+/root/machinex/patches/0071-rcu-Add-event-tracing-for-no-CBs-CPUs-grace-periods.patch
+/root/machinex/patches/0070-rcu-Add-event-tracing-for-no-CBs-CPUs-callbacks.patch
+/root/machinex/patches/0069-rcu-Introduce-proper-blocking-to-no-CBs-kthreads-GP.patch
+/root/machinex/patches/0068-rcu-Provide-compile-time-control-for-no-CBs-CPUs.patch
+
+/root/machinex/patches/0060-workqueue-remove-pwq_lock-which-is-no-longer-used.patch
+/root/machinex/patches/0059-workqueue-protect-wq-saved_max_active-with-wq-mutex.patch
+/root/machinex/patches/0058-workqueue-protect-wq-pwqs-and-iteration-with-wq-mute.patch
+/root/machinex/patches/0057-workqueue-protect-wq-nr_drainers-and-flags-with-wq-m.patch
+/root/machinex/patches/0056-workqueue-rename-wq-flush_mutex-to-wq-mutex.patch
+/root/machinex/patches/0055-workqueue-rename-wq_mutex-to-wq_pool_mutex.patch
+
+/root/machinex/patches/0141-workqueue-avoid-false-negative-in-assert_manager.patch
+/root/machinex/patches/0140-workqueue-kick-a-worker-in-pwq_adjust_max_active.patch
+/root/machinex/patches/0139-workqueue-simplify-current_is_workqueue_rescuer.patch
+
+/root/machinex/patches/0135-orkqueue-add-missing-POOL_FREEZING.patch
+
+/root/machinex/patches/0123-workqueue-restore-CPU-affinity-of-unbound-workers.patch
+/root/machinex/patches/0122-workqueue-directly-restore-CPU-affinity-of-workers-f.patch
+/root/machinex/patches/0121-workqueue-relocate-rebind_workers.patch
+/root/machinex/patches/0120-attempting-to-hack-in-the-old-idr-allocation-routine.patch
+/root/machinex/patches/0119-nevermind.patch
+/root/machinex/patches/0118-workqueue-the-two-commits-that-came-after-the-idr-on.patch
+
