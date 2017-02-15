927f6c91de6d384eec4efa0095ae4ff06b4ad713
robcore <robpatershuk@gmail.com>
2017-02-14 19:51:27 -0700

    revert those for a sec my computer is grepping VERY FRUSTRATINGLY SLOWLY

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -325,7 +325,7 @@ static DEFINE_HASHTABLE(unbound_pool_hash, UNBOUND_POOL_HASH_ORDER);
 static struct workqueue_attrs *unbound_std_wq_attrs[NR_STD_WORKER_POOLS];
 
 struct workqueue_struct *system_wq __read_mostly;
-EXPORT_SYMBOL(system_wq);
+EXPORT_SYMBOL_GPL(system_wq);
 struct workqueue_struct *system_highpri_wq __read_mostly;
 EXPORT_SYMBOL_GPL(system_highpri_wq);
 struct workqueue_struct *system_long_wq __read_mostly;
@@ -1442,7 +1442,7 @@ bool queue_work_on(int cpu, struct workqueue_struct *wq,
 	local_irq_restore(flags);
 	return ret;
 }
-EXPORT_SYMBOL(queue_work_on);
+EXPORT_SYMBOL_GPL(queue_work_on);
 
 void delayed_work_timer_fn(unsigned long __data)
 {
@@ -1516,7 +1516,7 @@ bool queue_delayed_work_on(int cpu, struct workqueue_struct *wq,
 	local_irq_restore(flags);
 	return ret;
 }
-EXPORT_SYMBOL(queue_delayed_work_on);
+EXPORT_SYMBOL_GPL(queue_delayed_work_on);
 
 /**
  * mod_delayed_work_on - modify delay of or queue a delayed work on specific CPU
@@ -2090,7 +2090,6 @@ static bool manage_workers(struct worker *worker)
 	if (unlikely(!mutex_trylock(&pool->manager_mutex))) {
 		spin_unlock_irq(&pool->lock);
 		mutex_lock(&pool->manager_mutex);
-		spin_lock_irq(&pool->lock);
 		ret = true;
 	}
 
