ba470a62755dd12db33fe42ae25de76ed2fa4dd9
robcore <robpatershuk@gmail.com>
2017-02-14 20:38:23 -0700

    pos script

--- pos.sh
+++ pos.sh
@@ -1,53 +1,54 @@
-/root/machinex/patches/0150-nope-this-is-garbage.patch
-/root/machinex/patches/0149-SUCK-IT-3.15.patch
-/root/machinex/patches/0148-workqueues-3.14.patch
-/root/machinex/patches/0147-workqueues-are-now-at-3.10.patch
-/root/machinex/patches/0146-and-that.patch
-/root/machinex/patches/0145-k-brought-those-back.patch
-/root/machinex/patches/0144-workqueue-workqueue_congested-shouldnt-translate-WOR.patch
-/root/machinex/patches/0143-workqueue-avoid-false-negative-WARN_ON.patch
-/root/machinex/patches/0142-revert-those-for-a-sec-my-computer-is-grepping-VERY-.patch
-/root/machinex/patches/0140-and-more-doing-this-till-FUCKING-3.15.patch
-/root/machinex/patches/0139-there-now-REALLY-brought-up-workqueues.patch
-/root/machinex/patches/0138-brought-workqueues-back-up-going-all-the-way-to-3.15.patch
-/root/machinex/patches/0137-workqueue-move-struct-worker-definition-to-workqueue.patch
-/root/machinex/patches/0136-REVERTED-back-to-idr-conversion-because-i-am-going-t.patch
-/root/machinex/patches/0134-Reverted-some-rcu-shit-i-will-do-it-when-im-not-exha.patch
-/root/machinex/patches/0133-k-i-cheated-a-bit.patch
-/root/machinex/patches/0130-synced-my-rcu-headers.patch
+#!/bin/bash
+patch -p1 -R < "/root/machinex/patches/0150-nope-this-is-garbage.patch"
+patch -p1 -R < "/root/machinex/patches/0149-SUCK-IT-3.15.patch"
+patch -p1 -R < "/root/machinex/patches/0148-workqueues-3.14.patch"
+patch -p1 -R < "/root/machinex/patches/0147-workqueues-are-now-at-3.10.patch"
+patch -p1 -R < "/root/machinex/patches/0146-and-that.patch"
+patch -p1 -R < "/root/machinex/patches/0145-k-brought-those-back.patch"
+patch -p1 -R < "/root/machinex/patches/0144-workqueue-workqueue_congested-shouldnt-translate-WOR.patch"
+patch -p1 -R < "/root/machinex/patches/0143-workqueue-avoid-false-negative-WARN_ON.patch"
+patch -p1 -R < "/root/machinex/patches/0142-revert-those-for-a-sec-my-computer-is-grepping-VERY-.patch"
+patch -p1 -R < "/root/machinex/patches/0140-and-more-doing-this-till-FUCKING-3.15.patch"
+patch -p1 -R < "/root/machinex/patches/0139-there-now-REALLY-brought-up-workqueues.patch"
+patch -p1 -R < "/root/machinex/patches/0138-brought-workqueues-back-up-going-all-the-way-to-3.15.patch"
+patch -p1 -R < "/root/machinex/patches/0137-workqueue-move-struct-worker-definition-to-workqueue.patch"
+patch -p1 -R < "/root/machinex/patches/0136-REVERTED-back-to-idr-conversion-because-i-am-going-t.patch"
+patch -p1 -R < "/root/machinex/patches/0134-Reverted-some-rcu-shit-i-will-do-it-when-im-not-exha.patch"
+patch -p1 -R < "/root/machinex/patches/0133-k-i-cheated-a-bit.patch"
+patch -p1 -R < "/root/machinex/patches/0130-synced-my-rcu-headers.patch"
 
-/root/machinex/patches/0081-rcu-Make-rcu_accelerate_cbs-note-need-for-future.patch
-/root/machinex/patches/0080-rcu-Abstract-rcu_start_future_gp-from-rcu_nocb_wait.patch
-/root/machinex/patches/0079-rcu-Rename-n_nocb_gp_requests-to-need_future_gp.patch
-/root/machinex/patches/0078-rcu-Push-lock-release-to-rcu_start_gp-s-callers.patch
-/root/machinex/patches/0077-rcu-Repurpose-no-CBs-event-tracing-to-future-GP-even.patch
-/root/machinex/patches/0076-trying.also-rcu-Rearrange-locking-in-rcu_start_gp.patch
-/root/machinex/patches/0075-less-fuck-but-still-fuck.patch
-/root/machinex/patches/0074-fuck.patch
-/root/machinex/patches/0073-rcu-Accelerate-RCU-callbacks-at-grace-period-end.patch
-/root/machinex/patches/0072-rcu-Distinguish-rcuo-kthreads-by-RCU-flavor.patch
-/root/machinex/patches/0071-rcu-Add-event-tracing-for-no-CBs-CPUs-grace-periods.patch
-/root/machinex/patches/0070-rcu-Add-event-tracing-for-no-CBs-CPUs-callbacks.patch
-/root/machinex/patches/0069-rcu-Introduce-proper-blocking-to-no-CBs-kthreads-GP.patch
-/root/machinex/patches/0068-rcu-Provide-compile-time-control-for-no-CBs-CPUs.patch
+patch -p1 -R < "/root/machinex/patches/0081-rcu-Make-rcu_accelerate_cbs-note-need-for-future.patch"
+patch -p1 -R < "/root/machinex/patches/0080-rcu-Abstract-rcu_start_future_gp-from-rcu_nocb_wait.patch"
+patch -p1 -R < "/root/machinex/patches/0079-rcu-Rename-n_nocb_gp_requests-to-need_future_gp.patch"
+patch -p1 -R < "/root/machinex/patches/0078-rcu-Push-lock-release-to-rcu_start_gp-s-callers.patch"
+patch -p1 -R < "/root/machinex/patches/0077-rcu-Repurpose-no-CBs-event-tracing-to-future-GP-even.patch"
+patch -p1 -R < "/root/machinex/patches/0076-trying.also-rcu-Rearrange-locking-in-rcu_start_gp.patch"
+patch -p1 -R < "/root/machinex/patches/0075-less-fuck-but-still-fuck.patch"
+patch -p1 -R < "/root/machinex/patches/0074-fuck.patch"
+patch -p1 -R < "/root/machinex/patches/0073-rcu-Accelerate-RCU-callbacks-at-grace-period-end.patch"
+patch -p1 -R < "/root/machinex/patches/0072-rcu-Distinguish-rcuo-kthreads-by-RCU-flavor.patch"
+patch -p1 -R < "/root/machinex/patches/0071-rcu-Add-event-tracing-for-no-CBs-CPUs-grace-periods.patch"
+patch -p1 -R < "/root/machinex/patches/0070-rcu-Add-event-tracing-for-no-CBs-CPUs-callbacks.patch"
+patch -p1 -R < "/root/machinex/patches/0069-rcu-Introduce-proper-blocking-to-no-CBs-kthreads-GP.patch"
+patch -p1 -R < "/root/machinex/patches/0068-rcu-Provide-compile-time-control-for-no-CBs-CPUs.patch"
 
-/root/machinex/patches/0060-workqueue-remove-pwq_lock-which-is-no-longer-used.patch
-/root/machinex/patches/0059-workqueue-protect-wq-saved_max_active-with-wq-mutex.patch
-/root/machinex/patches/0058-workqueue-protect-wq-pwqs-and-iteration-with-wq-mute.patch
-/root/machinex/patches/0057-workqueue-protect-wq-nr_drainers-and-flags-with-wq-m.patch
-/root/machinex/patches/0056-workqueue-rename-wq-flush_mutex-to-wq-mutex.patch
-/root/machinex/patches/0055-workqueue-rename-wq_mutex-to-wq_pool_mutex.patch
+patch -p1 -R < "/root/machinex/patches/0060-workqueue-remove-pwq_lock-which-is-no-longer-used.patch"
+patch -p1 -R < "/root/machinex/patches/0059-workqueue-protect-wq-saved_max_active-with-wq-mutex.patch"
+patch -p1 -R < "/root/machinex/patches/0058-workqueue-protect-wq-pwqs-and-iteration-with-wq-mute.patch"
+patch -p1 -R < "/root/machinex/patches/0057-workqueue-protect-wq-nr_drainers-and-flags-with-wq-m.patch"
+patch -p1 -R < "/root/machinex/patches/0056-workqueue-rename-wq-flush_mutex-to-wq-mutex.patch"
+patch -p1 -R < "/root/machinex/patches/0055-workqueue-rename-wq_mutex-to-wq_pool_mutex.patch"
 
-/root/machinex/patches/0141-workqueue-avoid-false-negative-in-assert_manager.patch
-/root/machinex/patches/0140-workqueue-kick-a-worker-in-pwq_adjust_max_active.patch
-/root/machinex/patches/0139-workqueue-simplify-current_is_workqueue_rescuer.patch
+patch -p1 -R < "/root/machinex/patches/0141-workqueue-avoid-false-negative-in-assert_manager.patch"
+patch -p1 -R < "/root/machinex/patches/0140-workqueue-kick-a-worker-in-pwq_adjust_max_active.patch"
+patch -p1 -R < "/root/machinex/patches/0139-workqueue-simplify-current_is_workqueue_rescuer.patch"
 
-/root/machinex/patches/0135-orkqueue-add-missing-POOL_FREEZING.patch
+patch -p1 -R < "/root/machinex/patches/0135-orkqueue-add-missing-POOL_FREEZING.patch"
 
-/root/machinex/patches/0123-workqueue-restore-CPU-affinity-of-unbound-workers.patch
-/root/machinex/patches/0122-workqueue-directly-restore-CPU-affinity-of-workers-f.patch
-/root/machinex/patches/0121-workqueue-relocate-rebind_workers.patch
-/root/machinex/patches/0120-attempting-to-hack-in-the-old-idr-allocation-routine.patch
-/root/machinex/patches/0119-nevermind.patch
-/root/machinex/patches/0118-workqueue-the-two-commits-that-came-after-the-idr-on.patch
+patch -p1 -R < "/root/machinex/patches/0123-workqueue-restore-CPU-affinity-of-unbound-workers.patch"
+patch -p1 -R < "/root/machinex/patches/0122-workqueue-directly-restore-CPU-affinity-of-workers-f.patch"
+patch -p1 -R < "/root/machinex/patches/0121-workqueue-relocate-rebind_workers.patch"
+patch -p1 -R < "/root/machinex/patches/0120-attempting-to-hack-in-the-old-idr-allocation-routine.patch"
+patch -p1 -R < "/root/machinex/patches/0119-nevermind.patch"
+patch -p1 -R < "/root/machinex/patches/0118-workqueue-the-two-commits-that-came-after-the-idr-on.patch"
 
