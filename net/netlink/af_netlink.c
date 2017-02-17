31e71e234a22f268f8b8587681fe6202827a559e
robcore <robpatershuk@gmail.com>
2017-02-17 07:22:05 -0700

    REVERT - netlink - fix skb ref counting

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -1765,6 +1765,7 @@ int __netlink_dump_start(struct sock *ssk, struct sk_buff *skb,
 	cb->data = control->data;
 	cb->module = control->module;
 	cb->min_dump_alloc = control->min_dump_alloc;
+	atomic_inc(&skb->users);
 	cb->skb = skb;
 
 	sk = netlink_lookup(sock_net(ssk), ssk->sk_protocol, NETLINK_CB(skb).pid);
