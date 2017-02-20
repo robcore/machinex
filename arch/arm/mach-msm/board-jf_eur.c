219edf3531625a7d4ffd19f8e728b9f92db3e4de
robcore <robpatershuk@gmail.com>
2017-02-20 14:48:13 -0700

    trying a different value for modem subsys name

--- arch/arm/mach-msm/board-jf_eur.c
+++ arch/arm/mach-msm/board-jf_eur.c
@@ -3105,7 +3105,7 @@ static struct mdm_platform_data mdm_platform_data = {
 	.mdm2ap_status_gpio_run_cfg = &mdm2ap_status_gpio_run_cfg,
 	.sysmon_subsys_id_valid = 1,
 	.sysmon_subsys_id = SYSMON_SS_EXT_MODEM,
-	.subsys_name = "external_modem",
+	.subsys_name = "mdm2_modem",
 };
 
 static struct mdm_platform_data amdm_platform_data = {
