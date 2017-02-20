eb5655188ce8df057677a700487ad281b5a4ee01
robcore <robpatershuk@gmail.com>
2017-02-20 01:20:19 -0700

    fixup for last commit

--- arch/arm/common/timer-sp.c
+++ arch/arm/common/timer-sp.c
@@ -27,7 +27,6 @@
 #include <linux/io.h>
 #include <linux/sched_clock.h>
 
-#include <asm/sched_clock.h>
 #include <asm/hardware/arm_timer.h>
 
 static long __init sp804_get_clock_rate(const char *name)
