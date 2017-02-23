5048501f50eaf32b73e8d22ecc711a8af446f381
robcore <robpatershuk@gmail.com>
2017-02-22 21:29:09 -0700

    SHIT undefined reference

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -245,7 +245,7 @@ struct workqueue_struct {
 	int			nr_drainers;	/* WQ: drain in progress */
 	int			saved_max_active; /* WQ: saved pwq max_active */
 
-#if 0
+#ifdef CONFIG_SYSFS
 	struct wq_device	*wq_dev;	/* I: for sysfs interface */
 #endif
 #ifdef CONFIG_LOCKDEP
@@ -2999,7 +2999,7 @@ int execute_in_process_context(work_func_t fn, struct execute_work *ew)
 }
 EXPORT_SYMBOL(execute_in_process_context);
 
-#if 0
+#ifdef CONFIG_SYSFS
 /*
  * Workqueues with WQ_SYSFS flag set is visible to userland via
  * /sys/bus/workqueue/devices/WQ_NAME.  All visible workqueues have the
