c5b0ceba818e832cc0843934e9fdde15b7bc02dc
robcore <robpatershuk@gmail.com>
2017-02-25 03:01:30 -0700

    REVERT - qdsp commit for a sec while I test the mipi hack

--- drivers/base/power/domain.c
+++ drivers/base/power/domain.c
@@ -711,7 +711,7 @@ static int __init pd_ignore_unused_setup(char *__unused)
 	pd_ignore_unused = true;
 	return 1;
 }
-__setup("pd_ignore_unused", pd_ignore_unused_setup);
+__setup("pd_ignore_unused=", pd_ignore_unused_setup);
 
 /**
  * pm_genpd_poweroff_unused - Power off all PM domains with no devices in use.
