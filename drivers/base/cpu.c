f97c6f2ce6a84c126a3d80ce35dcf45d6d4c11bc
robcore <robpatershuk@gmail.com>
2017-02-25 23:42:02 -0700

    drivers base-cpu: revert all recent commits

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -16,56 +16,65 @@
 
 #include "base.h"
 
+struct bus_type cpu_subsys = {
+	.name = "cpu",
+	.dev_name = "cpu",
+};
+EXPORT_SYMBOL_GPL(cpu_subsys);
+
 static DEFINE_PER_CPU(struct device *, cpu_sys_devices);
 
 #ifdef CONFIG_HOTPLUG_CPU
-static void change_cpu_under_node(struct cpu *cpu,
-			unsigned int from_nid, unsigned int to_nid)
-{
-	int cpuid = cpu->dev.id;
-	unregister_cpu_under_node(cpuid, from_nid);
-	register_cpu_under_node(cpuid, to_nid);
-	cpu->node_id = to_nid;
-}
-
-static int __ref cpu_subsys_online(struct device *dev)
+static ssize_t show_online(struct device *dev,
+			   struct device_attribute *attr,
+			   char *buf)
 {
 	struct cpu *cpu = container_of(dev, struct cpu, dev);
-	int cpuid = dev->id;
-	int from_nid, to_nid;
-	int ret;
 
-	cpu_hotplug_driver_lock();
-
-	from_nid = cpu_to_node(cpuid);
-	ret = cpu_up(cpuid);
-	/*
-	 * When hot adding memory to memoryless node and enabling a cpu
-	 * on the node, node number of the cpu may internally change.
-	 */
-	to_nid = cpu_to_node(cpuid);
-	if (from_nid != to_nid)
-		change_cpu_under_node(cpu, from_nid, to_nid);
-
-	cpu_hotplug_driver_unlock();
-	return ret;
+	return sprintf(buf, "%u\n", !!cpu_online(cpu->dev.id));
 }
 
-static int cpu_subsys_offline(struct device *dev)
+static ssize_t __ref store_online(struct device *dev,
+				  struct device_attribute *attr,
+				  const char *buf, size_t count)
 {
-	int ret;
+	struct cpu *cpu = container_of(dev, struct cpu, dev);
+	ssize_t ret;
 
 	cpu_hotplug_driver_lock();
-	ret = cpu_down(dev->id);
+	switch (buf[0]) {
+	case '0':
+		ret = cpu_down(cpu->dev.id);
+		if (!ret)
+			kobject_uevent(&dev->kobj, KOBJ_OFFLINE);
+		break;
+	case '1':
+		ret = cpu_up(cpu->dev.id);
+		if (!ret)
+			kobject_uevent(&dev->kobj, KOBJ_ONLINE);
+		break;
+	default:
+		ret = -EINVAL;
+	}
 	cpu_hotplug_driver_unlock();
+
+	if (ret >= 0)
+		ret = count;
 	return ret;
 }
+static DEVICE_ATTR(online, 0644, show_online, store_online);
 
+static void register_cpu_control(struct cpu *cpu)
+{
+	device_create_file(&cpu->dev, &dev_attr_online);
+}
 void unregister_cpu(struct cpu *cpu)
 {
 	int logical_cpu = cpu->dev.id;
 
 	unregister_cpu_under_node(logical_cpu, cpu_to_node(logical_cpu));
+
+	device_remove_file(&cpu->dev, &dev_attr_online);
 
 	device_unregister(&cpu->dev);
 	per_cpu(cpu_sys_devices, logical_cpu) = NULL;
@@ -93,17 +102,11 @@ static DEVICE_ATTR(probe, S_IWUSR, NULL, cpu_probe_store);
 static DEVICE_ATTR(release, S_IWUSR, NULL, cpu_release_store);
 #endif /* CONFIG_ARCH_CPU_PROBE_RELEASE */
 
+#else /* ... !CONFIG_HOTPLUG_CPU */
+static inline void register_cpu_control(struct cpu *cpu)
+{
+}
 #endif /* CONFIG_HOTPLUG_CPU */
-
-struct bus_type cpu_subsys = {
-	.name = "cpu",
-	.dev_name = "cpu",
-#ifdef CONFIG_HOTPLUG_CPU
-	.online = cpu_subsys_online,
-	.offline = cpu_subsys_offline,
-#endif
-};
-EXPORT_SYMBOL_GPL(cpu_subsys);
 
 #ifdef CONFIG_KEXEC
 #include <linux/kexec.h>
@@ -129,17 +132,6 @@ static ssize_t show_crash_notes(struct device *dev, struct device_attribute *att
 	return rc;
 }
 static DEVICE_ATTR(crash_notes, 0400, show_crash_notes, NULL);
-
-static ssize_t show_crash_notes_size(struct device *dev,
-				     struct device_attribute *attr,
-				     char *buf)
-{
-	ssize_t rc;
-
-	rc = sprintf(buf, "%u\n", sizeof(note_buf_t));
-	return rc;
-}
-static DEVICE_ATTR(crash_notes_size, 0400, show_crash_notes_size, NULL);
 #endif
 
 /*
@@ -253,12 +245,12 @@ int register_cpu(struct cpu *cpu, int num)
 	cpu->dev.id = num;
 	cpu->dev.bus = &cpu_subsys;
 	cpu->dev.release = cpu_device_release;
-	cpu->dev.offline_disabled = !cpu->hotpluggable;
-	cpu->dev.offline = !cpu_online(num);
 #ifdef CONFIG_ARCH_HAS_CPU_AUTOPROBE
 	cpu->dev.bus->uevent = arch_cpu_uevent;
 #endif
 	error = device_register(&cpu->dev);
+	if (!error && cpu->hotpluggable)
+		register_cpu_control(cpu);
 	if (!error)
 		per_cpu(cpu_sys_devices, num) = &cpu->dev;
 	if (!error)
@@ -267,9 +259,6 @@ int register_cpu(struct cpu *cpu, int num)
 #ifdef CONFIG_KEXEC
 	if (!error)
 		error = device_create_file(&cpu->dev, &dev_attr_crash_notes);
-	if (!error)
-		error = device_create_file(&cpu->dev,
-					   &dev_attr_crash_notes_size);
 #endif
 	return error;
 }
