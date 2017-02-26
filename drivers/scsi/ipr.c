322023164d61a26678941d2a6416587db22950d2
robcore <robpatershuk@gmail.com>
2017-02-26 07:24:08 -0700

    SCSI-ipr-missing-unlock-before-a-return

--- drivers/scsi/ipr.c
+++ drivers/scsi/ipr.c
@@ -5826,8 +5826,11 @@ static int ipr_queuecommand_lck(struct scsi_cmnd *scsi_cmd,
 		return 0;
 	}
 
-	if (ipr_is_gata(res) && res->sata_port)
-		return ata_sas_queuecmd(scsi_cmd, res->sata_port->ap);
+	if (ipr_is_gata(res) && res->sata_port) {
+		rc = ata_sas_queuecmd(scsi_cmd, res->sata_port->ap);
+		spin_unlock_irqrestore(shost->host_lock, lock_flags);
+		return rc;
+	}
 
 	ipr_cmd = ipr_get_free_ipr_cmnd(ioa_cfg);
 	ioarcb = &ipr_cmd->ioarcb;
