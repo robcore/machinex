dfe8f947f3f578c833f78b7eba2b62b556679d8e
robcore <robpatershuk@gmail.com>
2017-03-03 10:29:26 -0700

    adding in power efficient workqueues

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -280,6 +280,15 @@ EXPORT_SYMBOL(system_unbound_wq);
 struct workqueue_struct *system_freezable_wq __read_mostly;
 EXPORT_SYMBOL(system_freezable_wq);
 
+/* see the comment above the definition of WQ_POWER_EFFICIENT */
+#ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
+static bool wq_power_efficient = true;
+#else
+static bool wq_power_efficient;
+#endif
+
+module_param_named(power_efficient, wq_power_efficient, bool, 0644);
+
 static int worker_thread(void *__worker);
 
 #define CREATE_TRACE_POINTS
@@ -3451,6 +3460,10 @@ struct workqueue_struct *__alloc_workqueue_key(const char *fmt,
 	struct workqueue_struct *wq;
 	struct pool_workqueue *pwq;
 	size_t namelen;
+
+	/* see the comment above the definition of WQ_POWER_EFFICIENT */
+	if ((flags & WQ_POWER_EFFICIENT) && wq_power_efficient)
+		flags |= WQ_UNBOUND;
 
 	/* determine namelen, allocate wq and format name */
 	va_start(args, lock_name);
