514c44fd5d9bccd5f3ce6b1ccf29305429f9850e
robcore <robpatershuk@gmail.com>
2017-03-03 13:42:28 -0700

    rcu-Exit-RCU-extended-QS-on-user-preemption

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3476,6 +3476,21 @@ static void __wake_up_common(wait_queue_head_t *q, unsigned int mode,
 	}
 }
 
+#ifdef CONFIG_RCU_USER_QS
+asmlinkage void __sched schedule_user(void)
+{
+	/*
+	 * If we come here after a random call to set_need_resched(),
+	 * or we have been woken up remotely but the IPI has not yet arrived,
+	 * we haven't yet exited the RCU idle mode. Do it here manually until
+	 * we find a better solution.
+	 */
+	rcu_user_exit();
+	schedule();
+	rcu_user_enter();
+}
+#endif
+
 /**
  * __wake_up - wake up threads blocked on a waitqueue.
  * @q: the waitqueue
