c9f06d77c778267e711532010ada5cba6544ea66
robcore <robpatershuk@gmail.com>
2017-03-03 20:10:25 -0700

    dunno bout that

--- include/asm-generic/unistd.h
+++ include/asm-generic/unistd.h
@@ -1,4 +1,5 @@
 #include <uapi/asm-generic/unistd.h>
+#include <linux/export.h>
 
 /*
  * These are required system calls, we should
@@ -8,4 +9,7 @@
 #if __BITS_PER_LONG == 32
 #define __ARCH_WANT_STAT64
 #define __ARCH_WANT_SYS_LLSEEK
+#define cond_syscall(x) asm(".weak\t" VMLINUX_SYMBOL_STR(x) "\n\t"	\
+			    ".set\t" VMLINUX_SYMBOL_STR(x) ","	\
+			    VMLINUX_SYMBOL_STR(sys_ni_syscall))
 #endif
