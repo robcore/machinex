1a9df461d6985667ae8d84f323cb312db1008b57
robcore <robpatershuk@gmail.com>
2017-03-05 06:22:25 -0700

    gonna try this out before overhaul

--- arch/arm/mach-msm/devices-8064.c
+++ arch/arm/mach-msm/devices-8064.c
@@ -2797,7 +2797,7 @@ struct platform_device apq8064_rpm_log_device = {
 
 static struct msm_pm_init_data_type msm_pm_data = {
 	.use_sync_timer = true,
-	.pc_mode = MSM_PM_PC_NOTZ_L2_EXT,
+	.pc_mode = MSM_PC_TZ_L2_INT,
 };
 
 struct platform_device apq8064_pm_8x60 = {
