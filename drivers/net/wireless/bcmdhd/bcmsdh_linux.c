4eb17e9ae0c4e14720816bcef91338f13e0390b5
robcore <robpatershuk@gmail.com>
2017-03-05 01:03:51 -0700

    dhd/partialresume - adapt find_irq function logic to our irq naming scheme

--- drivers/net/wireless/bcmdhd/bcmsdh_linux.c
+++ drivers/net/wireless/bcmdhd/bcmsdh_linux.c
@@ -394,9 +394,9 @@ void bcmsdh_oob_intr_unregister(bcmsdh_info_t *bcmsdh)
 
 int bcmsdh_get_irq(void)
 {
-	if (!sdhcinfo)
+	if (!bcmsdh_osinfo)
 		return -1;
-	return sdhcinfo->oob_irq;
+	return sdhcinfo->oob_irq_num;
 }
 
 /* Module parameters specific to each host-controller driver */
