121baa33eb9cda7366654bf8bccd9dd32822edb6
robcore <robpatershuk@gmail.com>
2017-03-08 17:51:53 -0700

    Revert-sched-Improve-scalability-via-CPU-buddies

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3958,7 +3958,7 @@ static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preemp
  *
  * The adjacency matrix of the resulting graph is given by:
  *
- *             log_2 n     
+ *             log_2 n
  *   A_i,j = \Union     (i % 2^k == 0) && i / 2^(k+1) == j / 2^(k+1)  (6)
  *             k = 0
  *
@@ -4004,7 +4004,7 @@ static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preemp
  *
  * [XXX write more on how we solve this.. _after_ merging pjt's patches that
  *      rewrite all of this once again.]
- */ 
+ */
 
 static unsigned long __read_mostly max_load_balance_interval = HZ/10;
 
@@ -5506,6 +5506,8 @@ static int active_load_balance_cpu_stop(void *data)
 	int target_cpu = busiest_rq->push_cpu;
 	struct rq *target_rq = cpu_rq(target_cpu);
 	struct sched_domain *sd;
+	struct sched_group *sg;
+	int i;
 
 	raw_spin_lock_irq(&busiest_rq->lock);
 
