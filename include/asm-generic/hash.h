4391c843764bcf6f03d05b6365a7231e369206ea
robcore <robpatershuk@gmail.com>
2017-03-10 02:32:47 -0700

    REVERT - lib-introduce-arch-optimized-hash-library

--- include/asm-generic/hash.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ASM_GENERIC_HASH_H
-#define __ASM_GENERIC_HASH_H
-
-struct arch_hash_ops;
-static inline void setup_arch_fast_hash(struct arch_hash_ops *ops)
-{
-}
-
-#endif /* __ASM_GENERIC_HASH_H */
