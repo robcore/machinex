a71172e2b3580c034952217bab4492a71fa4c0b0
robcore <robpatershuk@gmail.com>
2017-03-12 19:55:07 -0600

    fixup legacy seq

--- include/linux/genhd.h
+++ include/linux/genhd.h
@@ -3,7 +3,7 @@
 
 /*
  * 	genhd.h Copyright (C) 1992 Drew Eckhardt
- *	Generic hard disk header file by  
+ *	Generic hard disk header file by
  * 		Drew Eckhardt
  *
  *		<drew@colorado.edu>
@@ -103,7 +103,7 @@ struct hd_struct {
 	 * can be non-atomic on 32bit machines with 64bit sector_t.
 	 */
 	sector_t nr_sects;
-	seqcount_t nr_sects_seq;
+	legacy_seqcount_t nr_sects_seq;
 	sector_t alignment_offset;
 	unsigned int discard_alignment;
 	struct device __dev;
@@ -497,7 +497,7 @@ struct bsd_disklabel {
 	__s16	d_type;			/* drive type */
 	__s16	d_subtype;		/* controller/d_type specific */
 	char	d_typename[16];		/* type name, e.g. "eagle" */
-	char	d_packname[16];			/* pack identifier */ 
+	char	d_packname[16];			/* pack identifier */
 	__u32	d_secsize;		/* # of bytes per sector */
 	__u32	d_nsectors;		/* # of data sectors per track */
 	__u32	d_ntracks;		/* # of tracks per cylinder */
@@ -679,9 +679,9 @@ static inline sector_t part_nr_sects_read(struct hd_struct *part)
 	sector_t nr_sects;
 	unsigned seq;
 	do {
-		seq = read_seqcount_begin(&part->nr_sects_seq);
+		seq = read_legacy_seqcount_begin(&part->nr_sects_seq);
 		nr_sects = part->nr_sects;
-	} while (read_seqcount_retry(&part->nr_sects_seq, seq));
+	} while (read_legacy_seqcount_retry(&part->nr_sects_seq, seq));
 	return nr_sects;
 #elif BITS_PER_LONG==32 && defined(CONFIG_LBDAF) && defined(CONFIG_PREEMPT)
 	sector_t nr_sects;
@@ -703,9 +703,9 @@ static inline sector_t part_nr_sects_read(struct hd_struct *part)
 static inline void part_nr_sects_write(struct hd_struct *part, sector_t size)
 {
 #if BITS_PER_LONG==32 && defined(CONFIG_LBDAF) && defined(CONFIG_SMP)
-	write_seqcount_begin(&part->nr_sects_seq);
+	write_legacy_seqcount_begin(&part->nr_sects_seq);
 	part->nr_sects = size;
-	write_seqcount_end(&part->nr_sects_seq);
+	write_legacy_seqcount_end(&part->nr_sects_seq);
 #elif BITS_PER_LONG==32 && defined(CONFIG_LBDAF) && defined(CONFIG_PREEMPT)
 	preempt_disable();
 	part->nr_sects = size;
