9b14df0f73cc467b74083fbda94a565187600d5b
robcore <robpatershuk@gmail.com>
2017-03-14 22:03:59 -0600

    perf_pmu_migrate_context - fix missing arg

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7218,7 +7218,7 @@ void perf_pmu_migrate_context(struct pmu *pmu, int src_cpu, int dst_cpu)
 	mutex_lock_double(&src_ctx->mutex, &dst_ctx->mutex);
 	list_for_each_entry_safe(event, tmp, &src_ctx->event_list,
 				 event_entry) {
-		perf_remove_from_context(event);
+		perf_remove_from_context(event, false);
 		unaccount_event_cpu(event, src_cpu);
 		put_ctx(src_ctx);
 		list_add(&event->event_entry, &events);
