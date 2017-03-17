121e3c61e5ef079b593d9cd4332fe0972c08df28
robcore <robpatershuk@gmail.com>
2017-03-17 00:44:41 -0600

    remove "unlikely" from mmc work/business taking place.  the compiler shouldnt assume a common occurance for us is unlikely

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -6941,7 +6941,7 @@ msmsdcc_runtime_suspend(struct device *dev)
 		 */
 		pm_runtime_get_noresume(dev);
 		/* If there is pending detect work abort runtime suspend */
-		if (unlikely(work_busy(&mmc->detect.work)))
+		if (work_busy(&mmc->detect.work))
 			rc = -EAGAIN;
 		else
 			rc = mmc_suspend_host(mmc);
