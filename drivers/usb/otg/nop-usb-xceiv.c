363f353c97b527a8861cf14df0b6e18770bf3e35
robcore <robpatershuk@gmail.com>
2017-03-18 10:01:43 -0600

    fuck that

--- drivers/usb/otg/nop-usb-xceiv.c
+++ drivers/usb/otg/nop-usb-xceiv.c
@@ -97,14 +97,15 @@ static int nop_usb_xceiv_probe(struct platform_device *pdev)
 	struct nop_usb_xceiv	*nop;
 	int err;
 
-	nop = devm_kzalloc(&pdev->dev, sizeof(*nop), GFP_KERNEL);
+	nop = kzalloc(sizeof *nop, GFP_KERNEL);
 	if (!nop)
 		return -ENOMEM;
 
-	nop->phy.otg = devm_kzalloc(&pdev->dev, sizeof(*nop->phy.otg),
-							GFP_KERNEL);
-	if (!nop->phy.otg)
+	nop->phy.otg = kzalloc(sizeof *nop->phy.otg, GFP_KERNEL);
+	if (!nop->phy.otg) {
+		kfree(nop);
 		return -ENOMEM;
+	}
 
 	nop->dev		= &pdev->dev;
 	nop->phy.dev		= nop->dev;
@@ -120,7 +121,7 @@ static int nop_usb_xceiv_probe(struct platform_device *pdev)
 	if (err) {
 		dev_err(&pdev->dev, "can't register transceiver, err: %d\n",
 			err);
-		return err;
+		goto exit;
 	}
 
 	platform_set_drvdata(pdev, nop);
@@ -128,6 +129,10 @@ static int nop_usb_xceiv_probe(struct platform_device *pdev)
 	ATOMIC_INIT_NOTIFIER_HEAD(&nop->phy.notifier);
 
 	return 0;
+exit:
+	kfree(nop->phy.otg);
+	kfree(nop);
+	return err;
 }
 
 static int nop_usb_xceiv_remove(struct platform_device *pdev)
@@ -137,6 +142,8 @@ static int nop_usb_xceiv_remove(struct platform_device *pdev)
 	usb_set_transceiver(NULL);
 
 	platform_set_drvdata(pdev, NULL);
+	kfree(nop->phy.otg);
+	kfree(nop);
 
 	return 0;
 }
