36560528bbaddcd75362bf637860a1af0b3ca1c1
robcore <robpatershuk@gmail.com>
2017-03-19 14:20:29 -0600

    adapt task_dead method to our out-of-whack, half upstreamed tree

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1915,8 +1915,8 @@ void sched_fork(struct task_struct *p)
 		p->prio = p->normal_prio = __normal_prio(p);
 		set_load_weight(p);
 
-		if (prev->sched_class->task_dead)
-			prev->sched_class->task_dead(prev);
+		if (p->sched_class->task_dead)
+			p->sched_class->task_dead(p);
 
 		/*
 		 * We don't need the reset flag anymore after the fork. It has
