3cd391a9db59d6a8dc1456011edeb8723d18b99a
robcore <robpatershuk@gmail.com>
2017-03-20 10:36:40 -0600

    sched/deadline: Add SCHED_DEADLINE SMP-related data structures & logic (rob note: fixed this commit as it was designed to free the dlo mask if rdo needed to be released which is clearly incorrect)

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1969,6 +1969,7 @@ int sched_fork(struct task_struct *p)
 #endif
 #ifdef CONFIG_SMP
 	plist_node_init(&p->pushable_tasks, MAX_PRIO);
+	RB_CLEAR_NODE(&p->pushable_dl_tasks);
 #endif
 
 	put_cpu();
@@ -5160,6 +5161,7 @@ static void free_rootdomain(struct rcu_head *rcu)
 	struct root_domain *rd = container_of(rcu, struct root_domain, rcu);
 
 	cpupri_cleanup(&rd->cpupri);
+	free_cpumask_var(rd->dlo_mask);
 	free_cpumask_var(rd->rto_mask);
 	free_cpumask_var(rd->online);
 	free_cpumask_var(rd->span);
@@ -5211,6 +5213,8 @@ static int init_rootdomain(struct root_domain *rd)
 		goto out;
 	if (!zalloc_cpumask_var(&rd->online, GFP_KERNEL))
 		goto free_span;
+	if (!zalloc_cpumask_var(&rd->dlo_mask, GFP_KERNEL))
+		goto free_dlo_mask;
 	if (!zalloc_cpumask_var(&rd->rto_mask, GFP_KERNEL))
 		goto free_online;
 
@@ -5220,6 +5224,8 @@ static int init_rootdomain(struct root_domain *rd)
 
 free_rto_mask:
 	free_cpumask_var(rd->rto_mask);
+free_dlo_mask:
+	free_cpumask_var(rd->dlo_mask);
 free_online:
 	free_cpumask_var(rd->online);
 free_span:
@@ -6497,6 +6503,7 @@ void __init sched_init_smp(void)
 	free_cpumask_var(non_isolated_cpus);
 
 	init_sched_rt_class();
+	init_sched_dl_class();
 }
 #else
 void __init sched_init_smp(void)
