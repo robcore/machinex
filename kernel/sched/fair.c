dbcfe72c9ef234fd17d881f663e00b7f7137c801
robcore <robpatershuk@gmail.com>
2017-03-20 22:01:57 -0600

    fixup some of the compatibility with the older/newer commit model

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5174,7 +5174,7 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 	unsigned long busiest_load = 0, busiest_power = 1;
 	int i;
 
-	for_each_cpu_and(i, sched_group_cpus(group), cpus) {
+	for_each_cpu_and(i, sched_group_cpus(group), env->cpus) {
 		unsigned long power = power_of(i);
 		unsigned long capacity = DIV_ROUND_CLOSEST(power,
 							   SCHED_POWER_SCALE);
