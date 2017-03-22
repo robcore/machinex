bffaaaba4c04a6a5b7e7a609fbd2e7fd2949012e
robcore <robpatershuk@gmail.com>
2017-03-22 08:39:01 -0600

    -sched-Preserve-the-nice-level-over-sched_setschedule.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3501,7 +3501,8 @@ int sched_setscheduler(struct task_struct *p, int policy,
 {
 	struct sched_attr attr = {
 		.sched_policy   = policy,
-		.sched_priority = param->sched_priority
+		.sched_priority = param->sched_priority,
+		.sched_nice	= PRIO_TO_NICE(p->static_prio),
 	};
 	return __sched_setscheduler(p, &attr, true);
 }
@@ -3531,7 +3532,8 @@ int sched_setscheduler_nocheck(struct task_struct *p, int policy,
 {
 	struct sched_attr attr = {
 		.sched_policy   = policy,
-		.sched_priority = param->sched_priority
+		.sched_priority = param->sched_priority,
+		.sched_nice	= PRIO_TO_NICE(p->static_prio),
 	};
 	return __sched_setscheduler(p, &attr, false);
 }
