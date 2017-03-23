bc5f9586c6ff3a412f4aaf869cd3deb256dc22f2
robcore <robpatershuk@gmail.com>
2017-03-23 10:43:37 -0600

    kvm-Move-guest-entry-exit-APIs-to-context_tracking

--- include/linux/context_tracking.h
+++ include/linux/context_tracking.h
@@ -21,6 +21,26 @@ struct context_tracking {
 	} state;
 };
 
+static inline void __guest_enter(void)
+{
+	/*
+	 * This is running in ioctl context so we can avoid
+	 * the call to vtime_account() with its unnecessary idle check.
+	 */
+	vtime_account_system(current);
+	current->flags |= PF_VCPU;
+}
+
+static inline void __guest_exit(void)
+{
+	/*
+	 * This is running in ioctl context so we can avoid
+	 * the call to vtime_account() with its unnecessary idle check.
+	 */
+	vtime_account_system(current);
+	current->flags &= ~PF_VCPU;
+}
+
 #ifdef CONFIG_CONTEXT_TRACKING
 extern struct static_key context_tracking_enabled;
 DECLARE_PER_CPU(struct context_tracking, context_tracking);
@@ -39,6 +59,9 @@ extern void context_tracking_cpu_set(int cpu);
 
 extern void user_enter(void);
 extern void user_exit(void);
+
+extern void guest_enter(void);
+extern void guest_exit(void);
 
 static inline enum ctx_state exception_enter(void)
 {
@@ -62,6 +85,17 @@ extern void context_tracking_task_switch(struct task_struct *prev,
 static inline bool context_tracking_in_user(void) { return false; }
 static inline void user_enter(void) { }
 static inline void user_exit(void) { }
+
+static inline void guest_enter(void)
+{
+	__guest_enter();
+}
+
+static inline void guest_exit(void)
+{
+	__guest_exit();
+}
+
 static inline enum ctx_state exception_enter(void) { return 0; }
 static inline void exception_exit(enum ctx_state prev_ctx) { }
 static inline void context_tracking_task_switch(struct task_struct *prev,
