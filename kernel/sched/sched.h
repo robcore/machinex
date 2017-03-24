9bd3e27c7ea697e9a500c3b7dac21ddc38e2da93
robcore <robpatershuk@gmail.com>
2017-03-24 10:43:21 -0600

    sched: Put rq sched_avg under CONFIG_FAIR_GROUP_SCHED

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -549,6 +549,8 @@ struct rq {
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	/* list of leaf cfs_rq on this cpu: */
 	struct list_head leaf_cfs_rq_list;
+
+	struct sched_avg avg;
 #endif /* CONFIG_FAIR_GROUP_SCHED */
 
 	/*
@@ -648,8 +650,6 @@ struct rq {
 #ifdef CONFIG_SMP
 	struct llist_head wake_list;
 #endif
-
-	struct sched_avg avg;
 };
 
 static inline u64 rq_clock(struct rq *rq)
