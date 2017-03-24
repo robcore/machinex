79d27230d82b729c867d418432aad59250ce5f00
robcore <robpatershuk@gmail.com>
2017-03-23 23:21:47 -0600

    sched/idle, ARM: Remove redundant cpuidle_idle_call()

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -31,7 +31,6 @@
 #include <linux/uaccess.h>
 #include <linux/random.h>
 #include <linux/hw_breakpoint.h>
-#include <linux/cpuidle.h>
 #include <linux/console.h>
 
 #include <asm/cacheflush.h>
@@ -194,7 +193,11 @@ EXPORT_SYMBOL_GPL(cpu_idle_wait);
 extern void arch_idle(void);
 void (*arm_pm_idle)(void) = arch_idle;
 
-static void default_idle(void)
+/*
+ * Called from the core idle loop.
+ */
+
+void arch_cpu_idle(void)
 {
 	if (arm_pm_idle)
 		arm_pm_idle();
@@ -227,15 +230,6 @@ void arch_cpu_idle_dead(void)
 	cpu_die();
 }
 #endif
-
-/*
- * Called from the core idle loop.
- */
-void arch_cpu_idle(void)
-{
-	if (cpuidle_idle_call())
-		default_idle();
-}
 
 static char reboot_mode = 'h';
 
