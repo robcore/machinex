dab5911d141a1b7493e7c677939f694f0ac8cee0
robcore <robpatershuk@gmail.com>
2017-03-24 02:42:20 -0600

    sched-Fix-hotplug-task-migration

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3885,8 +3885,7 @@ again:
 	if (!cfs_rq->h_nr_running)
 		goto idle;
 
-	if (prev)
-		prev->sched_class->put_prev_task(rq, prev);
+	put_prev_task(rq, prev);
 
 	do {
 		se = pick_next_entity(cfs_rq, NULL);
