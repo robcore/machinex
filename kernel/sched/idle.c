0826d99cee55ed36e692f0ad61c708e400be7e6c
robcore <robpatershuk@gmail.com>
2017-03-23 23:56:55 -0600

    fix up my idle.c

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -75,8 +75,8 @@ static void cpu_idle_loop(void)
 			check_pgt_cache();
 			rmb();
 
-			if (cpu_is_offline(smp_processor_id()))
-				arch_cpu_idle_dead();
+			//if (cpu_is_offline(smp_processor_id()))
+				//arch_cpu_idle_dead();
 
 			local_irq_disable();
 			arch_cpu_idle_enter();
@@ -111,8 +111,8 @@ static void cpu_idle_loop(void)
 		}
 		tick_nohz_idle_exit();
 		schedule_preempt_disabled();
-		//if (cpu_is_offline(cpu))
-			//arch_cpu_idle_dead();
+		if (cpu_is_offline(cpu))
+			arch_cpu_idle_dead();
 	}
 }
 
