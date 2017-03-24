58d021f471978a8592643ea60594066b587b5f21
robcore <robpatershuk@gmail.com>
2017-03-23 23:57:15 -0600

    bring back "redundant" cpu idle call due to a panic

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -31,6 +31,7 @@
 #include <linux/uaccess.h>
 #include <linux/random.h>
 #include <linux/hw_breakpoint.h>
+#include <linux/cpuidle.h>
 #include <linux/console.h>
 
 #include <asm/cacheflush.h>
@@ -193,11 +194,7 @@ EXPORT_SYMBOL_GPL(cpu_idle_wait);
 extern void arch_idle(void);
 void (*arm_pm_idle)(void) = arch_idle;
 
-/*
- * Called from the core idle loop.
- */
-
-void arch_cpu_idle(void)
+static void default_idle(void)
 {
 	if (arm_pm_idle)
 		arm_pm_idle();
@@ -230,6 +227,15 @@ void arch_cpu_idle_dead(void)
 	cpu_die();
 }
 #endif
+
+/*
+ * Called from the core idle loop.
+ */
+void arch_cpu_idle(void)
+{
+	if (cpuidle_idle_call())
+		default_idle();
+}
 
 static char reboot_mode = 'h';
 
