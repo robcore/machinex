158214d831ba55281be15fca3a4fefe8ab286a3e
robcore <robpatershuk@gmail.com>
2017-03-25 21:15:34 -0600

    rtmutex - fix deadlock detector for realX2

--- kernel/locking/rtmutex.c
+++ kernel/locking/rtmutex.c
@@ -892,7 +892,7 @@ static int task_blocks_on_rt_mutex(struct rt_mutex *lock,
 	 * which is wrong, as the other waiter is not in a deadlock
 	 * situation.
 	 */
-	if (owner == task)
+	if ((detect_deadlock && owner == task) || (owner == task))
 		return -EDEADLK;
 
 	raw_spin_lock_irqsave(&task->pi_lock, flags);
