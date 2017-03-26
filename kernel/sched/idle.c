656feb1dcb45006a37c18c78d35d056f5571599e
robcore <robpatershuk@gmail.com>
2017-03-26 16:34:24 -0600

    sched-idle-Clear-polling-before-descheduling-the-idle

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -65,12 +65,23 @@ void __weak arch_cpu_idle(void)
 
 /*
  * Generic idle loop implementation
+ * Called with polling cleared.
  */
 static void cpu_idle_loop(void)
 {
 	int cpu = smp_processor_id();
 
 	while (1) {
+		/*
+		 * If the arch has a polling bit, we maintain an invariant:
+		 *
+		 * Our polling bit is clear if we're not scheduled (i.e. if
+		 * rq->curr != rq->idle).  This means that, if rq->idle has
+		 * the polling bit set, then setting need_resched is
+		 * guaranteed to cause the cpu to reschedule.
+		 */
+
+		__current_set_polling();
 		tick_nohz_idle_enter();
 
 		while (!need_resched()) {
@@ -109,6 +120,15 @@ static void cpu_idle_loop(void)
 			arch_cpu_idle_exit();
 		}
 		tick_nohz_idle_exit();
+		__current_clr_polling();
+
+		/*
+		 * We promise to reschedule if need_resched is set while
+		 * polling is set.  That means that clearing polling
+		 * needs to be visible before rescheduling.
+		 */
+		smp_mb__after_atomic();
+
 		schedule_preempt_disabled();
 		if (cpu_is_offline(cpu))
 			arch_cpu_idle_dead();
@@ -133,7 +153,6 @@ void cpu_startup_entry(enum cpuhp_state state)
 	 */
 	boot_init_stack_canary();
 #endif
-	__current_set_polling();
 	arch_cpu_idle_prepare();
 	cpu_idle_loop();
 }
