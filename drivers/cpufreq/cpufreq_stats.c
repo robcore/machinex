b18884a47e3703592eb43a990e59bfc56163a8c9
robcore <robpatershuk@gmail.com>
2017-03-27 13:15:53 -0600

    reverted cpupower commit for now, need a prereq

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -23,7 +23,6 @@
 #include <linux/sort.h>
 #include <linux/err.h>
 #include <asm/cputime.h>
-#include <linux/sched.h>
 
 static spinlock_t cpufreq_stats_lock;
 
@@ -131,24 +130,6 @@ static int get_index_all_cpufreq_stat(struct all_cpufreq_stats *all_stat,
 	}
 	return -1;
 }
-
-void acct_update_power(struct task_struct *task, cputime_t cputime) {
-	struct cpufreq_power_stats *powerstats;
-	struct cpufreq_stats *stats;
-	unsigned int cpu_num, curr;
-
-	if (!task)
-		return;
-	cpu_num = task_cpu(task);
-	powerstats = per_cpu(cpufreq_power_stats, cpu_num);
-	stats = per_cpu(cpufreq_stats_table, cpu_num);
-	if (!powerstats || !stats)
-		return;
-
-	curr = powerstats->curr[stats->last_index];
-	task->cpu_power += curr * cputime_to_usecs(cputime);
-}
-EXPORT_SYMBOL_GPL(acct_update_power);
 
 static ssize_t show_all_time_in_state(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
