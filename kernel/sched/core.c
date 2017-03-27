44e5945f498ac0338688f7d95bd9e483fb8dd02f
robcore <robpatershuk@gmail.com>
2017-03-26 21:00:47 -0600

    nope

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2015,6 +2015,8 @@ int sched_fork(struct task_struct *p)
 
 		p->prio = p->normal_prio = __normal_prio(p);
 		set_load_weight(p);
+		if (prev->sched_class->task_dead)
+			prev->sched_class->task_dead(prev);
 
 		/*
 		 * We don't need the reset flag anymore after the fork. It has
