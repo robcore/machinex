4bd3f7848f7c8b1f8a5c2b6f820105b13bfc867f
robcore <robpatershuk@gmail.com>
2017-03-28 08:35:09 -0600

    reverted

--- include/asm-generic/atomic.h
+++ include/asm-generic/atomic.h
@@ -16,7 +16,6 @@
 #define __ASM_GENERIC_ATOMIC_H
 
 #include <asm/cmpxchg.h>
-#include <asm/barrier.h>
 
 #ifdef CONFIG_SMP
 /* Force people to define core atomics */
@@ -182,6 +181,12 @@ static inline void atomic_set_mask(unsigned int mask, atomic_t *v)
 	raw_local_irq_restore(flags);
 }
 #endif
+
+/* Assume that atomic operations are already serializing */
+#define smp_mb__before_atomic_dec()	barrier()
+#define smp_mb__after_atomic_dec()	barrier()
+#define smp_mb__before_atomic_inc()	barrier()
+#define smp_mb__after_atomic_inc()	barrier()
 
 #endif /* __KERNEL__ */
 #endif /* __ASM_GENERIC_ATOMIC_H */
