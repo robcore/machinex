08e46c2bb65bebd74948235da1e434e988cb0b1a
robcore <robpatershuk@gmail.com>
2017-03-27 23:43:20 -0600

    REVERT perf

--- kernel/trace/trace_event_perf.c
+++ kernel/trace/trace_event_perf.c
@@ -26,25 +26,9 @@ static int perf_trace_event_perm(struct ftrace_event_call *tp_event,
 				 struct perf_event *p_event)
 {
 	/* The ftrace function trace is allowed only for root. */
-	if (ftrace_event_is_function(tp_event)) {
-		if (perf_paranoid_tracepoint_raw() && !capable(CAP_SYS_ADMIN))
-			return -EPERM;
-
-		/*
-		 * We don't allow user space callchains for  function trace
-		 * event, due to issues with page faults while tracing page
-		 * fault handler and its overall trickiness nature.
-		 */
-		if (!p_event->attr.exclude_callchain_user)
-			return -EINVAL;
-
-		/*
-		 * Same reason to disable user stack dump as for user space
-		 * callchains above.
-		 */
-		if (p_event->attr.sample_type & PERF_SAMPLE_STACK_USER)
-			return -EINVAL;
-	}
+	if (ftrace_event_is_function(tp_event) &&
+	    perf_paranoid_tracepoint_raw() && !capable(CAP_SYS_ADMIN))
+		return -EPERM;
 
 	/* No tracing, just counting, so no obvious leak */
 	if (!(p_event->attr.sample_type & PERF_SAMPLE_RAW))
