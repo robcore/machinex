800b11b4828cd0b544a078a5658956e32b0ebd75
robcore <robpatershuk@gmail.com>
2017-03-28 00:12:32 -0600

    mm, mempolicy: remove per-process flag

--- mm/slab.c
+++ mm/slab.c
@@ -3407,7 +3407,7 @@ __do_cache_alloc(struct kmem_cache *cache, gfp_t flags)
 {
 	void *objp;
 
-	if (unlikely((current->flags & PF_MEMPOLICY) || cpuset_do_slab_mem_spread())) {
+	if (current->mempolicy || unlikely(current->flags & PF_SPREAD_SLAB)) {
 		objp = alternate_node_alloc(cache, flags);
 		if (objp)
 			goto out;
