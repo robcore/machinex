1d026de8c07e7ad7408cee025f9c780f72da361c
robcore <robpatershuk@gmail.com>
2017-03-31 14:30:22 -0600

    fix hrtimer hotplug race

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -21,6 +21,7 @@
 #include <linux/suspend.h>
 #include <linux/lockdep.h>
 #include <linux/ratelimit.h>
+#include <linux/tick.h>
 
 #include "smpboot.h"
 
@@ -376,6 +377,7 @@ static int __ref _cpu_down(unsigned int cpu, int tasks_frozen)
 	while (!idle_cpu(cpu))
 		cpu_relax();
 
+	hotplug_cpu__broadcast_tick_pull(cpu);
 	/* This actually kills the CPU. */
 	__cpu_die(cpu);
 
