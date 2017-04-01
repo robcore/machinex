83ba9cfe33aca077b38419cd3e0984723dbac7e4
robcore <robpatershuk@gmail.com>
2017-04-01 15:37:31 -0600

    initializing tkr just in case

--- arch/arm/kernel/update_vsyscall_arm.c
+++ arch/arm/kernel/update_vsyscall_arm.c
@@ -63,7 +63,7 @@ update_vsyscall_old(struct timespec *ts, struct timespec *wtm,
 	struct kernel_wtm_t *dgwtm = (struct kernel_wtm_t *)(vectors +
 		ARM_VSYSCALL_TIMER_WTM_TV_SEC);
 	struct timekeeper *tk = tk;
-	struct tk_read_base *tkr;
+	struct tk_read_base *tkr = tkr;
 
 	write_legacy_seqlock_irqsave(&kuh_time_lock, flags);
 	*seqnum = kuh_time_lock.sequence;
