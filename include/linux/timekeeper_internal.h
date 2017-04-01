534bcc2fd0374f28ed21bd09c992071fa595b8eb
robcore <robpatershuk@gmail.com>
2017-04-01 11:33:49 -0600

    clocksource - get rid of cycle last

--- include/linux/timekeeper_internal.h
+++ include/linux/timekeeper_internal.h
@@ -32,6 +32,8 @@ extern int __do_adjtimex(struct timex *, struct timespec *, s32 *);
 struct timekeeper {
 	/* Current clocksource used for timekeeping. */
 	struct clocksource	*clock;
+	/* Last cycle value */
+	cycle_t			cycle_last;
 	/* NTP adjusted clock multiplier */
 	u32			mult;
 	/* The shift value of the current clocksource. */
@@ -66,8 +68,6 @@ struct timekeeper {
 	struct timespec64	raw_time;
 	/* Number of clock cycles in one NTP interval. */
 	cycle_t			cycle_interval;
-	/* Last cycle value (also stored in clock->cycle_last) */
-	cycle_t			cycle_last;
 	/* Number of clock shifted nano seconds in one NTP interval. */
 	u64			xtime_interval;
 	/* shifted nano seconds left over when rounding cycle_interval */
@@ -95,7 +95,8 @@ extern void update_vsyscall_tz(void);
 #elif defined(CONFIG_GENERIC_TIME_VSYSCALL_OLD)
 
 extern void update_vsyscall_old(struct timespec *ts, struct timespec *wtm,
-				struct clocksource *c, u32 mult);
+				struct clocksource *c, u32 mult,
+				cycles_t cycle_last);
 extern void update_vsyscall_tz(void);
 
 #else
