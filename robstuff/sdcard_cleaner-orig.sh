0afea57036f52b0d16296c6602ebf9a005558698
robcore <robpatershuk@gmail.com>
2017-04-02 17:38:18 -0600

    did some cleaning

---
+++ robstuff/sdcard_cleaner-orig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+adb shell rm /external_sd/machinex-Mark93-P20.zip
+adb shell rm /external_sd/machinex-Mark93-P21.zip
+adb shell rm /external_sd/machinex-Mark93-P23.zip
+adb shell rm /external_sd/machinex-Mark93-P24.zip
+adb shell rm /external_sd/machinex-Mark93-P25.zip
+adb shell rm /external_sd/machinex-Mark93-P26.zip
+
