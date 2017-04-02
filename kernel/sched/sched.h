6fd0cf6977bc3fbd1b2998b52609cfa3b57f962b
robcore <robpatershuk@gmail.com>
2017-04-01 20:21:48 -0600

    sched: Match declaration with definition

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -680,7 +680,7 @@ static inline int cpu_of(struct rq *rq)
 #endif
 }
 
-DECLARE_PER_CPU(struct rq, runqueues);
+DECLARE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
 
 #define cpu_rq(cpu)		(&per_cpu(runqueues, (cpu)))
 #define this_rq()		this_cpu_ptr(&runqueues)
