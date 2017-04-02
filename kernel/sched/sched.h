2a64dafbc23597e21f2d0a86ae9653399ccce9a2
robcore <robpatershuk@gmail.com>
2017-04-02 11:15:56 -0600

    already had task on rq migrating

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -23,7 +23,6 @@ struct cpuidle_state;
 
 /* task_struct::on_rq states: */
 #define TASK_ON_RQ_QUEUED	1
-#define TASK_ON_RQ_MIGRATING	2
 
 extern __read_mostly int scheduler_running;
 
@@ -1043,11 +1042,6 @@ static inline int task_running(struct rq *rq, struct task_struct *p)
 static inline int task_on_rq_queued(struct task_struct *p)
 {
 	return p->on_rq == TASK_ON_RQ_QUEUED;
-}
-
-static inline int task_on_rq_migrating(struct task_struct *p)
-{
-	return p->on_rq == TASK_ON_RQ_MIGRATING;
 }
 
 static inline int task_on_rq_migrating(struct task_struct *p)
