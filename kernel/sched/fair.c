fe551e4f66f39ee694c0d5181f35bba7cf780b86
robcore <robpatershuk@gmail.com>
2017-04-03 17:27:13 -0600

    sched-Compute-cpu-capacity-available-at-current-freq.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4441,6 +4441,17 @@ static unsigned long source_load(int cpu, int type)
 	return min(rq->cpu_load[type-1], total);
 }
 
+/*
+ * Returns the current capacity of cpu after applying both
+ * cpu and freq scaling.
+ */
+static unsigned long capacity_curr_of(int cpu)
+{
+	return cpu_rq(cpu)->cpu_capacity_orig *
+	       arch_scale_freq_capacity(NULL, cpu)
+	       >> SCHED_CAPACITY_SHIFT;
+}
+
 static inline bool energy_aware(void)
 {
 	return sched_feat(ENERGY_AWARE);
