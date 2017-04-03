1b1c27d749c42e4b4ac1340198568ad3c6407088
robcore <robpatershuk@gmail.com>
2017-04-03 15:48:59 -0600

    sched-core-Delete-PF_EXITING-checks-from-cpu_cgroup

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8330,14 +8330,6 @@ static void
 cpu_cgroup_exit(struct cgroup *cgrp, struct cgroup *old_cgrp,
 		struct task_struct *task)
 {
-	/*
-	 * cgroup_exit() is called in the copy_process() failure path.
-	 * Ignore this case since the task hasn't ran yet, this avoids
-	 * trying to poke a half freed task state from generic code.
-	 */
-	if (!(task->flags & PF_EXITING))
-		return;
-
 	sched_move_task(task);
 }
 
