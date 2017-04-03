e223ed370bb97b036fc3999838cabb3e41b10ba4
robcore <robpatershuk@gmail.com>
2017-04-02 22:14:11 -0600

    REVERT - restart handler dont need it

--- include/linux/reboot.h
+++ include/linux/reboot.h
@@ -13,9 +13,6 @@
 extern int register_reboot_notifier(struct notifier_block *);
 extern int unregister_reboot_notifier(struct notifier_block *);
 
-extern int register_restart_handler(struct notifier_block *);
-extern int unregister_restart_handler(struct notifier_block *);
-extern void do_kernel_restart(char *cmd);
 
 /*
  * Architecture-specific implementations of sys_reboot commands.
