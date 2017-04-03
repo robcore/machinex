09424748b4b2da9d0fd8df9aed1238cc0c358c8c
robcore <robpatershuk@gmail.com>
2017-04-03 15:24:58 -0600

    sched-fair-Init-cfs_rq-s-sched_entity-load-average

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1315,7 +1315,7 @@ extern void init_dl_task_timer(struct sched_dl_entity *dl_se);
 
 unsigned long to_ratio(u64 period, u64 runtime);
 
-extern void init_task_runnable_average(struct task_struct *p);
+extern void init_entity_runnable_average(struct sched_entity *se);
 
 #ifdef CONFIG_PARAVIRT
 static inline u64 steal_ticks(u64 steal)
