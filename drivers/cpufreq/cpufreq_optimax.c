55bfb8888a5abb26f7e565b386be6cf34ab36c6e
robcore <robpatershuk@gmail.com>
2017-04-03 15:40:38 -0600

    two updates - govs - no more using set_cpus_allowed, and dsi_esc_clk? dont just print nicely, BUG_ON.  PANIC THAT SHIT YO

--- drivers/cpufreq/cpufreq_optimax.c
+++ drivers/cpufreq/cpufreq_optimax.c
@@ -1411,8 +1411,6 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			if (dbs_tuners_ins.ignore_nice)
 				j_dbs_info->prev_cpu_nice =
 						kcpustat_cpu(j).cpustat[CPUTIME_NICE];
-			set_cpus_allowed(j_dbs_info->sync_thread,
-					 *cpumask_of(j));
 			if (!dbs_tuners_ins.powersave_bias)
 				atomic_set(&j_dbs_info->sync_enabled, 1);
 		}
--- drivers/video/msm/msm_dss_io_8960.c
+++ drivers/video/msm/msm_dss_io_8960.c
@@ -372,9 +372,9 @@ void mipi_dsi_configure_dividers(int fps)
 {
 	struct dsiphy_pll_divider_config *dividers;
 	u32 tmp;
-     
+
 	dividers = &pll_divider_config;
-   
+
 	if(fps == 60) {
 		tmp = MIPI_INP(MIPI_DSI_BASE + 0x20C);
 		tmp &= ~0x3f;
@@ -384,7 +384,7 @@ void mipi_dsi_configure_dividers(int fps)
 		tmp = MIPI_INP(MIPI_DSI_BASE + 0x22C);
 		tmp &= ~0x10;
 		MIPI_OUTP(MIPI_DSI_BASE + 0x22C, tmp);
-			
+
 		wmb();
 	} else if(fps == 45) {
 		tmp = MIPI_INP(MIPI_DSI_BASE + 0x20C);
@@ -395,7 +395,7 @@ void mipi_dsi_configure_dividers(int fps)
 		tmp = MIPI_INP(MIPI_DSI_BASE + 0x22C);
 		tmp &= ~0x10;
 		MIPI_OUTP(MIPI_DSI_BASE + 0x22C, tmp);
-					
+
 		 wmb();
 	} else if(fps == 30) {
 		tmp = MIPI_INP(MIPI_DSI_BASE + 0x22C);
@@ -414,19 +414,19 @@ void mipi_dsi_configure_dividers(int fps)
 	}
 }
 #endif
-static int current_fps = 60; 
-void mipi_dsi_configure_dividers(int fps) 
+static int current_fps = 60;
+void mipi_dsi_configure_dividers(int fps)
 {
 	u32 fb_divider, rate, vco;
 	u32 div_ratio = 0;
 	struct dsiphy_pll_divider_config *dividers;
 
 	dividers = &pll_divider_config;
-	
+
 	if(fps >= 42 && fps <= 60)
 	{
 		rate = dividers->clk_rate / 1000000; /* In Mhz */
-		
+
 		if (rate < 125) {
 			vco = rate * 8;
 			div_ratio = 8;
@@ -450,7 +450,7 @@ void mipi_dsi_configure_dividers(int fps)
 	{
 		printk("Invalid fps value\n");
 	}
-} 
+}
 
 int mipi_dsi_clk_div_config(uint8 bpp, uint8 lanes,
 			    uint32 *expected_dsi_pclk)
@@ -755,7 +755,7 @@ void mipi_dsi_clk_enable(void)
 	if (clk_set_rate(dsi_byte_div_clk, 1) < 0)      /* divided by 1 */
 		pr_err("%s: dsi_byte_div_clk - "
 			"clk_set_rate failed\n", __func__);
-	if (clk_set_rate(dsi_esc_clk, esc_byte_ratio) < 0) /* divided by esc */
+	if (BUG_ON(clk_set_rate(dsi_esc_clk, esc_byte_ratio) < 0)) /* divided by esc */
 		pr_err("%s: dsi_esc_clk - "                      /* clk ratio */
 			"clk_set_rate failed\n", __func__);
 	mipi_dsi_pclk_ctrl(&dsi_pclk, 1);
