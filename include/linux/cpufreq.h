a4a2cb7a9c51257e26194be9d23d0be9b551bfa5
robcore <robpatershuk@gmail.com>
2017-04-03 18:33:12 -0600

    sched: scheduler-driven cpu frequency selection

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -661,6 +661,9 @@ extern struct cpufreq_governor cpufreq_gov_xperience;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHISKYACTIVE)
 extern struct cpufreq_governor cpufreq_gov_whiskeyactive;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_whiskyactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
+extern struct cpufreq_governor cpufreq_gov_sched;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
 #endif
 
 
