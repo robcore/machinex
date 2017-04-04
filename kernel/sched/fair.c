7d5d6fa9fde4c677a3220630358970f867468cd4
robcore <robpatershuk@gmail.com>
2017-04-03 18:28:47 -0600

    sched-Add-group_misfit_task-load-balance-type

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5732,6 +5732,8 @@ again:  __maybe_unused
 	if (hrtick_enabled(rq))
 		hrtick_start_fair(rq, p);
 
+	rq->misfit_task = !task_fits_max(p, rq->cpu);
+
 	return p;
 simple:
 	cfs_rq = &rq->cfs;
@@ -5753,9 +5755,13 @@ simple:
 	if (hrtick_enabled(rq))
 		hrtick_start_fair(rq, p);
 
+	rq->misfit_task = !task_fits_max(p, rq->cpu);
+
 	return p;
 
 idle:
+	rq->misfit_task = 0;
+
 	new_tasks = idle_balance(rq);
 	/*
 	 * Because idle_balance() releases (and re-acquires) rq->lock, it is
@@ -5960,6 +5966,7 @@ static unsigned long __read_mostly max_load_balance_interval = HZ/10;
 
 enum group_type {
 	group_other = 0,
+	group_misfit_task,
 	group_imbalanced,
 	group_overloaded,
 };
@@ -6335,6 +6342,11 @@ struct sg_lb_stats {
 	unsigned int group_weight;
 	enum group_type group_type;
 	int group_no_capacity;
+	int group_misfit_task; /* A cpu has a task too big for its capacity */
+#ifdef CONFIG_NUMA_BALANCING
+	unsigned int nr_numa_running;
+	unsigned int nr_preferred_running;
+#endif /* CONFIG_NUMA_BALANCING
 };
 
 /*
@@ -6660,6 +6672,9 @@ static enum group_type group_classify(struct lb_env *env,
 	if (sg_imbalanced(group))
 		return group_imbalanced;
 
+	if (sgs->group_misfit_task)
+		return group_misfit_task;
+
 	return group_other;
 }
 
@@ -6734,8 +6749,11 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		if (!nr_running && idle_cpu(i))
 			sgs->idle_cpus++;
 
-		if (cpu_overutilized(i))
+		if (cpu_overutilized(i)) {
 			*overutilized = true;
+			if (!sgs->group_misfit_task && rq->misfit_task)
+				sgs->group_misfit_task = capacity_of(i);
+		}
 	}
 
 	if (local_group && (env->idle != CPU_NEWLY_IDLE ||
@@ -8320,6 +8338,8 @@ static void task_tick_fair(struct rq *rq, struct task_struct *curr, int queued)
 	}
 	if (!rq->rd->overutilized && cpu_overutilized(task_cpu(curr)))
 		rq->rd->overutilized = true;
+
+	rq->misfit_task = !task_fits_max(curr, rq->cpu);
 }
 
 /*
