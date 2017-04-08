b929e034075fc40aaf939e73cafb073758b1fe71
robcore <robpatershuk@gmail.com>
2017-04-08 03:04:15 -0600

    revert that for a sec

--- mm/hugetlb_cgroup.c
+++ mm/hugetlb_cgroup.c
@@ -58,7 +58,9 @@ static inline bool hugetlb_cgroup_is_root(struct hugetlb_cgroup *h_cg)
 
 static inline struct hugetlb_cgroup *parent_hugetlb_cgroup(struct cgroup *cg)
 {
-	return hugetlb_cgroup_from_css(css_parent(&cg->css));
+	if (!cg->parent)
+		return NULL;
+	return hugetlb_cgroup_from_cgroup(cg->parent);
 }
 
 static inline bool hugetlb_cgroup_have_usage(struct cgroup *cg)
