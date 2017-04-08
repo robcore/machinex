bb3d996951309c12d5ae543d97417bb7ee009769
robcore <robpatershuk@gmail.com>
2017-04-08 14:54:08 -0600

    REVERT - UPSTREAM: PM / sleep: Re-implement suspend-to-idle handling;

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -7,7 +7,6 @@
 #include <linux/tick.h>
 #include <linux/mm.h>
 #include <linux/stackprotector.h>
-#include <linux/suspend.h>
 #include <linux/percpu.h>
 
 #include <asm/tlb.h>
@@ -131,21 +130,6 @@ static void cpuidle_idle_call(void)
 	 * critical sections latencies
 	 */
 	stop_critical_timings();
-
-	/*
-	 * Suspend-to-idle ("freeze") is a system state in which all user space
-	 * has been frozen, all I/O devices have been suspended and the only
-	 * activity happens here and in iterrupts (if any).  In that case bypass
-	 * the cpuidle governor and go stratight for the deepest idle state
-	 * available.  Possibly also suspend the local tick and the entire
-	 * timekeeping to prevent timer interrupts from kicking us out of idle
-	 * until a proper wakeup interrupt happens.
-	 */
-	if (idle_should_freeze()) {
-		cpuidle_enter_freeze();
-		local_irq_enable();
-		goto exit_idle;
-	}
 
 	/*
 	 * Tell the RCU framework we are entering an idle section,
