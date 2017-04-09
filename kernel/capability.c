3e934542ad19c51105b56fc5e03c6fa8a363a94f
robcore <robpatershuk@gmail.com>
2017-04-09 17:18:59 -0600

    housecleaning, exported bool "pm_freezing" taken from mainline, and realize now that it is held with a mutex lock that it is safer than my freezing_in_progress hack so I am going to replace it asap.  also turned off an annoying message about our deprecated capability lib

--- kernel/capability.c
+++ kernel/capability.c
@@ -45,7 +45,7 @@ static void warn_legacy_capability_use(void)
 {
 	char name[sizeof(current->comm)];
 
-	pr_info_once("warning: `%s' uses 32-bit capabilities (legacy support in use)\n",
+	pr_debug("warning: `%s' uses 32-bit capabilities (legacy support in use)\n",
 		     get_task_comm(name, current));
 }
 
@@ -313,6 +313,7 @@ bool has_capability(struct task_struct *t, int cap)
 {
 	return has_ns_capability(t, &init_user_ns, cap);
 }
+EXPORT_SYMBOL(has_capability);
 
 /**
  * has_ns_capability_noaudit - Does a task have a capability (unaudited)
