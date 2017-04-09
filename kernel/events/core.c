220debfecac6e4f760fc8334ca864ff3d8b439f4
robcore <robpatershuk@gmail.com>
2017-04-09 17:05:54 -0600

    cgroup: make cgroup_taskset deal with cgroup_subsys_state instead of cgroup

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -8007,7 +8007,7 @@ static void perf_cgroup_attach(struct cgroup_css *css,
 {
 	struct task_struct *task;
 
-	cgroup_taskset_for_each(task, css->cgroup, tset)
+	cgroup_taskset_for_each(task, css, tset)
 		task_function_call(task, __perf_cgroup_move, task);
 }
 
