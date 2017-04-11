8c2ef9ba26b5d7b452724000dd396a128f37ef72
robcore <robpatershuk@gmail.com>
2017-04-11 02:28:40 -0600

    revert and hooold up

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -384,9 +384,8 @@ struct cftype {
 	umode_t mode;
 
 	/*
-	 * The maximum length of string, excluding trailing nul, that can
-	 * be passed to write_string.  If < PAGE_SIZE-1, PAGE_SIZE-1 is
-	 * assumed.
+	 * If non-zero, defines the maximum length of string that can
+	 * be passed to write_string; defaults to 64
 	 */
 	size_t max_write_len;
 
