28db96086a6cd362cc97341c0ee60dc259c81bfd
robcore <robpatershuk@gmail.com>
2017-04-11 17:05:48 -0600

    sched-add-migration-load-change-notifier-for-freq-change

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2369,7 +2369,7 @@ static inline unsigned int task_load(struct task_struct *p)
 	return p->ravg.demand;
 }
 
-static inline unsigned int max_task_load(void)
+unsigned int max_task_load(void)
 {
 	if (sched_use_pelt)
 		return LOAD_AVG_MAX;
@@ -6176,7 +6176,9 @@ static DEFINE_PER_CPU(int, dbs_boost_load_moved);
 static void move_task(struct task_struct *p, struct lb_env *env)
 {
 	deactivate_task(env->src_rq, p, 0);
+	double_lock_balance(env->src_rq, env->dst_rq);
 	set_task_cpu(p, env->dst_cpu);
+	double_unlock_balance(env->src_rq, env->dst_rq);
 	activate_task(env->dst_rq, p, 0);
 	check_preempt_curr(env->dst_rq, p, 0);
 	if (task_notify_on_migrate(p))
