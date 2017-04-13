023c737439152bad98905de18df31738f29d6763
robcore <robpatershuk@gmail.com>
2017-04-12 20:14:26 -0600

    sched-Initialize-env-loop-variable-to-0

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8089,6 +8089,7 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 		.loop_break	= sched_nr_migrate_break,
 		.cpus		= cpus,
 		.flags		= 0,
+		.loop		= 0,
 	};
 
 	/*
@@ -8523,6 +8524,7 @@ static int active_load_balance_cpu_stop(void *data)
 		.src_rq		= busiest_rq,
 		.idle		= CPU_IDLE,
 		.flags		= 0,
+		.loop		= 0,
 	};
 
 	raw_spin_lock_irq(&busiest_rq->lock);
