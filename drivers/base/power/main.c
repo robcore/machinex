0ea138432fbff6d5504c489f06a19509d30b9e58
robcore <robpatershuk@gmail.com>
2017-04-14 21:40:11 -0600

    some fixup in async noirq

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -600,7 +600,7 @@ static void async_resume_noirq(void *data, async_cookie_t cookie)
 	struct device *dev = (struct device *)data;
 	int error;
 
-	error = device_resume_noirq(dev, pm_transition);
+	error = device_resume_noirq(dev, pm_transition, async);
 	if (error)
 		pm_dev_err(dev, pm_transition, " async", error);
 
@@ -637,7 +637,7 @@ void dpm_resume_noirq(pm_message_t state)
 
 		if (!is_async(dev)) {
 			int error;
-			error = device_resume_noirq(dev, state);
+			error = device_resume_noirq(dev, state, false);
 			if (error) {
 				suspend_stats.failed_resume_noirq++;
 				dpm_save_failed_step(SUSPEND_RESUME_NOIRQ);
