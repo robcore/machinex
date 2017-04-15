8c081685a65d43220ee2637b67af2fa488649e72
robcore <robpatershuk@gmail.com>
2017-04-14 21:46:04 -0600

    fixup my async resume_noirq

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -542,7 +542,7 @@ static void dpm_watchdog_clear(struct dpm_watchdog *wd)
  * The driver of @dev will not receive interrupts while this function is being
  * executed.
  */
-static int device_resume_noirq(struct device *dev, pm_message_t state)
+static int device_resume_noirq(struct device *dev, pm_message_t state, bool async)
 {
 	pm_callback_t callback = NULL;
 	char *info = NULL;
@@ -556,6 +556,8 @@ static int device_resume_noirq(struct device *dev, pm_message_t state)
 
 	if (!dev->power.is_noirq_suspended)
 		goto Out;
+
+	dpm_wait(dev->parent, async);
 
 	if (dev->pm_domain) {
 		info = "noirq power domain ";
@@ -586,19 +588,23 @@ static int device_resume_noirq(struct device *dev, pm_message_t state)
 
  Out:
 	TRACE_RESUME(error);
-
+	complete_all(&dev->power.completion);
 	pm_runtime_enable(dev);
 	return error;
 }
 
-static bool is_async(struct device *dev);
+static bool is_async(struct device *dev)
+{
+	return dev->power.async_suspend && pm_async_enabled
+		&& !pm_trace_is_enabled();
+}
 
 static void async_resume_noirq(void *data, async_cookie_t cookie)
 {
 	struct device *dev = (struct device *)data;
 	int error;
 
-	error = device_resume_noirq(dev, pm_transition);
+	error = device_resume_noirq(dev, pm_transition, true);
 	if (error)
 		pm_dev_err(dev, pm_transition, " async", error);
 
@@ -635,7 +641,7 @@ void dpm_resume_noirq(pm_message_t state)
 
 		if (!is_async(dev)) {
 			int error;
-			error = device_resume_noirq(dev, state);
+			error = device_resume_noirq(dev, state, false);
 			if (error) {
 				suspend_stats.failed_resume_noirq++;
 				dpm_save_failed_step(SUSPEND_RESUME_NOIRQ);
@@ -852,12 +858,6 @@ static void async_resume(void *data, async_cookie_t cookie)
 	if (error)
 		pm_dev_err(dev, pm_transition, " async", error);
 	put_device(dev);
-}
-
-static bool is_async(struct device *dev)
-{
-	return dev->power.async_suspend && pm_async_enabled
-		&& !pm_trace_is_enabled();
 }
 
 /**
