4090b50250c069f9c1101b67154ac78715d7bf54
robcore <robpatershuk@gmail.com>
2017-04-17 14:04:22 -0600

    possing a bit

--- pos.sh
+++ pos.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
-/root/machinex/patches/0035-i-may-want-to-try-using-count-and-just-making-it-nr_.patch
-/root/machinex/patches/0034-TECHNICALLY-i-should-be-able-to-get-away-with-this.patch
-/root/machinex/patches/0033-fix-up-nr_running-count.patch
-/root/machinex/patches/0032-sched-avoid-running-idle_balance-on-behalf-of-wrong-.patch
-/root/machinex/patches/0031-sched-Fix-bug-in-average-nr_running-and-nr_iowait-ca.patch
-/root/machinex/patches/0030-fix-wait.c-smp_store_mb.patch
-/root/machinex/patches/0029-dunno-if-we-were-building-our-much-need-generic-hard.patch
-/root/machinex/patches/0028-endif.patch
-/root/machinex/patches/0027-a-doubled-commit-in-wait.c.patch
-/root/machinex/patches/0026-add-need_rebind.patch
-/root/machinex/patches/0025-cleaned-up-config.patch
-/root/machinex/patches/0024-synced-workqueue-with-3.18-sammy-changed-idr_get_new.patch
-/root/machinex/patches/0023-workqueue-apply-__WQ_ORDERED-to-create_singlethread.patch
-/root/machinex/patches/0022-workqueue-use-nr_node_ids-instead-of-wq_numa_tbl_len.patch
-/root/machinex/patches/0021-workqueue-remove-the-misnamed-out_unlock-label.patch
-/root/machinex/patches/0020-dox.patch
-/root/machinex/patches/0019-fixup-remove-rescuer-pool.patch
-/root/machinex/patches/0018-workqueue-unfold-start_worker-into-create_worker.patch
-/root/machinex/patches/0017-workqueue-remove-wakeup-from-worker_set_flags.patch
-/root/machinex/patches/0016-workqueue-remove-an-unneeded-UNBOUND-test-before-wak.patch
-/root/machinex/patches/0015-workqueue-alloc-struct-worker-on-its-local-node.patch
-/root/machinex/patches/0014-workqueue-reuse-the-already-calculated-pwq.patch
-/root/machinex/patches/0013-workqueue-zero-cpumask-of-wq_numa_possible_cpumask.patch
-/root/machinex/patches/0012-missing-POOL-DISASOSSIATED-flag-in-rebind.patch
-/root/machinex/patches/0011-workqueue-fix-dev_set_uevent_suppress-imbalance.patch
-/root/machinex/patches/0010-workqueue-clear-POOL_DISASSOCIATED-in-rebind_workers.patch
-/root/machinex/patches/0009-workqueue-sanity-check-pool-cpu-in-wq_worker_sleepin.patch
-/root/machinex/patches/0008-workqueue-clear-leftover-flags-when-detached.patch
-/root/machinex/patches/0007-workqueue-remove-useless-WARN_ON_ONCE.patch
-/root/machinex/patches/0006-turn-off-arm-mpu-and-msm-cache-erp.patch
-/root/machinex/patches/0005-workqueue-remove-the-empty-check-in-too_many_workers.patch
-/root/machinex/patches/0004-workqueue-use-pool-cpu-0-to-stand-for-an-unbound-poo.patch
-/root/machinex/patches/0003-sched-wait-Provide-infrastructure-to-deal-with-neste.patch
-/root/machinex/patches/0002-SCHED-add-some-wait.on_bit.timeout-interfaces.patch
-/root/machinex/patches/0001-there-it-is-just-use-init-work-in-place-of-PREPARE-D.patch
+patch -p1 -R < "/root/machinex/patches/0035-i-may-want-to-try-using-count-and-just-making-it-nr_.patch"
+patch -p1 -R < "/root/machinex/patches/0034-TECHNICALLY-i-should-be-able-to-get-away-with-this.patch"
+patch -p1 -R < "/root/machinex/patches/0033-fix-up-nr_running-count.patch"
+patch -p1 -R < "/root/machinex/patches/0032-sched-avoid-running-idle_balance-on-behalf-of-wrong-.patch"
+patch -p1 -R < "/root/machinex/patches/0031-sched-Fix-bug-in-average-nr_running-and-nr_iowait-ca.patch"
+patch -p1 -R < "/root/machinex/patches/0030-fix-wait.c-smp_store_mb.patch"
+patch -p1 -R < "/root/machinex/patches/0029-dunno-if-we-were-building-our-much-need-generic-hard.patch"
+patch -p1 -R < "/root/machinex/patches/0028-endif.patch"
+patch -p1 -R < "/root/machinex/patches/0027-a-doubled-commit-in-wait.c.patch"
+patch -p1 -R < "/root/machinex/patches/0026-add-need_rebind.patch"
+patch -p1 -R < "/root/machinex/patches/0025-cleaned-up-config.patch"
+patch -p1 -R < "/root/machinex/patches/0024-synced-workqueue-with-3.18-sammy-changed-idr_get_new.patch"
+patch -p1 -R < "/root/machinex/patches/0023-workqueue-apply-__WQ_ORDERED-to-create_singlethread.patch"
+patch -p1 -R < "/root/machinex/patches/0022-workqueue-use-nr_node_ids-instead-of-wq_numa_tbl_len.patch"
+patch -p1 -R < "/root/machinex/patches/0021-workqueue-remove-the-misnamed-out_unlock-label.patch"
+patch -p1 -R < "/root/machinex/patches/0020-dox.patch"
+patch -p1 -R < "/root/machinex/patches/0019-fixup-remove-rescuer-pool.patch"
+patch -p1 -R < "/root/machinex/patches/0018-workqueue-unfold-start_worker-into-create_worker.patch"
+patch -p1 -R < "/root/machinex/patches/0017-workqueue-remove-wakeup-from-worker_set_flags.patch"
+patch -p1 -R < "/root/machinex/patches/0016-workqueue-remove-an-unneeded-UNBOUND-test-before-wak.patch"
+patch -p1 -R < "/root/machinex/patches/0015-workqueue-alloc-struct-worker-on-its-local-node.patch"
+patch -p1 -R < "/root/machinex/patches/0014-workqueue-reuse-the-already-calculated-pwq.patch"
+patch -p1 -R < "/root/machinex/patches/0013-workqueue-zero-cpumask-of-wq_numa_possible_cpumask.patch"
