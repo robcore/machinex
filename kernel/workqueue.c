9c5d656dd177f61f966dfdc77d7fcced9b127030
robcore <robpatershuk@gmail.com>
2017-04-17 13:07:46 -0600

    workqueue-clear-leftover-flags-when-detached.

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1682,6 +1682,9 @@ static void worker_detach_from_pool(struct worker *worker,
 		detach_completion = pool->detach_completion;
 	mutex_unlock(&pool->attach_mutex);
 
+	/* clear leftover flags without pool->lock after it is detached */
+	worker->flags &= ~(WORKER_UNBOUND | WORKER_REBOUND);
+
 	if (detach_completion)
 		complete(detach_completion);
 }
