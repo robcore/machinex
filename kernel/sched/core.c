e4046b48f84dac03cdcc2a1808eae5624ee14fa9
robcore <robpatershuk@gmail.com>
2017-04-18 13:02:12 -0600

    sched/core: Get rid of cpu argument in wq_worker_sleeping()

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3269,7 +3269,7 @@ need_resched:
 			if (prev->flags & PF_WQ_WORKER) {
 				struct task_struct *to_wakeup;
 
-				to_wakeup = wq_worker_sleeping(prev, cpu);
+				to_wakeup = wq_worker_sleeping(prev);
 				if (to_wakeup)
 					try_to_wake_up_local(to_wakeup);
 			}
