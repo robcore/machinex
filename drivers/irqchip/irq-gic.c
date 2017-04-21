9921dfcb472ce3a365a42a2c0126746bb05644ca
robcore <robpatershuk@gmail.com>
2017-04-21 16:06:52 -0600

    naw its good

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -278,8 +278,6 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = "stray irq";
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
-
-		log_base_wakeup_reason(i + gic->irq_offset);
 	}
 }
 
