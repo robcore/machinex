44bae50c876c8bfe135561290d5280da1c0b48cf
robcore <robpatershuk@gmail.com>
2017-04-20 19:33:17 -0600

    cpuboost - ensure boost respects hardlimit again

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -27,6 +27,9 @@
 #ifdef CONFIG_STATE_NOTIFIER
 #include <linux/state_notifier.h>
 #endif
+#ifdef CONFIG_CPUFREQ_HARDLIMIT
+#include <linux/cpufreq_hardlimit.h>
+#endif
 
 struct cpu_sync {
 	int cpu;
@@ -122,11 +125,9 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 		 cpu, policy->min);
 	pr_debug("CPU%u boost min: %u kHz\n", cpu, min);
 
-#if 0
 	cpufreq_verify_within_limits(policy, min, check_cpufreq_hardlimit(policy->max)); /* Yank555.lu - Enforce hardlimit */
-#else
-	cpufreq_verify_within_limits(policy, min, UINT_MAX);
-#endif
+//	cpufreq_verify_within_limits(policy, min, UINT_MAX);
+
 
 	pr_debug("CPU%u policy min after boost: %u kHz\n",
 		 cpu, policy->min);
