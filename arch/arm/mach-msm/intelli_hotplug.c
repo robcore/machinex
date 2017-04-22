3e3aa2616fa7d61875e815d4443d146ed45aa483
robcore <robpatershuk@gmail.com>
2017-04-22 15:00:54 -0600

    nope dont freeze the cpu work!

--- arch/arm/mach-msm/intelli_hotplug.c
+++ arch/arm/mach-msm/intelli_hotplug.c
@@ -274,7 +274,7 @@ static void intelli_plug_work_fn(struct work_struct *work)
 
 	if (atomic_read(&intelli_plug_active) == 1) {
 		target_cpus = calculate_thread_stats();
-		queue_work_on(0, system_freezable_wq, &up_down_work);
+		schedule_work_on(0, &up_down_work);
 	}
 }
 
@@ -416,7 +416,7 @@ static void intelli_plug_input_event(struct input_handle *handle,
 		return;
 
 	target_cpus = cpus_boosted;
-	queue_work_on(0, system_freezable_wq, &up_down_work);
+	schedule_work_on(0, &up_down_work);
 	last_boost_time = ktime_to_us(ktime_get());
 }
 
