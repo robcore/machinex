997e0a603468bfc8c6b5102a555a2d70acd178ee
robcore <robpatershuk@gmail.com>
2017-04-28 14:40:00 -0600

    hooold up imma gonna create a krait entry

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -759,4 +759,5 @@ struct cpufreq_power_stats_platform_data {
 
 struct sched_domain;
 unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
+unsigned long cpufreq_scale_max_freq_capacity(int cpu);
 #endif /* _LINUX_CPUFREQ_H */
