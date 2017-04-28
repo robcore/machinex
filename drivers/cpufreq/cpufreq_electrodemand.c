ea0ea0d88462cbbde1ce200f3d668576359d3ed2
robcore <robpatershuk@gmail.com>
2017-04-27 19:43:51 -0600

    fix a stupid derp in electro

--- drivers/cpufreq/cpufreq_electrodemand.c
+++ drivers/cpufreq/cpufreq_electrodemand.c
@@ -83,7 +83,7 @@ static void do_dbs_timer(struct work_struct *work);
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				unsigned int event);
 
-#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_electrodemand
+#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRODEMAND
 static
 #endif
 struct cpufreq_governor cpufreq_gov_electrodemand = {
