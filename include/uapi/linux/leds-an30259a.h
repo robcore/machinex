3a69e5590d3f4cbcd5814ff6f203527fcd10d114
robcore <robpatershuk@gmail.com>
2017-04-29 11:32:31 -0600

    leds - fix my uapi header

--- include/uapi/linux/leds-an30259a.h
+++ include/uapi/linux/leds-an30259a.h
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2011 Samsung Electronics Co. Ltd. All Rights Reserved.
- *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- */
-
 #ifndef _UAPI_LEDS_AN30259A_H
 #define _UAPI_LEDS_AN30259A_H
 
