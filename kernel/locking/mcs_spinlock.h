27f7c5d5970e315bf6bcceb4f0bca008a230c6ec
robcore <robpatershuk@gmail.com>
2017-04-29 10:14:35 -0600

    TODO: get compxchange release. wish i knew which branch samsung was using

--- kernel/locking/mcs_spinlock.h
+++ kernel/locking/mcs_spinlock.h
@@ -104,7 +104,7 @@ void mcs_spin_unlock(struct mcs_spinlock **lock, struct mcs_spinlock *node)
 		/*
 		 * Release the lock by setting it to NULL
 		 */
-		if (likely(cmpxchg_release(lock, node, NULL) == node))
+		if (likely(cmpxchg(lock, node, NULL) == node))
 			return;
 		/* Wait until the next pointer is set */
 		while (!(next = READ_ONCE(node->next)))
