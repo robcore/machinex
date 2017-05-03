5a11ece9b8e81e5a7e14d905b9f0f0f860f641b0
robcore <robpatershuk@gmail.com>
2017-05-03 14:15:27 -0600

    missing default idle call from cpuidle enter

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -100,12 +100,13 @@ void __weak arch_cpu_idle(void)
 	local_irq_enable();
 }
 
-static void default_idle_call(void)
+/**
+ * default_idle_call - Default CPU idle routine.
+ *
+ * To use when the cpuidle framework cannot be used.
+ */
+void default_idle_call(void)
 {
-	/*
-	 * We can't use the cpuidle framework, let's use the default idle
-	 * routine.
-	 */
 	if (current_clr_polling_and_test())
 		local_irq_enable();
 	else
