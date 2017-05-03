1f69d7dc11e6b92395a48619eeaffcc953849c71
robcore <robpatershuk@gmail.com>
2017-05-03 14:28:10 -0600

    REVERT block cleanup plug

--- block/blk-core.c
+++ block/blk-core.c
@@ -2864,21 +2864,21 @@ void blk_start_plug(struct blk_plug *plug)
 {
 	struct task_struct *tsk = current;
 
-	/*
-	 * If this is a nested plug, don't actually assign it.
-	 */
-	if (tsk->plug)
-		return;
-
 	plug->magic = PLUG_MAGIC;
 	INIT_LIST_HEAD(&plug->list);
 	INIT_LIST_HEAD(&plug->cb_list);
 
 	/*
-	 * Store ordering should not be needed here, since a potential
-	 * preempt will imply a full memory barrier
+	 * If this is a nested plug, don't actually assign it. It will be
+	 * flushed on its own.
 	 */
-	tsk->plug = plug;
+	if (!tsk->plug) {
+		/*
+		 * Store ordering should not be needed here, since a potential
+		 * preempt will imply a full memory barrier
+		 */
+		tsk->plug = plug;
+	}
 }
 EXPORT_SYMBOL(blk_start_plug);
 
