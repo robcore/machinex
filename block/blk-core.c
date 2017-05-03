78bcfd5ec3a53c72c58a30b0cf9050ff387019fb
robcore <robpatershuk@gmail.com>
2017-05-03 13:39:43 -0600

    blk: clean up plug

--- block/blk-core.c
+++ block/blk-core.c
@@ -2864,20 +2864,21 @@ void blk_start_plug(struct blk_plug *plug)
 {
 	struct task_struct *tsk = current;
 
+	/*
+	 * If this is a nested plug, don't actually assign it.
+	 */
+	if (tsk->plug)
+		return;
+
 	plug->magic = PLUG_MAGIC;
 	INIT_LIST_HEAD(&plug->list);
 	INIT_LIST_HEAD(&plug->cb_list);
 
 	/*
-	 * If this is a nested plug, don't actually assign it. It will be
-	 * flushed on its own.
+	 * Store ordering should not be needed here, since a potential
+	 * preempt will imply a full memory barrier
 	 */
-	if (!tsk->plug) {
-		/*
-		 * Store ordering should not be needed here, since a potential
-		 * preempt will imply a full memory barrier
-		 */
-		tsk->plug = plug;
+	tsk->plug = plug;
 	}
 }
 EXPORT_SYMBOL(blk_start_plug);
