9cff88f84fb0f87b62cae8bdd85480c510423c5e
robcore <robpatershuk@gmail.com>
2017-05-03 13:33:38 -0600

    perf: Fix software migrate events

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1333,7 +1333,7 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 		if (p->sched_class->migrate_task_rq)
 			p->sched_class->migrate_task_rq(p, new_cpu);
 		p->se.nr_migrations++;
-		perf_sw_event(PERF_COUNT_SW_CPU_MIGRATIONS, 1, NULL, 0);
+		perf_event_task_migrate(p);
 
 		walt_fixup_busy_time(p, new_cpu);
 	}
