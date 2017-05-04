8e77bdaaa34e07f45493e6f9ea26f9ad0e8e4111
robcore <robpatershuk@gmail.com>
2017-05-04 16:07:13 -0600

    clockevents: Check state instead of mode in suspend/resume path

--- kernel/time/clockevents.c
+++ kernel/time/clockevents.c
@@ -641,7 +641,7 @@ void clockevents_suspend(void)
 	struct clock_event_device *dev;
 
 	list_for_each_entry_reverse(dev, &clockevent_devices, list)
-		if (dev->suspend && dev->mode != CLOCK_EVT_MODE_UNUSED)
+		if (dev->suspend && !clockevent_state_detached(dev))
 			dev->suspend(dev);
 }
 
@@ -653,7 +653,7 @@ void clockevents_resume(void)
 	struct clock_event_device *dev;
 
 	list_for_each_entry(dev, &clockevent_devices, list)
-		if (dev->resume && dev->mode != CLOCK_EVT_MODE_UNUSED)
+		if (dev->resume && !clockevent_state_detached(dev))
 			dev->resume(dev);
 }
 
