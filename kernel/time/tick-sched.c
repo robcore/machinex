191aa86cd26b0508d079b044eecd0d4653e43e21
robcore <robpatershuk@gmail.com>
2017-05-05 17:58:10 -0600

    get rid of pending deferrable timers

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -794,11 +794,6 @@ static void __tick_nohz_idle_enter(struct tick_sched *ts)
 
 	now = tick_nohz_start_idle(ts);
 
-#ifdef CONFIG_SMP
-	if (check_pending_deferrable_timers(cpu))
-		raise_softirq_irqoff(TIMER_SOFTIRQ);
-#endif
-
 	if (can_stop_idle_tick(cpu, ts)) {
 		int was_stopped = ts->tick_stopped;
 
