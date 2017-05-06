b5eea3b13c0e9d777294170fec88ed6395ef1e55
robcore <robpatershuk@gmail.com>
2017-05-05 20:30:22 -0600

    tracing-Prefer-kcalloc-over-kzalloc-with-multiply

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -2488,7 +2488,7 @@ __tracing_open(struct inode *inode, struct file *file)
 	if (!iter)
 		return ERR_PTR(-ENOMEM);
 
-	iter->buffer_iter = kzalloc(sizeof(*iter->buffer_iter) * num_possible_cpus(),
+	iter->buffer_iter = kcalloc(num_possible_cpus(), sizeof(*iter->buffer_iter),
 				    GFP_KERNEL);
 	if (!iter->buffer_iter)
 		goto release;
