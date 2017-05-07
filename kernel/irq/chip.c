d5be354df022890113a8d8473b3ae574ab25f275
robcore <robpatershuk@gmail.com>
2017-05-06 18:43:12 -0600

    genirq: Remove the second parameter from handle_irq_event_percpu()

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -708,7 +708,7 @@ handle_percpu_irq(struct irq_desc *desc)
 	if (chip->irq_ack)
 		chip->irq_ack(&desc->irq_data);
 
-	handle_irq_event_percpu(desc, desc->action);
+	handle_irq_event_percpu(desc);
 
 	if (chip->irq_eoi)
 		chip->irq_eoi(&desc->irq_data);
