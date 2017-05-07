8dcf4a9bea920937aec1e3d686eab154aab323cc
robcore <robpatershuk@gmail.com>
2017-05-06 18:47:08 -0600

    genirq: Introduce generic irq migration for cpu hotunplug

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -465,6 +465,8 @@ extern int irq_set_vcpu_affinity(unsigned int irq, void *vcpu_info);
 
 #ifdef CONFIG_GENERIC_HARDIRQS
 
+extern void irq_migrate_all_off_this_cpu(void);
+
 #if defined(CONFIG_SMP) && defined(CONFIG_GENERIC_PENDING_IRQ)
 void irq_move_irq(struct irq_data *data);
 void irq_move_masked_irq(struct irq_data *data);
