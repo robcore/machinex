565fd24c318cfbc33279d5d95fa3c635716e0b4f
robcore <robpatershuk@gmail.com>
2017-05-07 12:19:52 -0600

    revert signals-avoid-random-wakeups-in-sigsuspend

--- kernel/signal.c
+++ kernel/signal.c
@@ -3577,10 +3577,8 @@ int sigsuspend(sigset_t *set)
 	current->saved_sigmask = current->blocked;
 	set_current_blocked(set);
 
-	while (!signal_pending(current)) {
-		__set_current_state(TASK_INTERRUPTIBLE);
-		schedule();
-	}
+	__set_current_state(TASK_INTERRUPTIBLE);
+	schedule();
 	set_restore_sigmask();
 	return -ERESTARTNOHAND;
 }
