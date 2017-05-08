47ee12b5234743d10ba8418978f01787d5798c5a
robcore <robpatershuk@gmail.com>
2017-05-08 11:14:22 -0600

    security keys too...though i think i need to back track soo...

--- security/keys/request_key.c
+++ security/keys/request_key.c
@@ -561,10 +561,9 @@ int wait_for_key_construction(struct key *key, bool intr)
 	int ret;
 
 	ret = wait_on_bit(&key->flags, KEY_FLAG_USER_CONSTRUCT,
-			  intr ? key_wait_bit_intr : key_wait_bit,
 			  intr ? TASK_INTERRUPTIBLE : TASK_UNINTERRUPTIBLE);
-	if (ret < 0)
-		return ret;
+	if (ret)
+		return -ERESTARTSYS;
 	if (test_bit(KEY_FLAG_NEGATIVE, &key->flags))
 		return key->type_data.reject_error;
 	return key_validate(key);
