7e4909feb97bb3aa711e31ccbaad29e51cdd850a
robcore <robpatershuk@gmail.com>
2017-05-10 03:52:31 -0600

    space

--- kernel/sched/loadavg.c
+++ kernel/sched/loadavg.c
@@ -358,7 +358,6 @@ static unsigned long
 calc_load_n(unsigned long load, unsigned long exp,
 	    unsigned long active, unsigned int n)
 {
-
 	return calc_load(load, fixed_power_int(exp, FSHIFT, n), active);
 }
 
