e5dd616b4ccbed81f5b3399d59d37f432e291e29
robcore <robpatershuk@gmail.com>
2017-05-11 11:04:20 -0600

    synaptics rmi - apparently this fixes the double suspend/resume issues

--- drivers/input/touchscreen/synaptics_i2c_rmi.c
+++ drivers/input/touchscreen/synaptics_i2c_rmi.c
@@ -4047,7 +4047,7 @@ static int synaptics_rmi4_resume(struct device *dev)
 }
 #endif
 
-#ifdef CONFIG_PM
+#if 0
 static const struct dev_pm_ops synaptics_rmi4_dev_pm_ops = {
 	.suspend = synaptics_rmi4_suspend,
 	.resume  = synaptics_rmi4_resume,
