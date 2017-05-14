b6a6e186540b5a648ba6d3c1a5c7591dc09fda26
robcore <robpatershuk@gmail.com>
2017-05-14 14:36:01 -0600

    make pm_complete_with_resume_check just completion for now, until i update the firmware side

--- drivers/base/power/generic_ops.c
+++ drivers/base/power/generic_ops.c
@@ -339,8 +339,8 @@ void pm_complete_with_resume_check(struct device *dev)
 	 * the sleep state it is going out of and it has never been resumed till
 	 * now, resume it in case the firmware powered it up.
 	 */
-	if (dev->power.direct_complete && pm_resume_via_firmware())
-		pm_request_resume(dev);
+	/*if (dev->power.direct_complete && pm_resume_via_firmware())
+		pm_request_resume(dev); */
 }
 EXPORT_SYMBOL_GPL(pm_complete_with_resume_check);
 #endif /* CONFIG_PM_SLEEP */
