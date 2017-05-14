4f29612cf29a971c961223eb6610e8e0c5d6055b
robcore <robpatershuk@gmail.com>
2017-05-13 20:29:35 -0600

    TEST - drivercore / platform: Convert to dev_pm_domain_attach|detach()

--- drivers/base/platform.c
+++ drivers/base/platform.c
@@ -477,7 +477,12 @@ static int platform_drv_probe(struct device *_dev)
 	struct platform_device *dev = to_platform_device(_dev);
 	int ret;
 
-	ret = drv->probe(dev);
+	ret = dev_pm_domain_attach(_dev, true);
+	if (ret != -EPROBE_DEFER) {
+		ret = drv->probe(dev);
+		if (ret)
+			dev_pm_domain_detach(_dev, true);
+	}
 
 	if (drv->prevent_deferred_probe && ret == -EPROBE_DEFER) {
 		dev_warn(_dev, "probe deferral not supported\n");
@@ -499,6 +504,7 @@ static int platform_drv_remove(struct device *_dev)
 	int ret;
 
 	ret = drv->remove(dev);
+	dev_pm_domain_detach(_dev, true);
 
 	return ret;
 }
@@ -509,6 +515,7 @@ static void platform_drv_shutdown(struct device *_dev)
 	struct platform_device *dev = to_platform_device(_dev);
 
 	drv->shutdown(dev);
+	dev_pm_domain_detach(_dev, true);
 }
 
 /**
