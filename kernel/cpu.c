b9ebdb4d91aaf02be54cc6caa08d7551b82a4163
robcore <robpatershuk@gmail.com>
2017-05-15 10:57:22 -0600

    smp/cfd: Convert core to hotplug state machine

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1196,6 +1196,11 @@ static struct cpuhp_step cpuhp_bp_states[] = {
 		.startup = perf_event_init_cpu,
 		.teardown = perf_event_exit_cpu,
 	},
+	[CPUHP_SMPCFD_PREPARE] = {
+		.name = "SMPCFD prepare",
+		.startup = smpcfd_prepare_cpu,
+		.teardown = smpcfd_dead_cpu,
+	},
 	[CPUHP_TIMERS_DEAD] = {
 		.name = "timers dead",
 		.startup = NULL,
@@ -1223,6 +1228,10 @@ static struct cpuhp_step cpuhp_bp_states[] = {
 		.name = "hrtimers prepare",
 		.startup = hrtimers_prepare_cpu,
 		.teardown = hrtimers_dead_cpu,
+	},
+	[CPUHP_AP_SMPCFD_DYING] = {
+		.startup = NULL,
+		.teardown = smpcfd_dying_cpu,
 	},
 	/*
 	 * Handled on controll processor until the plugged processor manages
