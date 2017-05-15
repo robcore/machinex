c0af709586b1d5072e1f785b7789a63be1ced289
robcore <robpatershuk@gmail.com>
2017-05-14 20:31:52 -0600

    reverting that runtime pm commit again...lol of all the things, the same old mmc/wifi sharing system is what makes us NEED runtime idle

--- drivers/base/power/runtime.c
+++ drivers/base/power/runtime.c
@@ -416,8 +416,7 @@ static int rpm_idle(struct device *dev, int rpmflags)
 			dev->power.request_pending = true;
 			queue_work(pm_wq, &dev->power.work);
 		}
-		trace_rpm_return_int_rcuidle(dev, _THIS_IP_, 0);
-		return 0;
+		goto out;
 	}
 
 	dev->power.idle_notification = true;
@@ -425,14 +424,14 @@ static int rpm_idle(struct device *dev, int rpmflags)
 	callback = RPM_GET_CALLBACK(dev, runtime_idle);
 
 	if (callback)
-		retval = __rpm_callback(callback, dev);
+		__rpm_callback(callback, dev);
 
 	dev->power.idle_notification = false;
 	wake_up_all(&dev->power.wait_queue);
 
  out:
 	trace_rpm_return_int_rcuidle(dev, _THIS_IP_, retval);
-	return retval ? retval : rpm_suspend(dev, rpmflags | RPM_AUTO);
+	return retval;
 }
 
 /**
