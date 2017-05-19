1d10de3845c2fc63ec043d374f0639701e631f36
robcore <robpatershuk@gmail.com>
2017-05-19 15:41:14 -0600

    mmc-card-Do-not-scan-RPMB-partitions

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2467,6 +2467,8 @@ static struct mmc_blk_data *mmc_blk_alloc_req(struct mmc_card *card,
 	if (area_type & (MMC_BLK_DATA_AREA_RPMB | MMC_BLK_DATA_AREA_BOOT))
 		md->disk->flags |= GENHD_FL_NO_PART_SCAN;
 	md->disk->flags = GENHD_FL_EXT_DEVT;
+	if (area_type & MMC_BLK_DATA_AREA_RPMB)
+		md->disk->flags |= GENHD_FL_NO_PART_SCAN;
 
 	/*
 	 * As discussed on lkml, GENHD_FL_REMOVABLE should:
