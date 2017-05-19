2d3bf4195f45225b31634d8ad4124b5d979d00c6
robcore <robpatershuk@gmail.com>
2017-05-19 08:58:51 -0600

    brought back the normal stuff

--- sop.sh
+++ sop.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-patch -p1 < "/root/machinex/patches/0083-sched-Fix-crash-trying-to-dequeue-enqueue-the-idle-t.patch"
-patch -p1 < "/root/machinex/patches/0084-sched-core-Make-policy-testing-consistent.patch"
-patch -p1 < "/root/machinex/patches/0085-numa-bs.patch"
-patch -p1 < "/root/machinex/patches/0086-sched-fair-Remove-unnecessary-parameter-for-group_cl.patch"
-patch -p1 < "/root/machinex/patches/0089-locking-pvqspinlock-Kick-the-PV-CPU-unconditionally.patch"
-patch -p1 < "/root/machinex/patches/0090-rcu-Suppress-lockdep-false-positive-for-rcp-exp.patch"
-patch -p1 < "/root/machinex/patches/0091-rcu-Use-rsp-expedited_wq-instead-of-sync_rcu_preempt.patch"
-patch -p1 < "/root/machinex/patches/0092-rcu-Move-rcu_report_exp_rnp-to-allow-consolidation.patch"
-patch -p1 < "/root/machinex/patches/0093-rcu-Consolidate-tree-setup-for-synchronize_rcu_exp.patch"
-patch -p1 < "/root/machinex/patches/0094-rcu-Use-single-stage-IPI-algorithm-for-RCU-expedited.patch"
-patch -p1 < "/root/machinex/patches/0095-rcu-Move-synchronize_sched_expedited.patch"
-patch -p1 < "/root/machinex/patches/0096-rcu-Rename-qs_pending-to-core_needs_qs.patch"
-patch -p1 < "/root/machinex/patches/0097-rcu-Invert-passed_quiesce-and-rename-to-cpu_no_qs.patch"
-patch -p1 < "/root/machinex/patches/0098-rcu-Make-cpu_no_qs-be-a-union-for-aggregate-OR.patch"
-patch -p1 < "/root/machinex/patches/0099-dox.patch"
-patch -p1 < "/root/machinex/patches/0100-timers-Fix-data-race-in-timer_stats_account_timer.patch"
-patch -p1 < "/root/machinex/patches/0101-futex-Force-hot-variables-into-a-single-cache-line.patch"
-
+/root/machinex/patches/0233-genirq-Fix-potential-memleak-when-failing-to-get-irq.patch
+/root/machinex/patches/0243-cpu-hotplug-Prevent-alloc-free-of-irq-descriptors-du.patch
+/root/machinex/patches/0244-irqchip-UP-commit-again.patch
+/root/machinex/patches/0245-genirq-affinity-Use-get-put_online_cpus-around-cpuma.patch
+/root/machinex/patches/0246-dox.patch
+/root/machinex/patches/0247-rcu-Exclude-RCU-offline-CPUs-from-expedited-grace-p.patch
+/root/machinex/patches/0248-timekeeping-Avoid-taking-lock-in-NMI-path.patch
+/root/machinex/patches/0249-hung-tasks-show-all-locks.patch
