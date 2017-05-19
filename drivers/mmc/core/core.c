a4f888751f0ee72e776085695bf87be87b1c67fe
robcore <robpatershuk@gmail.com>
2017-05-19 15:29:42 -0600

    mmc - dont pre-claim host in mmc suspend, just use it where it is needed

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -3200,6 +3200,7 @@ int mmc_cache_ctrl(struct mmc_host *host, u8 enable)
 		return err;
 	timeout = card->ext_csd.generic_cmd6_time;
 
+	mmc_claim_host(host);
 	if (card && mmc_card_mmc(card) &&
 			(card->ext_csd.cache_size > 0)) {
 		enable = !!enable;
@@ -3229,6 +3230,7 @@ int mmc_cache_ctrl(struct mmc_host *host, u8 enable)
 			}
 		}
 	}
+	mmc_release_host(host);
 
 	return err;
 }
@@ -3257,53 +3259,36 @@ int mmc_suspend_host(struct mmc_host *host)
 
 	mmc_bus_get(host);
 	if (host->bus_ops && !host->bus_dead) {
-		/*
-		 * A long response time is not acceptable for device drivers
-		 * when doing suspend. Prevent mmc_claim_host in the suspend
-		 * sequence, to potentially wait "forever" by trying to
-		 * pre-claim the host.
-		 *
-		 * Skip try claim host for SDIO cards, doing so fixes deadlock
-		 * conditions. The function driver suspend may again call into
-		 * SDIO driver within a different context for enabling power
-		 * save mode in the card and hence wait in mmc_claim_host
-		 * causing deadlock.
-		 */
-		if (!(host->card && mmc_card_sdio(host->card))) {
-			if (!mmc_try_claim_host(host))
-				return -EBUSY;
+
+		if (host->bus_ops->suspend) {
+			mmc_claim_host(host);
+			err = mmc_stop_bkops(host->card);
+			if (err) {
+				if (!(host->card && mmc_card_sdio(host->card)))
+					mmc_release_host(host);
+				goto stop_bkops_err;
+			} else
+				mmc_release_host(host);
 		}
+		err = host->bus_ops->suspend(host);
 
-			if (host->bus_ops->suspend) {
-				err = mmc_stop_bkops(host->card);
-				if (err) {
-					if (!(host->card && mmc_card_sdio(host->card)))
-						mmc_release_host(host);
-					goto stop_bkops_err;
-				}
-			}
-			err = host->bus_ops->suspend(host);
-
-			if (!(host->card && mmc_card_sdio(host->card)))
-				mmc_release_host(host);
-
-			if (err == -ENOSYS || !host->bus_ops->resume) {
-				/*
-				 * We simply "remove" the card in this case.
-				 * It will be redetected on resume.  (Calling
-				 * bus_ops->remove() with a claimed host can
-				 * deadlock.)
-				 * It will be redetected on resume.
-				 */
-				if (host->bus_ops->remove)
-					host->bus_ops->remove(host);
-				mmc_claim_host(host);
-				mmc_detach_bus(host);
-				mmc_power_off(host);
-				mmc_release_host(host);
-				host->pm_flags = 0;
-				err = 0;
-			}
+		if (err == -ENOSYS || !host->bus_ops->resume) {
+			/*
+			 * We simply "remove" the card in this case.
+			 * It will be redetected on resume.  (Calling
+			 * bus_ops->remove() with a claimed host can
+			 * deadlock.)
+			 * It will be redetected on resume.
+			 */
+			if (host->bus_ops->remove)
+				host->bus_ops->remove(host);
+			mmc_claim_host(host);
+			mmc_detach_bus(host);
+			mmc_power_off(host);
+			mmc_release_host(host);
+			host->pm_flags = 0;
+			err = 0;
+		}
 	}
 
 	if (!err && !mmc_card_keep_power(host)) {
