ad795ce1359dfbfec795a9cb33a341c473023a22
robcore <robpatershuk@gmail.com>
2017-05-21 16:17:56 -0600

    real fix

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1308,7 +1308,7 @@ void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 	if (queued)
 		enqueue_task(rq, p, ENQUEUE_RESTORE);
 	if (running)
-		p->sched_class->set_curr_task(rq, p);
+		p->sched_class->set_curr_task(rq);
 }
 
 /*
@@ -3934,7 +3934,7 @@ void rt_mutex_setprio(struct task_struct *p, int prio)
 	if (queued)
 		enqueue_task(rq, p, queue_flag);
 	if (running)
-		p->sched_class->set_curr_task(rq, p);
+		p->sched_class->set_curr_task(rq);
 
 	check_class_changed(rq, p, prev_class, oldprio);
 out_unlock:
@@ -3993,7 +3993,7 @@ void set_user_nice(struct task_struct *p, long nice)
 			resched_curr(rq);
 	}
 	if (running)
-		set_curr_task(rq, p);
+		p->sched_class->set_curr_task(rq);
 out_unlock:
 	task_rq_unlock(rq, p, &rf);
 }
@@ -4507,7 +4507,7 @@ change:
 		enqueue_task(rq, p, queue_flags);
 	}
 	if (running)
-		p->sched_class->set_curr_task(rq, p);
+		p->sched_class->set_curr_task(rq);
 
 	check_class_changed(rq, p, prev_class, oldprio);
 	preempt_disable(); /* avoid rq from going away on us */
@@ -5593,7 +5593,7 @@ void sched_setnuma(struct task_struct *p, int nid)
 	if (queued)
 		enqueue_task(rq, p, ENQUEUE_RESTORE);
 	if (running)
-		p->sched_class->set_curr_task(rq, p);
+		p->sched_class->set_curr_task(rq);
 	task_rq_unlock(rq, p, &rf);
 }
 #endif /* CONFIG_NUMA_BALANCING */
@@ -8301,7 +8301,7 @@ void sched_move_task(struct task_struct *tsk)
 	if (queued)
 		enqueue_task(rq, tsk, ENQUEUE_RESTORE | ENQUEUE_MOVE);
 	if (unlikely(running))
-		tsk->sched_class->set_curr_task(rq, p);
+		tsk->sched_class->set_curr_task(rq);
 
 	task_rq_unlock(rq, tsk, &rf);
 }
