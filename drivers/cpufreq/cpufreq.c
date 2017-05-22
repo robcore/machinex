8c3afd2482f8ed76d241ba49e51d796aa9b5d9b2
robcore <robpatershuk@gmail.com>
2017-05-22 13:04:18 -0600

    we dont add/remove shit during hotplug

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2365,19 +2365,12 @@ static int cpufreq_cpu_callback(struct notifier_block *nfb,
 		switch (action) {
 		case CPU_ONLINE:
 		case CPU_ONLINE_FROZEN:
-			cpufreq_add_dev(dev, NULL);
+		case CPU_DOWN_FAILED:
+		case CPU_DOWN_FAILED_FROZEN:
 			cpufreq_update_policy(cpu);
 			break;
 		case CPU_DOWN_PREPARE:
 		case CPU_DOWN_PREPARE_FROZEN:
-			if (unlikely(lock_policy_rwsem_write(cpu)))
-				BUG();
-
-			__cpufreq_remove_dev(dev, NULL);
-			break;
-		case CPU_DOWN_FAILED:
-		case CPU_DOWN_FAILED_FROZEN:
-			cpufreq_add_dev(dev, NULL);
 			break;
 		}
 	}
