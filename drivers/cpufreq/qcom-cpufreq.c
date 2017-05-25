691bcd251bbcc6599ae913070510a075888dabc3
robcore <robpatershuk@gmail.com>
2017-05-25 00:49:05 -0600

    removed workqueue from msm cpufreq, and some other little tweaks to see if i destroy myself

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -20,8 +20,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/cpufreq.h>
-#include <linux/workqueue.h>
-#include <linux/completion.h>
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
 #include <linux/suspend.h>
@@ -51,18 +49,6 @@ static struct msm_bus_scale_pdata bus_bw = {
 	.active_only = 1,
 };
 static u32 bus_client;
-
-struct cpufreq_work_struct {
-	struct work_struct work;
-	struct cpufreq_policy *policy;
-	struct completion complete;
-	int frequency;
-	unsigned int index;
-	int status;
-};
-
-static DEFINE_PER_CPU(struct cpufreq_work_struct, cpufreq_work);
-static struct workqueue_struct *msm_cpufreq_wq;
 
 struct cpufreq_suspend_t {
 	struct mutex suspend_mutex;
@@ -151,16 +137,6 @@ static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq,
 	return ret;
 }
 
-static void set_cpu_work(struct work_struct *work)
-{
-	struct cpufreq_work_struct *cpu_work =
-		container_of(work, struct cpufreq_work_struct, work);
-
-	cpu_work->status = set_cpu_freq(cpu_work->policy, cpu_work->frequency,
-					cpu_work->index);
-	complete(&cpu_work->complete);
-}
-
 static int msm_cpufreq_target(struct cpufreq_policy *policy,
 				unsigned int target_freq,
 				unsigned int relation)
@@ -168,8 +144,6 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 	int ret = 0;
 	int index;
 	struct cpufreq_frequency_table *table;
-
-	struct cpufreq_work_struct *cpu_work = NULL;
 
 	mutex_lock(&per_cpu(suspend_data, policy->cpu).suspend_mutex);
 
@@ -195,19 +169,8 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 		policy->cpu, target_freq, relation,
 		policy->min, policy->max, table[index].frequency);
 
-	cpu_work = &per_cpu(cpufreq_work, policy->cpu);
-	cpu_work->policy = policy;
-	cpu_work->frequency = table[index].frequency;
-	cpu_work->index = table[index].driver_data;
-	cpu_work->status = -ENODEV;
-
-	cancel_work_sync(&cpu_work->work);
-	reinit_completion(&cpu_work->complete);
-	queue_work_on(policy->cpu, msm_cpufreq_wq, &cpu_work->work);
-	wait_for_completion(&cpu_work->complete);
-
-	ret = cpu_work->status;
-
+	ret = set_cpu_freq(policy, table[index].frequency,
+			   table[index].driver_data);
 done:
 	mutex_unlock(&per_cpu(suspend_data, policy->cpu).suspend_mutex);
 	return ret;
@@ -300,7 +263,7 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 	int index;
 	int ret = 0;
 	struct cpufreq_frequency_table *table;
-	struct cpufreq_work_struct *cpu_work = NULL;
+	unsigned int cpu;
 
 	table = cpufreq_frequency_get_table(policy->cpu);
 	if (table == NULL)
@@ -313,14 +276,16 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 	if (is_sync)
 		cpumask_setall(policy->cpus);
 	 */
-
-	cpu_work = &per_cpu(cpufreq_work, policy->cpu);
-	INIT_WORK(&cpu_work->work, set_cpu_work);
-	init_completion(&cpu_work->complete);
-
-	/* synchronous cpus share the same policy */
+	for_each_possible_cpu(cpu)
+		cpumask_set_cpu(cpu, policy->cpus);
+	/* synchronous cpus share the same policy
 	if (is_clk && !cpu_clk[policy->cpu])
-		return 0;
+		return 0;  */
+	ret = cpufreq_table_validate_and_show(policy, table);
+	if (ret) {
+		pr_err("cpufreq: failed to get policy min/max\n");
+		return ret;
+	}
 
 
 	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
@@ -720,7 +685,6 @@ static int __init msm_cpufreq_register(void)
 	}
 
 	platform_driver_probe(&msm_cpufreq_plat_driver, msm_cpufreq_probe);
-	msm_cpufreq_wq = alloc_workqueue("msm-cpufreq", WQ_HIGHPRI | WQ_CPU_INTENSIVE, 0);
 #ifdef CONFIG_OF
 	register_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
 #endif
