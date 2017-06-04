da80675036d52ba900229dea990b076a49b1ee18
robcore <robpatershuk@gmail.com>
2017-06-04 01:15:30 -0600

    sched/core: Add wrappers for lockdep_(un)pin_lock()

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5610,7 +5610,7 @@ preempt:
 }
 
 static struct task_struct *
-pick_next_task_fair(struct rq *rq, struct task_struct *prev, struct pin_cookie cookie)
+pick_next_task_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 {
 	struct cfs_rq *cfs_rq = &rq->cfs;
 	struct sched_entity *se;
@@ -5722,9 +5722,9 @@ simple:
 
 idle:
 	rq->misfit_task = 0;
-	lockdep_unpin_lock(&rq->lock, cookie);
+	rq_unpin_lock(rq, rf);
 	new_tasks = idle_balance(rq);
-	lockdep_repin_lock(&rq->lock, cookie);
+	rq_repin_lock(rq, rf);
 	/*
 	 * Because idle_balance() releases (and re-acquires) rq->lock, it is
 	 * possible for any higher priority task to appear. In that case we
