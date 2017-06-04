e95030a7f75c61bf256dd345810c60a8fcc3d9a1
robcore <robpatershuk@gmail.com>
2017-06-03 23:14:46 -0600

    added a policy quick get min function because i need it

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -193,6 +193,7 @@ void cpufreq_put_global_kobject(void);
 unsigned int cpufreq_get(unsigned int cpu);
 unsigned int cpufreq_quick_get(unsigned int cpu);
 unsigned int cpufreq_quick_get_max(unsigned int cpu);
+unsigned int cpufreq_quick_get_min(unsigned int cpu);
 unsigned int cpufreq_quick_get_util(unsigned int cpu);
 void disable_cpufreq(void);
 
