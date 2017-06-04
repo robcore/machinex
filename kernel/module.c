bbee27008dfbb5768fdd823e1d9324915ebec3e7
robcore <robpatershuk@gmail.com>
2017-06-04 13:16:10 -0600

    core-migrate-exception-table-users;

--- kernel/module.c
+++ kernel/module.c
@@ -17,6 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 #include <linux/export.h>
+#include <linux/extable.h>
 #include <linux/moduleloader.h>
 #include <linux/ftrace_event.h>
 #include <linux/init.h>
