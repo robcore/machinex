f6b6c1ee322a88bae005094121191041724de5f6
robcore <robpatershuk@gmail.com>
2017-06-05 14:38:00 -0600

    do more of the same in check_preempt_curr, no more of this funny business

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1097,6 +1097,8 @@ void check_preempt_curr(struct rq *rq, struct task_struct *p, int flags)
 {
 	const struct sched_class *class;
 
+	if (class == NULL || rq == NULL || p == NULL)
+		return;
 	if (p->sched_class == rq->curr->sched_class) {
 		rq->curr->sched_class->check_preempt_curr(rq, p, flags);
 	} else {
