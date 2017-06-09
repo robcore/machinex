e75d13166ecac5f69b7743282ddd6108ac120662
robcore <robpatershuk@gmail.com>
2017-06-09 03:21:20 -0600

    oops, POLICY curr_limits

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2678,8 +2678,8 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	policy->min = new_policy->min;
 	policy->max = new_policy->max;
 
-	if (policy->min != curr_limit_min ||
-		policy->max != curr_limit_max)
+	if (policy->min != policy->curr_limit_min ||
+		policy->max != policy->curr_limit_max)
 		reapply_hard_limits(policy->cpu);		
 
 	policy->util_thres = new_policy->util_thres;
