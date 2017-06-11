899fedb8224434df230915e97613ffc89b704de8
robcore <robpatershuk@gmail.com>
2017-06-11 17:40:19 -0600

    finally found our cpu transition latency from a 4.7 caf(?)-based kernel

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -1374,6 +1374,8 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 	int ret = 0;
 	int cpu;
 
+	policy->cpuinfo.transition_latency = 100000;
+
 	if (policy->cpu > NR_CPUS) {
 		ret = -EINVAL;
 		goto out;
