4ca1893979a97fac10e78e90ae87820836c67c2d
robcore <robpatershuk@gmail.com>
2017-06-11 19:09:25 -0600

    that solves that...

--- block/bsg.c
+++ block/bsg.c
@@ -653,9 +653,6 @@ bsg_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
 
 	dprintk("%s: write %Zd bytes\n", bd->name, count);
 
-	WARN_ONCE(uaccess_kernel()),
-		  "%s: attempting get_fs from KERNEL_DS space\n", __func__);
-
 	bsg_set_block(bd, file);
 
 	bytes_written = 0;
