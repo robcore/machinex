4d5f409513599c96b6368ca15da1a6eea3566017
robcore <robpatershuk@gmail.com>
2017-06-11 20:18:41 -0600

    nope just breaks it...

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -312,7 +312,7 @@ static void leds_set_slope_mode(struct i2c_client *client,
 							delay << 4 | dutymin;
 	data->shadow_reg[AN30259A_REG_LED1CNT3 + led * 4] = dt2 << 4 | dt1;
 	data->shadow_reg[AN30259A_REG_LED1CNT4 + led * 4] = dt4 << 4 | dt3;
-	data->shadow_reg[AN30259A_REG_LED1SLP + led] = slptt2 << 2 | slptt1;
+	data->shadow_reg[AN30259A_REG_LED1SLP + led] = slptt2 << 4 | slptt1;
 }
 
 static void leds_on(enum an30259a_led_enum led, bool on, bool slopemode,
