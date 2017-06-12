ce724687266511b6986021c0fd01a29e3631c4c1
robcore <robpatershuk@gmail.com>
2017-06-12 10:48:06 -0600

    freq control max should be an unsigned int

--- drivers/thermal/intelli/msm_thermal.c
+++ drivers/thermal/intelli/msm_thermal.c
@@ -76,7 +76,7 @@ module_param_named(poll_ms, msm_thermal_info.poll_ms, uint, 0664);
 module_param_named(limit_temp_degC, msm_thermal_info.limit_temp_degC,
 			int, 0664);
 module_param_named(freq_control_mask, msm_thermal_info.freq_control_mask,
-			int, 0664);
+			uint, 0664);
 module_param_named(core_limit_temp_degC, msm_thermal_info.core_limit_temp_degC,
 			int, 0664);
 module_param_named(core_control_mask, msm_thermal_info.core_control_mask,
