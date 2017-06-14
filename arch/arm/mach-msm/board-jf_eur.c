db4a52a8a4456a6612e2f6e979881aa5db8b14bc
robcore <robpatershuk@gmail.com>
2017-06-14 12:04:03 -0600

    now that the sensors are properly set up, raise the limit back up to 65

--- arch/arm/mach-msm/board-jf_eur.c
+++ arch/arm/mach-msm/board-jf_eur.c
@@ -3201,13 +3201,13 @@ static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id_three = 9,
 	.sensor_id_four = 10,
 	.poll_ms = 250,
-	.limit_temp_degC = 60,
+	.limit_temp_degC = 65,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 #ifdef CONFIG_INTELLI_THERMAL
 	.freq_control_mask = 0xf,
 #endif
-	.core_limit_temp_degC = 70,
+	.core_limit_temp_degC = 75,
 	.core_temp_hysteresis_degC = 10,
 	.core_control_mask = 0xe,
 };
