06edd25bc43b824549996cbdc5331b3934a8b0d8
robcore <robpatershuk@gmail.com>
2017-06-14 20:04:43 -0600

    i think that fixes my formatting issue

--- drivers/slimbus/slim-msm-ctrl.c
+++ drivers/slimbus/slim-msm-ctrl.c
@@ -1619,8 +1619,8 @@ static struct msm_slim_sat *msm_slim_alloc_sat(struct msm_slim_ctrl *dev)
 	}
 	dev->satd[dev->nsats] = sat;
 	sat->dev = dev;
+	snprintf(name, SLIMBUS_NAME_SIZE, "msm_sat%d", dev->nsats);
 	sat->satcl.name = name;
-	snprintf(name, SLIMBUS_NAME_SIZE, "msm_sat%d\n", dev->nsats);
 	spin_lock_init(&sat->lock);
 	INIT_WORK(&sat->wd, slim_sat_rxprocess);
 	sat->wq = create_singlethread_workqueue(sat->satcl.name);
