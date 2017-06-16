4b404a311909920071970e024ec97788519aff74
robcore <robpatershuk@gmail.com>
2017-06-15 22:43:57 -0600

    locking-Optimize-lock_bh-functions.

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -113,7 +113,7 @@ static void __local_bh_disable(unsigned long ip, unsigned int cnt)
 	/*
 	 * Were softirqs turned off above:
 	 */
-	if (softirq_count() == cnt)
+	if (softirq_count() == (cnt & SOFTIRQ_MASK))
 		trace_softirqs_off(ip);
 	raw_local_irq_restore(flags);
 
@@ -143,7 +143,7 @@ static void __local_bh_enable(unsigned int cnt)
 {
 	WARN_ON_ONCE(!irqs_disabled());
 
-	if (softirq_count() == cnt)
+	if (softirq_count() == (cnt & SOFTIRQ_MASK))
 		trace_softirqs_on(_RET_IP_);
 	sub_preempt_count(cnt);
 }
