2b6d07f91f3799bc552cd6254fcf95aa271dc708
robcore <robpatershuk@gmail.com>
2017-06-17 14:10:16 -0600

    ?

--- drivers/net/wireless/bcmdhd/wl_android.c
+++ drivers/net/wireless/bcmdhd/wl_android.c
@@ -3304,7 +3304,7 @@ int wl_android_priv_cmd(struct net_device *net, struct ifreq *ifr, int cmd)
 		ret = -EINVAL;
 		goto exit;
 	}
-	command = kmalloc((priv_cmd.total_len + 1), GFP_KERNEL);
+	command = kmalloc((priv_cmd.total_len + 1), kflagsKERNEL);
 	if (!command)
 	{
 		DHD_ERROR(("%s: failed to allocate memory\n", __FUNCTION__));
