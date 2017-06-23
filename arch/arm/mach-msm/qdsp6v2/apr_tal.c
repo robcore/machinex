2fb0803bcaccf33fcf146c63517d7418bfc8d63d
robcore <robpatershuk@gmail.com>
2017-06-23 13:42:21 -0600

    remove extra mutex locks within apr_tal_close, as it is only called by processes that are already holding the same mutex lock (potentially a cause for lock contention...in fact, I am considering doing the same for apr_tal_open as the functions calling it are also already protecting it, but will hold off for now. that said, only one mutexs unlock had to be moved in order for this to be relatively failsafe, so lets do it. lock contention is bad

--- arch/arm/mach-msm/qdsp6v2/apr_tal.c
+++ arch/arm/mach-msm/qdsp6v2/apr_tal.c
@@ -150,12 +150,10 @@ int apr_tal_close(struct apr_svc_ch_dev *apr_ch)
 	if (!apr_ch->ch)
 		return -EINVAL;
 
-	mutex_lock(&apr_ch->m_lock);
 	r = smd_close(apr_ch->ch);
 	apr_ch->ch = NULL;
 	apr_ch->func = NULL;
 	apr_ch->priv = NULL;
-	mutex_unlock(&apr_ch->m_lock);
 	return r;
 }
 
@@ -202,8 +200,8 @@ struct apr_svc_ch_dev *apr_tal_open(uint32_t svc, uint32_t dest,
 		(apr_svc_ch[dl][dest][svc].smd_state == 1), msecs_to_jiffies(APR_OPEN_TIMEOUT_MS));
 	if (rc == 0) {
 		pr_err("apr_tal:TIMEOUT for OPEN event\n");
-		mutex_unlock(&apr_svc_ch[dl][dest][svc].m_lock);
 		apr_tal_close(&apr_svc_ch[dl][dest][svc]);
+		mutex_unlock(&apr_svc_ch[dl][dest][svc].m_lock);
 		return NULL;
 	}
 	if (!apr_svc_ch[dl][dest][svc].dest_state) {
