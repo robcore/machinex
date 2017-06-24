65a402d0b0af70b9e2fb40b2a037f364a6c12ca5
robcore <robpatershuk@gmail.com>
2017-06-24 02:54:24 -0600

    blkcg-change-a-spin_lock-to-spin_lock_irq.

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -1616,7 +1616,7 @@ static int blkiocg_pre_destroy(struct cgroup_subsys *subsys,
 		} else {
 			spin_unlock_irq(&blkcg->lock);
 			cpu_relax();
-			spin_lock(&blkcg->lock);
+			spin_lock_irq(&blkcg->lock);
 		}
 	}
 
