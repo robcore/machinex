a8956a63fe10e2b8e05984c686ceda3cc693068b
robcore <robpatershuk@gmail.com>
2017-06-24 01:06:30 -0600

    a couple more sio fixes that would be coming anyhow

--- block/sioplus-iosched.c
+++ block/sioplus-iosched.c
@@ -3,8 +3,8 @@
  * Based on Noop, Deadline and V(R) IO schedulers.
  *
  * Copyright (C) 2012 Miguel Boton <mboton@gmail.com>
- *           (C) 2013 Boy Petersen <boypetersen@gmail.com>
- *
+ *           (C) 2013, 2014 Boy Petersen <boypetersen@gmail.com>
+ *.              2015 Linux 3.10 compatibility by Matthew Alex <matthewalex@outlook.com>
  *
  * This algorithm does not do any kind of sorting, as it is aimed for
  * aleatory access devices, but it does some basic merging. We try to
@@ -13,8 +13,7 @@
  * Asynchronous and synchronous requests are not treated separately, but
  * we relay on deadlines to ensure fairness.
  *
- * The plus version fixes writes_starved not being initialized on startup
- * and also modifies the write starvation counting logic.
+ * The plus version incorporates several fixes and logic improvements.
  *
  */
 #include <linux/blkdev.h>
@@ -30,14 +29,14 @@
 enum { ASYNC, SYNC };
 
 /* Tunables */
-static const int sync_read_expire = (HZ / 16) * 9;	/* max time before a sync read is submitted. */
-static const int sync_write_expire = (HZ / 2) * 5;	/* max time before a sync write is submitted. */
+static const int sync_read_expire = (HZ / 4);	/* max time before a sync read is submitted. */
+static const int sync_write_expire = (HZ / 4) * 5;	/* max time before a sync write is submitted. */
 
-static const int async_read_expire = HZ * 4;	/* ditto for async, these limits are SOFT! */
-static const int async_write_expire = HZ * 16;	/* ditto for async, these limits are SOFT! */
+static const int async_read_expire = (HZ / 2);	/* ditto for async, these limits are SOFT! */
+static const int async_write_expire = (HZ * 2);	/* ditto for async, these limits are SOFT! */
 
-static const int writes_starved = 4;		/* max times reads can starve a write */
-static const int fifo_batch     = 1;		/* # of sequential requests treated as one
+static const int writes_starved = 1;		/* max times reads can starve a write */
+static const int fifo_batch     = 3;		/* # of sequential requests treated as one
 						   by the above parameters. For throughput. */
 
 /* Elevator data */
@@ -113,6 +112,9 @@ sio_choose_expired_request(struct sio_data *sd)
 {
 	struct request *rq;
 
+	/* Reset (non-expired-)batch-counter */
+	sd->batched = 0;
+
 	/*
 	 * Check expired requests.
 	 * Asynchronous requests have priority over synchronous.
@@ -141,6 +143,9 @@ sio_choose_request(struct sio_data *sd, int data_dir)
 {
 	struct list_head *sync = sd->fifo_list[SYNC];
 	struct list_head *async = sd->fifo_list[ASYNC];
+
+	/* Increase (non-expired-)batch-counter */
+	sd->batched++;
 
 	/*
 	 * Retrieve request from available fifo list.
@@ -171,8 +176,6 @@ sio_dispatch_request(struct sio_data *sd, struct request *rq)
 	rq_fifo_clear(rq);
 	elv_dispatch_add_tail(rq->q, rq);
 
-	sd->batched++;
-
 	if (rq_data_dir(rq)) {
 		sd->starved = 0;
 	} else {
@@ -193,14 +196,12 @@ sio_dispatch_requests(struct request_queue *q, int force)
 	 * Retrieve any expired request after a batch of
 	 * sequential requests.
 	 */
-	if (sd->batched > sd->fifo_batch) {
-		sd->batched = 0;
+	if (sd->batched >= sd->fifo_batch)
 		rq = sio_choose_expired_request(sd);
-	}
 
 	/* Retrieve request */
 	if (!rq) {
-		if (sd->starved > sd->writes_starved)
+		if (sd->starved >= sd->writes_starved)
 			data_dir = WRITE;
 
 		rq = sio_choose_request(sd, data_dir);
