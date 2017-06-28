25b4397eae40968c286e3030089f9f70393b51a3
robcore <robpatershuk@gmail.com>
2017-06-28 03:35:40 -0600

    percpu-refcount: add @gfp to percpu_ref_init()

--- include/linux/percpu-refcount.h
+++ include/linux/percpu-refcount.h
@@ -49,6 +49,7 @@
 #include <linux/kernel.h>
 #include <linux/percpu.h>
 #include <linux/rcupdate.h>
+#include <linux/gfp.h>
 
 struct percpu_ref;
 typedef void (percpu_ref_func_t)(struct percpu_ref *);
@@ -65,8 +66,8 @@ struct percpu_ref {
 	struct rcu_head		rcu;
 };
 
-__must_check int percpu_ref_init(struct percpu_ref *ref,
-				percpu_ref_func_t *release);
+int __must_check percpu_ref_init(struct percpu_ref *ref,
+				 percpu_ref_func_t *release, gfp_t gfp);
 void percpu_ref_reinit(struct percpu_ref *ref);
 void percpu_ref_exit(struct percpu_ref *ref);
 void percpu_ref_kill_and_confirm(struct percpu_ref *ref,
