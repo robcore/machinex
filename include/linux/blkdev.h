552c96a0c06f3fedf32ffd727228421a9cb4fb05
robcore <robpatershuk@gmail.com>
2017-06-27 19:21:51 -0600

    block: add queue flag for disabling SG merging

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -515,7 +515,8 @@ struct request_queue {
 #define QUEUE_FLAG_SAME_FORCE  18	/* force complete on same CPU */
 #define QUEUE_FLAG_DEAD        19	/* queue tear-down finished */
 #define QUEUE_FLAG_INIT_DONE   20	/* queue is initialized */
-#define QUEUE_FLAG_SANITIZE    21	/* supports SANITIZE */
+#define QUEUE_FLAG_NO_SG_MERGE 21	/* don't attempt to merge SG segments*/
+#define QUEUE_FLAG_SANITIZE    22	/* supports SANITIZE */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
