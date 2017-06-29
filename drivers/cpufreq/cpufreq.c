74715a3f87696c69fb970ce32f0ae87093c003ca
robcore <robpatershuk@gmail.com>
2017-06-28 20:30:48 -0600

    disable input boost while mitigating thermal so that the temperatures come down faster

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -332,7 +332,8 @@ void reapply_hard_limits(unsigned int cpu)
 	if (current_screen_state == CPUFREQ_HARDLIMIT_SCREEN_ON) {
 		if (input_boost_limit > policy->hlimit_min_screen_on &&
 			input_boost_limit < limited_max_freq_thermal &&
-			input_boost_limit <= policy->hlimit_max_screen_on)
+			input_boost_limit <= policy->hlimit_max_screen_on &&
+			limited_max_freq_thermal == policy->hlimit_max_screen_on)
 			policy->curr_limit_min = input_boost_limit;
 		else
 			policy->curr_limit_min = policy->hlimit_min_screen_on;
