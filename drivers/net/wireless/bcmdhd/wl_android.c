7cf94e4d5de5e483e4cab9779239d43c3af9a8d3
robcore <robpatershuk@gmail.com>
2017-07-02 23:57:16 -0600

    oops fixed up some shit

--- drivers/net/wireless/bcmdhd/wl_android.c
+++ drivers/net/wireless/bcmdhd/wl_android.c
@@ -486,20 +486,20 @@ static int wl_android_set_suspendopt(struct net_device *dev, char *command, int 
 #endif /* CUSTOMER_HW4 */
 		suspend_flag = *(command + strlen(CMD_SETSUSPENDOPT) + 1) - '0';
 
-	if (suspend_flag != 0) {
-		suspend_flag = 1;
-	}
-	ret_now = net_os_set_suspend_disable(dev, suspend_flag);
+		if (suspend_flag != 0)
+			suspend_flag = 1;
+		ret_now = net_os_set_suspend_disable(dev, suspend_flag);
 
-	if (ret_now != suspend_flag) {
-		if (!(ret = net_os_set_suspend(dev, ret_now, 1))) {
-			DHD_INFO(("%s: Suspend Flag %d -> %d\n",
-				__FUNCTION__, ret_now, suspend_flag));
-		} else {
-			DHD_ERROR(("%s: failed %d\n", __FUNCTION__, ret));
+		if (ret_now != suspend_flag) {
+			if (!(ret = net_os_set_suspend(dev, ret_now, 1)))
+				DHD_INFO(("%s: Suspend Flag %d -> %d\n",
+					__FUNCTION__, ret_now, suspend_flag));
+			else
+				DHD_ERROR(("%s: failed %d\n", __FUNCTION__, ret));
 		}
+#ifdef CUSTOMER_HW4
 	}
-
+#endif /* CUSTOMER_HW4 */
 	return ret;
 }
 
