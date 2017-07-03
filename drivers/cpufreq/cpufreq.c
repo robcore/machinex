3875cc302b2cefb1359a171f7034238dc71d5e8b
robcore <robpatershuk@gmail.com>
2017-07-03 02:19:46 -0600

    REVERT the thermal/input disabler for now, my guess is it needs far more syncronization

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -318,9 +318,6 @@ struct cpufreq_frequency_table *cpufreq_frequency_get_table(unsigned int cpu)
 EXPORT_SYMBOL_GPL(cpufreq_frequency_get_table);
 /* Update limits in cpufreq */
 
-static bool thermal_disables_boost = true;
-module_param_named(tdb, thermal_disables_boost, bool, 0644);
-
 void reapply_hard_limits(unsigned int cpu)
 {
 	struct cpufreq_policy *policy;
@@ -336,14 +333,12 @@ void reapply_hard_limits(unsigned int cpu)
 	if (current_screen_state == CPUFREQ_HARDLIMIT_SCREEN_ON) {
 		if (input_boost_limit > policy->hlimit_min_screen_on &&
 			input_boost_limit < limited_max_freq_thermal &&
-			input_boost_limit <= policy->hlimit_max_screen_on) {
-			if (thermal_disables_boost && 
-				limited_max_freq_thermal < policy->hlimit_max_screen_on)
-				policy->curr_limit_min = policy->hlimit_min_screen_on;
-			else if (!thermal_disables_boost &&
-					limited_max_freq_thermal < policy->hlimit_max_screen_on)
-				policy->curr_limit_min = input_boost_limit;
-		}
+			input_boost_limit <= policy->hlimit_max_screen_on &&
+			limited_max_freq_thermal == policy->hlimit_max_screen_on)
+			policy->curr_limit_min = input_boost_limit;
+		else
+			policy->curr_limit_min = policy->hlimit_min_screen_on;
+
 		if (limited_max_freq_thermal >= policy->cpuinfo.min_freq &&
 			limited_max_freq_thermal < policy->hlimit_max_screen_on)
 			policy->curr_limit_max = limited_max_freq_thermal;
