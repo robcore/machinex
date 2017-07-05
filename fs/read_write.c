b004e7386807ba9a919d2efcffbd9ab489186e0a
robcore <robpatershuk@gmail.com>
2017-07-05 16:09:55 -0600

    revert me

--- fs/read_write.c
+++ fs/read_write.c
@@ -17,11 +17,25 @@
 #include <linux/pagemap.h>
 #include <linux/splice.h>
 #include <linux/compat.h>
-#include "read_write.h"
 #include "internal.h"
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
+
+ssize_t do_aio_read(struct kiocb *kiocb, const struct iovec *iov,
+		unsigned long nr_segs, loff_t pos);
+ssize_t do_aio_write(struct kiocb *kiocb, const struct iovec *iov,
+		unsigned long nr_segs, loff_t pos);
+ssize_t do_sync_readv_writev(struct file *filp, const struct iovec *iov,
+		unsigned long nr_segs, size_t len, loff_t *ppos, iov_fn_t fn);
+ssize_t do_loop_readv_writev(struct file *filp, struct iovec *iov,
+		unsigned long nr_segs, loff_t *ppos, io_fn_t fn);
+
+/*
+typedef ssize_t (*io_fn_t)(struct file *, char __user *, size_t, loff_t *);
+typedef ssize_t (*iov_fn_t)(struct kiocb *, const struct iovec *,
+		unsigned long, loff_t);
+*/
 
 const struct file_operations generic_ro_fops = {
 	.llseek		= generic_file_llseek,
