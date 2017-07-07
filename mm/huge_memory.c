0605ba13cfba64e9b38b37357060d49657cdb8d5
robcore <robpatershuk@gmail.com>
2017-07-07 10:18:17 -0600

    thp-remove-some-code-depend-on-CONFIG_NUMA

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2612,11 +2612,8 @@ static void khugepaged_wait_work(void)
 
 static void khugepaged_loop(void)
 {
-	struct page *hpage;
+	struct page *hpage = NULL;
 
-#ifdef CONFIG_NUMA
-	hpage = NULL;
-#endif
 	while (likely(khugepaged_enabled())) {
 #ifndef CONFIG_NUMA
 		hpage = khugepaged_alloc_hugepage();
@@ -2630,10 +2627,9 @@ static void khugepaged_loop(void)
 #endif
 
 		khugepaged_do_scan(&hpage);
-#ifndef CONFIG_NUMA
-		if (hpage)
+
+		if (!IS_ERR_OR_NULL(hpage))
 			put_page(hpage);
-#endif
 
 		khugepaged_wait_work();
 	}
