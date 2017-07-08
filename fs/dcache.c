fa72ea797d70f9a844f768a7b325e2d75a9574c0
robcore <robpatershuk@gmail.com>
2017-07-08 00:06:30 -0600

    revert that for now

--- fs/dcache.c
+++ fs/dcache.c
@@ -2741,6 +2741,7 @@ static char *__dentry_path(struct dentry *dentry, char *buf, int buflen)
 
 	while (!IS_ROOT(dentry)) {
 		struct dentry *parent = dentry->d_parent;
+		int error;
 
 		prefetch(parent);
 		spin_lock(&dentry->d_lock);
