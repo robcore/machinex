ef193d256445793f842255135b703acd2a5d5fc4
robcore <robpatershuk@gmail.com>
2017-07-09 22:11:22 -0600

    revert that

--- fs/proc/namespaces.c
+++ fs/proc/namespaces.c
@@ -11,7 +11,6 @@
 #include <net/net_namespace.h>
 #include <linux/ipc_namespace.h>
 #include <linux/pid_namespace.h>
-#include <linux/user_namespace.h>
 #include "internal.h"
 
 
@@ -27,9 +26,6 @@ static const struct proc_ns_operations *ns_entries[] = {
 #endif
 #ifdef CONFIG_PID_NS
 	&pidns_operations,
-#endif
-#ifdef CONFIG_USER_NS
-	&userns_operations,
 #endif
 	&mntns_operations,
 };
