c4729b3b8b2411e90e7aa834b8c12e5a64a5117a
robcore <robpatershuk@gmail.com>
2017-07-14 22:09:15 -0600

    posix-timers: Move compat_timer_create() to native

--- kernel/compat.c
+++ kernel/compat.c
@@ -576,24 +576,6 @@ int put_compat_itimerspec(struct compat_itimerspec __user *dst,
 	return 0;
 }
 
-COMPAT_SYSCALL_DEFINE3(timer_create, clockid_t, which_clock,
-		       struct compat_sigevent __user *, timer_event_spec,
-		       timer_t __user *, created_timer_id)
-{
-	struct sigevent __user *event = NULL;
-
-	if (timer_event_spec) {
-		struct sigevent kevent;
-
-		event = compat_alloc_user_space(sizeof(*event));
-		if (get_compat_sigevent(&kevent, timer_event_spec) ||
-		    copy_to_user(event, &kevent, sizeof(*event)))
-			return -EFAULT;
-	}
-
-	return sys_timer_create(which_clock, event, created_timer_id);
-}
-
 static long compat_clock_nanosleep_restart(struct restart_block *restart)
 {
 	long err;
