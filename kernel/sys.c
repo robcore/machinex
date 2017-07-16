aed976e79514fc3ccade4b2d5612d4b926b7c6b3
robcore <robpatershuk@gmail.com>
2017-07-16 12:49:19 -0600

    moar

--- kernel/sys.c
+++ kernel/sys.c
@@ -51,6 +51,7 @@
 #include <linux/kprobes.h>
 #include <linux/user_namespace.h>
 #include <linux/sched.h>
+#include <linux/sched/autogroup.h>
 #include <linux/sched/loadavg.h>
 #include <linux/rcupdate.h>
 #include <linux/cred.h>
