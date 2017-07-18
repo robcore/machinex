9432d9f4fe0c15855c7a5539b3d389ecc126305f
robcore <robpatershuk@gmail.com>
2017-07-18 08:55:21 -0600

    some typos

--- drivers/video/s1d13xxxfb.c
+++ drivers/video/s1d13xxxfb.c
@@ -864,7 +864,7 @@ s1d13xxxfb_probe(struct platform_device *pdev)
 		printk(KERN_INFO PFX
 			"unknown chip production id %i, revision %i\n",
 			prod_id, revision);
-		printk(KERN_INFO PFX "please contant maintainer\n");
+		printk(KERN_INFO PFX "please contact maintainer\n");
 		goto bail;
 	}
 
