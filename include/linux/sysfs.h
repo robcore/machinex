db8e99c400b2ab672153eea466ba8a77ca3a9551
robcore <robpatershuk@gmail.com>
2017-07-21 08:12:24 -0600

    sysfs - add generic ATTR_RW macro

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -89,6 +89,8 @@ struct attribute_group {
 #define __ATTR_RW(_name) __ATTR(_name, (S_IWUSR | S_IRUGO),		\
 			 _name##_show, _name##_store)
 
+#define __ATTR_RW(_name) __ATTR(_name, 0644, _name##_show, _name##_store)
+
 #define __ATTR_NULL { .attr = { .name = NULL } }
 
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
