3971f8fc501d01c1de2528630b1cd035ed256adb
robcore <robpatershuk@gmail.com>
2017-07-20 20:11:00 -0600

    reverted

--- include/uapi/linux/msdos_fs.h
+++ include/uapi/linux/msdos_fs.h
@@ -102,7 +102,6 @@ struct __fat_dirent {
 /* <linux/videotext.h> has used 0x72 ('r') in collision, so skip a few */
 #define FAT_IOCTL_GET_ATTRIBUTES	_IOR('r', 0x10, __u32)
 #define FAT_IOCTL_SET_ATTRIBUTES	_IOW('r', 0x11, __u32)
-/*Android kernel has used 0x12, so we use 0x13*/
 #define VFAT_IOCTL_GET_VOLUME_ID	_IOR('r', 0x12, __u32)
 #if defined(CONFIG_VMWARE_MVP)
 #define FAT_IOCTL_VMW_EXTEND		_IO('r', 0x42)
@@ -131,10 +130,6 @@ struct fat_boot_sector {
 			__u8	drive_number;	/* Physical drive number */
 			__u8	state;		/* undocumented, but used
 						   for mount state. */
-			__u8	signature;  /* extended boot signature */
-			__u8	vol_id[4];	/* volume ID */
-			__u8	vol_label[11];	/* volume label */
-			__u8	fs_type[8];		/* file system type */
 			/* other fiealds are not added here */
 		} fat16;
 
@@ -154,10 +149,6 @@ struct fat_boot_sector {
 			__u8	drive_number;   /* Physical drive number */
 			__u8    state;       	/* undocumented, but used
 						   for mount state. */
-			__u8	signature;  /* extended boot signature */
-			__u8	vol_id[4];	/* volume ID */
-			__u8	vol_label[11];	/* volume label */
-			__u8	fs_type[8];		/* file system type */
 			/* other fiealds are not added here */
 		} fat32;
 	};
