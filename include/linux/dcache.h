8186ed682b4f0a3069bf0224e7e6b207f7f2ef25
robcore <robpatershuk@gmail.com>
2017-07-22 16:00:34 -0600

    super - fix calc of shrinkable objects

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -452,4 +452,8 @@ static inline struct dentry *d_backing_dentry(struct dentry *upper)
 	return upper;
 }
 
+static inline unsigned long vfs_pressure_ratio(unsigned long val)
+{
+	return mult_frac(val, sysctl_vfs_cache_pressure, 100);
+}
 #endif	/* __LINUX_DCACHE_H */
