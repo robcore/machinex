db5863452a1855f1260af3bcb1c739a02144e6fb
robcore <robpatershuk@gmail.com>
2017-07-22 11:07:20 -0600

    revert fs stuff for now

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1861,7 +1861,7 @@ SYSCALL_DEFINE4(epoll_ctl, int, epfd, int, op, int, fd,
 {
 	int error;
 	int full_check = 0;
-	struct fd f, tf;
+	struct file *file, *tfile;
 	struct eventpoll *ep;
 	struct epitem *epi;
 	struct epoll_event epds;
@@ -1872,19 +1872,20 @@ SYSCALL_DEFINE4(epoll_ctl, int, epfd, int, op, int, fd,
 	    copy_from_user(&epds, event, sizeof(struct epoll_event)))
 		goto error_return;
 
+	/* Get the "struct file *" for the eventpoll file */
 	error = -EBADF;
-	f = fdget(epfd);
-	if (!f.file)
+	file = fget(epfd);
+	if (!file)
 		goto error_return;
 
 	/* Get the "struct file *" for the target file */
-	tf = fdget(fd);
-	if (!tf.file)
+	tfile = fget(fd);
+	if (!tfile)
 		goto error_fput;
 
 	/* The target file descriptor must support poll */
 	error = -EPERM;
-	if (!tf.file->f_op || !tf.file->f_op->poll)
+	if (!tfile->f_op || !tfile->f_op->poll)
 		goto error_tgt_fput;
 
 	/* Check if EPOLLWAKEUP is allowed */
@@ -1897,14 +1898,14 @@ SYSCALL_DEFINE4(epoll_ctl, int, epfd, int, op, int, fd,
 	 * adding an epoll file descriptor inside itself.
 	 */
 	error = -EINVAL;
-	if (f.file == tf.file || !is_file_epoll(f.file))
+	if (file == tfile || !is_file_epoll(file))
 		goto error_tgt_fput;
 
 	/*
 	 * At this point it is safe to assume that the "private_data" contains
 	 * our own data structure.
 	 */
-	ep = f.file->private_data;
+	ep = file->private_data;
 
 	/*
 	 * When we insert an epoll file descriptor, inside another epoll file
@@ -1924,19 +1925,19 @@ SYSCALL_DEFINE4(epoll_ctl, int, epfd, int, op, int, fd,
 	mutex_lock_nested(&ep->mtx, 0);
 	if (op == EPOLL_CTL_ADD) {
 		if (!list_empty(&file->f_ep_links) ||
-						is_file_epoll(tf.file)) {
+						is_file_epoll(tfile)) {
 			full_check = 1;
 			mutex_unlock(&ep->mtx);
 			mutex_lock(&epmutex);
 			if (is_file_epoll(tfile)) {
 				error = -ELOOP;
-				if (ep_loop_check(ep, tf.file) != 0) {
+				if (ep_loop_check(ep, tfile) != 0) {
 					clear_tfile_check_list();
 					goto error_tgt_fput;
 				}
 			} else
-				list_add(&tf.file->f_tfile_llink, &tfile_check_list);
-
+				list_add(&tfile->f_tfile_llink,
+							&tfile_check_list);
 			mutex_lock_nested(&ep->mtx, 0);
 			if (is_file_epoll(tfile)) {
 				tep = tfile->private_data;
@@ -1950,14 +1951,14 @@ SYSCALL_DEFINE4(epoll_ctl, int, epfd, int, op, int, fd,
 	 * above, we can be sure to be able to use the item looked up by
 	 * ep_find() till we release the mutex.
 	 */
-	epi = ep_find(ep, tf.file, fd);
+	epi = ep_find(ep, tfile, fd);
 
 	error = -EINVAL;
 	switch (op) {
 	case EPOLL_CTL_ADD:
 		if (!epi) {
 			epds.events |= POLLERR | POLLHUP;
-			error = ep_insert(ep, &epds, tf.file, fd);
+			error = ep_insert(ep, &epds, tfile, fd, full_check);
 		} else
 			error = -EEXIST;
 		if (full_check)
@@ -1991,9 +1992,9 @@ error_tgt_fput:
 	if (full_check)
 		mutex_unlock(&epmutex);
 
-	fdput(tf);
+	fput(tfile);
 error_fput:
-	fdput(f);
+	fput(file);
 error_return:
 
 	return error;
--- fs/open.c
+++ fs/open.c
@@ -504,13 +504,14 @@ out_unlock:
 
 SYSCALL_DEFINE2(fchmod, unsigned int, fd, umode_t, mode)
 {
-	struct fd f = fdget(fd);
+	struct file * file;
 	int err = -EBADF;
 
-	if (f.file) {
-		audit_inode(NULL, f.file->f_path.dentry, 0);
-		err = chmod_common(&f.file->f_path, mode);
-		fdput(f);
+	file = fget(fd);
+	if (file) {
+		audit_inode(NULL, file->f_path.dentry, 0);
+		err = chmod_common(&file->f_path, mode);
+		fput(file);
 	}
 	return err;
 }
