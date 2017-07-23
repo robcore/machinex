2b164b1a4a9eac6f69412d14bc24a0f75263bf39
robcore <robpatershuk@gmail.com>
2017-07-23 00:07:29 -0600

    fix my ref init(flags)

--- fs/aio.c
+++ fs/aio.c
@@ -563,10 +563,10 @@ static struct kioctx *ioctx_alloc(unsigned nr_events)
 
 	ctx->max_reqs = nr_events;
 
-	if (percpu_ref_init(&ctx->users, free_ioctx_users))
+	if (percpu_ref_init(&ctx->users, free_ioctx_users, GFP_KERNEL))
 		goto err;
 
-	if (percpu_ref_init(&ctx->reqs, free_ioctx_reqs))
+	if (percpu_ref_init(&ctx->reqs, free_ioctx_reqs, GFP_KERNEL))
 		goto err;
 
 	spin_lock_init(&ctx->ctx_lock);
