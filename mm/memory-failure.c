fce38ade041d7e6fd7ef7893bd0b26d7c77c7672
robcore <robpatershuk@gmail.com>
2017-07-22 20:10:56 -0600

    hwpoison-always-unset-MIGRATE_ISOLATE-before-return

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -1560,7 +1560,7 @@ int soft_offline_page(struct page *page, int flags)
 
 	ret = get_any_page(page, pfn, flags);
 	if (ret < 0)
-		return ret;
+		goto unset;
 	if (ret) { /* for in-use pages */
 		if (PageHuge(page))
 			ret = soft_offline_huge_page(page, flags);
@@ -1577,6 +1577,7 @@ int soft_offline_page(struct page *page, int flags)
 			atomic_long_inc(&num_poisoned_pages);
 		}
 	}
+unset:
 	unset_migratetype_isolate(page, MIGRATE_MOVABLE);
 	return ret;
 }
