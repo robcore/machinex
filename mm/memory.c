c3ddce67497d24e1a4fe279464ee17eefd843541
robcore <robpatershuk@gmail.com>
2017-07-23 08:29:48 -0600

    more dox

--- mm/memory.c
+++ mm/memory.c
@@ -456,8 +456,6 @@ static inline void free_pud_range(struct mmu_gather *tlb, pgd_t *pgd,
 
 /*
  * This function frees user-level page tables of a process.
- *
- * Must be called with pagetable lock held.
  */
 void free_pgd_range(struct mmu_gather *tlb,
 			unsigned long addr, unsigned long end,
