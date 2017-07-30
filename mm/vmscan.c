20bd0fe12b47fe8954e18388e2787d20e3bd332b
robcore <robpatershuk@gmail.com>
2017-07-29 19:49:02 -0600

    mm-vmscan-do-not-check-compaction_ready-on-promoted

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2359,6 +2359,7 @@ static bool shrink_zones(struct zonelist *zonelist, struct scan_control *sc)
 	struct shrink_control shrink = {
 		.gfp_mask = sc->gfp_mask,
 	};
+	enum zone_type requested_highidx = gfp_zone(sc->gfp_mask);
 
 	/*
 	 * If the number of buffer_heads in the machine exceeds the maximum
@@ -2399,7 +2400,8 @@ static bool shrink_zones(struct zonelist *zonelist, struct scan_control *sc)
 				 * noticeable problem, like transparent huge
 				 * page allocations.
 				 */
-				if (compaction_ready(zone, sc)) {
+				if ((zonelist_zone_idx(z) <= requested_highidx)
+				    && compaction_ready(zone, sc)) {
 					aborted_reclaim = true;
 					continue;
 				}
