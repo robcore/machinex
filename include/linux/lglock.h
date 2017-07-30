caa2902656701aad07d486b3328a2b03cd95406d
robcore <robpatershuk@gmail.com>
2017-07-29 20:10:32 -0600

    lglock: map to spinlock when !CONFIG_SMP

--- include/linux/lglock.h
+++ include/linux/lglock.h
@@ -37,6 +37,8 @@
 
 #ifdef CONFIG_SMP
 
+#ifdef CONFIG_SMP
+
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 #define LOCKDEP_INIT_MAP lockdep_init_map
 #else
@@ -73,6 +75,20 @@ void lg_double_unlock(struct lglock *lg, int cpu1, int cpu2);
 
 void lg_global_lock(struct lglock *lg);
 void lg_global_unlock(struct lglock *lg);
+
+#else
+/* When !CONFIG_SMP, map lglock to spinlock */
+#define lglock spinlock
+#define DEFINE_LGLOCK(name) DEFINE_SPINLOCK(name)
+#define DEFINE_STATIC_LGLOCK(name) static DEFINE_SPINLOCK(name)
+#define lg_lock_init(lg, name) spin_lock_init(lg)
+#define lg_local_lock spin_lock
+#define lg_local_unlock spin_unlock
+#define lg_local_lock_cpu(lg, cpu) spin_lock(lg)
+#define lg_local_unlock_cpu(lg, cpu) spin_unlock(lg)
+#define lg_global_lock spin_lock
+#define lg_global_unlock spin_unlock
+#endif
 
 #else
 /* When !CONFIG_SMP, map lglock to spinlock */
