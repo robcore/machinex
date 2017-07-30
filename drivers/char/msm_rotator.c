ca708efd748613e7a5afce57a9cbaba37811901b
robcore <robpatershuk@gmail.com>
2017-07-30 01:31:08 -0600

    speed shit up in rotator, use system_wq so we can mod the delayed work. also change the immediately scheduled work to delayed (but at zero) so we can use mod

--- drivers/char/msm_rotator.c
+++ drivers/char/msm_rotator.c
@@ -469,12 +469,12 @@ void msm_rotator_imem_free(int requestor)
 {
 #ifdef CONFIG_MSM_ROTATOR_USE_IMEM
 	if (msm_rotator_dev->imem_owner == requestor) {
-		schedule_delayed_work(&msm_rotator_dev->imem_clk_work, HZ);
+		mod_delayed_work(system_wq, &msm_rotator_dev->imem_clk_work, msecs_to_jiffies(100));
 		mutex_unlock(&msm_rotator_dev->imem_lock);
 	}
 #else
 	if (requestor == JPEG_REQUEST)
-		schedule_delayed_work(&msm_rotator_dev->imem_clk_work, HZ);
+		mod_delayed_work(system_wq, &msm_rotator_dev->imem_clk_work, msecs_to_jiffies(100));
 #endif
 }
 EXPORT_SYMBOL(msm_rotator_imem_free);
@@ -2159,7 +2159,7 @@ do_rotate_exit:
 #ifdef CONFIG_MSM_ROTATOR_USE_IMEM
 	msm_rotator_imem_free(ROTATOR_REQUEST);
 #endif
-	schedule_delayed_work(&msm_rotator_dev->rot_clk_work, HZ);
+	mod_delayed_work(system_wq, &msm_rotator_dev->rot_clk_work, msecs_to_jiffies(100));
 	put_img(dstp1_file, dstp1_ihdl, ROTATOR_DST_DOMAIN,
 		img_info->secure);
 	put_img(srcp1_file, srcp1_ihdl, ROTATOR_SRC_DOMAIN, 0);
@@ -2249,7 +2249,7 @@ static int msm_rotator_do_rotate(unsigned long arg)
 		atomic_set(&mrd->commit_q_w, 0);
 	atomic_inc(&mrd->commit_q_cnt);
 
-	schedule_work(&mrd->commit_work);
+	mod_delayed_work(system_wq, &mrd->commit_work, 0);
 	mutex_unlock(&mrd->commit_mutex);
 
 	if (info.wait_for_finish)
@@ -3011,7 +3011,7 @@ static int msm_rotator_probe(struct platform_device *pdev)
 	}
 
 	init_waitqueue_head(&msm_rotator_dev->wq);
-	INIT_WORK(&msm_rotator_dev->commit_work, rot_commit_wq_handler);
+	INIT_DELAYED_WORK(&msm_rotator_dev->commit_work, rot_commit_wq_handler);
 	init_completion(&msm_rotator_dev->commit_comp);
 	mutex_init(&msm_rotator_dev->commit_mutex);
 	mutex_init(&msm_rotator_dev->commit_wq_mutex);
