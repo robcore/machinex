a824727ac43eb4b274277cb074456d93263c0765
robcore <robpatershuk@gmail.com>
2017-07-29 20:01:59 -0600

    lib/idr.c: use RCU_INIT_POINTER(x, NULL)

--- lib/idr.c
+++ lib/idr.c
@@ -375,7 +375,7 @@ static void sub_remove(struct idr *idp, int shift, int id)
 	n = id & IDR_MASK;
 	if (likely(p != NULL && test_bit(n, &p->bitmap))){
 		__clear_bit(n, &p->bitmap);
-		rcu_assign_pointer(p->ary[n], NULL);
+		RCU_INIT_POINTER(p->ary[n], NULL);
 		to_free = NULL;
 		while(*paa && ! --((**paa)->count)){
 			if (to_free)
@@ -456,7 +456,7 @@ void idr_remove_all(struct idr *idp)
 
 	n = idp->layers * IDR_BITS;
 	p = idp->top;
-	rcu_assign_pointer(idp->top, NULL);
+	RCU_INIT_POINTER(idp->top, NULL);
 	max = 1 << n;
 
 	id = 0;
