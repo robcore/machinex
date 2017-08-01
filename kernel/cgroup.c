4f130e31e97a5eb91814196499e8f7ed4e6c0b8a
robcore <robpatershuk@gmail.com>
2017-08-01 13:23:39 -0600

    change rcu_lockdep_assert back to our upstreamed RCU_LOCKDEP_WARN

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -105,7 +105,7 @@ static DEFINE_SPINLOCK(cgroup_idr_lock);
 static DEFINE_SPINLOCK(release_agent_path_lock);
 
 #define cgroup_assert_mutex_or_rcu_locked()				\
-	rcu_lockdep_assert(rcu_read_lock_held() ||			\
+	RCU_LOCKDEP_WARN(rcu_read_lock_held() ||			\
 			   lockdep_is_held(&cgroup_mutex),		\
 			   "cgroup_mutex or RCU read lock required");
 
