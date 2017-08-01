9e927b2460466cbabd0e9eb9af63986a289a07ba
robcore <robpatershuk@gmail.com>
2017-08-01 13:03:05 -0600

    cgroup-remove-cgroup_destory_css_killed

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -157,9 +157,6 @@ struct cgroup {
 
 	int id;				/* idr allocated in-hierarchy ID */
 
-	/* the number of attached css's */
-	int nr_css;
-
 	/*
 	 * If this cgroup contains any tasks, it contributes one to
 	 * populated_cnt.  All children with non-zero popuplated_cnt of
