2619af0aca1135ba6f61cdae7014c1d12be91333
robcore <robpatershuk@gmail.com>
2017-08-02 18:16:34 -0600

    genirq-Move-irq_fixup_move_pending-to-core.;

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -484,15 +484,10 @@ extern int irq_affinity_online_cpu(unsigned int cpu);
 void irq_move_irq(struct irq_data *data);
 void irq_move_masked_irq(struct irq_data *data);
 void irq_force_complete_move(struct irq_desc *desc);
-bool irq_fixup_move_pending(struct irq_desc *desc, bool force_clear);
 #else
 static inline void irq_move_irq(struct irq_data *data) { }
 static inline void irq_move_masked_irq(struct irq_data *data) { }
 static inline void irq_force_complete_move(struct irq_desc *desc) { }
-static inline bool irq_fixup_move_pending(struct irq_desc *desc, bool fclear)
-{
-	return false;
-}
 #endif
 
 extern int no_irq_affinity;
