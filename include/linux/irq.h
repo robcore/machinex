997c3511abe9ca99de96948dd4521f0d718d589d
robcore <robpatershuk@gmail.com>
2017-08-02 18:16:09 -0600

    genirq/cpuhotplug: Add support for cleaning up move in progress;

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -483,10 +483,12 @@ extern int irq_affinity_online_cpu(unsigned int cpu);
 #if defined(CONFIG_SMP) && defined(CONFIG_GENERIC_PENDING_IRQ)
 void irq_move_irq(struct irq_data *data);
 void irq_move_masked_irq(struct irq_data *data);
+void irq_force_complete_move(struct irq_desc *desc);
 bool irq_fixup_move_pending(struct irq_desc *desc, bool force_clear);
 #else
 static inline void irq_move_irq(struct irq_data *data) { }
 static inline void irq_move_masked_irq(struct irq_data *data) { }
+static inline void irq_force_complete_move(struct irq_desc *desc) { }
 static inline bool irq_fixup_move_pending(struct irq_desc *desc, bool fclear)
 {
 	return false;
