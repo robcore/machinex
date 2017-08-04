83578c4c068cd4ae2a704e9c40353a4cd11104ca
robcore <robpatershuk@gmail.com>
2017-08-03 22:40:25 -0600

    kexec-export-free_huge_page-to-VMCOREINFO.

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -70,6 +70,7 @@ int dequeue_hwpoisoned_huge_page(struct page *page);
 bool isolate_huge_page(struct page *page, struct list_head *list);
 void putback_active_hugepage(struct page *page);
 bool is_hugepage_active(struct page *page);
+void free_huge_page(struct page *page);
 void copy_huge_page(struct page *dst, struct page *src);
 
 #ifdef CONFIG_ARCH_WANT_HUGE_PMD_SHARE
