1e406aadce340f40d5e7b1418a39eb5eafa1c78b
robcore <robpatershuk@gmail.com>
2017-08-04 12:41:23 -0600

    proc hacking part 2

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2873,7 +2873,7 @@ struct dentry *proc_pid_lookup(struct inode *dir, struct dentry * dentry, unsign
 	unsigned tgid;
 	struct pid_namespace *ns;
 
-	tgid = name_to_int(dentry);
+	tgid = name_to_int(&dentry->d_name);
 	if (tgid == ~0U)
 		goto out;
 
