db92fc0af1889ce670709ad151f8e4f09c599a70
robcore <robpatershuk@gmail.com>
2017-08-04 17:04:14 -0600

    trying something neat with our shut down op

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -546,15 +546,12 @@ static void an30259a_start_led_pattern(int mode)
 			booted = true;
 		} else {
 			pr_info("LED Powering Pattern OFF\n");
-			leds_on(LED_R, true, true, LED_DYNAMIC_CURRENT);
 			leds_on(LED_G, true, true, LED_DYNAMIC_CURRENT);
 			leds_on(LED_B, true, true, LED_DYNAMIC_CURRENT);
-			leds_set_slope_mode(client, LED_R,
-					0, 5, 0, 0, 1, 1, 0, 0, 0, 0);
 			leds_set_slope_mode(client, LED_G,
-					0, 8, 15, 15, 1, 1, 0, 0, 0, 0);
+					0, 15, 7, 1, 6, 3, 0, 0, 0, 0);
 			leds_set_slope_mode(client, LED_B,
-					0, 0, 0, 15, 1, 1, 0, 0, 0, 0);
+					0, 15, 7, 1, 6, 3, 0, 0, 0, 0);
 		}
 		break;
 
