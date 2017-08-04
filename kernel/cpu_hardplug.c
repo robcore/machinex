0040d375b1a54df322df99501fdd3269cfe5c835
robcore <robpatershuk@gmail.com>
2017-08-04 10:05:56 -0600

    working on cpu hardplug

---
+++ kernel/cpu_hardplug.c
@@ -0,0 +1,19 @@
+/* CPU Hardplug.
+ * (C) 2017 Rob Patershuk "robcore"
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ */
+#include <linux/suspend.h>
+#include <linux/smp.h>
+#include <linux/init.h>
+#include <linux/notifier.h>
+#include <linux/stop_machine.h>
+#include <linux/mutex.h>
+#include <linux/tick.h>
+#include <linux/irq.h>
+#include <linux/smpboot.h>
+#include <linux/cpu.h>
+#include <linux/powersuspend.h>
+
