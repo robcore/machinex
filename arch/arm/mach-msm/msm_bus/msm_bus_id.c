1ea50a176b48bea2c4ed790bbd20db35c1fd5110
robcore <robpatershuk@gmail.com>
2017-08-08 10:47:57 -0600

    whitespace and lisence

--- arch/arm/mach-msm/msm_bus/msm_bus_id.c
+++ arch/arm/mach-msm/msm_bus/msm_bus_id.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -71,7 +71,6 @@ static int msm_bus_get_iid(int id)
 	return CHECK_ID(((id < SLAVE_ID_KEY) ? master_iids[id] :
 		slave_iids[id - SLAVE_ID_KEY]), id);
 }
-
 
 static struct msm_bus_board_algorithm msm_bus_id_algo = {
 	.get_iid = msm_bus_get_iid,
