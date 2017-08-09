a671c88aca58f99c50627ae0bc728bc1d5864bc1
robcore <robpatershuk@gmail.com>
2017-08-09 01:29:11 -0600

    a teeeensy bit of led work

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -581,13 +581,13 @@ static void an30259a_start_led_pattern(int mode)
 			pr_info("LED Powering Pattern ON\n");
 			leds_on(LED_R, true, true, LED_DEFAULT_CURRENT);
 			leds_set_slope_mode(client, LED_R,
-					0, 15, 7, 0, 2, 2, 0, 0, 0, 0);
+					0, 5, 3, 0, 2, 2, 0, 0, 0, 0);
 			leds_on(LED_G, true, true, LED_DEFAULT_CURRENT);
 			leds_set_slope_mode(client, LED_G,
-					0, 15, 7, 0, 2, 2, 0, 0, 0, 0);
+					0, 15, 8, 3, 2, 2, 0, 0, 0, 0);
 			leds_on(LED_B, true, true, LED_DEFAULT_CURRENT);
 			leds_set_slope_mode(client, LED_B,
-					3, 15, 7, 0, 2, 2, 0, 0, 0, 0);
+					3, 15, 8, 0, 2, 2, 0, 0, 0, 0);
 			booted = true;
 			break;
 		} else {
