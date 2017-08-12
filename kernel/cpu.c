9e04f83b5ac29782fa21b87b080bc75eabe02962
robcore <robpatershuk@gmail.com>
2017-08-11 18:39:29 -0600

    spacing

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1176,7 +1176,6 @@ void enable_nonboot_cpus(void)
 	for_each_cpu(cpu, frozen_cpus) {
 		if (cpumask_test_cpu(cpu, max_screen_off))
 			continue;
-
 		error = _cpu_up(cpu, 1, CPUHP_ONLINE);
 		if (!error) {
 			pr_info("CPU%d is up\n", cpu);
