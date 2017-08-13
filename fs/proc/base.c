c334afa6eededabce89b2986f9d90018cc9cfb6c
robcore <robpatershuk@gmail.com>
2017-08-12 19:21:07 -0600

    remove the annoying fucking oom score adjacent bs warning

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -967,9 +967,6 @@ static ssize_t oom_adjust_write(struct file *file, const char __user *buf,
 	 * Warn that /proc/pid/oom_adj is deprecated, see
 	 * Documentation/feature-removal-schedule.txt.
 	 */
-	pr_warn_once("%s (%d): /proc/%d/oom_adj is deprecated, please use /proc/%d/oom_score_adj instead.\n",
-		  current->comm, task_pid_nr(current), task_pid_nr(task),
-		  task_pid_nr(task));
 	task->signal->oom_adj = oom_adjust;
 	/*
 	 * Scale /proc/pid/oom_score_adj appropriately ensuring that a maximum
