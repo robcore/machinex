d8a65f72851b4637227654df7f7e28939dd76500
robcore <robpatershuk@gmail.com>
2017-08-14 21:51:16 -0600

    reverted

--- fs/dcache.c
+++ fs/dcache.c
@@ -2720,17 +2720,6 @@ out_err:
 	return ret;
 }
 
-char *simple_dname(struct dentry *dentry, char *buffer, int buflen)
-{
-	char *end = buffer + buflen;
-	/* these dentries are never renamed, so d_lock is not needed */
-	if (prepend(&end, &buflen, " (deleted)", 11) ||
-	    prepend_name(&end, &buflen, &dentry->d_name) ||
-	    prepend(&end, &buflen, "/", 1))  
-		end = ERR_PTR(-ENAMETOOLONG);
-	return end;  
-}
-
 /*
  * Prepare an anonymous dentry for life in the superblock's dentry tree as a
  * named dentry in place of the dentry to be replaced.
