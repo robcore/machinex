a30010a71986f5957ebc4becc8204a36819f28d3
robcore <robpatershuk@gmail.com>
2017-08-16 12:21:47 -0600

    tools/lockdep/remove private headers

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -42,7 +42,6 @@
 #include <linux/lockdep.h>
 #include <asm/processor.h>
 #include <linux/cpumask.h>
-#include <linux/spinlock.h>
 
 #define ULONG_CMP_GE(a, b)	(ULONG_MAX / 2 >= (a) - (b))
 #define ULONG_CMP_LT(a, b)	(ULONG_MAX / 2 < (a) - (b))
