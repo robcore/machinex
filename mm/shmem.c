61b85f3ffda7462c703520cbf44d2489a46f14c8
robcore <robpatershuk@gmail.com>
2017-08-17 14:39:17 -0600

    trivial cleanup

--- mm/shmem.c
+++ mm/shmem.c
@@ -814,7 +814,8 @@ static struct page *shmem_swapin(swp_entry_t swap, gfp_t gfp,
 	/* Create a pseudo vma that just contains the policy */
 	pvma.vm_start = 0;
 	/* Bias interleave by inode number to distribute better across nodes */
-	pvma.vm_pgoff = index + info->vfs_inode.i_ino;	pvma.vm_ops = NULL;
+	pvma.vm_pgoff = index + info->vfs_inode.i_ino;
+	pvma.vm_ops = NULL;
 	pvma.vm_policy = mpol_shared_policy_lookup(&info->policy, index);
 
 	page = swapin_readahead(swap, gfp, &pvma, 0);
