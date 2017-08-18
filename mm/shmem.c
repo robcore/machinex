c744a89f78d084c80d5bbb8d24d2bdceec9842b7
robcore <robpatershuk@gmail.com>
2017-08-18 10:47:43 -0600

    mm-implement-map_pages-for-shmem-tmpfs.

--- mm/shmem.c
+++ mm/shmem.c
@@ -2728,6 +2728,7 @@ static const struct super_operations shmem_ops = {
 
 static const struct vm_operations_struct shmem_vm_ops = {
 	.fault		= shmem_fault,
+	.map_pages	= filemap_map_pages,
 #ifdef CONFIG_NUMA
 	.set_policy     = shmem_set_policy,
 	.get_policy     = shmem_get_policy,
