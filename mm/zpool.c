e2f0156d49cd90dc22a84bcc12481ae2540efc48
robcore <robpatershuk@gmail.com>
2017-08-18 10:36:57 -0600

    zpool - pass gfp flag from caller

--- mm/zpool.c
+++ mm/zpool.c
@@ -161,7 +161,7 @@ struct zpool *zpool_create_pool(char *type, char *name, gfp_t gfp,
 		return NULL;
 	}
 
-	zpool = kmalloc(sizeof(*zpool), GFP_KERNEL);
+	zpool = kmalloc(sizeof(*zpool), gfp);
 	if (!zpool) {
 		pr_err("couldn't create zpool - out of memory\n");
 		zpool_put_driver(driver);
