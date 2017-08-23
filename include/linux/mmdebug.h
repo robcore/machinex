ca2f60187d488a301a8a5f4c71d6d01a1e8ce9c0
robcore <robpatershuk@gmail.com>
2017-08-23 13:32:58 -0600

    fix a thing

--- include/linux/mmdebug.h
+++ include/linux/mmdebug.h
@@ -12,7 +12,9 @@
 #else
 #define VM_BUGGERED(cond) BUG_ON(cond)
 #define VM_BUG_ON(cond) VM_BUGGERED(cond)
-#define VM_BUG_ON_PAGE(cond, page) VM_BUGGERED(cond)
+#define VM_BUG_ON_PAGE(cond, page) VM_BUG_ON(cond)
+#define VM_BUG_ON_VMA(cond, vma) VM_BUG_ON(cond)
+#define VM_BUG_ON_MM(cond, mm) VM_BUG_ON(cond)
 #define VM_WARN_ON(cond) BUILD_BUG_ON_INVALID(cond)
 #define VM_WARN_ON_ONCE(cond) BUILD_BUG_ON_INVALID(cond)
 #define VM_WARN_ONCE(cond, format...) BUILD_BUG_ON_INVALID(cond)
