1fed456031e41c4beefb58fb997b5d038b96f336
robcore <robpatershuk@gmail.com>
2017-08-23 18:10:14 -0600

    fix a missing #endif

--- mm/memblock.c
+++ mm/memblock.c
@@ -973,6 +973,7 @@ int __init_memblock memblock_set_node(phys_addr_t base, phys_addr_t size,
 	memblock_merge_regions(type);
 	return 0;
 }
+#endif /* CONFIG_HAVE_MEMBLOCK_NODE_MAP */
 
 static phys_addr_t __init memblock_alloc_base_nid(phys_addr_t size,
 					phys_addr_t align, phys_addr_t max_addr,
@@ -1361,6 +1362,7 @@ int __init_memblock memblock_is_region_memory(phys_addr_t base, phys_addr_t size
 int __init_memblock memblock_overlaps_memory(phys_addr_t base, phys_addr_t size)
 {
 	memblock_cap_size(base, &size);
+
 	return memblock_overlaps_region(&memblock.memory, base, size) >= 0;
 }
 
