2e319c9a606afba3155e7d0d1184e01e38f8bbb2
robcore <robpatershuk@gmail.com>
2017-08-25 20:59:23 -0600

    testing a little something to see if we can fix the dumbass issue i caused with the keycode power button

--- build_kernel.sh
+++ build_kernel.sh
@@ -118,7 +118,13 @@ if [[ $ONLINE == recovery ]]; then
 	adbcountdown
 elif [[ $ONLINE == device ]]; then
 	echo "connected"
-	adb shell su -c "input keyevent KEYCODE_POWER";
+		adb shell su -c "dumpsys power | grep -i 'SCREEN_ON' >/dev/null";
+		if [ $? -eq 0 ]; then
+			echo "Screen is on...";
+		else
+			echo "Pressing buttons to wake-up...";
+			adb shell su -c "input keyevent KEYCODE_WAKEUP";
+		fi;
 	adb push $OUTFOLDER.zip /storage/extSdCard
 	echo "push complete"
 	adb kill-server
@@ -306,7 +312,13 @@ if [[ $ONLINE == recovery ]]; then #if we are in recovery
 elif [[ $ONLINE == device ]]; then #if we are in os, connected via usb
 	echo "connected"
 	adbcountdown
-	adb shell su -c "input keyevent KEYCODE_POWER";
+		adb shell su -c "dumpsys power | grep -i 'SCREEN_ON' >/dev/null";
+		if [ $? -eq 0 ]; then
+			echo "Screen is on...";
+		else
+			echo "Pressing buttons to wake-up...";
+			adb shell su -c "input keyevent KEYCODE_WAKEUP";
+		fi;
 	adb push $OUTFOLDER.zip /storage/extSdCard
 	echo "push complete"
 	adb kill-server
@@ -493,7 +505,13 @@ if [[ $ONLINE == recovery ]]; then #if we are in recovery
 elif [[ $ONLINE == device ]]; then #if we are in os, connected via usb
 	echo "connected"
 	adbcountdown
-	adb shell su -c "input keyevent KEYCODE_POWER";
+		adb shell su -c "dumpsys power | grep -i 'SCREEN_ON' >/dev/null";
+		if [ $? -eq 0 ]; then
+			echo "Screen is on...";
+		else
+			echo "Pressing buttons to wake-up...";
+			adb shell su -c "input keyevent KEYCODE_WAKEUP";
+		fi;
 	adb push $OUTFOLDER.zip /storage/extSdCard
 	echo "push complete"
 else
@@ -664,7 +682,13 @@ if [[ $ONLINE == recovery ]]; then #if we are in recovery
 elif [[ $ONLINE == device ]]; then #if we are in os, connected via usb
 	echo "connected"
 	adbcountdown
-	adb shell su -c "input keyevent KEYCODE_POWER";
+		adb shell su -c "dumpsys power | grep -i 'SCREEN_ON' >/dev/null";
+		if [ $? -eq 0 ]; then
+			echo "Screen is on...";
+		else
+			echo "Pressing buttons to wake-up...";
+			adb shell su -c "input keyevent KEYCODE_WAKEUP";
+		fi;
 	adb push $OUTFOLDER.zip /storage/extSdCard
 	echo "push complete"
 else
@@ -865,7 +889,13 @@ if [[ $ONLINE == recovery ]]; then #if we are in recovery
 elif [[ $ONLINE == device ]]; then #if we are in os, connected via usb
 	echo "connected"
 	adbcountdown
-	adb shell su -c "input keyevent KEYCODE_POWER";
+		adb shell su -c "dumpsys power | grep -i 'SCREEN_ON' >/dev/null";
+		if [ $? -eq 0 ]; then
+			echo "Screen is on...";
+		else
+			echo "Pressing buttons to wake-up...";
+			adb shell su -c "input keyevent KEYCODE_WAKEUP";
+		fi;
 	adb push $OUTFOLDER.zip /storage/extSdCard
 	echo "push complete"
 	adb kill-server
@@ -973,7 +1003,13 @@ if [[ $ONLINE == recovery ]]; then #if we are in recovery
 elif [[ $ONLINE == device ]]; then #if we are in os, connected via usb
 	echo "connected"
 	adbcountdown
-	adb shell su -c "input keyevent KEYCODE_POWER";
+		adb shell su -c "dumpsys power | grep -i 'SCREEN_ON' >/dev/null";
+		if [ $? -eq 0 ]; then
+			echo "Screen is on...";
+		else
+			echo "Pressing buttons to wake-up...";
+			adb shell su -c "input keyevent KEYCODE_WAKEUP";
+		fi;
 	adb push $OUTFOLDER.zip /storage/extSdCard
 	echo "push complete"
 else
