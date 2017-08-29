70904e741182c53918a04de0f2ebe7aacdc14a1b
robcore <robpatershuk@gmail.com>
2017-08-28 20:09:50 -0600

    mm-compaction-trace-compaction-begin-and-end.

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -44,15 +44,6 @@ static unsigned int default_powersave_bias;
  */
 static int should_io_be_busy(void)
 {
-#if defined(CONFIG_X86)
-	/*
-	 * For Intel, Core 2 (model 15) and later have an efficient idle.
-	 */
-	if (boot_cpu_data.x86_vendor == X86_VENDOR_INTEL &&
-			boot_cpu_data.x86 == 6 &&
-			boot_cpu_data.x86_model >= 15)
-		return 1;
-#endif
 	return 0;
 }
 
