305996afeb1b2653e4240c4fe9f9001d5291be45
robcore <robpatershuk@gmail.com>
2017-09-01 16:36:29 -0600

    avoid some annoying repetition in final script output

--- build_kernel.sh
+++ build_kernel.sh
@@ -200,7 +200,6 @@ if [ -e ~/machinex/out/arch/arm/boot/zImage ]; then
 	zip -r -9 - * > $OUTFOLDER.zip
 	#SDB=`adb shell md5sum /storage/extSdCard/$OUTFOLDER.zip`
 	SUMMY=`md5sum /media/root/robcore/AIK/$OUTFOLDER/$OUTFOLDER.zip`
-	echo "Kernel is located in /media/root/robcore/AIK/$OUTFOLDER/$OUTFOLDER.zip"
 	echo "$OUTFOLDER was built on:" >> ~/machinex/datetracker.txt
 	date >> ~/machinex/datetracker.txt
 	echo "------------------------" >> ~/machinex/datetracker.txt
@@ -237,8 +236,7 @@ if [ -e ~/machinex/out/arch/arm/boot/zImage ]; then
 		echo "cleanup finished"
 	fi;
 
-	echo "Kernel is located in /media/root/robcore/AIK/$OUTFOLDER/$OUTFOLDER.zip"
-	echo "MD5 is $SUMMY"
+	echo "MD5 and Kernel Location: $SUMMY"
 else
 	WASHME
 	echo "Build failed, Skipped Ramdisk Creation"
@@ -305,8 +303,7 @@ if [ -e ~/machinex/out/arch/arm/boot/zImage ]; then
 	cd ~/machinex
 	WASHME
 	echo "cleanup finished"
-	echo "Kernel is located in /media/root/robcore/AIK/$OUTFOLDER/$OUTFOLDER.zip"
-	echo "MD5 is $SUMMY"
+	echo "MD5 and Kernel Location: $SUMMY"
 else
 	WASHME
 	echo "Build failed, Skipped Ramdisk Creation"
