5ff244d9ca3630e28e152d2f992d0b78d4704725
robcore <robpatershuk@gmail.com>
2017-09-03 17:14:04 -0600

    hugetlb: restrict hugepage_migration_support() to x86_64

--- arch/x86/mm/hugetlbpage.c
+++ arch/x86/mm/hugetlbpage.c
@@ -221,11 +221,6 @@ follow_huge_pmd(struct mm_struct *mm, unsigned long address,
 {
 	return NULL;
 }
-
-int pmd_huge_support(void)
-{
-	return 0;
-}
 #else
 
 struct page *
@@ -266,11 +261,6 @@ follow_huge_pud(struct mm_struct *mm, unsigned long address,
 	if (page)
 		page += ((address & ~PUD_MASK) >> PAGE_SHIFT);
 	return page;
-}
-
-int pmd_huge_support(void)
-{
-	return 1;
 }
 #endif
 
