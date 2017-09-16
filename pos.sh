5f108e026a3c32aeb0cdf87703be797d86c9a40d
robcore <robpatershuk@gmail.com>
2017-09-16 12:56:30 -0600

    got rid of the last three

--- pos.sh
+++ pos.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-patch -p1 -R < "/root/machinex/patches/0015-changed-my-mind-about-workqueues-but-down-bother-war.patch"
-patch -p1 -R < "/root/machinex/patches/0014-if-the-workqueue-is-already-freezing-still-label-it-.patch"
-patch -p1 -R < "/root/machinex/patches/0013-fix-one-last-conversion-to-call_single_data_t.patch"
-patch -p1 -R < "/root/machinex/patches/0012-added-smps-own-typedef-to-avoid-using-two-cachelines.patch"
+patch -p1 -R < "/root/machinex/patches/0010-fuck-i-would-have-to-look-into-this-too-much-and-am-.patch"
+patch -p1 -R < "/root/machinex/patches/0009-fix-up-some-jiffies-calculations.patch"
+patch -p1 -R < "/root/machinex/patches/0008-ntp-fix-timespec-adjust-calculation.patch"
