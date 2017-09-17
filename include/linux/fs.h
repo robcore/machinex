bc145ee81a64924183228b2ba1ae56e749308b01
robcore <robpatershuk@gmail.com>
2017-09-16 22:21:27 -0600

    well fuck it was easy as changing it in the fucking header

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1548,7 +1548,7 @@ struct file_operations {
 	int (*setlease)(struct file *, long, struct file_lock **, void **);
 	long (*fallocate)(struct file *file, int mode, loff_t offset,
 			  loff_t len);
-	int (*show_fdinfo)(struct seq_file *m, struct file *f);
+	void (*show_fdinfo)(struct seq_file *m, struct file *f);
 	/* get_lower_file is for stackable file system */
 	struct file* (*get_lower_file)(struct file *f);
 };
