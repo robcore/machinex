3ce9aad86a042a45ced063f0c23d3cdeb0ca884b
robcore <robpatershuk@gmail.com>
2017-09-20 19:02:42 -0600

    temporarily fix open exec

--- fs/exec.c
+++ fs/exec.c
@@ -818,7 +818,7 @@ struct file *open_exec(const char *name)
 	struct file *f = ERR_CAST(filename);
 
 	if (!IS_ERR(filename)) {
-		f = do_open_execat(AT_FDCWD, filename, 0);
+		f = do_open_exec(filename);
 		putname(filename);
 	}
 	return f;
