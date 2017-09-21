2b71f2ef5e1bb3f9ee21c793af717a3478d19cdf
robcore <robpatershuk@gmail.com>
2017-09-21 13:15:42 -0600

    rid ourselves of more PR_SET_VMA shit

--- include/uapi/linux/prctl.h
+++ include/uapi/linux/prctl.h
@@ -170,9 +170,6 @@ struct prctl_mm_map {
  */
 #define PR_SET_TIMERSLACK_PID 40
 
-#define PR_SET_VMA		0x53564d41
-# define PR_SET_VMA_ANON_NAME		0
-
 #define PR_SET_THP_DISABLE	41
 #define PR_GET_THP_DISABLE	42
 
--- kernel/sys.c
+++ kernel/sys.c
@@ -2137,9 +2137,6 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 		error = put_user(me->signal->is_child_subreaper,
 				 (int __user *)arg2);
 		break;
-	case PR_SET_VMA:
-		error = prctl_set_vma(arg2, arg3, arg4, arg5);
-		break;
 	case PR_SET_TIMERSLACK_PID:
 		if (task_pid_vnr(current) != (pid_t)arg3 &&
 				!capable(CAP_SYS_NICE))
