6988bc2eff14bb3b906c79927b25589215d1da0a
robcore <robpatershuk@gmail.com>
2017-09-22 03:30:21 -0600

    vfs-remove-get_xip_mem.

--- fs/exofs/inode.c
+++ fs/exofs/inode.c
@@ -977,7 +977,6 @@ const struct address_space_operations exofs_aops = {
 	.direct_IO	= NULL, /* TODO: Should be trivial to do */
 
 	/* With these NULL has special meaning or default is not exported */
-	.get_xip_mem	= NULL,
 	.migratepage	= NULL,
 	.launder_page	= NULL,
 	.is_partially_uptodate = NULL,
