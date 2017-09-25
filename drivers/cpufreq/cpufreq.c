22d2100053e292f39b4f5e7d93614dfdf15e15b6
robcore <robpatershuk@gmail.com>
2017-09-25 16:57:06 -0600

    account for potential hardlimit policy NULL pointers on suspend/resume

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -833,6 +833,9 @@ static void cpufreq_hardlimit_suspend(struct power_suspend *h)
 	struct cpufreq_policy *policy = per_cpu(cpufreq_cpu_data, cpu);
 	current_screen_state = CPUFREQ_HARDLIMIT_SCREEN_OFF;
 
+	if (policy == NULL)
+		return;
+
 	for_each_possible_cpu(policy->cpu) {
 		reapply_hard_limits(policy->cpu);
 		return;
@@ -845,6 +848,9 @@ static void cpufreq_hardlimit_resume(struct power_suspend *h)
 	struct cpufreq_policy *policy = per_cpu(cpufreq_cpu_data, cpu);
 
 	current_screen_state = CPUFREQ_HARDLIMIT_SCREEN_ON;
+
+	if (policy == NULL)
+		return;
 
 	for_each_possible_cpu(policy->cpu) {
 		reapply_hard_limits(policy->cpu);
