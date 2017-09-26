d12791882ed936a1ba950d8e4fd6f2467f625ca8
robcore <robpatershuk@gmail.com>
2017-09-25 21:08:24 -0600

    in try to wake up, use smp_mb__after_spinlock instead of before

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2007,8 +2007,8 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 	 * reordered with p->state check below. This pairs with mb() in
 	 * set_current_state() the waiting thread does.
 	 */
-	smp_mb__before_spinlock();
 	raw_spin_lock_irqsave(&p->pi_lock, flags);
+	smp_mb__after_spinlock();
 	if (!(p->state & state))
 		goto out;
 
