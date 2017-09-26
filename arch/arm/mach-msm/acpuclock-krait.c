6a82d9e353788643da8034e30111fb31f4371329
robcore <robpatershuk@gmail.com>
2017-09-25 21:28:29 -0600

    msm_cpufreq_driver - i believe we may benefit from CPUFREQ_ASYNC_NOTIFICATION (trying it anyway)

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -1444,7 +1444,7 @@ static struct notifier_block msm_cpufreq_pm_notifier = {
 static struct cpufreq_driver msm_cpufreq_driver = {
 	.flags		= CPUFREQ_STICKY | CPUFREQ_CONST_LOOPS |
 				  CPUFREQ_NEED_INITIAL_FREQ_CHECK |
-				  CPUFREQ_PM_NO_WARN |
+				  CPUFREQ_PM_NO_WARN | CPUFREQ_ASYNC_NOTIFICATION |
 				  CPUFREQ_HAVE_GOVERNOR_PER_POLICY,
 	.init		= msm_cpufreq_init,
 	.verify		= cpufreq_generic_frequency_table_verify,
