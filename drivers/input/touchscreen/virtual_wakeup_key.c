0d480311d388948dd1992dedaf09b7eee32eb1a3
robcore <robpatershuk@gmail.com>
2017-10-02 18:36:12 -0600

    did some work on virtual power key

--- drivers/input/touchscreen/virtual_wakeup_key.c
+++ drivers/input/touchscreen/virtual_wakeup_key.c
@@ -29,96 +29,61 @@ static void wakeup_key_release(struct work_struct *work);
 static void wakeup_key_press(struct work_struct *work);
 static struct work_struct virtkey_input_work;
 struct wake_lock vwklock;
-static unsigned int key_is_pressed = 0;
 unsigned int screen_wake_lock = 0;
 
 static void press_key(unsigned int pressed)
 {
-	if ((pressed != 0 && pressed != 1) ||
-		(pressed == key_is_pressed))
-		return;
+	unsigned long flags;
+
+	/* disable keyboard interrupts */
+	local_irq_save(flags);
+	/* don't change CPU */
+	preempt_disable();
 
 	switch (pressed) {
-	case 0:
-		wake_unlock(&vwklock);
-		break;
-	case 1:
-		wake_lock(&vwklock);
-		break;
-	default:
-		break;
+		case 1:
+			wake_lock(&vwklock);
+			input_report_key(virtkeydev, KEY_WAKEUP, 1);
+			input_sync(virtkeydev);
+			break;
+		case 0:
+			input_report_key(virtkeydev, KEY_WAKEUP, 0);
+			input_sync(virtkeydev);
+			wake_unlock(&vwklock);
+			break;
+		default:
+			break;
 	}
-
-	input_report_key(virtkeydev, KEY_WAKEUP, pressed);
-	input_sync(virtkeydev);
-	key_is_pressed = pressed;
-}
-
-/* WakeKeyReleased work func */
-static void wakeup_key_release(struct work_struct *work)
-{
-	if (!key_is_pressed)
-		return;
-	press_key(0);
+	/* reenable preemption */
+	preempt_enable();
+	/* reenable keyboard interrupts */
+	local_irq_restore(flags);
 }
 
 /* WakeKeyPressed work func */
 static void wakeup_key_press(struct work_struct *work)
 {
-	if (key_is_pressed)
-		return;
-
 	press_key(1);
-	schedule_delayed_work(&wakeup_key_release_work, msecs_to_jiffies(WRTIMEOUT));
+	mdelay(100);
+	press_key(0);
 }
 
 /* PowerKey trigger */
 void virt_wakeup_key_trig(void) {
-	schedule_delayed_work_on(0, &wakeup_key_press_work, msecs_to_jiffies(WPTIMEOUT));
+	schedule_delayed_work_on(0, &wakeup_key_press_work, 0);
 }
 EXPORT_SYMBOL(virt_wakeup_key_trig);
 
-static int virtkey_input_connect(struct input_handler *handler,
-				struct input_dev *dev, const struct input_device_id *id) {
-	struct input_handle *handle;
-	int error;
-
-
-	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
-	if (!handle)
-		return -ENOMEM;
-
-	handle->dev = dev;
-	handle->handler = handler;
-	handle->name = "virt_wakeup";
-
-	error = input_register_handle(handle);
-
-	error = input_open_device(handle);
-
-	return 0;
-
-}
-
-static void virtkey_input_disconnect(struct input_handle *handle)
+static void do_screen_wake(void)
 {
-	input_close_device(handle);
-	input_unregister_handle(handle);
-	kfree(handle);
+checker:
+	if (!screen_wake_lock)
+			return;
+	press_key(1);
+	mdelay(100);
+	press_key(0);
+	goto checker;
 }
-
-static const struct input_device_id virtkey_ids[] = {
-	{ .driver_info = 1 },
-	{ },
-};
-
-static struct input_handler virtkey_input_handler = {
-	.connect	= virtkey_input_connect,
-	.disconnect	= virtkey_input_disconnect,
-	.name		= "virtual_wakeup_key",
-	.id_table	= virtkey_ids,
-};
-
 static ssize_t screen_wake_lock_show(struct kobject *kobj,
 				struct kobj_attribute *attr, char *buf)
 {
@@ -137,12 +102,14 @@ static ssize_t screen_wake_lock_store(struct kobject *kobj,
 	if (ret != 1)
 		return -EINVAL;
 
-	sanitize_min_max(input, 0, 1);
-
 	if (input == screen_wake_lock)
 		return count;
 
+	sanitize_min_max(input, 0, 1);
+
 	screen_wake_lock = input;
+
+	do_screen_wake();
 
 	return count;
 }
@@ -162,39 +129,6 @@ static struct attribute_group virtual_wakeup_key_attr_group = {
 	.name = "virtual_wakeup_key",
 };
 
-/*
-static int bootup_force_screen_on(void)
-{
-	while (!late_init_complete) {
-		press_key(1);
-		msleep(10);
-		press_key(0);
-		msleep(10);
-		if (late_init_complete)
-			break;
-	}
-
-	if (key_is_pressed)
-		press_key(0);
-
-	return key_is_pressed;
-}
-
-static int __init virtual_wakeup_key_late_init(void)
-{
-	int ret;
-
-	ret = bootup_force_screen_on();
-	if (ret)
-		pr_err("VPK System Late Init ERROR!!\n");
-	else
-		pr_info("VPK System Late Init Complete:%u\n", late_init_complete);
-
-	return ret;
-}
-
-late_initcall(virtual_wakeup_key_late_init);
-*/
 static int __init virtual_wakeup_key_init(void)
 {
 	int rc = 0;
@@ -206,10 +140,14 @@ static int __init virtual_wakeup_key_init(void)
 		goto err_alloc_dev;
 	}
 
-	input_set_capability(virtkeydev, EV_KEY, KEY_WAKEUP);
 
 	virtkeydev->name = "virtual_wakeup_key";
-	virtkeydev->phys = "virtual_wake_key/input0";
+	virtkeydev->id.bustype = BUS_VIRTUAL;
+	virtkeydev->phys = "virtual_wake_key/virtual_wake_key";
+	virtkeydev->dev.parent = NULL;
+
+	__set_bit(EV_KEY, virtkeydev->evbit);
+	__set_bit(KEY_WAKEUP, virtkeydev->keybit);
 
 	rc = input_register_device(virtkeydev);
 	if (rc) {
@@ -219,12 +157,6 @@ static int __init virtual_wakeup_key_init(void)
 
 	INIT_DELAYED_WORK(&wakeup_key_press_work, wakeup_key_press);
 	INIT_DELAYED_WORK(&wakeup_key_release_work, wakeup_key_release);
-
-	rc = input_register_handler(&virtkey_input_handler);
-	if (rc) {
-		pr_err("%s: Failed to register virtkey_input_handler\n", __func__);
-		goto err_unregister;
-	}
 
 	rc = sysfs_create_group(kernel_kobj, &virtual_wakeup_key_attr_group);
 	if (rc) {
@@ -236,13 +168,10 @@ static int __init virtual_wakeup_key_init(void)
 
 	return 0;
 
-err_handler:
-	input_unregister_handler(&virtkey_input_handler);
 err_unregister:
 	input_unregister_device(virtkeydev);
 err_input_dev:
 	input_free_device(virtkeydev);
-
 err_alloc_dev:
 	pr_info("%s Failed\n", __func__);
 
