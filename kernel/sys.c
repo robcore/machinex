5df64ce14878a27d769b1ff50ff6a1a5c25c9244
robcore <robpatershuk@gmail.com>
2017-10-06 11:18:15 -0600

    naw hold up there

--- kernel/sys.c
+++ kernel/sys.c
@@ -128,9 +128,10 @@ static bool set_one_prio_perm(struct task_struct *p)
 {
 	const struct cred *cred = current_cred(), *pcred = __task_cred(p);
 
-	if (uid_eq(pcred->uid,  cred->euid) ||
-	    uid_eq(pcred->euid, cred->euid))
-		return true;
+	if (pcred->user_ns == cred->user_ns &&
+	    (pcred->uid  == cred->euid ||
+	     pcred->euid == cred->euid))
+z		return true;
 	if (ns_capable(pcred->user_ns, CAP_SYS_NICE))
 		return true;
 	return false;
