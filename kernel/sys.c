8385a12925540ce82354f853afd90efd2fbd8272
robcore <robpatershuk@gmail.com>
2017-10-06 11:16:50 -0600

    sys too

--- kernel/sys.c
+++ kernel/sys.c
@@ -128,9 +128,8 @@ static bool set_one_prio_perm(struct task_struct *p)
 {
 	const struct cred *cred = current_cred(), *pcred = __task_cred(p);
 
-	if (pcred->user_ns == cred->user_ns &&
-	    (pcred->uid  == cred->euid ||
-	     pcred->euid == cred->euid))
+	if (uid_eq(pcred->uid,  cred->euid) ||
+	    uid_eq(pcred->euid, cred->euid))
 		return true;
 	if (ns_capable(pcred->user_ns, CAP_SYS_NICE))
 		return true;
