f432ab2c6d533720fd72fc3726467d2a154032a9
robcore <robpatershuk@gmail.com>
2017-10-13 16:54:33 -0600

    fuck it all

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -282,5 +282,6 @@ extern int cpuhp_acpuclk_offline(unsigned int cpu);
 int cpuhp_acpuclk_offline_prepare(unsigned int cpu);
 extern int cpuhp_intelli_online(unsigned int cpu);
 extern int cpuhp_intelli_offline(unsigned int cpu);
+extern int cpuhp_blk_iopoll(unsigned int cpu);
 #endif /* _LINUX_CPU_H_ */
 
