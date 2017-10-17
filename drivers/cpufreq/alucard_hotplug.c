bf9e453ae344b4c87675d6d6590ef57ee20925d1
robcore <robpatershuk@gmail.com>
2017-10-17 16:11:46 -0600

    alucardhp - ensure that we restart the workqueue upon resume from suspend

--- drivers/cpufreq/alucard_hotplug.c
+++ drivers/cpufreq/alucard_hotplug.c
@@ -293,6 +293,21 @@ static void hotplug_work_fn(struct work_struct *work)
 							  delay);
 }
 
+static void alucardhp_suspend(struct power_suspend * h)
+{
+}
+static void alucardhp_resume(struct power_suspend * h)
+{
+	queue_delayed_work_on(0, alucardhp_wq, &alucard_hotplug_work,
+						msecs_to_jiffies(hotplug_tuners_ins.hotplug_sampling_rate));
+}
+
+static struct power_suspend alucardhp_suspend_data =
+{
+	.suspend = alucardhp_suspend,
+	.resume = alucardhp_resume,
+};
+
 static int hotplug_start(void)
 {
 	unsigned int cpu;
@@ -326,6 +341,7 @@ static int hotplug_start(void)
 
 	init_rq_avg_stats();
 	INIT_DELAYED_WORK(&alucard_hotplug_work, hotplug_work_fn);
+	register_power_suspend(&alucardhp_suspend_data);
 	queue_delayed_work_on(0, alucardhp_wq, &alucard_hotplug_work,
 						msecs_to_jiffies(hotplug_tuners_ins.hotplug_sampling_rate));
 
@@ -334,10 +350,9 @@ static int hotplug_start(void)
 
 static void hotplug_stop(void)
 {
+	unregister_power_suspend(&alucardhp_suspend_data);
 	cancel_delayed_work_sync(&alucard_hotplug_work);
-
 	exit_rq_avg();
-
 	destroy_workqueue(alucardhp_wq);
 }
 
