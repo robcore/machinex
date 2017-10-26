9b5ad75427141745e62b6b447fc443f9e1213974
robcore <robpatershuk@gmail.com>
2017-10-26 01:41:30 -0600

    trying out instigatorxs hotplug driver

--- drivers/cpufreq/ix_hotplug.c
+++ drivers/cpufreq/ix_hotplug.c
@@ -38,9 +38,7 @@
 #include <linux/mutex.h>
 #include <linux/cpufreq.h>
 #include <linux/kobject.h>
-#ifdef CONFIG_STATE_NOTIFIER
-#include <linux/state_notifier.h>
-#endif
+#include <linux/sysfs_helpers.h>
 #include <linux/machinex_defines.h>
 
 #include <linux/powersuspend.h>
@@ -56,13 +54,11 @@
  * These two are scaled based on num_online_cpus()
  */
 
+static unsigned int ix_hotplug_active;
+static bool hotplug_suspended;
+
 static struct delayed_work hotplug_decision_work;
-static struct work_struct suspend;
-static struct work_struct resume;
 static struct workqueue_struct *ixwq;
-static bool hotplug_suspended = false;
-static unsigned int hotplug_suspend = 0
-static atomic_t ix_hotplug_active = ATOMIC_INIT(0);
 
 static unsigned int enable_all_load = 800;
 static unsigned int enable_load[5] = {0, 120, 220, 340, 0};
@@ -73,32 +69,50 @@ static unsigned int offline_sampling_periods[5] = {0, 0, 8, 3, 4};
 static unsigned int online_cpus;
 static unsigned int min_cpus_online = DEFAULT_MIN_CPUS_ONLINE;
 static unsigned int max_cpus_online = DEFAULT_MAX_CPUS_ONLINE;
-static unsigned int sampling_rate;
-static unsigned int available_cpus;
+
+#define DEF_SRATE 100
+static unsigned int sampling_rate = DEF_SRATE;
 static unsigned int online_sample;
 static unsigned int offline_sample;
 
 
 static void hotplug_online_single_work(void)
 {
-	unsigned int cpuid = 0;
+	unsigned int cpuinc, cpuid = 0;
 
-	if (hotplug_suspended)
+	if (hotplug_suspended || !hotplug_ready ||
+		!ix_hotplug_active)
 		return;
 
-	cpuid = cpumask_next_zero(0, cpu_online_mask);
-	if (cpu_in_range_hp(cpuid))
-		cpu_up(cpuid);
+	cpuinc = 0;
+
+retry:
+	cpuid = cpumask_next_zero(cpuinc, cpu_online_mask);
+	if (cpu_out_of_range_hp(cpuid))
+		return;
+
+	if (cpu_online(cpuid) || !is_cpu_allowed(cpuid) ||
+		thermal_core_controlled(cpuid)) {
+		cpuinc++;
+		goto retry;
+	}
+
+	cpu_up(cpuid);
 }
 
 static void hotplug_online_all_work(void)
 {
 	unsigned int cpu;
 
+	if (hotplug_suspended || !hotplug_ready ||
+		!ix_hotplug_active)
+		return;
+
 	for_each_nonboot_offline_cpu(cpu) {
 		if (cpu_out_of_range_hp(cpu))
 			break;
-		if (unlikely(cpu_online(cpu)))
+		if (cpu_online(cpu) || !is_cpu_allowed(cpu) ||
+			thermal_core_controlled(cpu))
 			continue;
 			cpu_up(cpu);
 			//pr_info("ix_hotplug: CPU%d up.\n", cpu);
@@ -110,7 +124,8 @@ static void hotplug_offline_work(void)
 {
 	unsigned int cpuid = 0;
 
-	if (hotplug_suspended)
+	if (hotplug_suspended || !hotplug_ready ||
+		!ix_hotplug_active)
 		return;
 
 	cpuid = cpumask_next(0, cpu_online_mask);
@@ -121,32 +136,28 @@ static void hotplug_offline_work(void)
 static void __ref hotplug_decision_work_fn(struct work_struct *work)
 {
 	unsigned int avg_running, io_wait;
-	//unsigned int rq_depth;
 
-	if (hotplug_suspended)
+	if (hotplug_suspended || !ix_hotplug_active)
 		return;
 
+	if (!hotplug_ready)
+		goto resched;
+
+	online_cpus = num_online_cpus();
+
 	sched_get_nr_running_avg(&avg_running, &io_wait);
-	//rq_depth = rq_info.rq_avg;
 
 	if ((avg_running <= disable_load[online_cpus]) &&
 			(online_cpus > min_cpus_online)) {
-		//pr_info("ix_hotplug: Disable Exit - %d %d %d %d %d\n", online_cpus, offline_sample, offline_sampling_periods[online_cpus], disable_load[online_cpus], avg_running);
 		if (offline_sample >= offline_sampling_periods[online_cpus]) {
-			//pr_info("ix_hotplug: Disable Single\n");
 			hotplug_offline_work();
 			offline_sample = 0;
-			online_cpus = num_online_cpus();
-			//pr_info("ix_hotplug: Threshold: %d Load: %d Sampling: %d RQ: %d\n", load_disable, avg_running, sampling_rate, rq_depth);
 		}
 		offline_sample++;
 		online_sample = 1;
-		goto exit;
-	}
-
-	if ((avg_running >= enable_all_load || avg_running >= enable_load[online_cpus]) &&
-			(online_cpus < available_cpus)) {
-		//pr_info("ix_hotplug: Enable Exit - %d %d %d %d %d\n", online_cpus, online_sample, online_sampling_periods[online_cpus], enable_load[online_cpus], avg_running);
+	} else if ((avg_running >= enable_all_load ||
+				avg_running >= enable_load[online_cpus]) &&
+			(online_cpus < max_cpus_online)) {
 		if (online_sample >= online_sampling_periods[online_cpus]) {
 			if (avg_running >= enable_all_load) {
 				//pr_info("ix_hotplug: Enable All\n");
@@ -155,75 +166,165 @@ static void __ref hotplug_decision_work_fn(struct work_struct *work)
 				//pr_info("ix_hotplug: Enable Single\n");
 				hotplug_online_single_work();
 			}
-			online_cpus = num_online_cpus();
 			online_sample = 0;
 		}
 		online_sample++;
 		offline_sample = 1;
-		goto exit;
 	}
 
-	//pr_info("ix_hotplug: Idle, CPUs: %d %d\n", online_cpus, num_online_cpus());
+	sampling_rate = sample_rate[num_online_cpus()];
 
-exit:
-
-	sampling_rate = sample_rate[online_cpus];
-
-	//pr_info("ix_hotplug: CPUs: %d Load: %d Sampling: %d\n", online_cpus, avg_running, sampling_rate);
-
-	queue_delayed_work_on(0, ixwq, &hotplug_decision_work, msecs_to_jiffies(sampling_rate));
+resched:
+	queue_delayed_work_on(0, ixwq, &hotplug_decision_work, sampling_rate);
 }
 
-static ssize_t show_enable_all_load(struct kobject *kobj,
-				     struct attribute *attr, char *buf)
+static void ix_hotplug_suspend(struct power_suspend *h)
 {
-	return sprintf(buf, "%d\n", enable_all_load);
+
+	if (!hotplug_suspended) {
+		hotplug_suspended = true;
+		cancel_delayed_work(&hotplug_decision_work);
+	}
 }
 
-static ssize_t store_enable_all_load(struct kobject *kobj,
-				  struct attribute *attr, const char *buf,
-				  size_t count)
+static void ix_hotplug_resume(struct power_suspend *h)
+{
+	if (hotplug_suspended) {
+		hotplug_suspended = false;
+		offline_sample = 1;
+		online_sample = 1;
+		queue_delayed_work_on(0, ixwq, &hotplug_decision_work, sampling_rate);
+	}
+}
+
+static struct power_suspend ix_suspend_data =
+{
+	.suspend = ix_hotplug_suspend,
+	.resume = ix_hotplug_resume,
+};
+
+static void ix_hotplug_start(void)
+{
+	if (!ix_hotplug_active)
+		return;
+	ixwq = create_singlethread_workqueue("ix_hotplug_workqueue");
+
+    if (!ixwq)
+        return;
+
+	INIT_DELAYED_WORK(&hotplug_decision_work, hotplug_decision_work_fn);
+	register_power_suspend(&ix_suspend_data);
+	queue_delayed_work_on(0, ixwq, &hotplug_decision_work, sampling_rate);
+}
+
+static void ix_hotplug_stop(void)
+{
+	if (!ixwq || ix_hotplug_active)
+		return;
+	cancel_delayed_work_sync(&hotplug_decision_work);
+	unregister_power_suspend(&ix_suspend_data);
+	destroy_workqueue(ixwq);
+}
+
+static void ix_startstop(unsigned int status)
+{
+	if (status)
+		ix_hotplug_start();
+	else
+		ix_hotplug_stop();
+}
+
+#define ix_show_one(object)				\
+static ssize_t show_##object					\
+(struct kobject *kobj, struct kobj_attribute *attr, char *buf)	\
+{								\
+	return sprintf(buf, "%u\n", object);			\
+}
+
+ix_show_one(ix_hotplug_active);
+ix_show_one(min_cpus_online);
+ix_show_one(max_cpus_online);
+
+static ssize_t store_ix_hotplug_active(struct kobject *kobj,
+					 struct kobj_attribute *attr,
+					 const char *buf, size_t count)
 {
 	int ret;
-	long unsigned int val;
+	int input;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = sscanf(buf, "%d", &input);
 	if (ret < 0)
 		return ret;
-	enable_all_load = val;
+
+	sanitize_min_max(input, 0, 1);
+	if (input == ix_hotplug_active)
+		return count;
+
+	ix_hotplug_active = input;
+	ix_startstop(ix_hotplug_active);
+
 	return count;
 }
 
-static struct global_attr enable_all_load_attr = __ATTR(enable_all_load, 0666,
-		show_enable_all_load, store_enable_all_load);
 
-
-static ssize_t show_disable_load(struct kobject *kobj,
-				     struct attribute *attr, char *buf)
-{
-	return sprintf(buf, "%d\n", disable_load);
-}
-
-static ssize_t store_disable_load(struct kobject *kobj,
-				  struct attribute *attr, const char *buf,
-				  size_t count)
+static ssize_t store_min_cpus_online(struct kobject *kobj,
+				     struct kobj_attribute *attr,
+				     const char *buf, size_t count)
 {
 	int ret;
-	long unsigned int val;
+	unsigned int val;
 
-	ret = strict_strtoul(buf, 0, &val);
-	if (ret < 0)
-		return ret;
-	disable_load = val;
+	ret = sscanf(buf, "%u", &val);
+	if (ret != 1)
+		return -EINVAL;
+
+	if (val <= 1)
+		val = 1;
+	if (val >= NR_CPUS)
+		val = NR_CPUS;
+	if (val >= max_cpus_online)
+		val = max_cpus_online;
+
+	min_cpus_online = val;
+
 	return count;
 }
 
-static struct global_attr disable_load_attr = __ATTR(disable_load, 0666,
-		show_disable_load, store_disable_load);
+static ssize_t store_max_cpus_online(struct kobject *kobj,
+				     struct kobj_attribute *attr,
+				     const char *buf, size_t count)
+{
+	int ret;
+	unsigned int val;
+
+	ret = sscanf(buf, "%u", &val);
+	if (ret != 1)
+		return -EINVAL;
+
+	if (val <= 1)
+		val = 1;
+	if (val >= NR_CPUS)
+		val = NR_CPUS;
+	if (val <= min_cpus_online)
+		val = min_cpus_online;
+
+	max_cpus_online = val;
+
+	return count;
+}
+
+#define IX_ATTR_RW(_name) \
+static struct kobj_attribute _name##_attr = \
+	__ATTR(_name, 0644, show_##_name, store_##_name)
+
+IX_ATTR_RW(ix_hotplug_active);
+IX_ATTR_RW(min_cpus_online);
+IX_ATTR_RW(max_cpus_online);
 
 static struct attribute *ix_hotplug_attributes[] = {
-	&enable_all_load_attr.attr,
-	&disable_load_attr.attr,
+	&ix_hotplug_active_attr.attr,
+	&min_cpus_online_attr.attr,
+	&max_cpus_online_attr.attr,
 	NULL,
 };
 
@@ -232,120 +333,21 @@ static struct attribute_group ix_hotplug_attr_group = {
 	.name = "ix_hotplug",
 };
 
-static void __ref ix_hotplug_suspend(struct work_struct *work)
-{
-		hotplug_suspended = true;
-}
-
-static void __ref ix_hotplug_resume(struct work_struct *work)
-{
-	offline_sample = 1;
-	online_sample = 1;
-	cpu_up(1);
-	online_cpus = num_online_cpus();
-	queue_delayed_work_on(0,  ixwq, &hotplug_decision_work, msecs_to_jiffies(2500));
-	pr_info("ix_hotplug: Power Resume\n");
-}
-
-#ifdef CONFIG_STATE_NOTIFIER
-static int state_notifier_callback(struct notifier_block *this,
-				unsigned long event, void *data)
-{
-	if ((atomic_read(&ix_hotplug_active) == 0) ||
-			hotplug_suspended)
-		return NOTIFY_OK;
-
-	switch (event) {
-		case STATE_NOTIFIER_ACTIVE:
-			ix_hotplug_resume();
-			break;
-		case STATE_NOTIFIER_SUSPEND:
-			ix_hotplug_suspend();
-			break;
-		default:
-			break;
-	}
-
-	return NOTIFY_OK;
-}
-#endif
-
-static int __ref ix_hotplug_start(void)
+static int ix_hotplug_init(void)
 {
 	int sysfs_result;
 
-	online_cpus = num_online_cpus();
-	available_cpus = num_possible_cpus();
-
-	ix_hotplug_kobj = kobject_create_and_add("ix_hotplug",
-		kernel_kobj);
-
-	if (!ix_hotplug_kobj) {
-		pr_err("%s kobject create failed!\n", __FUNCTION__);
-		return -ENOMEM;
-	}
-
-	sysfs_result = sysfs_create_group(ix_hotplug_kobj,
+	sysfs_result = sysfs_create_group(kernel_kobj,
 		&ix_hotplug_attr_group);
 
 	if (sysfs_result) {
 		pr_info("%s group create failed!\n", __FUNCTION__);
-		kobject_put(ix_hotplug_kobj);
 		return -ENOMEM;
-
-	ixwq = alloc_workqueue("ix_hotplug_workqueue", WQ_HIGHPRI | WQ_FREEZABLE, 0);
-
-    if (!ixwq)
-        return -ENOMEM;
-
-#ifdef CONFIG_STATE_NOTIFIER
-	notif.notifier_call = state_notifier_callback;
-	if (state_register_client(&notif)) {
-		pr_err("%s: Failed to register State notifier callback\n",
-			IX_HOTPLUG);
-		goto err_dev;
 	}
-#endif
-
-	mutex_init(&ix_hotplug_mutex);
-
-	INIT_WORK(&suspend, ix_hotplug_suspend);
-	INIT_WORK(&resume, ix_hotplug_resume);
-	INIT_DELAYED_WORK(&hotplug_decision_work, hotplug_decision_work_fn);
-
-	/*
-	 * Give the system time to boot before fiddling with hotplugging.
-	 */
-
-	queue_delayed_work_on(0, ixwq, &hotplug_decision_work, msecs_to_jiffies(10000));
-
 	return 0;
-err_dev:
-	destroy_workqueue(ix_hotplug_workqueue);
-err_out:
-	atomic_set(&ix_hotplug_active, 0);
-	return ret;
-}
-
-static void ix_hotplug_stop(void)
-{
-
-	cancel_delayed_work_sync(&hotplug_decision_work);
-	flush_work(&suspend_work);
-	flush_work(&resume_work);
-	mutex_destroy(&ix_hotplug_mutex);
-
-#ifdef CONFIG_STATE_NOTIFIER
-	state_unregister_client(&notif);
-#endif
-	notif.notifier_call = NULL;
-
-	destroy_workqueue(ixwq);
 }
 
 late_initcall(ix_hotplug_init);
-module_exit(ix_hotplug_exit);
-
 MODULE_AUTHOR("InstigatorX, \
 		Rob Patershuk <robpatershuk@gmail.com>");
 MODULE_DESCRIPTION("'ix_hotplug' - A simple hotplug driver "
