82fc3ec65697ffef713adecf85475abbaa0e89b6
robcore <robpatershuk@gmail.com>
2017-10-28 02:09:45 -0600

    fuck it we dont have latency issues and that causes warnings in mmc sleep resulting in perma wakelocks

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -14,7 +14,6 @@
 #include <linux/slab.h>
 #include <linux/percpu.h>
 #include <linux/tick.h>
-#include <linux/pm_qos.h>
 
 #include "base.h"
 
@@ -320,8 +319,6 @@ int register_cpu(struct cpu *cpu, int num)
 
 	per_cpu(cpu_sys_devices, num) = &cpu->dev;
 	register_cpu_under_node(num, cpu_to_node(num));
-	dev_pm_qos_expose_latency_limit(&cpu->dev,
-					PM_QOS_RESUME_LATENCY_NO_CONSTRAINT);
 
 	return 0;
 }
