29c401d3f6e0cdd96ad81675d5d458f8e31a8092
robcore <robpatershuk@gmail.com>
2017-11-01 18:15:55 -0600

    spacing

--- include/linux/omniboost.h
+++ include/linux/omniboost.h
@@ -3,8 +3,8 @@
 
 #include <linux/notifier.h>
 
-#define BOOST_ON		0x01
-#define BOOST_OFF		0x02
+#define BOOST_ON 0x01
+#define BOOST_OFF 0x02
 
 int register_omniboost(struct notifier_block *nb);
 int unregister_omniboost(struct notifier_block *nb);
