239da65631f8c70eb6f5075cb4a9167d907ca111
robcore <robpatershuk@gmail.com>
2017-11-04 16:21:26 -0600

    maybe ill try this the opposite way

--- include/linux/pwm.h
+++ include/linux/pwm.h
@@ -55,7 +55,7 @@ enum {
 	PWMF_ENABLED = 1 << 1,
 };
 
-struct pwm_device {
+struct generic_pwm_device {
 	const char		*label;
 	unsigned long		flags;
 	unsigned int		hwpwm;
@@ -66,13 +66,13 @@ struct pwm_device {
 	unsigned int		period; /* in nanoseconds */
 };
 
-static inline void pwm_set_period(struct pwm_device *pwm, unsigned int period)
+static inline void pwm_set_period(struct generic_pwm_device *pwm, unsigned int period)
 {
 	if (pwm)
 		pwm->period = period;
 }
 
-static inline unsigned int pwm_get_period(struct pwm_device *pwm)
+static inline unsigned int pwm_get_period(struct generic_pwm_device *pwm)
 {
 	return pwm ? pwm->period : 0;
 }
@@ -80,7 +80,7 @@ static inline unsigned int pwm_get_period(struct pwm_device *pwm)
 /*
  * pwm_set_polarity - configure the polarity of a PWM signal
  */
-int pwm_set_polarity(struct pwm_device *pwm, enum pwm_polarity polarity);
+int pwm_set_polarity(struct generic_pwm_device *pwm, enum pwm_polarity polarity);
 
 /**
  * struct pwm_ops - PWM controller operations
@@ -95,19 +95,19 @@ int pwm_set_polarity(struct pwm_device *pwm, enum pwm_polarity polarity);
  */
 struct pwm_ops {
 	int			(*request)(struct pwm_chip *chip,
-					   struct pwm_device *pwm);
+					   struct generic_pwm_device *pwm);
 	void			(*free)(struct pwm_chip *chip,
-					struct pwm_device *pwm);
+					struct generic_pwm_device *pwm);
 	int			(*config)(struct pwm_chip *chip,
-					  struct pwm_device *pwm,
+					  struct generic_pwm_device *pwm,
 					  int duty_ns, int period_ns);
 	int			(*set_polarity)(struct pwm_chip *chip,
-					  struct pwm_device *pwm,
+					  struct generic_pwm_device *pwm,
 					  enum pwm_polarity polarity);
 	int			(*enable)(struct pwm_chip *chip,
-					  struct pwm_device *pwm);
+					  struct generic_pwm_device *pwm);
 	void			(*disable)(struct pwm_chip *chip,
-					   struct pwm_device *pwm);
+					   struct generic_pwm_device *pwm);
 #ifdef CONFIG_DEBUG_FS
 	void			(*dbg_show)(struct pwm_chip *chip,
 					    struct seq_file *s);
@@ -131,27 +131,27 @@ struct pwm_chip {
 	int			base;
 	unsigned int		npwm;
 
-	struct pwm_device	*pwms;
+	struct generic_pwm_device	*pwms;
 
-	struct pwm_device *	(*of_xlate)(struct pwm_chip *pc,
+	struct generic_pwm_device *	(*of_xlate)(struct pwm_chip *pc,
 					    const struct of_phandle_args *args);
 	unsigned int		of_pwm_n_cells;
 };
 
-int pwm_set_chip_data(struct pwm_device *pwm, void *data);
-void *pwm_get_chip_data(struct pwm_device *pwm);
+int pwm_set_chip_data(struct generic_pwm_device *pwm, void *data);
+void *pwm_get_chip_data(struct generic_pwm_device *pwm);
 
 int pwmchip_add(struct pwm_chip *chip);
 int pwmchip_remove(struct pwm_chip *chip);
-struct pwm_device *pwm_request_from_chip(struct pwm_chip *chip,
+struct generic_pwm_device *pwm_request_from_chip(struct pwm_chip *chip,
 					 unsigned int index,
 					 const char *label);
 
-struct pwm_device *pwm_get(struct device *dev, const char *consumer);
-void pwm_put(struct pwm_device *pwm);
+struct generic_pwm_device *pwm_get(struct device *dev, const char *consumer);
+void pwm_put(struct generic_pwm_device *pwm);
 
-struct pwm_device *devm_pwm_get(struct device *dev, const char *consumer);
-void devm_pwm_put(struct device *dev, struct pwm_device *pwm);
+struct generic_pwm_device *devm_pwm_get(struct device *dev, const char *consumer);
+void devm_pwm_put(struct device *dev, struct generic_pwm_device *pwm);
 
 struct pwm_lookup {
 	struct list_head list;
