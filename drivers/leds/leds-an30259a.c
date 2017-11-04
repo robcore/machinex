c99b87d8fb92f8a892b6d32946a06afaf5cc3fef
robcore <robpatershuk@gmail.com>
2017-11-04 13:59:43 -0600

    a  little more distinction for high level charge

--- drivers/leds/leds-an30259a.c
+++ drivers/leds/leds-an30259a.c
@@ -620,9 +620,9 @@ static void an30259a_start_led_pattern(unsigned int mode)
 				break;
 		case BATTERY_HIGH:
 				if (breathing_leds) {
-					leds_on(LED_R, true, true, 0x0F);
-					leds_on(LED_G, true, true, 0xEF);
-					leds_on(LED_B, true, true, 0x3F);
+					leds_on(LED_R, true, true, 0x14);
+					leds_on(LED_G, true, true, 0xE1);
+					leds_on(LED_B, true, true, 0x4F);
 					leds_set_slope_mode(client, LED_R,
 							0, 15, 10, 0, 2, 2, 1, 1, 1, 1);
 					leds_set_slope_mode(client, LED_G,
@@ -630,9 +630,9 @@ static void an30259a_start_led_pattern(unsigned int mode)
 					leds_set_slope_mode(client, LED_B,
 							0, 15, 10, 0, 2, 2, 1, 1, 1, 1);
 				} else {
-					leds_on(LED_R, true, false, 0x0F);
-					leds_on(LED_G, true, false, 0xEF);
-					leds_on(LED_B, true, false, 0x3F);
+					leds_on(LED_R, true, false, 0x14);
+					leds_on(LED_G, true, false, 0xE1);
+					leds_on(LED_B, true, false, 0x4F);
 				}
 				break;
 		}
@@ -703,9 +703,9 @@ static void an30259a_start_led_pattern(unsigned int mode)
 			leds_set_slope_mode(client, LED_R,
 					0, 15, 0, 0, 4, 4, 1, 1, 1, 1);
 			leds_set_slope_mode(client, LED_G,
-					0, 15, 5, 1, 4, 4, 1, 1, 1, 1);
+					0, 15, 10, 1, 4, 4, 1, 1, 1, 1);
 			leds_set_slope_mode(client, LED_B,
-					4, 15, 1, 0, 4, 4, 1, 1, 1, 1);
+					4, 15, 0, 0, 4, 4, 1, 1, 1, 1);
 			booted = true;
 		} else {
 			pr_info("Fade to Black\n");
@@ -731,9 +731,9 @@ static void an30259a_start_led_pattern(unsigned int mode)
 			leds_set_slope_mode(client, LED_R,
 					0, 15, 0, 0, 4, 4, 1, 1, 1, 1);
 			leds_set_slope_mode(client, LED_G,
-					0, 15, 5, 1, 4, 4, 1, 1, 1, 1);
+					0, 15, 10, 1, 4, 4, 1, 1, 1, 1);
 			leds_set_slope_mode(client, LED_B,
-					4, 15, 1, 0, 4, 4, 1, 1, 1, 1);
+					4, 15, 0, 0, 4, 4, 1, 1, 1, 1);
 			break;
 	case BOOTING:
 		if (poweroff_charging) {
