c0686b03fcbce28845580bcb184b091b0f67c4e1
robcore <robpatershuk@gmail.com>
2017-11-07 16:18:10 -0700

    sneaky little takeover of freezer debugging

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -94,11 +94,11 @@ static int try_to_freeze_tasks(bool user_only)
 		if (wq_busy)
 			show_workqueue_state();
 
-		if (!wakeup) {
+		if (debug_unfrozen_tasks) {
 			read_lock(&tasklist_lock);
 			for_each_process_thread(g, p) {
 				if (p != current && !freezer_should_skip(p)
-				    && freezing(p) && !frozen(p) && debug_unfrozen_tasks)
+				    && freezing(p) && !frozen(p))
 					pr_info("%s refused to freeze\n", p->comm);
 			}
 			read_unlock(&tasklist_lock);
