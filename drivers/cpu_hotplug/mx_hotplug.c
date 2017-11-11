362a6a5814771a9636a57c589258f030a153e7ce
robcore <robpatershuk@gmail.com>
2017-11-10 23:54:49 -0700

    lower our thresholds in mx plug again

--- drivers/cpu_hotplug/mx_hotplug.c
+++ drivers/cpu_hotplug/mx_hotplug.c
@@ -48,14 +48,14 @@ static struct workqueue_struct *mx_hp_engine;
 static struct delayed_work motor;
 static struct task_struct *transmission;
 
-static unsigned long sixthgear = 1021ul;
-static unsigned long thirdgear = 505ul;
-static unsigned long secondgear = 419ul;
-static unsigned long firstgear = 315ul;
-static unsigned long sixthgear_rpm = 60ul;
-static unsigned long thirdgear_rpm = 45ul;
-static unsigned long secondgear_rpm = 30ul;
-static unsigned long firstgear_rpm = 15ul;
+static unsigned long sixthgear = 1011ul;
+static unsigned long thirdgear = 495ul;
+static unsigned long secondgear = 409ul;
+static unsigned long firstgear = 305ul;
+static unsigned long sixthgear_rpm = 55ul;
+static unsigned long thirdgear_rpm = 40ul;
+static unsigned long secondgear_rpm = 25ul;
+static unsigned long firstgear_rpm = 10ul;
 
 static unsigned long sampling_rate = MX_SAMPLE_RATE;
 unsigned long air_to_fuel;
