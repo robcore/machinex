54a72849e6ecd047d85580914959fcee7ca3a5e9
robcore <robpatershuk@gmail.com>
2017-11-19 13:21:19 -0700

    stick with old ioprio_best method

--- block/ioprio.c
+++ block/ioprio.c
@@ -161,12 +161,22 @@ out:
 
 int ioprio_best(unsigned short aprio, unsigned short bprio)
 {
+	unsigned short aclass;
+	unsigned short bclass;
+
 	if (!ioprio_valid(aprio))
 		aprio = IOPRIO_PRIO_VALUE(IOPRIO_CLASS_BE, IOPRIO_NORM);
 	if (!ioprio_valid(bprio))
 		bprio = IOPRIO_PRIO_VALUE(IOPRIO_CLASS_BE, IOPRIO_NORM);
 
-	return min(aprio, bprio);
+	aclass = IOPRIO_PRIO_CLASS(aprio);
+	bclass = IOPRIO_PRIO_CLASS(bprio);
+	if (aclass == bclass)
+		return min(aprio, bprio);
+	if (aclass > bclass)
+		return bprio;
+	else
+		return aprio;
 }
 
 SYSCALL_DEFINE2(ioprio_get, int, which, int, who)
