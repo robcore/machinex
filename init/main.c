34359100f8476818c1b64b21efc176022ac50c43
robcore <robpatershuk@gmail.com>
2017-11-22 15:21:17 -0700

    again out of curiousity, printing out the initcall levels as they happen

--- init/main.c
+++ init/main.c
@@ -926,8 +926,10 @@ static void __init do_initcalls(void)
 {
 	int level;
 
-	for (level = 0; level < ARRAY_SIZE(initcall_levels) - 1; level++)
+	for (level = 0; level < ARRAY_SIZE(initcall_levels) - 1; level++) {
 		do_initcall_level(level);
+		pr_info("Init Level: %s\n", initcall_level_names[level]);
+	}
 }
 
 /*
