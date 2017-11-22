5c124bbd9fea814979572dffa193c1811d4d16f1
robcore <robpatershuk@gmail.com>
2017-11-21 17:27:27 -0700

    forget sending the override to mx plug for nwo

--- drivers/cpu_hotplug/mx_hotplug.c
+++ drivers/cpu_hotplug/mx_hotplug.c
@@ -225,7 +225,7 @@ again:
 	delta = ktime_sub(ktime_get(), last_fuelcheck);
 
 	if ((!should_boost && ktime_compare(delta, ms_to_ktime(sampling_rate))  < 0) ||
-		!clutch || hotplug_suspended || thermal_override()) {
+		!clutch || hotplug_suspended) {
 		mutex_unlock(&mx_mutex);
 		schedule();
 		mutex_unlock(&mx_mutex);
