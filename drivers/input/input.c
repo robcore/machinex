f7556258c71bd6388b7c3b606f386c53c6ae1e0f
robcore <robpatershuk@gmail.com>
2017-11-26 21:14:38 -0700

    input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -78,7 +78,7 @@ static void input_start_autorepeat(struct input_dev *dev, int code)
 {
 	if (test_bit(EV_REP, dev->evbit) &&
 	    dev->rep[REP_PERIOD] && dev->rep[REP_DELAY] &&
-	    dev->timer.data) {
+	    dev->timer.function) {
 		dev->repeat_key = code;
 		mod_timer(&dev->timer,
 			  jiffies + msecs_to_jiffies(dev->rep[REP_DELAY]));
