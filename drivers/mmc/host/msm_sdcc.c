cd854bbe04b0ab3f08ccb367786d0d36b17a71d5
robcore <robpatershuk@gmail.com>
2017-11-26 22:00:15 -0700

    msmsdcc

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -5413,9 +5413,9 @@ static void msmsdcc_dump_sdcc_state(struct msmsdcc_host *host)
 	msmsdcc_print_rpm_info(host);
 }
 
-static void msmsdcc_req_tout_timer_hdlr(unsigned long data)
+static void msmsdcc_req_tout_timer_hdlr(struct timer_list *t)
 {
-	struct msmsdcc_host *host = (struct msmsdcc_host *)data;
+	struct msmsdcc_host *host = from_timer(host, t, req_tout_timer);
 	struct mmc_request *mrq;
 	unsigned long flags;
 
@@ -6505,8 +6505,8 @@ msmsdcc_probe(struct platform_device *pdev)
 	}
 #endif
 	host->idle_tout = MSM_MMC_DEFAULT_IDLE_TIMEOUT;
-	setup_timer(&host->req_tout_timer, msmsdcc_req_tout_timer_hdlr,
-			(unsigned long)host);
+	timer_setup(&host->req_tout_timer, msmsdcc_req_tout_timer_hdlr,
+			0);
 
 	mmc_add_host(mmc);
 
