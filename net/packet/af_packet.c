24be3a0f690d6e80887ea0a359079f121b093507
robcore <robpatershuk@gmail.com>
2017-11-30 00:25:22 -0700

    dont need the tx ring arg for init_prb_bdqc anymore

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -3642,7 +3642,7 @@ static int packet_set_ring(struct sock *sk, union tpacket_req_u *req_u,
 		 * it above but just being paranoid
 		 */
 			if (!tx_ring)
-				init_prb_bdqc(po, rb, pg_vec, req_u, tx_ring);
+				init_prb_bdqc(po, rb, pg_vec, req_u);
 				break;
 		default:
 			break;
