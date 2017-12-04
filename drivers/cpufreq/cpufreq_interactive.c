82e5934f969b3855cd195c37274512da3f6e4255
robcore <robpatershuk@gmail.com>
2017-12-04 08:31:28 -0700

    aw gotcha this is what the slack timer is for, wake up to lower the freq

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -353,7 +353,8 @@ static u64 update_load(struct interactive_cpu *icpu, unsigned int cpu)
 	if (delta_time > delta_idle) {
 		active_time = delta_time - delta_idle;
 		icpu->cputime_speedadj += active_time * icpu->ipolicy->policy->cur;
-	}
+	} else
+		icpu->cputime_speedadj += icpu->ipolicy->policy->min;
 
 	icpu->time_in_idle = now_idle;
 	icpu->time_in_idle_timestamp = now;
