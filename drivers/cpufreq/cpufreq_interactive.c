5bd1127f053a5ee5a5acf105a66ea0333bc03453
robcore <robpatershuk@gmail.com>
2017-12-08 22:29:37 -0700

    need to do those in reverse, otherwise it will just keep calculating up

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -285,12 +285,12 @@ static unsigned int choose_freq(struct interactive_cpu *icpu,
 		 * than or equal to the target load.
 		 */
 		load_over_target = DIV_ROUND_CLOSEST(loadadjfreq, tl);
-		if (load_over_target >= 100 && load_over_target < 1000)
-			load_over_target *= 1000;
-		if (load_over_target >= 1000 && load_over_target < 10000)
-			load_over_target *= 100;
 		if (load_over_target >= 10000 && load_over_target < 100000)
 			load_over_target *= 10;
+		if (load_over_target >= 1000 && load_over_target < 10000)
+			load_over_target *= 100;
+		if (load_over_target >= 100 && load_over_target < 1000)
+			load_over_target *= 1000;
 		clamp_val(load_over_target, 0, DEFAULT_HARD_MAX);
 		index = cpufreq_frequency_table_target(policy, load_over_target,
 						       CPUFREQ_RELATION_C);
