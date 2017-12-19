ca98b15b59a3261cef0d83b40233b85affe538e4
robcore <robpatershuk@gmail.com>
2017-12-18 18:48:44 -0700

    constify data for class_find_device

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -278,7 +278,7 @@ struct power_supply_info {
 };
 
 #if defined(CONFIG_POWER_SUPPLY) || defined(CONFIG_POWER_SUPPLY_MODULE)
-extern struct power_supply *power_supply_get_by_name(char *name);
+extern struct power_supply *power_supply_get_by_name(const char *name);
 extern void power_supply_changed(struct power_supply *psy);
 extern int power_supply_am_i_supplied(struct power_supply *psy);
 extern int power_supply_set_battery_charged(struct power_supply *psy);
@@ -290,7 +290,7 @@ extern int power_supply_set_supply_type(struct power_supply *psy,
 					enum power_supply_type supply_type);
 extern int power_supply_is_system_supplied(void);
 #else
-static inline struct power_supply *power_supply_get_by_name(char *name)
+static inline struct power_supply *power_supply_get_by_name(const char *name)
 							{ return -ENOSYS; }
 static inline int power_supply_am_i_supplied(struct power_supply *psy)
 							{ return -ENOSYS; }
