05223d459b5a1fee5bcf28f9a5d0f1c3e3c91ec6
robcore <robpatershuk@gmail.com>
2017-12-25 14:46:43 -0700

    make those unsigned

--- sound/soc/codecs/sound_control.c
+++ sound/soc/codecs/sound_control.c
@@ -395,8 +395,8 @@ static ssize_t sound_control_register_list_show(struct kobject *kobj,
 			"RX4_Vol_B2_Ctrl reg:%u val:%u\n"
 			"RX6_Vol_B2_Ctrl reg:%u val:%u\n"
 			"RX7_Vol_B2_Ctrl reg:%u val:%u\n"
-			"HPH L Gain reg:%u val:%u\n"
-			"HPH R Gain reg:%u val:%u\n",
+			"HPH L Gain reg:%u val:%d\n"
+			"HPH R Gain reg:%u val:%d\n",
 			TABLA_A_CDC_RX1_VOL_CTL_B2_CTL, human_readable(TABLA_A_CDC_RX1_VOL_CTL_B2_CTL),
 			TABLA_A_CDC_RX2_VOL_CTL_B2_CTL, human_readable(TABLA_A_CDC_RX2_VOL_CTL_B2_CTL),
 			TABLA_A_CDC_RX5_VOL_CTL_B2_CTL, human_readable(TABLA_A_CDC_RX5_VOL_CTL_B2_CTL),
